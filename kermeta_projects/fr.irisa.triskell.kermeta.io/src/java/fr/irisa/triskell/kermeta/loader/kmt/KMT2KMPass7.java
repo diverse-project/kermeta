7b7c97d50dd433fb8e86107d3d24ffdae77a45b7
zdrey <zdrey@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2005-03-02 16:55:10 +0000

    temp version (not tested yetbut generate no compil error)
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@82 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/loader/kmt/KMT2KMPass7.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/loader/kmt/KMT2KMPass7.java
@@ -1,6 +1,6 @@
-/* $Id: KMT2KMPass7.java,v 1.2 2005-03-01 17:17:54 zdrey Exp $
+/* $Id: KMT2KMPass7.java,v 1.3 2005-03-02 16:55:10 zdrey Exp $
  * Project : Kermeta (First iteration)
- * File : KMT2KMPass7.java
+ * File : KMT2KMPrettyPrinter.java
  * License : GPL
  * Copyright : IRISA / Universite de Rennes 1
  * ----------------------------------------------------------------------------
@@ -10,98 +10,66 @@
  *  The 7th pass to convert a KMT to KM, which consist to adding the commments 
  * in the code as annotation of the kermeta model elements
  * 
- * TODO :
- * 	1. Get the annotations associated to each operation and print it
- *  2. Get the annotations juxtaposed (no space)
- * 
  * We assume, until I don't know when, that the following rules :
  * ----------------------
  * 	class someclass1
- *  n1* whitespace
+ *  n1*whitespace
  *  c1
- *  n2* whitespace
+ *  n2*whitespace
  *  c2
- *  n3* whitespace
+ *  n3*whitespace
  *  class someclass2
  *  -----------------------
- *  n1 > n3 : // c2 belongs to the element "someclass1"
-	n1 < n3 : //c1 belongs to the element "someclass1"
-	
-the above rules dominate the below ones :
-	n1 > n3 and n2 > n1 : // c1 belongs to "someclass1"
-	n1 > n3 and n1 > n2 : // c1 belongs to "someclass2"
-	n1 < n3 and n2 < n3 : // c2 belongs to "someclass1"
-	n1 < n3 and n2 > n3 : // c2 belongs to "someclass2"
- * 
- * 
- * NOTE :
- *   - there is a main problem that I m unable to resolve yet : 
- * I don't know where some annotations have disappeared, thought I implemented the visitor for Annotation...
- * A reason : the annotation is stored as another object than "annotation" object itself
- * 
- * FIXME (?) :
- * 	- getText(), which is supposed to be the method to access the textual representation of something
- * (so, we expect that we can use it one any annotation, because this is finally only a text),
- * is not "applicable" to any ASTnode...
+ *  n1 > n3 : // c2 C "someclass1" ;	n1 < n3 : //c1 C "someclass1"
+ *  the above rules dominate the below ones :
+	n1 > n3 and n2 > n1 : // c1 C "someclass1, n1 > n3 and n1 > n2 : // c1 C "someclass2"
+	n1 < n3 and n2 < n3 : // c2 C "someclass1", n1 < n3 and n2 > n3 : // c2 C "someclass2"
  */
 package fr.irisa.triskell.kermeta.loader.kmt;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.Stack;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.URIConverter;
+import org.eclipse.emf.ecore.resource.impl.URIConverterImpl;
 
 import com.ibm.eclipse.ldt.core.ast.ASTNode;
 import com.sun.rsasign.t;
 
-import fr.irisa.triskell.kermeta.ast.Annotation;
-import fr.irisa.triskell.kermeta.ast.Annotations;
-import fr.irisa.triskell.kermeta.ast.AsingleExpression;
-import fr.irisa.triskell.kermeta.ast.ClassDecl;
-import fr.irisa.triskell.kermeta.ast.ClassMemberDecl;
-import fr.irisa.triskell.kermeta.ast.DataTypeDecl;
-import fr.irisa.triskell.kermeta.ast.EnumDecl;
-import fr.irisa.triskell.kermeta.ast.EnumLiteral;
-import fr.irisa.triskell.kermeta.ast.FAssignement;
-import fr.irisa.triskell.kermeta.ast.FBlock;
-import fr.irisa.triskell.kermeta.ast.FExpression;
-import fr.irisa.triskell.kermeta.ast.GetterBody;
-import fr.irisa.triskell.kermeta.ast.MultiLineComment;
-import fr.irisa.triskell.kermeta.ast.Operation;
-import fr.irisa.triskell.kermeta.ast.OperationAnnotationBody;
-import fr.irisa.triskell.kermeta.ast.OperationEmptyBody;
-import fr.irisa.triskell.kermeta.ast.OperationExpressionBody;
-import fr.irisa.triskell.kermeta.ast.PackageDecl;
-import fr.irisa.triskell.kermeta.ast.Param;
-import fr.irisa.triskell.kermeta.ast.ScompUnit;
-import fr.irisa.triskell.kermeta.ast.SetterBody;
-import fr.irisa.triskell.kermeta.ast.SingleLineComment;
-import fr.irisa.triskell.kermeta.ast.SubPackageDecl;
-import fr.irisa.triskell.kermeta.ast.Tag;
-import fr.irisa.triskell.kermeta.ast.TopLevelDecl;
-import fr.irisa.triskell.kermeta.loader.KMUnitError;
+import fr.irisa.triskell.kermeta.ast.*;
 import fr.irisa.triskell.kermeta.loader.KermetaUnit;
-import fr.irisa.triskell.kermeta.loader.expression.ExpressionParser;
+import fr.irisa.triskell.kermeta.structure.FClass;
 import fr.irisa.triskell.kermeta.structure.FClassDefinition;
-import fr.irisa.triskell.kermeta.structure.FOperation;
-import fr.irisa.triskell.kermeta.structure.FParameter;
-import fr.irisa.triskell.kermeta.structure.FProperty;
 import fr.irisa.triskell.kermeta.structure.FTag;
-import fr.irisa.triskell.kermeta.structure.FTypeVariable;
-
 
 /**
  * Pass that adds the comments (annotations) as tags of a model element (class, assignment,
- * operation)  
- * 
+ * operation).
+ * How does it work :
+ * 	- each grammar rule (see kermeta.ast) that is a sequence containing a annotationLst list 
+ * corresponds to a model element that is "allowed" to contain Tags (FTag kermeta elements). So, for 
+ * such model elements, we implement a specific visit() method. 
  */
 public class KMT2KMPass7 extends KMT2KMPass {
 
     protected Stack tagStack;
+    protected Annotations preceding_annLst = null; // we need it to "position" it as a pre-tag for preceding class or a post tag for next class (current class)
+    protected FClassDefinition preceding_class;
+    protected String fileData =  null;
 	/**
 	 * 
 	 */
 	public KMT2KMPass7(KermetaUnit builder) {
 		super(builder);
 		tagStack = new Stack();
+		fileData = getFileContentFromBuilder();
 	}
 
 
@@ -117,19 +85,93 @@ public class KMT2KMPass7 extends KMT2KMPass {
 	    /* left annotations, and right annotations*/
 	    Annotations preAnnLst  = classDecl.getPreAnnotations();
 	    Annotations postAnnLst = classDecl.getPostAnnotations();
-
-	    System.out.println("Class is : "+ builder.current_class.getFName());
-	    System.out.println(builder.current_class.getFName()+ ":"+ preAnnLst.getChildCount());
 	    
 	    /* If preAnnLst is too far away from classDecl
 	     * we need to get preceding classDecl and attach it to this preceding one
 	     */
-	    this.processAnnotations(preAnnLst, PRE_TAGNAME);
-	    this.processAnnotations(postAnnLst, POST_TAGNAME);
+	    System.out.print("Classe:"+builder.current_class.getFName());
+
+	    // If preceding_annLst is null, it means that our parser won't find a preAnnLst (null as well so) 
+	    preAnnLst = (preceding_annLst!=null)?preceding_annLst:preAnnLst;
+	    this.processAnnotations(preAnnLst, PRE_TAGNAME, (ASTNode)classDecl);
 	    
-        return super.beginVisit(classDecl);
+	    // if space between post-annotation and this class end is not one or more new
+	    java.util.regex.Pattern p = java.util.regex.Pattern.compile("\n[\n\r]+");
+	   // java.util.regex.Matcher m = p.matcher("aaaaab");
+	   // boolean b = m.matches();
+	    // Get the chars between current class and its post annotation
+	     
+	    int class_end = classDecl.getChild(getBeforeLastChildPosition(classDecl)).getRangeEnd();
+	    int posta_start = postAnnLst.getRangeStart();
+	    // TODO : find out what the chars betwxeen class_end and posta_start are.
+	    //System.out.println("class end:"+class_end-1+" - post start:"+posta_start-1);
+	    System.out.println("post_dfsdf:"+postAnnLst.getFirstChild().getFirstChild().getText());
+	    System.out.println(fileData.charAt(class_end));
+	    String spacelines = fileData.substring(class_end, posta_start);
+	    
+	    // If there is at least one new line between post annotation and its class
+	    // then we do not consider it as a post annotation
+	    if (p.matcher(spacelines).matches())
+	    {
+	        preceding_annLst = postAnnLst; 
+	    }
+	    else
+	    {
+	        preceding_annLst = null;
+		    this.processAnnotations(postAnnLst, POST_TAGNAME, (ASTNode)classDecl);
+	    }
+	    
+	    return super.beginVisit(classDecl);
 	}
 
+	/**
+	 * Get the before-lastchild position for the given file
+	 * TODO : extend it to any ASTNode
+	 * @param node
+	 * @return the before-lastchild position
+	 */
+	public int getBeforeLastChildPosition(ClassDecl classDecl)
+	{
+	    int count = 0;
+	    if (classDecl.getChildCount() > 1 ) //&& classDecl.getLastChild() != null)
+	        count = classDecl.getChildCount()-2;
+	    else if ((classDecl.getChildCount() > 1 && classDecl.getLastChild()!=null)||classDecl.getChildCount()==1)
+	        count = classDecl.getChildCount()-1;
+	    else 
+	        count = 0;
+	    return count;
+	}
+	
+	/**
+	 * As skipped tokens are not accessible through ANTLR generated parser, we load the entire
+	 * file (this is a patchy method...) and get the chars at the given position
+	 * @return fileData filled (it is an attribute of this class)
+	 */
+	public String getFileContentFromBuilder() {
+	    
+	    String str_uri = builder.getUri();
+	    String result = "";
+	    try {
+	      //  BufferedReader r = new BufferedReader(new FileReader(uri));
+	        			
+	         URIConverter converter = new URIConverterImpl();
+	         BufferedReader r = new BufferedReader(
+	         new InputStreamReader(converter.createInputStream(URI.createURI(str_uri))));
+	        
+	        while (r.ready()) {
+	            result += r.readLine() + "\n";
+	        }
+	        r.close();
+	    } catch (FileNotFoundException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    } catch (IOException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    }
+	 
+		return result;
+	}
 
 /*
     
@@ -179,6 +221,7 @@ public class KMT2KMPass7 extends KMT2KMPass {
 	 */
 	public FTag createFTagFromAnnotationLst(Annotations annLst, String name)
 	{
+	    Annotation prec_a = null; // the preceding annotation
 	    String tag_value = "";
 	    String tag_name =name;
         ASTNode[] children = annLst.getChildren();
@@ -188,7 +231,12 @@ public class KMT2KMPass7 extends KMT2KMPass {
         // of lines after the postAnnotationList of the preceding class
         for (int i=0; i< annLst.getChildCount(); i++) // children.length
         {
+            if (i>0) prec_a = (Annotation)children[i-1];
             Annotation a = (Annotation)children[i];
+            /*if (a!=null) System.out.println(
+                    "offset:"+a.getRangeLength()+"=="+"text:"+a.getTextLength()+
+                    ".   beg:"+a.getRangeStart()+"-cur_beg:"+a.getRangeEnd());
+                    */
             if (Tag.class.isInstance(a))
             {
                 tag_name  = ((Tag)a).getName().getFirstChild().getText();
@@ -202,8 +250,10 @@ public class KMT2KMPass7 extends KMT2KMPass {
             
             
         }
+        System.out.println("list start : "+annLst.getRangeStart());
+        
+        System.out.println("content : ["+tag_value+"]");
         FTag tag = builder.struct_factory.createFTag();
-        System.out.println("VALUE {"+ tag_name + "}:"+tag_value);
         tag.setFName(name);
         tag.setFValue(tag_value);
 	    return tag;
@@ -238,7 +288,7 @@ public class KMT2KMPass7 extends KMT2KMPass {
 	 * @param annType the name of the tag that will be defined with annotations text 
 	 * (like "pre", "post", "orphan")
 	 */
-	protected FTag processAnnotations(Annotations annLst, String annType)
+	protected FTag processAnnotations(Annotations annLst, String annType, ASTNode refnode)
 	{
 	    FTag tag = null;
 	    // tagStack.push(tag)? // 
@@ -248,6 +298,9 @@ public class KMT2KMPass7 extends KMT2KMPass {
             builder.current_class.getFTag().add(tag);
 	        builder.tags.put(tag.toString(), tag);// FIXME : tag.toStirng() is not a relevant key...
 	    }
+	    
+	    
+	    
 	    return tag;
 	}
 
