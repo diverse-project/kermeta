a4ab1d5cd6c3feefc2e92142595f0dd220b1ca34
zdrey <zdrey@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2005-03-25 16:42:17 +0000

    - slight modif for Variable attributes (handle VariableDecl and Parameters of op. call) - polluting comments from omondo
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@185 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.interpreter/src/java/fr/irisa/triskell/kermeta/interpreter/Variable.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.interpreter/src/java/fr/irisa/triskell/kermeta/interpreter/Variable.java
@@ -1,4 +1,4 @@
-/* $Id: Variable.java,v 1.3 2005-03-22 12:58:14 zdrey Exp $
+/* $Id: Variable.java,v 1.4 2005-03-25 16:42:17 zdrey Exp $
  * Project : Kermeta (First iteration)
  * File : Variable.java
  * License : GPL
@@ -22,65 +22,113 @@ import fr.irisa.triskell.kermeta.structure.FType;
  */
 public class Variable {
 
-    /** @deprecated The declaration in kermeta that correspond to this Variable */
-    protected FVariableDecl declaration ;
-    /** The type of the variable */
+    /**
+     * @deprecated The declaration in kermeta that correspond to this Variable
+     * 
+     * @uml.property name="declaration"
+     * @uml.associationEnd multiplicity="(0 1)"
+     */
+    protected FVariableDecl declaration;
+
+    /**
+     * The type of the variable
+     * 
+     * @uml.property name="type"
+     * @uml.associationEnd multiplicity="(0 1)"
+     */
     protected FType type;
-    /** The value of the variable */
+
+    /**
+     * The value of the variable
+     * 
+     * @uml.property name="runtimeObject"
+     * @uml.associationEnd qualifier="constant:java.lang.String java.util.ArrayList" multiplicity=
+     * "(0 1)"
+     */
     protected RuntimeObject RuntimeObject;
-    /** The name of the variable */
+
+    /**
+     * The name of the variable
+     * 
+     * @uml.property name="name" multiplicity="(0 1)"
+     */
     protected String name;
+
     /**
      * 
      */
     public Variable() {
         // TODO Auto-generated constructor stub
     }
-    
-    
 
     /**
      * @return Returns the declaration.
+     * 
+     * @uml.property name="declaration"
      */
     public FVariableDecl getDeclaration() {
         return declaration;
     }
+
     /**
      * @param declaration The declaration to set.
+     * 
+     * @uml.property name="declaration"
      */
     public void setDeclaration(FVariableDecl declaration) {
         this.declaration = declaration;
     }
+
     /**
      * @return Returns the name.
+     * 
+     * @uml.property name="name"
      */
     public String getName() {
         return name;
     }
+
     /**
      * @param name The name to set.
+     * 
+     * @uml.property name="name"
      */
     public void setName(String name) {
         this.name = name;
     }
+
     /**
      * @return Returns the value.
+     * 
+     * @uml.property name="runtimeObject"
      */
     public RuntimeObject getRuntimeObject() {
         return RuntimeObject;
     }
+
     /**
      * @param value2 The value to set.
+     * 
+     * @uml.property name="runtimeObject"
      */
     public void setRuntimeObject(RuntimeObject kObject) {
         this.RuntimeObject = kObject;
     }
-    
+
+    /**
+     * 
+     * @uml.property name="type"
+     */
     public FType getType() {
         return type;
     }
-    
+
+    /**
+     * 
+     * @uml.property name="type"
+     */
     public void setType(FType pType) {
         type = pType;
     }
+
 }
