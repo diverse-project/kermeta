c5f3331a5674ed91b4d377fc681bb210518e900e
zdrey <zdrey@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2005-03-25 16:44:35 +0000

    - polluting comments from Ommondo
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@187 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.interpreter/src/java/fr/irisa/triskell/kermeta/interpreter/CallFrame.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.interpreter/src/java/fr/irisa/triskell/kermeta/interpreter/CallFrame.java
@@ -1,4 +1,4 @@
-/* $Id: CallFrame.java,v 1.1 2005-03-22 12:56:08 zdrey Exp $
+/* $Id: CallFrame.java,v 1.2 2005-03-25 16:44:35 zdrey Exp $
  * Project : Kermeta (First iteration)
  * File : CallFrame.java
  * License : GPL
@@ -24,19 +24,59 @@ import fr.irisa.triskell.kermeta.runtime.RuntimeObject;
  * CallFrame is the context for an operation call
  */
 public class CallFrame {
-    
-    /** the interpreter global context */
+
+    /**
+     * the interpreter global context
+     * 
+     * @uml.property name="context"
+     * @uml.associationEnd inverse="frame_stack:fr.irisa.triskell.kermeta.interpreter.InterpreterContext"
+     * multiplicity="(1 1)"
+     */
     InterpreterContext context;
-    /** self object (inside which the operation call is done) */
+
+    /**
+     * self object (inside which the operation call is done)
+     * 
+     * @uml.property name="self"
+     * @uml.associationEnd qualifier="constant:java.lang.String java.util.ArrayList" multiplicity=
+     * "(0 1)"
+     */
     RuntimeObject self;
-    /** the operation result of the operation linked to this CallFrame */
+
+    /**
+     * the operation result of the operation linked to this CallFrame
+     * 
+     * @uml.property name="operation_result"
+     * @uml.associationEnd multiplicity="(0 1)"
+     */
     RuntimeObject operation_result;
-    /** the current expression context block_stack.peek() */
+
+    /**
+     * the current expression context block_stack.peek()
+     * 
+     * @uml.property name="current_context"
+     * @uml.associationEnd multiplicity="(0 1)"
+     */
     ExpressionContext current_context;
-    /** the list of variables for the operation associated with this CallFrame (RuntimeObjects)*/
-    ArrayList parameters ;
-    /** the list of expression contexts that are linked to this frame (@see ExpressionContext class javadoc)*/
+
+    /**
+     * the list of variables for the operation associated with this CallFrame (RuntimeObjects)
+     * 
+     * @uml.property name="parameters"
+     * @uml.associationEnd elementType="fr.irisa.triskell.kermeta.runtime.RuntimeObject"
+     * multiplicity="(0 -1)"
+     */
+    ArrayList parameters;
+
+    /**
+     * the list of expression contexts that are linked to this frame (@see ExpressionContext class javadoc)
+     * 
+     * @uml.property name="block_stack"
+     * @uml.associationEnd elementType="fr.irisa.triskell.kermeta.interpreter.ExpressionContext"
+     * multiplicity="(0 -1)"
+     */
     Stack block_stack = new Stack();
+
     
     /**
      * Constructor
@@ -70,20 +110,22 @@ public class CallFrame {
     {
         block_stack.pop();
     }
-    
+
     /*
      * 
      * A C C E S S O R S 
      *
      */
-    
+
     /**
      * @return the context of interpreter
+     * 
+     * @uml.property name="context"
      */
-    public InterpreterContext getContext()
-    {
+    public InterpreterContext getContext() {
         return context;
     }
+
     
     public Stack getBlockStack()
     {
@@ -104,33 +146,41 @@ public class CallFrame {
     {
         operation_result = pResult;
     }
-    
-    
 
     /**
      * @return Returns the self.
+     * 
+     * @uml.property name="self"
      */
     public RuntimeObject getSelf() {
         return self;
     }
+
     /**
      * @param self The self to set.
+     * 
+     * @uml.property name="self"
      */
     public void setSelf(RuntimeObject self) {
         this.self = self;
     }
-    
-    
+
     /**
      * @return Returns the parameters.
+     * 
+     * @uml.property name="parameters"
      */
     public ArrayList getParameters() {
         return parameters;
     }
+
     /**
      * @param parameters The parameters to set.
+     * 
+     * @uml.property name="parameters"
      */
     public void setParameters(ArrayList parameters) {
         this.parameters = parameters;
     }
+
 }
