e8906874cd43a0da8a227ef83048dbc1666f6e34
jpthibau <jpthibau@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2005-05-02 08:49:47 +0000

    Interprter V0.1.1 (Boolean,String;Integer operations seem OK)
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@396 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/loader/StandardKermetaUnit.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/loader/StandardKermetaUnit.java
@@ -11,7 +11,6 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.emf.ecore.resource.impl.URIConverterImpl;
 
-import com.sun.rsasign.p;
 
 import fr.irisa.triskell.kermeta.loader.kmt.KMTUnit;
 import fr.irisa.triskell.kermeta.utils.OperationBodyLoader;
