f6cd77a03d489b05406264e0037419c6f86abe1b
zdrey <zdrey@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2005-05-23 13:16:02 +0000

    helper methods (that may be used by many undependant classes) should be put here
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@567 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

---
+++ kermeta_projects/fr.irisa.triskell.kermeta.runner.deprecated/src/fr/irisa/triskell/kermeta/runner/launching/KermetaRunHelper.java
@@ -0,0 +1,61 @@
+/* $Id: KermetaRunHelper.java,v 1.1 2005-05-23 13:16:02 zdrey Exp $
+ * Project: Kermeta (First iteration)
+ * File: KermetaRunHelper.java
+ * License: GPL
+ * Copyright: IRISA / INRIA / Universite de Rennes 1
+ * ----------------------------------------------------------------------------
+ * Creation date: May 23, 2005
+ * Authors: zdrey
+ */
+package fr.irisa.triskell.kermeta.runner.launching;
+
+import fr.irisa.triskell.kermeta.behavior.impl.BehaviorPackageImpl;
+import fr.irisa.triskell.kermeta.loader.KMUnitError;
+import fr.irisa.triskell.kermeta.loader.KermetaUnit;
+import fr.irisa.triskell.kermeta.loader.KermetaUnitFactory;
+import fr.irisa.triskell.kermeta.loader.kmt.KMTUnit;
+import fr.irisa.triskell.kermeta.structure.impl.StructurePackageImpl;
+
+/**
+ * 
+ */
+public class KermetaRunHelper {
+
+	/**
+	 * FIXME : move it to another class (also used in RunPopupDialog!!)
+	 * Parse the given file and return its KermetaUnit
+	 * @param file
+	 * @return KermetaUnit the compilation unit for the given file
+	 */
+    public static KermetaUnit parse(String filename)
+    {
+        KermetaUnit.STD_LIB_URI = "platform:/plugin/fr.irisa.triskell.kermeta.runner/lib/framework.km";
+
+    	StructurePackageImpl.init();
+    	BehaviorPackageImpl.init();
+    	// FIXME : adaptation...
+    	//String uri = "platform:/resource/" + filename;
+    	String uri = "file://" +filename;
+    	KermetaUnitFactory.getDefaultLoader().unloadAll();
+    	KMTUnit result = null;
+       // System.out.println("file.getFullPath().toOSString() : " + file.getFullPath().toOSString());
+        try {
+        	result = (KMTUnit)KermetaUnitFactory.getDefaultLoader().createKermetaUnit(uri);
+	        result.load();
+	        
+	        result.typeCheck();
+	        
+        }
+        catch(Throwable e) {
+            KermetaUnit.internalLog.error("load error ", e);
+        	if (result == null) {
+        		e.printStackTrace();
+        		return null;
+        	}
+        	else if (result.error.size() == 0)
+        		result.error.add(new KMUnitError("INTERNAL ERROR : " + e, null));
+        }
+        return result;
+    }
+    
+}
