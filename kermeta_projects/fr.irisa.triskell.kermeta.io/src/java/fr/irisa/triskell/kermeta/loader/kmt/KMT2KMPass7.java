721cefb7c3a921fc66d14313825171c5054bbff9
zdrey <zdrey@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2005-06-06 10:29:52 +0000

    removed useless methods. rules about Annotations have changed (see kermeta.ast, KermetaLexer.g in "io" project)
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@628 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/loader/kmt/KMT2KMPass7.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/loader/kmt/KMT2KMPass7.java
@@ -1,4 +1,4 @@
-/* $Id: KMT2KMPass7.java,v 1.11 2005-04-22 01:46:18 ffleurey Exp $
+/* $Id: KMT2KMPass7.java,v 1.12 2005-06-06 10:29:52 zdrey Exp $
  * Project : Kermeta (First iteration)
  * File : KMT2KMPrettyPrinter.java
  * License : GPL
@@ -10,39 +10,20 @@
  *  The 7th pass to convert a KMT to KM, which consist to adding the commments 
  * in the code as annotation of the kermeta model elements
  * 
- * We assume, until I don't know when, that the following rules :
- * ----------------------
- * 	class someclass1
- *  n1*whitespace
- *  c1
- *  n2*whitespace
- *  c2
- *  n3*whitespace
- *  class someclass2
- *  -----------------------
- *  n1 > n3 : // c2 C "someclass1" ;	n1 < n3 : //c1 C "someclass1"
- *  the above rules dominate the below ones :
-	n1 > n3 and n2 > n1 : // c1 C "someclass1, n1 > n3 and n1 > n2 : // c1 C "someclass2"
-	n1 < n3 and n2 < n3 : // c2 C "someclass1", n1 < n3 and n2 > n3 : // c2 C "someclass2"
-	
-	
-	In a first time we will consider the following more simple rules:
-	
-	* a space between a post annotation list and its class -> post is a pre
-	* a space inside a post annotation list -> cut it into 2 sets of annotations.
+ * History :
+ * 		06/06/05 Removed almost everything.
+ * 		Now only @ and "/**" prefixed annotations are stored as tags.
+ * 		TODO : now, visit all the annotable elements :
+ * 		subPackageDecl, classDecl, enumDecl, parameters (?), enumLiteral, 
+ * 		fAssignment (i.e any Expression?), blocks (when empty), require/using?
  */
 package fr.irisa.triskell.kermeta.loader.kmt;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Stack;
 import java.util.regex.Pattern;
 
@@ -50,16 +31,13 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.emf.ecore.resource.impl.URIConverterImpl;
 
-import com.ibm.eclipse.ldt.core.ast.ASTNode;
 //import com.sun.rsasign.t;
 
 import fr.irisa.triskell.kermeta.ast.*;
 import fr.irisa.triskell.kermeta.loader.KermetaUnit;
-import fr.irisa.triskell.kermeta.structure.FClass;
 import fr.irisa.triskell.kermeta.structure.FClassDefinition;
 import fr.irisa.triskell.kermeta.structure.FNamedElement;
 import fr.irisa.triskell.kermeta.structure.FTag;
-import fr.irisa.triskell.kermeta.utils.UserDirURI;
 
 /**
  * Pass that adds the comments (annotations) as tags of a model element (class, assignment,
@@ -94,7 +72,7 @@ public class KMT2KMPass7 extends KMT2KMPass {
 	 * @see kermeta.ast.MetacoreASTNodeVisitor#beginVisit(metacore.ast.ClassDecl)
 	 */
 	public boolean beginVisit(PackageDecl packageDecl) {
-	    Annotations annLst = packageDecl.getPreAnnotations();
+	    Annotations annLst = packageDecl.getAnnotations();
 	    FNamedElement pkg = (FNamedElement)builder.getModelElementByNode(packageDecl);
 	    processAnnotations(annLst, pkg);
 	    return super.beginVisit(packageDecl);
@@ -147,10 +125,9 @@ public class KMT2KMPass7 extends KMT2KMPass {
 	         
 	         while (r.ready()) {
 	            char[] c = {(char)r.read()};
-	            String sc = (c[0]=='\t')?" ":new String(c);
+	            String sc = (c[0]=='\t')?"    ":new String(c);
 	            result += sc;
-	            //result += r.readLine() + "\r\n"; // FIXME : new line are not same according to the filetype..
-	            //System.getProperty("line.separator").length(); 
+	            //result += r.readLine() + "\r\n"; // FIXME : new line character is OS dependant.
 	        }
 	        r.close();
 	    } catch (FileNotFoundException e) {
@@ -162,139 +139,6 @@ public class KMT2KMPass7 extends KMT2KMPass {
 	    }
 		return result;
 	}
-
-/*
-    
-	public boolean beginVisit(Annotations annLst)
-	{
-        if (annLst!=null && annLst.getChildCount()>0)
-        {
-            FTag tag = this.createFTagFromAnnotationLst(annLst, "orphan");
-            if (!builder.tags.containsKey(tag.toString())) {
-                builder.tags.put(tag.toString(), tag);
-            }
-            else System.err.println("oups, duplicate tag..");
-        }    
-		return super.beginVisit(annLst);
-	}
-*/	
-	/*public boolean beginVisit(Annotation annotation)
-	{
-        if (annotation!=null)
-        {
-    	    FTag tag = createFTagFromAnnotation(annotation);
-    	    // Define an Id for tag, so that we can find it easilier?.
-    	    String tagkey = tag.toString();
-    		if (!builder.tags.containsKey(tagkey)) {
-    			builder.tags.put(tagkey, tag);
-    		}
-    		else System.out.println("oups, duplicate tag..");
-        }    
-		return super.beginVisit(annotation);
-	}
-	*/
-	
-	/**
-	 * 
-	 * @param annLst The annotation list from which we get the Tag value
-	 * @param name the name of the tag 
-	 * @return an Array that contains 2 lists, one for the tags that belong
-	 * If there is one or more lines separating the annotation list (Annotations), we consider
-	 * that we have 2 list of tags : one list for the current class (if there is one), as "post" tags,
-	 * and one for the next class, as "pre" tags;
-	 */
-	public ArrayList[] createFTagFromAnnotationLst(Annotations annLst)
-	{
-	    boolean isDouble = false;
-	    ArrayList ftagLstPre = new ArrayList(); // list of post tags
-	    ArrayList ftagLstPost = new ArrayList();// list of pre tags (for following class)
-	    ArrayList[] ftagLstArray = new ArrayList[2];
-	    Annotation prec_a = null; // the preceding annotation
-	    String tag_value = "";
-	    //String tag_name = name;
-	    String tag_name = "";
-	    String spacelines = "";
-	    String tag_value1 = ""; String tag_value2 = "";
-        ASTNode[] children = annLst.getChildren();
-        FTag tag;
-        
-        // Get each single annotation
-        for (int i=0; i< annLst.getChildCount(); i++) // children.length
-        {
-            if (i>0) prec_a = (Annotation)children[i-1];
-            Annotation a = (Annotation)children[i];
-            // Space between Annotation elements
-            int sep = (prec_a!=null)?(prec_a.getRangeEnd() - a.getRangeStart()):0;
-            spacelines = (sep>0)?fileData.substring(a.getRangeStart()-prec_a.getRangeEnd()):"";
-            
-            // The first separator found inside the annotationLst is enough to decide that
-            // its preceding part is a postTag for current Class and its following one is a preTag
-            // for the following class
-            if (pattern.matcher(spacelines).matches() || isDouble == true)
-            {
-                if (isDouble == false)
-                {
-                    tag_value = "";
-                }
-                isDouble = true;
-                if (Tag.class.isInstance(a))
-                {
-                    tag_name  = ((Tag)a).getName().getFirstChild().getText();
-                    tag_value = ((Tag)a).getVal().getText();
-                    tag = builder.struct_factory.createFTag();
-                    tag.setFName(tag_name); tag.setFValue(tag_value);
-                    // add the tag the second list of tags
-                    ftagLstPre.add(tag);
-                    
-                }
-                // A multiline comment? [tested] FIXME : many juxt. single lines are 1 tag
-                // We assume that user commented in a tidy way. We reassemble here all
-                // comments that are not @tags in a unique tag. This is one of the many
-                // choices that we could make.
-                else
-                {
-                    
-                    tag_value2 += a.getFirstChild().getText() + "\n";
-                }
-            }
-            else
-            {
-      //          System.out.println("No double annotation");
-                if (Tag.class.isInstance(a))
-                {
-                    tag_name  = ((Tag)a).getName().getFirstChild().getText();
-                    tag_value = ((Tag)a).getVal().getText();
-                    tag = builder.struct_factory.createFTag();
-                    tag.setFName(tag_name); tag.setFValue(tag_value);
-                    // add the tag the first list of tags
-                    ftagLstPost.add(tag);
-                }
-                // A multiline comment? [tested]
-                else
-                {
-                    tag_value2 += a.getFirstChild().getText();
-                }
-            }
-        }
-        // Create one FTag for each assembled tag_value
-        if (tag_value1 != "")
-        {
-            tag = builder.struct_factory.createFTag();
-            tag.setFName(POST_TAGNAME); tag.setFValue(tag_value1);
-            ftagLstPost.add(tag);
-        }
-        if (tag_value2 != "")
-        {
-            tag = builder.struct_factory.createFTag();
-            tag.setFName(PRE_TAGNAME); tag.setFValue(tag_value2);
-            ftagLstPre.add(tag);
-        }
-        // remember : post tags for current class, pre tags for next class
-        ftagLstArray[0] = ftagLstPost; ftagLstArray[1] = ftagLstPre;
-        
-        return ftagLstArray;
-	}
-	
 	
 	public FTag createFTagFromAnnotation(Annotation a)
 	{
@@ -328,7 +172,6 @@ public class KMT2KMPass7 extends KMT2KMPass {
 	 */
 	protected void processAnnotations(Annotations annLst, FNamedElement element)
 	{
-		
 	    String tag_name = null;
 	    String tag_value = null;
 	    FTag tag = null;
