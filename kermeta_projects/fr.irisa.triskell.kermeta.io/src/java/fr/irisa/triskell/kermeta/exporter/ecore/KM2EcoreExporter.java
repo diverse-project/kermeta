270e5d9982d85595138f14be4253e17efe74d2b3
Didier Vojtisek <didier.vojtisek@inria.fr>
2005-07-13 10:04:40 +0000

    added support for ClassDefinition
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@756 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/exporter/ecore/KM2EcoreExporter.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/exporter/ecore/KM2EcoreExporter.java
@@ -1,4 +1,4 @@
-/* $Id: KM2EcoreExporter.java,v 1.1 2005-07-11 15:34:20 dvojtise Exp $
+/* $Id: KM2EcoreExporter.java,v 1.2 2005-07-13 10:04:40 dvojtise Exp $
  * Project    : fr.irisa.triskell.kermeta.io
  * File       : KM2EcoreExporter.java
  * License    : EPL
@@ -12,19 +12,18 @@
  */
 package fr.irisa.triskell.kermeta.exporter.ecore;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.log4j.Logger;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.emf.ecore.resource.Resource;
 
-import fr.irisa.triskell.kermeta.structure.FNamedElement;
+import fr.irisa.triskell.kermeta.structure.FClass;
+import fr.irisa.triskell.kermeta.structure.FClassDefinition;
 import fr.irisa.triskell.kermeta.structure.FPackage;
 import fr.irisa.triskell.kermeta.util.LogConfigurationHelper;
 import fr.irisa.triskell.kermeta.utils.KMTHelper;
@@ -41,7 +40,7 @@ public class KM2EcoreExporter extends KermetaVisitor{
 	protected ArrayList imports = new ArrayList();
 	protected String root_pname;
 	protected String current_pname;
-	protected TextTabs loggerTabs =  new TextTabs();
+	protected TextTabs loggerTabs =  new TextTabs("   ","|");
 	
 	// the resource to populate
 	protected Resource ecoreResource = null;
@@ -68,8 +67,8 @@ public class KM2EcoreExporter extends KermetaVisitor{
 	
 	public Object visit(FPackage p) {
 		current_pname = KMTHelper.getQualifiedName(p);
-		internalLog.debug(loggerTabs + "Visiting Package "+ current_pname);
-		loggerTabs.decrement();
+		internalLog.debug(loggerTabs + "Visiting Package: "+ current_pname);
+		loggerTabs.increment();
 		
 		EPackage newEPackage = EcoreFactory.eINSTANCE.createEPackage();
 		newEPackage.setName(current_pname);
@@ -81,15 +80,89 @@ public class KM2EcoreExporter extends KermetaVisitor{
 		while(it.hasNext()) {
 			newEPackage.getESubpackages().add(accept((EObject)it.next()));
 		}
-		/*Iterator it2 = p.getFOwnedTypeDefinition().iterator();
+		
+		
+		it = p.getFOwnedTypeDefinition().iterator();
 		while(it.hasNext()) {
-			newEPackage.getEClassifiers().add(accept((EObject)it2.next()));
-		}*/
+			Object o = accept((EObject)it.next());
+			if (o != null)
+				newEPackage.getEClassifiers().add(o);
+			else
+				internalLog.warn(loggerTabs + "accept of a OwnedTypeDefinition returned null !"); 
+				
+		}
 
-		loggerTabs.increment();
+		loggerTabs.decrement();
 		return newEPackage;
 	}
 	
+	/** 
+	 * @see kermeta.visitor.MetacoreVisitor#visit(metacore.structure.FClassDefinition)
+	 */
+	public Object visit(FClassDefinition node) {
+		EClass newEClass=null;
+		current_pname = node.getFName();
+		internalLog.debug(loggerTabs + "Visiting ClassDefinition: "+ current_pname);
+		loggerTabs.increment();
 		
+		try{
+			newEClass = EcoreFactory.eINSTANCE.createEClass();
+			newEClass.setName(current_pname);
+			ecoreResource.getContents().add(newEClass);
+		
+	/*	typedef = false;
+		String result = "";
+
+		result += ppTags(node.getFTag());
+		
+		if (node.isFIsAbstract()) result += "abstract ";
+		result += "class " + KMTHelper.getMangledIdentifier(node.getFName());
+		if (node.getFTypeParameter().size() > 0) {
+			result += "<";
+			result += ppTypeVariableDeclaration(node.getFTypeParameter());
+			result += ">";
+		}
+		if (node.getFSuperType().size() > 0) {
+			result += " inherits ";
+			result += ppComaSeparatedNodes(node.getFSuperType());
+		}
+		result += "\n" + getPrefix() + "{\n";
+		pushPrefix();
+		result += ppCRSeparatedNode(node.getFOwnedAttributes());
+		result += ppCRSeparatedNode(node.getFOwnedOperation());
+		popPrefix();
+		result += getPrefix() + "}";		
+		
+		typedef = true;
+		
+		return result;*/
+		}
+		catch(Exception e)
+		{
+			internalLog.error("Visiting ClassDefinition: "+ current_pname + ", Exception: " + e.getMessage() ,e);
+		}
+		loggerTabs.decrement();
+		return newEClass;
+	}
+	
+	/**
+	 * @see kermeta.visitor.MetacoreVisitor#visit(metacore.structure.FClass)
+	 */
+	public Object visit(FClass node) {
+
+		//current_pname = KMTHelper.getQualifiedName(node);
+		internalLog.debug(loggerTabs + "Visiting Class: "+ current_pname);
+		loggerTabs.decrement();
+		
+		/*String qname = KMTHelper.getQualifiedName(node.getFClassDefinition());
+		String name = KMTHelper.getMangledIdentifier(node.getFClassDefinition().getFName());
+		String result = ppTypeName(qname, name);
+		if (node.getFTypeParamBinding().size() > 0) {
+			result += "<" + ppComaSeparatedNodes(node.getFTypeParamBinding()) + ">";
+		}
+		return result;
+		*/
+		return null;
+	}	
 	
 }
