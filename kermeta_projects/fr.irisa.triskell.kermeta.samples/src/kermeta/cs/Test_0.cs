677eb5b7f29da4df8604c724fba8d518b74eb126
zdrey <zdrey@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2005-07-19 17:59:07 +0000

    added fast examples to show how to access an EMF model instance. Need to be reviewed, but are tested and work.
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@773 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

---
+++ kermeta_projects/fr.irisa.triskell.kermeta.samples/src/kermeta/cs/Test_0.cs
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<cs:Root xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" 
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xmlns:cs="file:///udd/zdrey/Workspaces/KMWorkspace/fr.irisa.triskell.kermeta.samples/src/ecore/cs/cs.ecore">
+  <template name="main template">
+    <decision name="first_decision" target="//@template.0"/>
+  </template>
+</cs:Root>
