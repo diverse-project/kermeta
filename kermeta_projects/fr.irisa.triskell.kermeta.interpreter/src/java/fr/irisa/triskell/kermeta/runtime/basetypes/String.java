2e1f6391740257ecf6c6be8ebf4db801963b29a5
Didier Vojtisek <didier.vojtisek@inria.fr>
2005-10-14 14:58:57 +0000

    removed a strange replaceall that prevented to load strings containing newline or backslashes updated all javadoc comments
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@976 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.interpreter/src/java/fr/irisa/triskell/kermeta/runtime/basetypes/String.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.interpreter/src/java/fr/irisa/triskell/kermeta/runtime/basetypes/String.java
@@ -1,91 +1,124 @@
-/* $Id : $
- * Implementation of Kermeta base type String 
- */
+/* $Id: String.java,v 1.6 2005-10-14 14:58:57 dvojtise Exp $
+* Project : Kermeta interpreter
+* File : String.java
+* License : EPL
+* Copyright : IRISA / INRIA / Universite de Rennes 1
+* ----------------------------------------------------------------------------
+* Creation date : 14 mai 2005
+* Author : Franck Fleurey
+*/ 
+
 
 package fr.irisa.triskell.kermeta.runtime.basetypes;
 
 import fr.irisa.triskell.kermeta.runtime.RuntimeObject;
 import fr.irisa.triskell.kermeta.runtime.factory.RuntimeObjectFactory;
 
+/** 
+ * Implementation of Kermeta base type String 
+ */
 public class String {
     
-	// Implementation of method compareTo called as :
-	// extern fr::irisa::triskell::kermeta::runtime::basetypes::String::compareTo(other)
+	/** Implementation of method compareTo called as :
+	 * extern fr::irisa::triskell::kermeta::runtime::basetypes::String::compareTo(other)
+	 */
 	public static RuntimeObject compareTo(RuntimeObject self, RuntimeObject param0) {
 		RuntimeObject result = self.getFactory().createObjectFromClassName("kermeta::standard::Integer");
 		Integer.setValue(result, getValue(self).compareTo(getValue(param0)));
 		return result;
 	}
 
-	// Implementation of method equals called as :
-	// extern fr::irisa::triskell::kermeta::runtime::basetypes::String::equals(element)
+	/** Implementation of method equals called as :
+	 * extern fr::irisa::triskell::kermeta::runtime::basetypes::String::equals(element)
+	 */
 	public static RuntimeObject equals(RuntimeObject self, RuntimeObject param0) {
 		if(getValue(self).equals(getValue(param0))) return self.getFactory().getMemory().trueINSTANCE;
 		else return self.getFactory().getMemory().falseINSTANCE;
 	}
 
-	// Implementation of method isNotEqual called as :
-	// extern fr::irisa::triskell::kermeta::runtime::basetypes::Character::isNotEqual(element)
+	/** Implementation of method isNotEqual called as :
+	 * extern fr::irisa::triskell::kermeta::runtime::basetypes::Character::isNotEqual(element)
+	 */
 	public static RuntimeObject isNotEqual(RuntimeObject self, RuntimeObject param0) {
 		if(getValue(self).equals(getValue(param0))) return self.getFactory().getMemory().falseINSTANCE;
 		else return self.getFactory().getMemory().trueINSTANCE;
 	}
 
-	// Implementation of method plus called as :
-	// extern fr::irisa::triskell::kermeta::runtime::basetypes::String::plus(other)
+	/** Implementation of method plus called as :
+	 * extern fr::irisa::triskell::kermeta::runtime::basetypes::String::plus(other)
+	 */
 	public static RuntimeObject plus(RuntimeObject self, RuntimeObject param0) {
 		RuntimeObject result = self.getFactory().createObjectFromClassName("kermeta::standard::String");
 		setValue(result, getValue(self) + getValue(param0));
 		return result;
 	}
 
-	// Implementation of method append called as :
-	// extern fr::irisa::triskell::kermeta::runtime::basetypes::String::append(other)
+	/** Implementation of method append called as :
+	 * extern fr::irisa::triskell::kermeta::runtime::basetypes::String::append(other)
+	 */
 	public static RuntimeObject append(RuntimeObject self, RuntimeObject param0) {
 		setValue(self, getValue(self) + getValue(param0));
 		return self.getFactory().getMemory().voidINSTANCE;
 	}
 
-	// Implementation of method elementAt called as :
-	// extern fr::irisa::triskell::kermeta::runtime::basetypes::String::elementAt(index)
+	/** Implementation of method elementAt called as :
+	 * extern fr::irisa::triskell::kermeta::runtime::basetypes::String::elementAt(index)
+	 */
 	public static RuntimeObject elementAt(RuntimeObject self, RuntimeObject param0) {
 		RuntimeObject result = self.getFactory().createObjectFromClassName("kermeta::standard::Character");
 		Character.setValue(result, getValue(self).charAt(Integer.getValue(param0)));
 		return result;
 	}
 
-	// Implementation of method indexOf called as :
-	// extern fr::irisa::triskell::kermeta::runtime::basetypes::String::indexOf(str)
+	/** Implementation of method indexOf called as :
+	 * extern fr::irisa::triskell::kermeta::runtime::basetypes::String::indexOf(str)
+	 */
 	public static RuntimeObject indexOf(RuntimeObject self, RuntimeObject param0) {
 		RuntimeObject result = self.getFactory().createObjectFromClassName("kermeta::standard::Integer");
 		Integer.setValue(result, getValue(self).indexOf(getValue(param0)));
 		return result;
 	}
 
-	// Implementation of method subString called as :
-	// extern fr::irisa::triskell::kermeta::runtime::basetypes::String::subString(index, length)
+	/** Implementation of method subString called as :
+	 * extern fr::irisa::triskell::kermeta::runtime::basetypes::String::subString(index, length)
+	 */
 	public static RuntimeObject substring(RuntimeObject self, RuntimeObject param0, RuntimeObject param1) {
 		RuntimeObject result = self.getFactory().createObjectFromClassName("kermeta::standard::String");
 		setValue(result, getValue(self).substring(Integer.getValue(param0), Integer.getValue(param1)));
 		return result;
 	}
 
-	// Implementation of method size called as :
-	// extern fr::irisa::triskell::kermeta::runtime::basetypes::String::size()
+	/** Implementation of method size called as :
+	 * extern fr::irisa::triskell::kermeta::runtime::basetypes::String::size()
+	 */
 	public static RuntimeObject size(RuntimeObject self) {
 		RuntimeObject result = self.getFactory().createObjectFromClassName("kermeta::standard::Integer");
 		Integer.setValue(result, getValue(self).length());
 		return result;
 	}
 
+
+	/** method used to set a RuntimeObject String with a java value
+	 * @param str
+	 * @param value
+	 */
 	public static void setValue(RuntimeObject str, java.lang.String value) {
-	    // FIXME : patch until we know how to use regexp in java...
-	    value = value.replaceAll("\\\\n","__newline__");
-	    value = value.replaceAll("\\\\", "");
-	    value = value.replaceAll("__newline__","\n");
+	    
+		// DVK : removed the replace all,
+		// I don't know why string should be transformed here
+		// if this is for the parser, then the parser must do it
+		// FIXME : patch until we know how to use regexp in java...
+		//value = value.replaceAll("\\\\n","__newline__");
+	    //value = value.replaceAll("\\\\", "");
+	    //value = value.replaceAll("__newline__","\n");
 	    str.getData().put("StringValue", value);
 	}
 	
+	/**
+	 * retreives the java string from the Runtime object Strings
+	 * @param str
+	 * @return
+	 */
 	public static java.lang.String getValue(RuntimeObject str) {
 		if (!str.getData().containsKey("StringValue")) setValue(str, "");
 		return (java.lang.String)str.getData().get("StringValue");
