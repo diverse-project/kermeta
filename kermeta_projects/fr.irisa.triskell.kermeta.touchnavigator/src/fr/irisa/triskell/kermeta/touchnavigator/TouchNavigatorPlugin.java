f483bca91cb5c8e807327379c1063221f048e5b1
Didier Vojtisek <didier.vojtisek@inria.fr>
2005-10-24 20:30:05 +0000

    Initial revision for a TouchGraph navigator for kermeta and metamodels
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@993 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

---
+++ kermeta_projects/fr.irisa.triskell.kermeta.touchnavigator/src/fr/irisa/triskell/kermeta/touchnavigator/TouchNavigatorPlugin.java
@@ -0,0 +1,54 @@
+package fr.irisa.triskell.kermeta.touchnavigator;
+
+import org.eclipse.ui.plugin.*;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class TouchNavigatorPlugin extends AbstractUIPlugin {
+
+	//The shared instance.
+	private static TouchNavigatorPlugin plugin;
+	
+	/**
+	 * The constructor.
+	 */
+	public TouchNavigatorPlugin() {
+		plugin = this;
+	}
+
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static TouchNavigatorPlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path.
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return AbstractUIPlugin.imageDescriptorFromPlugin("fr.irisa.triskell.kermeta.touchnavigator", path);
+	}
+}
