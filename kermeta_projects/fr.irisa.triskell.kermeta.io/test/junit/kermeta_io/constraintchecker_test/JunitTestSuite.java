e398345036c375a09a5d22c6a46c85a2266ec02d
Didier Vojtisek <didier.vojtisek@inria.fr>
2005-11-23 09:35:49 +0000

    added static constraint tests
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@1057 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

---
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/test/junit/kermeta_io/constraintchecker_test/JunitTestSuite.java
@@ -0,0 +1,138 @@
+/* $Id: JunitTestSuite.java,v 1.1 2005-11-23 09:35:49 dvojtise Exp $
+ * Project    : fr.irisa.triskell.kermeta.io
+ * File       : JunitTestSuite.java
+ * License    : EPL
+ * Copyright  : IRISA / INRIA / Universite de Rennes 1
+ * -------------------------------------------------------------------
+ * Creation date : 27 janv. 2005
+ * Authors : 
+ *        	Franck FLEUREY (ffleurey@irisa.fr)
+ * Description : 
+ *        	Kermeta TestSuite for io project
+ * 			this file is partially generated using GenerateJUnitTestSuite. 
+ * 			Do not manually modify the generated part. 
+ * 			test that valid test cases have no error
+ * 			tests that invalid test cases have at least one error
+ */
+
+package kermeta_io.constraintchecker_test;
+
+import junit.framework.TestCase;
+import fr.irisa.triskell.kermeta.loader.KermetaUnit;
+import fr.irisa.triskell.kermeta.loader.KermetaUnitFactory;
+
+
+/**
+ * @author Didier Vojtisek
+ * Kermeta TestSuite for io project, static constraints
+ */
+public class JunitTestSuite extends TestCase {
+
+
+	public JunitTestSuite(String arg0) {
+		super(arg0);
+		
+		 // SET THE STD LIB
+	    KermetaUnit.STD_LIB_URI = "lib/framework.km";
+	    // INIT TYPE CHECKER
+	  //  TypeCheckerContext.initializeTypeChecker(KermetaUnit.getStdLib());
+		
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+
+/*** BEGIN GENERATED TESTS ***/
+public void testvalid_003_associations() throws Exception {
+testvalidFile("test/constraintchecker_tests/valid","003_associations.kmt" );
+}
+
+public void testinvalid_003_associations_01() throws Exception {
+testinvalidFile("test/constraintchecker_tests/invalid","003_associations_01.kmt" );
+}
+
+public void testinvalid_003_associations_02() throws Exception {
+testinvalidFile("test/constraintchecker_tests/invalid","003_associations_02.kmt" );
+}
+
+public void testinvalid_003_associations_03() throws Exception {
+testinvalidFile("test/constraintchecker_tests/invalid","003_associations_03.kmt" );
+}
+
+public void testinvalid_003_associations_04() throws Exception {
+testinvalidFile("test/constraintchecker_tests/invalid","003_associations_04.kmt" );
+}
+
+public void testinvalid_003_associations_05() throws Exception {
+testinvalidFile("test/constraintchecker_tests/invalid","003_associations_05.kmt" );
+}
+
+public void testinvalid_004_cyclicCompositions_01() throws Exception {
+testinvalidFile("test/constraintchecker_tests/invalid","004_cyclicCompositions_01.kmt" );
+}
+
+public void testinvalid_004_cyclicCompositions_02() throws Exception {
+testinvalidFile("test/constraintchecker_tests/invalid","004_cyclicCompositions_02.kmt" );
+}
+
+/*** END GENERATED TESTS ***/
+	// do not modify this comment
+
+
+
+
+	public void testvalidFile(String dir, String file) throws Exception {
+	    
+	    KermetaUnitFactory.getDefaultLoader().unloadAll();
+
+	    // LOAD THE UNIT
+	    KermetaUnit builder = KermetaUnitFactory.getDefaultLoader().createKermetaUnit(dir + "/" + file);
+		try {
+		builder.load();
+		} catch(Exception e ) {if (builder.messages.getErrors().size() == 0) throw e;};
+		
+		if (builder.messages.getAllErrors().size() > 0) {
+			assertTrue(builder.messages.getAllMessagesAsString(), false);
+		}
+		
+		builder.typeCheck(null);
+		
+		//KermetaTypeChecker tc = builder.getTypeChecker();
+		
+		if (builder.messages.getAllErrors().size() > 0) {
+			assertTrue(builder.messages.getAllMessagesAsString(), false);
+		}
+	}
+	
+	public void testinvalidFile(String dir, String file) throws Exception {
+	    
+	    KermetaUnitFactory.getDefaultLoader().unloadAll();
+
+	    // LOAD THE UNIT
+	    KermetaUnit builder = KermetaUnitFactory.getDefaultLoader().createKermetaUnit(dir + "/" + file);
+		try {
+		builder.load();
+		} catch(Exception e ) {if (builder.messages.getErrors().size() == 0) throw e;};
+		
+		if (builder.messages.getAllErrors().size() > 0) {
+			assertTrue(builder.messages.getAllMessagesAsString(), false);
+			// loader test must not be done in this test suite
+		}
+		
+		
+		
+		builder.typeCheck(null);
+		
+		
+		if (builder.messages.getAllErrors().size() == 0) {
+			assertTrue("Looking for a constraint error but none found", false);
+		}
+	}
+}
