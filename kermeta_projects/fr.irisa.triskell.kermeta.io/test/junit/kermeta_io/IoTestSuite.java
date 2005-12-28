b6cd378defa55095f5c9f0ba603e3fa497b673bc
Didier Vojtisek <didier.vojtisek@inria.fr>
2005-12-28 12:51:10 +0000

    new global test suite tha facilitate the launch of the tests and coverage tests
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@1222 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

---
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/test/junit/kermeta_io/IoTestSuite.java
@@ -0,0 +1,56 @@
+/* $Id: IoTestSuite.java,v 1.1 2005-12-28 12:51:10 dvojtise Exp $
+ * Project    : fr.irisa.triskell.kermeta.io
+ * File       : IoTestSuite.java
+ * License    : EPL
+ * Copyright  : IRISA / INRIA / Universite de Rennes 1
+ * -------------------------------------------------------------------
+ * Creation date : 27 janv. 2005
+ * Authors : 
+ *        	Didier Vojtisek (dvojtise@irisa.fr)
+ * Description : 
+ *        	Kermeta Main TestSuite for io project
+ */
+
+package kermeta_io;
+
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+import fr.irisa.triskell.kermeta.loader.KermetaUnit;
+
+
+/**
+ * @author Didier Vojtisek
+ * Kermeta Global TestSuite for io project
+ */
+public class IoTestSuite extends TestSuite {
+
+
+	public IoTestSuite(String arg0) {
+		super(arg0);
+		
+		 // SET THE STD LIB
+	    KermetaUnit.STD_LIB_URI = "lib/framework.km";
+	    // INIT TYPE CHECKER
+	  //  TypeCheckerContext.initializeTypeChecker(KermetaUnit.getStdLib());
+	    this.addTest(new TestSuite(kermeta_io.constraintchecker_test.JunitTestSuite.class));
+	    this.addTest(new TestSuite(kermeta_io.roundtrip_test.JunitTestSuite.class));
+	    this.addTest(new TestSuite(kermeta_io.typechecker_test.JunitTestSuite.class));
+	    this.addTest(new TestSuite(kermeta_io.test.JunitTestSuite.class));
+	}
+	
+	public static void main(String args[]) { 
+		IoTestSuite ioSuite = new IoTestSuite("");
+		TestResult result = new TestResult();
+		ioSuite.run(result);
+		System.out.println(result.toString());
+	}
+	public void  testIo()
+	{
+		/*TestSuite suite= new TestSuite(kermeta_io.constraintchecker_test.JunitTestSuite.class);
+		TestResult result = new TestResult();
+		suite.run(result);*/ 
+		//return result;
+	}
+	
+	
+}
