b4cf4e5d92cd7619ff233b7430ce40fd6e39ce66
Olivier Barais <olivier.barais@gmail.com>
2006-01-17 08:46:27 +0000

    *** empty log message ***
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@1252 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

---
+++ kermeta_projects/fr.irisa.triskell.kermeta/src/java/fr/irisa/triskell/kermeta/popup/actions/EcoreMerge2EcoreAction.java
@@ -0,0 +1,91 @@
+/* $Id: EcoreMerge2EcoreAction.java,v 1.1 2006-01-17 08:46:04 barais Exp $
+ * Project : Kermeta
+ * File : Kermeta2EcoreAction.java
+ * License : EPL
+ * Copyright : IRISA / INRIA / Universite de Rennes 1
+ * ----------------------------------------------------------------------------
+ * Creation date : 8 juil. 2005
+ * Authors : 
+ * 		Didier Vojtisek	<dvojtise@irisa.fr>
+ * Description :  	
+ * 	see class javadoc.	
+ * History :
+ */
+package fr.irisa.triskell.kermeta.popup.actions;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
+
+import fr.irisa.triskell.kermeta.tools.wizards.EcoreMerge2EcoreWizard;
+
+/**
+ * This action will generate an ecore file from several ecore files The current
+ * version ovewrite the destination
+ */
+public class EcoreMerge2EcoreAction implements IObjectActionDelegate {
+
+	protected StructuredSelection currentSelection;
+
+	protected List<IFile> ecorefiles = new ArrayList<IFile>();
+
+	/**
+	 * Constructor for Kermeta2EcoreAction.
+	 */
+	public EcoreMerge2EcoreAction() {
+		super();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		Shell shell = new Shell();
+		/*
+		 * MessageDialog.openWarning( shell, "Warning", "This feature has not
+		 * been tested, it cannot be considered as stable.\n" + "The ecore file
+		 * produced may contain errors.");
+		 * 
+		 */
+
+		EcoreMerge2EcoreWizard wizard = new EcoreMerge2EcoreWizard();
+		wizard.init(PlatformUI.getWorkbench(), currentSelection);
+		WizardDialog wizDialog = new org.eclipse.jface.wizard.WizardDialog(
+				shell, wizard);
+		wizDialog.setTitle("Merge these ecore files into an ecore file");
+		wizDialog.open();
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		if (selection instanceof StructuredSelection) {
+			// the selection should be a single *.kmt (or a *.km ?) file
+			currentSelection = (StructuredSelection) selection;
+			ecorefiles.clear();
+			Iterator it = currentSelection.iterator();
+
+			while (it.hasNext()) {
+				ecorefiles.add((IFile) it.next());
+			}
+		}
+	}
+}
