29ac698ad90fad30af85185f4025c3e1ecc28f1e
zdrey <zdrey@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2006-01-18 16:07:16 +0000

    gendoxygen for mandarine
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@1253 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

---
+++ kermeta_projects/fr.irisa.triskell.kermeta.documentation/src/doxygen/mandarine_gendoxygenmerge.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# This file contains special configuration stuff for mandarine machine
+#
+export PATH=$PATH:/local/bin
+
+#
+# cd /<workspace_directory>/fr.irisa.triskell.kermeta.documentation/src/doxygen
+
+/bin/bash gendoxygenmerge.sh
