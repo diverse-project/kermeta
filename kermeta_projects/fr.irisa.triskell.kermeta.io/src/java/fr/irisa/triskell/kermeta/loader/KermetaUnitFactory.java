95e0f25c2a03829106a2e1010e4d96074c949f90
zdrey <zdrey@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2006-02-22 09:33:45 +0000

    removed dead commented code
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@1327 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/loader/KermetaUnitFactory.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/loader/KermetaUnitFactory.java
@@ -1,4 +1,4 @@
-/* $Id: KermetaUnitFactory.java,v 1.13 2005-07-08 12:22:39 dvojtise Exp $
+/* $Id: KermetaUnitFactory.java,v 1.14 2006-02-22 09:33:45 zdrey Exp $
  * Project: Kermeta.io
  * File: KermetaUnitFactory.java
  * License: EPL
@@ -158,67 +158,6 @@ public class KermetaUnitFactory {
         //if (std) standard_lib = result;
     	return result;
     }
-    
-    
-    
-    /*
-     * TODO : Gestion des URI
-     * 
-     * J'ai remis l'ancienne version qui apparement ne marche pas sous windows
-     * car les changemement fait par jean-phillipe font que ca ne marche plus du tout 
-     * lorsque les uri commence par platform:, http: ou tout autre chose que file:
-     * En plus ca ne marche pas sous linux.
-     * Il y a probablement quelque chose a ameliorer ici pour résoudre les problème
-     * de compatibilité d'OS. Cela dit il faut s'appuier sur la gestion d'URI de Eclipse
-     * qui si je ne me trompe pas marche bien sous linux et windows.
-     * 
-     * J'ai remis ci-dessous le code de JP
-     */
-    
-    /*
-    public KermetaUnit createKermetaUnit(String uri) {
-    	
-    	//System.out.println("ASK UNIT " + uri);
-    	
-    	// TODO : reslove URI if it is not
-    	KermetaUnit result = null;
-    	
-    	// resolve the URI if it is in the KMPath
-    	if (kmPath.containsKey(uri)) uri = (String)kmPath.get(uri);
-    	
-    	// resolve uri
-    	if (uri.startsWith("file:"))
-    		uri=uri.substring(5,uri.length());
-    	URI u = URI.createFileURI(uri);
-    	if (u.isRelative()) {
-    	//	URIConverter c = new URIConverterImpl();
-    	//	u = u.resolve(c.normalize(URI.createURI(".")));
-    		u=UserDirURI.createURI(u);
-    	}
-    	
-    
-    	// return the unit if it already exists
-    	if (loadedUnits.containsKey(u.toString())) return (KermetaUnit)loadedUnits.get(u.toString());
-    	
-    	//System.out.println("CREATE UNIT " + u.toString());
-    	
-    	// Create the appropriate Unit using the loader registerered for the file extension
-    	
-    	KermetaLoaderModule loader = null;
-    	if (u.fileExtension() != null) 
-        	loader = (KermetaLoaderModule)loadModules.get(u.fileExtension());
-        if (loader == null) {
-            	System.err.println("TODO: manage this error : loader is null !");
-        	// TODO : generate an error
-        	//result.error.add(new KMUnitError("Unable to load resource " + uri + " : no loader registered.", null));
-        	//return result;
-        }
-        result = loader.createKermetaUnit("file:"+u.toFileString().replaceAll("\\\\","/"));
-        loadedUnits.put(u.toString(), result);
-    	return result;
-    }
-    */
-    
     
     /**
      * Removes the current defaultLoader so the next cal to getDefaultLoader 
