0fb0e662853e68a9bed9d9078c13075774059feb
zdrey <zdrey@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2006-03-03 16:34:08 +0000

    new classes -> added objects in configurator
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@1377 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

---
+++ kermeta_projects/fr.irisa.triskell.kermeta.graphicaleditor/src/fr/irisa/triskell/kermeta/graphicaleditor/diagram/edit/TagLinkEditPart.java
@@ -0,0 +1,116 @@
+package fr.irisa.triskell.kermeta.graphicaleditor.diagram.edit;
+
+import org.eclipse.draw2d.ConnectionRouter;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.PolylineConnection;
+import org.eclipse.gmf.runtime.draw2d.ui.internal.figures.ConnectionLayerEx;
+import org.topcased.modeler.ModelerEditPolicyConstants;
+import org.topcased.modeler.di.model.EdgeObject;
+import org.topcased.modeler.di.model.GraphEdge;
+import org.topcased.modeler.edit.GraphEdgeEditPart;
+import org.topcased.modeler.figures.IEdgeObjectFigure;
+
+import fr.irisa.triskell.kermeta.graphicaleditor.diagram.figures.TagLinkFigure;
+
+/**
+ * TagLink controller <br>
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class TagLinkEditPart extends GraphEdgeEditPart {
+
+	/**
+	 * Constructor
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param model the graph object
+	 * @generated
+	 */
+	public TagLinkEditPart(GraphEdge model) {
+		super(model);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * 
+	 * @see org.eclipse.gef.editparts.AbstractEditPart#createEditPolicies()
+	 * @generated
+	 */
+	protected void createEditPolicies() {
+		super.createEditPolicies();
+		installEditPolicy(ModelerEditPolicyConstants.CHANGE_FONT_EDITPOLICY,
+				null);
+
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the Figure
+	 * @generated
+	 */
+	protected IFigure createFigure() {
+		TagLinkFigure connection = new TagLinkFigure();
+		createSourceDecoration(connection);
+		createTargetDecoration(connection);
+
+		return connection;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param connection the PolylineConnection
+	 * @generated
+	 */
+	private void createSourceDecoration(PolylineConnection connection) {
+
+		// no decoration                
+
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param connection the PolylineConnection
+	 * @generated
+	 */
+	private void createTargetDecoration(PolylineConnection connection) {
+
+		// no decoration                
+
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see org.topcased.modeler.edit.GraphEdgeEditPart#getEdgeObjectFigure(org.topcased.modeler.di.model.EdgeObject)
+	 * @generated
+	 */
+	public IEdgeObjectFigure getEdgeObjectFigure(EdgeObject edgeObject) {
+		return null;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see org.topcased.modeler.edit.GraphEdgeEditPart#getDefaultRouter(org.eclipse.gmf.runtime.draw2d.ui.internal.figures.ConnectionLayerEx)
+	 * @generated
+	 */
+	protected ConnectionRouter getDefaultRouter(ConnectionLayerEx cLayer) {
+		return cLayer.getObliqueRouter();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void refreshVisuals() {
+		super.refreshVisuals();
+
+	}
+
+}
---
+++ kermeta_projects/fr.irisa.triskell.kermeta.graphicaleditor/src/fr/irisa/triskell/kermeta/graphicaleditor/diagram/policies/InheritanceEdgeCreationEditPolicy.java
@@ -0,0 +1,107 @@
+package fr.irisa.triskell.kermeta.graphicaleditor.diagram.policies;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gef.EditDomain;
+import org.eclipse.gef.commands.Command;
+import org.topcased.modeler.commands.CreateTypedEdgeCommand;
+import org.topcased.modeler.di.model.GraphEdge;
+import org.topcased.modeler.di.model.GraphNode;
+import org.topcased.modeler.di.model.SimpleSemanticModelElement;
+import org.topcased.modeler.edit.policies.AbstractEdgeCreationEditPolicy;
+import org.topcased.modeler.utils.SourceTargetData;
+import org.topcased.modeler.utils.Utils;
+
+import fr.irisa.triskell.kermeta.graphicaleditor.StructureEditPolicyConstants;
+import fr.irisa.triskell.kermeta.graphicaleditor.diagram.commands.InheritanceEdgeCreationCommand;
+
+/**
+ * Inheritance edge creation <br>
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class InheritanceEdgeCreationEditPolicy extends
+		AbstractEdgeCreationEditPolicy {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected CreateTypedEdgeCommand createCommand(EditDomain domain,
+			GraphEdge edge, GraphNode source) {
+		return new InheritanceEdgeCreationCommand(domain, edge, source);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected boolean checkEdge(GraphEdge edge) {
+		if (edge.getSemanticModel() instanceof SimpleSemanticModelElement) {
+			return (StructureEditPolicyConstants.INHERITANCE_EDITPOLICY
+					.equals(((SimpleSemanticModelElement) edge
+							.getSemanticModel()).getTypeInfo()));
+		}
+		return false;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected boolean checkSource(GraphNode source) {
+		EObject object = Utils.getElement(source);
+		if (object instanceof fr.irisa.triskell.kermeta.structure.FClassDefinition) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected boolean checkTargetForSource(GraphNode source, GraphNode target) {
+		EObject sourceObject = Utils.getElement(source);
+		EObject targetObject = Utils.getElement(target);
+
+		if (sourceObject instanceof fr.irisa.triskell.kermeta.structure.FClassDefinition
+				&& targetObject instanceof fr.irisa.triskell.kermeta.structure.FClassDefinition) {
+			if (!sourceObject.equals(targetObject)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected boolean checkCommand(Command command) {
+		return command instanceof InheritanceEdgeCreationCommand;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected SourceTargetData getSourceTargetData(GraphNode source,
+			GraphNode target) {
+		EObject sourceObject = Utils.getElement(source);
+		EObject targetObject = Utils.getElement(target);
+
+		if (sourceObject instanceof fr.irisa.triskell.kermeta.structure.FClassDefinition
+				&& targetObject instanceof fr.irisa.triskell.kermeta.structure.FClassDefinition) {
+			return new SourceTargetData(false, false, 0, null, null, null,
+					null, null, null, null, null);
+		}
+		return null;
+	}
+
+}
