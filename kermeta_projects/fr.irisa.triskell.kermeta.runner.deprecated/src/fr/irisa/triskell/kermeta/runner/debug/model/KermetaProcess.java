310d672a2072860994f9d9d8ba89044ff87e6075
Olivier Barais <olivier.barais@gmail.com>
2006-03-06 11:10:26 +0000

    Correct the problem of the classpath users entry under linux
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@1378 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.runner.deprecated/src/fr/irisa/triskell/kermeta/runner/debug/model/KermetaProcess.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.runner.deprecated/src/fr/irisa/triskell/kermeta/runner/debug/model/KermetaProcess.java
@@ -1,4 +1,4 @@
-/* $Id: KermetaProcess.java,v 1.16 2006-01-25 16:06:21 dvojtise Exp $
+/* $Id: KermetaProcess.java,v 1.17 2006-03-06 11:10:26 barais Exp $
  * Project   : Kermeta runner
  * File      : KermetaProcess.java
  * License   : EPL
@@ -27,10 +27,12 @@ import fr.irisa.triskell.kermeta.util.LogConfigurationHelper;
 public class KermetaProcess extends Thread {
 
 	/** logger for the Process */
-	final static public Logger internalLog = LogConfigurationHelper.getLogger("Kermeta");
+	final static public Logger internalLog = LogConfigurationHelper
+			.getLogger("Kermeta");
 
-	public String defaultName = "Kermeta Execution Thread";
 	
+	public String defaultName = "Kermeta Execution Thread";
+
 	/**
 	 * Constructor, uses the default name for the Thread
 	 */
@@ -51,60 +53,67 @@ public class KermetaProcess extends Thread {
 	public synchronized void start() {
 		super.start();
 	}
-	
-	public void updateThreadClassLoader(List pathAttribute)
-	{
-        Vector<URL> urlsV = new Vector<URL>();
-        
+
+	public void updateThreadClassLoader(List pathAttribute) {
+		Vector<URL> urlsV = new Vector<URL>();
+
 		for (int i = 0; i < pathAttribute.size(); i++) {
-            String memento1 = (String)pathAttribute.get(i);
-            try {
-                IRuntimeClasspathEntry entry1 = 
-                   JavaRuntime.newRuntimeClasspathEntry(memento1);                
-                // resolve this classpath entry
-                //org.eclipse.jdt.launching.StandardClasspathProvider resolver;
-                try {
-                	//entry1.toString();
-                	if(entry1.getType() == IRuntimeClasspathEntry.ARCHIVE)
-                	{
-                    	urlsV.add(new URL("file:/" + entry1.getLocation()));
-                	}
-                	else
-                	{
-                    	urlsV.add(new URL("file:/" + entry1.getLocation() + "/"));
-                	}
-                	RunnerPlugin.pluginLog.debug("added " + "file:/" + entry1.getLocation() + " in Thread Class Loader");
+			String memento1 = (String) pathAttribute.get(i);
+			try {
+				IRuntimeClasspathEntry entry1 = JavaRuntime
+						.newRuntimeClasspathEntry(memento1);
+				// resolve this classpath entry
+				// org.eclipse.jdt.launching.StandardClasspathProvider resolver;
+				try {
+					// entry1.toString();
+					if (entry1.getLocation() != null) {
+						if (entry1.getType() == IRuntimeClasspathEntry.ARCHIVE) {
+							urlsV.add(new URL("file://" + entry1.getLocation().replaceAll("/", "//")));
+						} else {
+							urlsV.add(new URL("file://" +entry1.getLocation().replaceAll("/", "//")
+									+ "//"));
+						}
+						RunnerPlugin.pluginLog.debug("added " + "file://" + entry1.getLocation().replaceAll("/", "//")
+								+ " in Thread Class Loader " + this.getName());
+					}
 				} catch (MalformedURLException e) {
-					RunnerPlugin.pluginLog.warn("problem with an entry of the classpath, " + "file:/" + entry1.getLocation() + " cannot be added in classloader",e);
+					RunnerPlugin.pluginLog.warn(
+							"problem with an entry of the classpath, "
+									+ "file:/" + entry1.getLocation()
+									+ " cannot be added in classloader", e);
 				}
-                // IRuntimeClasspathEntryResolver
-              //  this.getIPathFromString()
-            } catch (CoreException e) {
-            	RunnerPlugin.pluginLog.warn("Problem reading classpath entry", e);
-                RunnerPlugin.log(e);
-            	return ;
-            }
-        }
+				// IRuntimeClasspathEntryResolver
+				// this.getIPathFromString()
+			} catch (CoreException e) {
+				RunnerPlugin.pluginLog.warn("Problem reading classpath entry",
+						e);
+				RunnerPlugin.log(e);
+				return;
+			}
+		}
 		URL[] urls = new URL[urlsV.size()];
 		for (int i = 0; i < urlsV.size(); i++) {
 			urls[i] = urlsV.elementAt(i);
 		}
-		//URLClassLoader cl = new URLClassLoader(urls, this.getContextClassLoader());
-		// use this object class loader as parent (instead of the default thread class loader)
-		//    because it also contains the plugin classloader rules
-		URLClassLoader cl = new URLClassLoader(urls, this.getClass().getClassLoader());
+		// URLClassLoader cl = new URLClassLoader(urls,
+		// this.getContextClassLoader());
+		// use this object class loader as parent (instead of the default thread
+		// class loader)
+		// because it also contains the plugin classloader rules
+		URLClassLoader cl = new URLClassLoader(urls, this.getClass()
+				.getClassLoader());
 		this.setContextClassLoader(cl);
-		/*URL res  = cl.findResource("waf/Test.class");
-		try {
-			System.err.println(cl.loadClass("waf.Test"));
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		System.err.println("After cl changed, Test is here : " +res);*/
+		
+		/*
+		 * URL res = cl.findResource("waf/Test.class"); try {
+		 * System.err.println(cl.loadClass("waf.Test")); } catch
+		 * (ClassNotFoundException e) { e.printStackTrace(); }
+		 * System.err.println("After cl changed, Test is here : " +res);
+		 */
 	}
-	
+
 	/** Terminates properly the interpreter */
-	public synchronized void terminate() {		
+	public synchronized void terminate() {
 		RunnerPlugin.pluginLog.debug("KermetaProcess terminates");
-	}	
+	}
 }
