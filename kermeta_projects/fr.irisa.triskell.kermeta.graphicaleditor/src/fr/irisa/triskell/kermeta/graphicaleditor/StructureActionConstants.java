0775c693a3643faacae115b91e1e03d8d14bfd14
zdrey <zdrey@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2006-03-09 18:25:47 +0000

    For additionnal actions specific to kmEditor
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@1391 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

---
+++ kermeta_projects/fr.irisa.triskell.kermeta.graphicaleditor/src/fr/irisa/triskell/kermeta/graphicaleditor/StructureActionConstants.java
@@ -0,0 +1,12 @@
+package fr.irisa.triskell.kermeta.graphicaleditor;
+
+/**
+ * Constants related to [GUI]-available actions on the model.
+ */
+public interface StructureActionConstants {
+	/** Public constant defining the Validate action id. */
+	public static final String VALIDATE = "fr.irisa.triskell.kermeta.graphicaleditor.validate";
+	/** Public constant defining the Validate action text. */
+	public static final String VALIDATE_TEXT = "Check constraints";
+
+}
