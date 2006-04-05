1861a0855657fcf10b12733ff17a3476d601096f
zdrey <zdrey@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2006-04-05 17:28:01 +0000

    not sure if it is the best place, but added a special "chunkParser" that parses a supposed-to-be-Operation and returns the model representation (namely, an Expression) of the body of this operation.
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@1463 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/loader/expression/ExpressionParser.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/loader/expression/ExpressionParser.java
@@ -1,4 +1,4 @@
-/* $Id: ExpressionParser.java,v 1.6 2006-03-03 15:22:18 dvojtise Exp $
+/* $Id: ExpressionParser.java,v 1.7 2006-04-05 17:28:01 zdrey Exp $
 * Project : Kermeta (First iteration)
 * File : DynamicExpressionUnit.java
 * License : EPL
@@ -13,10 +13,13 @@ import java.io.StringReader;
 
 import antlr.RecognitionException;
 import antlr.TokenStreamException;
+import fr.irisa.triskell.kermeta.ast.OperationExpressionBody;
 import fr.irisa.triskell.kermeta.language.behavior.Expression;
+import fr.irisa.triskell.kermeta.language.structure.Operation;
 import fr.irisa.triskell.kermeta.loader.KMUnitError;
 import fr.irisa.triskell.kermeta.loader.KermetaUnit;
 import fr.irisa.triskell.kermeta.loader.kmt.KMT2KMExperessionBuilder;
+import fr.irisa.triskell.kermeta.loader.kmt.KMT2KMPass;
 import fr.irisa.triskell.kermeta.parser.KermetaLexer;
 import fr.irisa.triskell.kermeta.parser.KermetaParser;
 
@@ -25,6 +28,13 @@ import fr.irisa.triskell.kermeta.parser.KermetaParser;
  */
 public class ExpressionParser {
 
+	/**
+	 * Parses a String that represents a Kermeta expression and return the corresponding model
+	 * element (which type is fr.irisa.triskell.kermeta.language.behavior.Expression)
+	 * @param unit The KermetaUnit-context in which this String should be evaluated
+	 * @param str A string that is supposed to be a Kermeta expression 
+	 * @return the model element representing an Expression.
+	 */
 	public static Expression parse(KermetaUnit unit, String str) {
 		KermetaParser parser = new KermetaParser(new KermetaLexer(new StringReader(str.replace('\t', ' '))));
 		fr.irisa.triskell.kermeta.ast.FExpression  ast_exp = null;
@@ -46,5 +56,38 @@ public class ExpressionParser {
 		return result;
 	}
 
+	/**
+	 * Parses a String which is supposed to represent a Kermeta Operation, and return the model object representing
+	 * the <b>Body</b> of this operation.
+	 * @param unit The KermetaUnit-context in which this String should be evaluated
+	 * @param str The string supposed to represent an operation. (a "firstKeyword" visitor?)
+	 * @return the model element representing an Expression.
+	 */
+	public static Expression parse_operation2body(KermetaUnit unit, String str) {
+		KermetaParser parser = new KermetaParser(new KermetaLexer(new StringReader(str.replace('\t', ' '))));
+		fr.irisa.triskell.kermeta.ast.OperationBody  ast_op = null;
+		fr.irisa.triskell.kermeta.ast.FExpression  ast_exp = null;
+		try {
+			ast_op = parser.operation().getOperationBody();
+			// If the operation contains an expression, we parse it (otherwise, its type is OperationEmptyBody)
+			if (ast_op!=null && ast_op instanceof OperationExpressionBody)
+			{
+				ast_exp = ((OperationExpressionBody) ast_op).getFExpression();
+			}
+			
+		} catch (RecognitionException e) {
+			 unit.messages.addMessage(new KMUnitError("Operation Parse error : " + e, null, ast_op));
+			 throw new Error(e);
+		} catch (TokenStreamException e) {
+			 unit.messages.addMessage(new KMUnitError("Operation Parse error : " + e, null, ast_op));
+			 throw new Error(e);
+		}
+		Expression result;
+		if (ast_exp != null)
+			result = KMT2KMExperessionBuilder.process(ast_exp, unit);
+		else 
+			result = null;
+		return result;
+	}
 
 }
