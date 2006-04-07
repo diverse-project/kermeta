d24fdbbc3d11e92aa67c24f73985144cef2c9715
Didier Vojtisek <didier.vojtisek@inria.fr>
2006-04-07 20:43:31 +0000

    extraction of the inner class PAckageHeaderFigureBorder so we workaround the deployment problem (worked fine in runtime workbench but failed in deployed mode)
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@1488 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.graphicaleditor/src/fr/irisa/triskell/kermeta/graphicaleditor/diagram/figures/StructureDiagramFigure.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.graphicaleditor/src/fr/irisa/triskell/kermeta/graphicaleditor/diagram/figures/StructureDiagramFigure.java
@@ -1,10 +1,6 @@
 package fr.irisa.triskell.kermeta.graphicaleditor.diagram.figures;
 
-import org.eclipse.draw2d.AbstractBorder;
-import org.eclipse.draw2d.Graphics;
-import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.PositionConstants;
-import org.eclipse.draw2d.geometry.Insets;
 import org.topcased.draw2d.figures.ILabel;
 import org.topcased.draw2d.figures.Label;
 import org.topcased.modeler.figures.DiagramFigure;
@@ -28,39 +24,6 @@ public class StructureDiagramFigure extends DiagramFigure {
 		header.setBorder(new PackageHeaderFigureBorder());
 
 		return header;
-	}
-
-	/**
-	 * A Border at the left, top and right of the label
-	 */
-	public static class PackageHeaderFigureBorder extends AbstractBorder {
-		private static final Insets INSETS = new Insets(3, 3, 1, 4);
-
-		/**
-		 * @see org.eclipse.draw2d.Border#getInsets(org.eclipse.draw2d.IFigure)
-		 */
-		public Insets getInsets(IFigure figure) {
-			return INSETS;
-		}
-
-		/**
-		 * Draw the 3 borders
-		 * @see org.eclipse.draw2d.Border#paint(org.eclipse.draw2d.IFigure, org.eclipse.draw2d.Graphics, org.eclipse.draw2d.geometry.Insets)
-		 */
-		public void paint(IFigure figure, Graphics graphics, Insets insets) {
-			graphics.pushState();
-
-			graphics.drawLine(
-					getPaintRectangle(figure, insets).getBottomLeft(), tempRect
-							.getTopLeft());
-			graphics.drawLine(getPaintRectangle(figure, insets).getTopLeft(),
-					tempRect.getTopRight());
-			graphics.drawLine(getPaintRectangle(figure, insets).getTopRight()
-					.translate(-1, 0), tempRect.getBottomRight().translate(-1,
-					0));
-
-			graphics.popState();
-		}
 	}
 
 }
