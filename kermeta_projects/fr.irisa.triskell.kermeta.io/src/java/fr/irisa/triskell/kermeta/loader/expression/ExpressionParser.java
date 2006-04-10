33eb84024f3685196b2f8fc2031c4865489f4f55
zdrey <zdrey@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2006-04-10 17:32:03 +0000

    added a method to parse only the body of an operation
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@1494 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/loader/expression/ExpressionParser.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/loader/expression/ExpressionParser.java
@@ -1,4 +1,4 @@
-/* $Id: ExpressionParser.java,v 1.7 2006-04-05 17:28:01 zdrey Exp $
+/* $Id: ExpressionParser.java,v 1.8 2006-04-10 17:32:03 zdrey Exp $
 * Project : Kermeta (First iteration)
 * File : DynamicExpressionUnit.java
 * License : EPL
@@ -14,12 +14,14 @@ import java.io.StringReader;
 import antlr.RecognitionException;
 import antlr.TokenStreamException;
 import fr.irisa.triskell.kermeta.ast.OperationExpressionBody;
+import fr.irisa.triskell.kermeta.ast.PrimitiveExpression;
 import fr.irisa.triskell.kermeta.language.behavior.Expression;
 import fr.irisa.triskell.kermeta.language.structure.Operation;
 import fr.irisa.triskell.kermeta.loader.KMUnitError;
 import fr.irisa.triskell.kermeta.loader.KermetaUnit;
 import fr.irisa.triskell.kermeta.loader.kmt.KMT2KMExperessionBuilder;
 import fr.irisa.triskell.kermeta.loader.kmt.KMT2KMPass;
+import fr.irisa.triskell.kermeta.loader.kmt.KMT2KMPrimitiveExpressionBuilder;
 import fr.irisa.triskell.kermeta.parser.KermetaLexer;
 import fr.irisa.triskell.kermeta.parser.KermetaParser;
 
@@ -85,6 +87,42 @@ public class ExpressionParser {
 		Expression result;
 		if (ast_exp != null)
 			result = KMT2KMExperessionBuilder.process(ast_exp, unit);
+		else 
+			result = null;
+		return result;
+	}
+	
+	/**
+	 * Parses a String which is supposed to represent a body, and return the model object representing
+	 * the <b>Body</b> of this operation.
+	 * @param unit The KermetaUnit-context in which this String should be evaluated
+	 * @param str The string supposed to represent an operation. (a "firstKeyword" visitor?)
+	 * @return the model element representing an Expression.
+	 */
+	public static Expression parse_operationbody(KermetaUnit unit, String str) {
+		KermetaParser parser = new KermetaParser(new KermetaLexer(new StringReader(str.replace('\t', ' '))));
+		fr.irisa.triskell.kermeta.ast.OperationBody  ast_op = null;
+		fr.irisa.triskell.kermeta.ast.FExpression  ast_exp = null;
+		try {
+			ast_op = parser.operationBody();
+			// If the operation contains an expression, we parse it (otherwise, its type is OperationEmptyBody)
+			if (ast_op!=null && ast_op instanceof OperationExpressionBody)
+			{
+				ast_exp = ((OperationExpressionBody) ast_op).getFExpression();
+			}
+			
+		} catch (RecognitionException e) {
+			 unit.messages.addMessage(new KMUnitError("Operation Parse error : " + e, null, ast_op));
+			 throw new Error(e);
+		} catch (TokenStreamException e) {
+			 unit.messages.addMessage(new KMUnitError("Operation Parse error : " + e, null, ast_op));
+			 throw new Error(e);
+		}
+		Expression result;
+		if (ast_exp != null)
+		{
+			result = KMT2KMExperessionBuilder.process(ast_exp, unit);
+		}
 		else 
 			result = null;
 		return result;
