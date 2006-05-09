297d8e90846eb4ef36c96277494c9a69c19dacda
jmottu <jmottu@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2006-05-09 16:23:34 +0000

    added support for constraints in kermeta
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@1611 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.interpreter/src/java/fr/irisa/triskell/kermeta/interpreter/ConstraintInterpreter.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.interpreter/src/java/fr/irisa/triskell/kermeta/interpreter/ConstraintInterpreter.java
@@ -1,19 +1,25 @@
 package fr.irisa.triskell.kermeta.interpreter;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import fr.irisa.triskell.kermeta.builder.RuntimeMemory;
+import fr.irisa.triskell.kermeta.language.behavior.CallFeature;
 import fr.irisa.triskell.kermeta.language.behavior.Expression;
 import fr.irisa.triskell.kermeta.language.structure.Constraint;
 import fr.irisa.triskell.kermeta.language.structure.ConstraintType;
 import fr.irisa.triskell.kermeta.language.structure.Operation;
 import fr.irisa.triskell.kermeta.runtime.RuntimeObject;
+import fr.irisa.triskell.kermeta.typechecker.CallableOperation;
+import fr.irisa.triskell.kermeta.typechecker.CallableProperty;
+import fr.irisa.triskell.kermeta.typechecker.SimpleType;
 
 public class ConstraintInterpreter extends ExpressionInterpreter {
+	AtPreVisitor atpreV;
 	
 	public ConstraintInterpreter(RuntimeMemory pMemory) {
 		super(pMemory);
-		// TODO Auto-generated constructor stub
+		atpreV = new AtPreVisitor(pMemory , this , interpreterContext);
 	}
 	
 	
@@ -30,8 +36,28 @@ public class ConstraintInterpreter extends ExpressionInterpreter {
 		// push expression context
 		interpreterContext.peekCallFrame().pushExpressionContext();
 		interpreterContext.peekCallFrame().peekExpressionContext().setStatement(node.getBody());
+		
+		Operation superOp = node.getSuperOperation();
 		try {
+			// Memorise the value of the @pre variables
+			Iterator itatpre = node.getPost().iterator();
+			while(itatpre.hasNext()){
+				atpreV.accept((Constraint)itatpre.next());
+			}
 			
+			if (superOp != null){
+				Iterator itparentatpre = superOp.getPost().iterator();
+				while(itparentatpre.hasNext()){
+					atpreV.accept((Constraint)itparentatpre.next());
+				}
+				
+				// Check the pre conditions
+				Iterator itParentPre = superOp.getPre().iterator();
+				while(itParentPre.hasNext()){
+					this.accept((Constraint)itParentPre.next());
+				}
+			}
+			//TODO : check that parentPre implies Pre			
 			Iterator itpre = node.getPre().iterator();
 			while(itpre.hasNext()){
 				this.accept((Constraint)itpre.next());
@@ -40,12 +66,19 @@ public class ConstraintInterpreter extends ExpressionInterpreter {
 			// Interpret body
 			this.accept(node.getBody());
 			
-			
+			// Check the post conditions
 			Iterator itpost = node.getPost().iterator();
 			while(itpost.hasNext()){
 				this.accept((Constraint)itpost.next());
 			}
 			
+			//TODO : check that post implies parentPost
+			if (superOp != null){
+				Iterator itParentPost = superOp.getPost().iterator();
+				while(itParentPost.hasNext()){
+					this.accept((Constraint)itParentPost.next());
+				}
+			}
 			// Set the result
 			result = interpreterContext.peekCallFrame().getOperationResult();
 		}
@@ -116,4 +149,136 @@ public class ConstraintInterpreter extends ExpressionInterpreter {
 		return result;
 	}
 	
+public Object visitCallFeature(CallFeature node) {
+	    
+	    // Handle call to enumeration literals :
+	    if (node.getStaticEnumLiteral() != null) {
+	        return memory.getRuntimeObjectForFObject(node.getStaticEnumLiteral());
+	    }
+	    
+	    // It is a real operation / property call
+	    
+	    fr.irisa.triskell.kermeta.language.structure.Class t_target = null; // Type of the "callee"
+	    RuntimeObject result = null; // The result to be returned by this visit
+	    RuntimeObject ro_target = null; // Runtime repr. of target
+	    
+	    // Get the target of the call
+	    if (node.getTarget() == null) {
+	        // Self if nothing is specified
+	        ro_target = interpreterContext.peekCallFrame().getSelf();
+	    }
+	    else {
+	        ro_target = (RuntimeObject)this.accept(node.getTarget());
+	    }
+	    
+	    
+	    if (ro_target == null) {
+	        internalLog.error("INTERPRETER INTERNAL ERROR : Call on a null target");
+	        throw new Error("INTERPRETER INTERNAL ERROR : Call on a null target");
+	    }
+	    
+	    // Get The type of the Object
+	    t_target =(fr.irisa.triskell.kermeta.language.structure.Class)((RuntimeObject)ro_target.getMetaclass()).getData().get("kcoreObject");
+	    
+	    // This is just a test for debbuging the interpreter. It should never occur
+	    if (t_target == null) {
+	        internalLog.error("INTERPRETER INTERNAL ERROR : unable to get the type of the target");
+	        throw new Error("INTERPRETER INTERNAL ERROR : unable to get the type of the target");
+	    }
+
+		// Get the feature
+	    SimpleType target_type = new SimpleType(t_target);
+	    
+	    if (node.getStaticOperation() == null && node.getStaticProperty() == null) {
+	    	node.getStaticOperation();
+	    	node.getStaticProperty();
+	        internalLog.error("INTERPRETER INTERNAL ERROR : the program does not seem to be correctly type checked : " + node.getName());
+	        throw new Error("INTERPRETER INTERNAL ERROR : the program does not seem to be correctly type checked : " + node.getName());
+	    }
+	    
+	    
+	    if (node.getStaticOperation() != null) {
+	        // It is an operation call --> 
+	        //CallableOperation operation = target_type.getOperationByName(node.getFName());
+	    	CallableOperation operation = this.interpreterContext.typeCache.getOperationByName(target_type, node.getName());
+	        
+//			 Check that target is not void
+		    if (operation == null && ro_target == memory.voidINSTANCE) {
+		    	String additionalMsg = "";
+		    	if(node.getStaticProperty() != null)
+		    		if(node.getStaticProperty().isIsDerived())
+		    			additionalMsg = "Warning, target was a Derived Property";
+		        internalLog.info(" >> INTERPRETER REPORTS Call on a void target: " + node.getName() +"; Operation: " +node.getStaticOperation());
+		        if(!additionalMsg.equals(""))
+		        	internalLog.info(additionalMsg);
+		        raiseCallOnVoidTargetException(node, additionalMsg);		        
+		    }
+		    
+//		  This should never happend is the type checker has checked the program
+			if (operation == null) {
+			    String msg = "INTERPRETER INTERNAL ERROR : unable to find a feature : "
+				    + "\noperation : '"+node.getName()+"' not found for an object of kind : "+ target_type;
+			    internalLog.error(msg);
+		        throw new Error(msg);
+			}
+	        
+//			 Get the parameters of this operation
+			ArrayList parameters = visitList(node.getParameters());
+			// Create a context for this operation call, setting self object to ro_target
+			interpreterContext.pushOperationCallFrame(ro_target, operation, parameters, node);
+			try {
+				// Resolve this operation call
+				result = (RuntimeObject)this.accept(operation.getOperation());
+				// After operation has been evaluated, pop its context
+			} finally {
+			    interpreterContext.popCallFrame();
+			}
+	    }
+	    else {
+	        // It is a property call
+	        CallableProperty property =  target_type.getPropertyByName(node.getName());
+	    	//CallableProperty property =  this.interpreterContext.typeCache.getPropertyByName(target_type, node.getFName());
+	        
+//			 Check that target is not void
+		    if (property == null && ro_target == memory.voidINSTANCE) {
+		        internalLog.debug(" >> INTERPRETER REPORTS Call of '"+ node.getName() +"' property on a void target. Exception raised.");
+		        raiseCallOnVoidTargetException(node,"");
+		    }
+		    
+//		  This should never happen is the type checker has checked the program
+			if (property == null) {
+			    internalLog.error("INTERPRETER INTERNAL ERROR : unable to find a feature : " + node.getName() + "in type : " + target_type);
+			    internalLog.error("May be the code was not successfully typechecked ? If the typechecker has no error, please contact kermeta developpers");		        
+		        throw new Error("INTERPRETER INTERNAL ERROR : unable to find a feature : " + node.getName());
+			}
+			
+//			 Get the runtime object corresponding to the property
+		    RuntimeObject ro_property = getRuntimeObjectForProperty(t_target, property.getProperty().getName());
+		    
+		    // This is just a debbuging check. It should never occur
+		    if (ro_property == null) {
+			    internalLog.error("INTERPRETER INTERNAL ERROR : Unable to find runtime object corresponding to property " + property.getName());
+			    internalLog.error("May be the code was not successfully typechecked ? If the typechecker has no error, please contact kermeta developpers");
+		        throw new Error("INTERPRETER INTERNAL ERROR : Unable to find runtime object corresponding to property " + property.getName());
+			}
+		    
+		    if(node.isIsAtpre()){
+		    	int index = atpreV.getListCallFeatureAtPre().indexOf(node);
+		    	result = (RuntimeObject)atpreV.getListCallFeatureAtPre().get(index + 1);
+		    }else{
+		    	if (!property.getProperty().isIsDerived())
+		    		
+		    	{
+		    		// Get the value of the property
+		    		result = fr.irisa.triskell.kermeta.runtime.language.Object.get(ro_target, ro_property);
+		    	}
+		    	else
+		    	{
+		    		result = this.getterDerivedProperty(property, ro_target, node);
+		    	}
+		    }
+	    }
+		
+		return result;
+	}
 }
