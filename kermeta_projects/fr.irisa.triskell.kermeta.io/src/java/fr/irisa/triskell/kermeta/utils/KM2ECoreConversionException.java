83ece4c14238c45e9cf1b431305eb63abac314e2
zdrey <zdrey@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2006-06-01 08:32:10 +0000

    basic exception raised when KM2Ecore conversion fails
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@1667 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

---
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/utils/KM2ECoreConversionException.java
@@ -0,0 +1,35 @@
+/* $Id: KM2ECoreConversionException.java,v 1.1 2006-06-01 08:32:10 zdrey Exp $
+ * Project    : fr.irisa.triskell.kermeta.io
+ * File       : KM2ECoreConversionException.java
+ * License    : EPL
+ * Copyright  : IRISA / INRIA / Universite de Rennes 1
+ * -------------------------------------------------------------------
+ * Creation date : May 31, 2006
+ * Authors : 
+ *    zdrey <zdrey@irisa.fr>
+ * Contributors :
+ */
+/**
+ * 
+ */
+package fr.irisa.triskell.kermeta.utils;
+
+/**
+ * @author zdrey
+ *
+ */
+public class KM2ECoreConversionException extends Error {
+
+	public KM2ECoreConversionException() {
+		super();
+	}
+
+	/**
+	 * @param message
+	 */
+	public KM2ECoreConversionException(String message) {
+		super(message);
+	}
+
+
+}
