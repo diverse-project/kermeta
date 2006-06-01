7279a7a6fd6f6abc6ec983ed13517de1d182b9bd
zdrey <zdrey@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2006-06-01 08:51:33 +0000

    added methods to format the prettyprinting of Tags (fixes the pb mentioned by mbellour about "/** */" problems during EMF code generation of km2ecore converted files.
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@1672 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/utils/KMTHelper.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/utils/KMTHelper.java
@@ -1,4 +1,4 @@
-/* $Id: KMTHelper.java,v 1.4 2006-03-03 15:22:19 dvojtise Exp $
+/* $Id: KMTHelper.java,v 1.5 2006-06-01 08:51:33 zdrey Exp $
  * Project    : fr.irisa.triskell.kermeta.io
  * File       : KMTHelper.java
  * License    : EPL
@@ -12,10 +12,13 @@
  */
 package fr.irisa.triskell.kermeta.utils;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import fr.irisa.triskell.kermeta.parser.SimpleKWList;
-//import fr.irisa.triskell.kermeta.language.structure.FClass;
 import fr.irisa.triskell.kermeta.language.structure.NamedElement;
 import fr.irisa.triskell.kermeta.language.structure.Type;
+import fr.irisa.triskell.kermeta.language.structure.impl.ClassImpl;
 
 /**
  * Some helper methods usefull when dealing with kermeta text syntax
@@ -37,13 +40,15 @@ public class KMTHelper {
 	 */
 	public static String getTypeQualifiedName(Type type)
 	{
-		if(type instanceof NamedElement){
+		if(type instanceof NamedElement)
+		{
 			NamedElement fNamedElement = (NamedElement)type;
 
 			return KMTHelper.getQualifiedName(fNamedElement); 	
 		}
-		else if (type instanceof fr.irisa.triskell.kermeta.language.structure.Class){
-			fr.irisa.triskell.kermeta.language.structure.Class fClass = (fr.irisa.triskell.kermeta.language.structure.Class)type;
+		else if (type instanceof fr.irisa.triskell.kermeta.language.structure.Class)
+		{
+			ClassImpl fClass = (ClassImpl)type;
 			return getQualifiedName(fClass.getTypeDefinition());
 		}
 		return "";
@@ -59,4 +64,55 @@ public class KMTHelper {
 			return "~" + id;
 		else return id;
 	}
+	
+	/*
+	 * 
+	 * HELPER METHODS TO FORMAT TAGS PRETTY PRINTING.
+	 * 
+	 */
+	
+	/**
+	 * Helper - join the strings of list l, delimited by delimiter
+	 * @param l the list to join in a single string
+	 * @param delimiter the delimiter
+	 * @return
+	 */
+	public static String join(Collection<String> l, String delimiter)
+	{
+		String result = "";
+		for (String s : l) { result += s + delimiter; }
+		return result.substring(0, result.length()-delimiter.length());
+	}
+	
+	/** Replace protected html chars ( &lt; becomes <code>&lt;</code> ) TODO : uncomplete! */
+	public static String html(String str)
+	{
+		String result = str.replaceAll("<", "&lt;");
+		result = result.replaceAll(">", "&gt;");
+		return result;
+	}
+	
+	/**
+     * Remove the comment <code>\/** *\/</code> syntax 
+     * @param node
+     * @return
+     */
+    public static String formatTagValue(String comment)
+    {
+    	String result = comment;
+    	int begin_i = 0; int end_i = 0;
+		if (result.startsWith("/**")) begin_i = 2;
+		if (result.endsWith("*/")) end_i = result.length()-2;
+		ArrayList<String> lresult = new ArrayList<String>();
+		String[] lines = result.substring(begin_i, end_i).split("\\n");
+		for (int i = 0; i<lines.length; i++)
+		{
+			String nline = lines[i].replaceFirst("\\s*\\*?(.*)", "$1");
+			// nline.matches("\\s*\\*?(.*)"));
+			nline = KMTHelper.html(nline);
+			lresult.add(nline);
+		}
+		return KMTHelper.join(lresult,"\n<br>");
+    }
+	
 }
