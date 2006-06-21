339c94db1b2673607d2ec254fc545e3a74ed39f5
zdrey <zdrey@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2006-06-21 16:40:46 +0000

    createKermetaUnit call with wrong parameter values : fixed
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@1793 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/loader/KermetaUnit.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/loader/KermetaUnit.java
@@ -1,4 +1,4 @@
-/* $Id: KermetaUnit.java,v 1.64 2006-06-21 12:00:51 zdrey Exp $
+/* $Id: KermetaUnit.java,v 1.65 2006-06-21 16:40:46 zdrey Exp $
  * Project : Kermeta (First iteration)
  * File : KermetaUnit.java
  * License : EPL
@@ -605,9 +605,9 @@ public abstract class KermetaUnit {
 		else {
 			KermetaUnit unit;
 			// This is a normal behavior
+			unit = KermetaUnitFactory.getDefaultLoader().createKermetaUnit(str_uri, packages);
 			// zoe comment 20/06/06
-			//unit = KermetaUnitFactory.getDefaultLoader().createKermetaUnit(str_uri, packages);
-			unit = KermetaUnitFactory.getDefaultLoader().createKermetaUnit(str_uri); //, new Hashtable());
+			//unit = KermetaUnitFactory.getDefaultLoader().createKermetaUnit(str_uri); //, new Hashtable());
 			if (messages.unitHasError) {
 				messages.addError("Errors in imported model " + str_uri + " : \n" +  unit.messages.getAllMessagesAsString(), null);
 			}
@@ -886,12 +886,15 @@ public abstract class KermetaUnit {
 	 * @param file_path the xmi file. the extension of the file should be .km
 	 */
 	public void saveAsXMIModel(String file_path) {
+		System.err.println("Save as XMI : " + file_path);
 	    Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("km",new XMIResourceFactoryImpl());
 	    ResourceSet resource_set = new ResourceSetImpl();
 	    ArrayList resource_tags = new ArrayList();
 	    Resource resource = resource_set.createResource(URI.createFileURI(file_path));
+	    System.err.println("Save as XMI : " + packages.size());
 	    Iterator it = packages.values().iterator();
 	    while(it.hasNext()) {
+	    	
 	        Package p = (Package)it.next();    
 	        if (p.eResource() == null && p.eContainer() == null) {
 	            fixTypeContainement(p);
