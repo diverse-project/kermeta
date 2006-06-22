aed6fde03c3869577c99fb0e7bc3baec1cc2d739
Didier Vojtisek <didier.vojtisek@inria.fr>
2006-06-22 07:53:57 +0000

    correctly initialise the console ...
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@1798 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.runner.deprecated/src/fr/irisa/triskell/kermeta/runner/launching/ConstraintRunTarget.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.runner.deprecated/src/fr/irisa/triskell/kermeta/runner/launching/ConstraintRunTarget.java
@@ -1,15 +1,16 @@
+/* $Id: ConstraintRunTarget.java,v 1.4 2006-06-22 07:53:57 dvojtise Exp $
+ * Project: KermetaRunner
+ * File: ConstraintRunTarget.java
+ * License: EPL
+ * Copyright: IRISA / INRIA / Universite de Rennes 1
+ * ----------------------------------------------------------------------------
+ * Creation date: May 18, 2006
+ * Authors: jmottu
+ */
 package fr.irisa.triskell.kermeta.runner.launching;
 
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.ILaunch;
-import org.eclipse.debug.core.model.IThread;
 
-import fr.irisa.triskell.kermeta.error.KermetaError;
-import fr.irisa.triskell.kermeta.runner.debug.model.AbstractKermetaTarget;
 import fr.irisa.triskell.kermeta.runner.debug.process.KermetaRunProcess;
 
 public class ConstraintRunTarget extends KermetaRunTarget {
@@ -20,7 +21,8 @@ public class ConstraintRunTarget extends KermetaRunTarget {
 	 public void start()
 	 { 
 		 initPath();
-		 kermeta_process = new KermetaRunProcess(startFile, className, opName, args, "Kermeta Run Thread", true);
+	     initConsole();
+		 kermeta_process = new KermetaRunProcess(startFile, className, opName, args, "Kermeta Run Thread", true, console);
 		 
 		 
 		 kermeta_process.updateThreadClassLoader( this.javaClassPathAttribute, getCurrentProjectPath());
