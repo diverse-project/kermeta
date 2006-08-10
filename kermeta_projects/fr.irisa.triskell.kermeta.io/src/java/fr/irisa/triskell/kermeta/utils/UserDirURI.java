900b4ff4bc75bdd294ca9352e6079ffcae92ce34
zdrey <zdrey@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2006-08-10 14:21:33 +0000

    the texas chainsaw massacre
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@2051 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/utils/UserDirURI.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/utils/UserDirURI.java
@@ -1,4 +1,4 @@
-/* $Id: UserDirURI.java,v 1.4 2006-07-25 15:58:29 dvojtise Exp $
+/* $Id: UserDirURI.java,v 1.5 2006-08-10 14:21:33 zdrey Exp $
  * Project : Kermeta (First iteration)
  * File : UserDirURI.java
  * License : GPL
@@ -27,25 +27,6 @@ public class UserDirURI
 {
 	public static String userDir=System.getProperty("user.dir")+'/';
 	
-	
-	public static void createDirFromName(String dirname){
-		String filenameWithSlashes=dirname.replaceAll("\\\\","/");
-		if (filenameWithSlashes.startsWith("file:"))
-			filenameWithSlashes=filenameWithSlashes.substring(5,filenameWithSlashes.length());
-		int fileIndex=filenameWithSlashes.lastIndexOf('/');
-		int curSlash=filenameWithSlashes.indexOf('/');
-		boolean reached=false;
-		do {
-			if (curSlash==fileIndex) reached=true;
-			String directory=filenameWithSlashes.substring(0,curSlash);
-			File dir=new File(directory);
-			if (! dir.exists()) {
-				System.out.println("creating directory "+directory+" ...");
-				dir.mkdir();
-			}
-			curSlash=curSlash+1+filenameWithSlashes.substring(curSlash+1).indexOf('/');
-		} while (! reached);
-	}
 	public static File createFileFromFileName(String filename) {
 	
 		String filenameWithSlashes=filename.replaceAll("\\\\","/");
