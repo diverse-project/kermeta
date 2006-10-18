eb69d0a667d5274626f73eda2d5918722170a753
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2006-10-18 15:12:52 +0000

    Changing the class cast in the method getTypeName: Class to ParameterizedType. A ParameterizedType can have a name too and ParameterizedType is a super class of Class
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@2345 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/utils/KMTHelper.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/utils/KMTHelper.java
@@ -1,4 +1,4 @@
-/* $Id: KMTHelper.java,v 1.9 2006-09-06 14:06:51 dtouzet Exp $
+/* $Id: KMTHelper.java,v 1.10 2006-10-18 15:12:52 cfaucher Exp $
  * Project    : fr.irisa.triskell.kermeta.io
  * File       : KMTHelper.java
  * License    : EPL
@@ -19,6 +19,7 @@ import fr.irisa.triskell.kermeta.parser.SimpleKWList;
 import fr.irisa.triskell.kermeta.language.structure.NamedElement;
 import fr.irisa.triskell.kermeta.language.structure.Type;
 import fr.irisa.triskell.kermeta.language.structure.impl.ClassImpl;
+import fr.irisa.triskell.kermeta.language.structure.ParameterizedType;
 
 /**
  * Some helper methods usefull when dealing with kermeta text syntax
@@ -29,123 +30,132 @@ public class KMTHelper {
 	 * Get the fully qualified name of an NamedElement
 	 */
 	public static String getQualifiedName(NamedElement element) {
-		if (element.eContainer() != null && element.eContainer() instanceof NamedElement)
-			return getQualifiedName( (NamedElement)element.eContainer() ) + "::" + getMangledIdentifier(element.getName());
-		else return element.getName();
+		if (element.eContainer() != null
+				&& element.eContainer() instanceof NamedElement)
+			return getQualifiedName((NamedElement) element.eContainer()) + "::"
+					+ getMangledIdentifier(element.getName());
+		else
+			return element.getName();
 	}
-	/**
-	 * Get the fully qualified name of FType 
-	 * as the concrete types usually inherit from NamedElement
-	 * or is a FClass. In the later case, returns the class definition qualified name
-	 */
-	public static String getTypeQualifiedName(Type type)
-	{
-		if(type instanceof NamedElement)
-		{
-			NamedElement fNamedElement = (NamedElement)type;
 
-			return KMTHelper.getQualifiedName(fNamedElement); 	
-		}
-		else if (type instanceof fr.irisa.triskell.kermeta.language.structure.Class)
-		{
-			ClassImpl fClass = (ClassImpl)type;
+	/**
+	 * Get the fully qualified name of FType as the concrete types usually
+	 * inherit from NamedElement or is a FClass. In the later case, returns the
+	 * class definition qualified name
+	 */
+	public static String getTypeQualifiedName(Type type) {
+		if (type instanceof NamedElement) {
+			NamedElement fNamedElement = (NamedElement) type;
+
+			return KMTHelper.getQualifiedName(fNamedElement);
+		} else if (type instanceof fr.irisa.triskell.kermeta.language.structure.Class) {
+			ClassImpl fClass = (ClassImpl) type;
 			return getQualifiedName(fClass.getTypeDefinition());
 		}
 		return "";
 	}
-	/**
-	 * Get thename of FType 
-	 * as the concrete types usually inherit from NamedElement
-	 * or is a FClass. In the later case, returns the class definition name
-	 */
-	public static String getTypeName(Type type)
-	{
-		if(type instanceof NamedElement)
-		{
-			NamedElement fNamedElement = (NamedElement)type;
 
-			return fNamedElement.getName(); 	
-		}
-		else if (type instanceof fr.irisa.triskell.kermeta.language.structure.Class)
-		{
-			ClassImpl fClass = (ClassImpl)type;
+	/**
+	 * Get thename of FType as the concrete types usually inherit from
+	 * NamedElement or is a FClass. In the later case, returns the class
+	 * definition name
+	 */
+	public static String getTypeName(Type type) {
+		if (type instanceof NamedElement) {
+			NamedElement fNamedElement = (NamedElement) type;
+
+			return fNamedElement.getName();
+		} else if (type instanceof ParameterizedType) {
+			ParameterizedType fClass = (ParameterizedType) type;
 			return fClass.getTypeDefinition().getName();
 		}
 		return "";
 	}
+
 	/**
 	 * 
 	 * @param id
-	 * @return the identifier eventually mangled in order to safely fit in the kermeta text syntax
+	 * @return the identifier eventually mangled in order to safely fit in the
+	 *         kermeta text syntax
 	 */
 	public static String getMangledIdentifier(String id) {
-		if (id == null) return id;
+		if (id == null)
+			return id;
 		if (SimpleKWList.getInstance().isKeyword(id))
 			return "~" + id;
-		else return id;
+		else
+			return id;
 	}
-	
+
 	/**
 	 * @param id
 	 * @return the identifier with its eventual escape character (~) removed
 	 */
 	public static String getUnescapedIdentifier(String id) {
-		if (id == null) return id;
-		if(id.startsWith("~"))
+		if (id == null)
+			return id;
+		if (id.startsWith("~"))
 			return id.substring(1);
 		else
 			return id;
 	}
-	
+
 	/*
 	 * 
 	 * HELPER METHODS TO FORMAT TAGS PRETTY PRINTING.
 	 * 
 	 */
-	
+
 	/**
 	 * Helper - join the strings of list l, delimited by delimiter
-	 * @param l the list to join in a single string
-	 * @param delimiter the delimiter
+	 * 
+	 * @param l
+	 *            the list to join in a single string
+	 * @param delimiter
+	 *            the delimiter
 	 * @return
 	 */
-	public static String join(Collection<String> l, String delimiter)
-	{
+	public static String join(Collection<String> l, String delimiter) {
 		String result = "";
-		for (String s : l) { result += s + delimiter; }
-		return result.substring(0, result.length()-delimiter.length());
+		for (String s : l) {
+			result += s + delimiter;
+		}
+		return result.substring(0, result.length() - delimiter.length());
 	}
-	
-	/** Replace protected html chars ( &lt; becomes <code>&lt;</code> ) TODO : uncomplete! */
-	public static String html(String str)
-	{
+
+	/**
+	 * Replace protected html chars ( &lt; becomes <code>&lt;</code> ) TODO :
+	 * uncomplete!
+	 */
+	public static String html(String str) {
 		String result = str.replaceAll("<", "&lt;");
 		result = result.replaceAll(">", "&gt;");
 		return result;
 	}
-	
+
 	/**
-     * Remove the comment <code>\/** *\/</code> syntax 
-     * @param node
-     * @return
-     */
-    public static String formatTagValue(String comment)
-    {
-    	String result = comment;
-    	int begin_i = 0;
-    	int end_i = comment.length();
-		if (result.startsWith("/**")) begin_i = 2;
-		if (result.endsWith("*/")) end_i = result.length()-2;
+	 * Remove the comment <code>\/** *\/</code> syntax
+	 * 
+	 * @param node
+	 * @return
+	 */
+	public static String formatTagValue(String comment) {
+		String result = comment;
+		int begin_i = 0;
+		int end_i = comment.length();
+		if (result.startsWith("/**"))
+			begin_i = 2;
+		if (result.endsWith("*/"))
+			end_i = result.length() - 2;
 		ArrayList<String> lresult = new ArrayList<String>();
 		String[] lines = result.substring(begin_i, end_i).split("\\n");
-		for (int i = 0; i<lines.length; i++)
-		{
+		for (int i = 0; i < lines.length; i++) {
 			String nline = lines[i].replaceFirst("(\\s*)\\*?(.*)", "$1$2");
 			// nline.matches("\\s*\\*?(.*)"));
 			nline = KMTHelper.html(nline);
 			lresult.add(nline);
 		}
-		return KMTHelper.join(lresult,"\n");
-    }
-	
+		return KMTHelper.join(lresult, "\n");
+	}
+
 }
