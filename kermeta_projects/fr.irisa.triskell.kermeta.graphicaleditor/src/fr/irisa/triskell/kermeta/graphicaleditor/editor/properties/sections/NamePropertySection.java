1f2421826fd24567e4939dde07b1f3a7f4664607
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2006-11-02 10:21:49 +0000

    Disable the pretty property view for this version, import troubles: tpc10 => tpc11.
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@2415 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95

--- kermeta_projects/fr.irisa.triskell.kermeta.graphicaleditor/src/fr/irisa/triskell/kermeta/graphicaleditor/editor/properties/sections/NamePropertySection.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.graphicaleditor/src/fr/irisa/triskell/kermeta/graphicaleditor/editor/properties/sections/NamePropertySection.java
@@ -1,27 +1,30 @@
 package fr.irisa.triskell.kermeta.graphicaleditor.editor.properties.sections;
 
 import org.eclipse.emf.ecore.EAttribute;
-import org.topcased.modeler.editor.properties.sections.AbstractStringPropertySection;
-import org.topcased.modeler.editor.properties.sections.AbstractTextPropertySection;
+
+////import org.topcased.modeler.editor.properties.sections.AbstractStringPropertySection;
+
+// To uncomment for the upgrade to topcased 0.11.0 and update the annotations "@see"
+//import org.topcased.tabbedproperties.sections.AbstractStringPropertySection;
 
 import fr.irisa.triskell.kermeta.language.structure.StructurePackage;
 
-public class NamePropertySection extends AbstractStringPropertySection {
+public class NamePropertySection /*extends AbstractStringPropertySection*/ {
 
 	/**
      * @see org.topcased.modeler.editor.properties.sections.AbstractTextPropertySection#getLabelText()
      */
-    protected String getLabelText()
+    /*protected String getLabelText()
     {
         return "Name:";
-    }
+    }*/
 
     /**
      * @see org.topcased.modeler.editor.properties.sections.AbstractTextPropertySection#getFeature()
      */
-    protected EAttribute getFeature()
+    /*protected EAttribute getFeature()
     {
         return StructurePackage.eINSTANCE.getNamedElement_Name();
-    }
+    }*/
     
 }
