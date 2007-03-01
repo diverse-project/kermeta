6610d487a915310ffb7d0e2492468dd46992933d
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2007-03-01 17:06:35 +0000

    Fixing bug #2154.
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@3034 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: f1ee99a3ecd3b139fe5d6c1e72047bb9c70993e1

--- kermeta_projects/fr.irisa.triskell.kermeta.graphicaleditor/src/fr/irisa/triskell/kermeta/graphicaleditor/cd/policies/InheritanceEdgeCreationEditPolicy.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.graphicaleditor/src/fr/irisa/triskell/kermeta/graphicaleditor/cd/policies/InheritanceEdgeCreationEditPolicy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * $Id: InheritanceEdgeCreationEditPolicy.java,v 1.1 2007-02-06 17:45:46 cfaucher Exp $
+ * $Id: InheritanceEdgeCreationEditPolicy.java,v 1.2 2007-03-01 17:06:35 cfaucher Exp $
  * License: EPL
  * Copyright: IRISA / INRIA / Universite de Rennes 1
  ******************************************************************************/
@@ -20,17 +20,20 @@ import fr.irisa.triskell.kermeta.graphicaleditor.cd.KmSimpleObjectConstants;
 import fr.irisa.triskell.kermeta.graphicaleditor.cd.commands.InheritanceEdgeCreationCommand;
 import fr.irisa.triskell.kermeta.language.structure.ClassDefinition;
 import fr.irisa.triskell.kermeta.language.structure.StructurePackage;
+import fr.irisa.triskell.kermeta.modelhelper.ClassDefinitionHelper;
 import fr.irisa.triskell.kermeta.util.KermetaCommonUtils;
 
 /**
  * Inheritance edge creation
- *
+ * 
  * @generated
  */
 public class InheritanceEdgeCreationEditPolicy extends
 		AbstractEdgeCreationEditPolicy {
 	/**
-	 * @see org.topcased.modeler.edit.policies.AbstractEdgeCreationEditPolicy#createCommand(org.eclipse.gef.EditDomain, org.topcased.modeler.di.model.GraphEdge, org.topcased.modeler.di.model.GraphElement)
+	 * @see org.topcased.modeler.edit.policies.AbstractEdgeCreationEditPolicy#createCommand(org.eclipse.gef.EditDomain,
+	 *      org.topcased.modeler.di.model.GraphEdge,
+	 *      org.topcased.modeler.di.model.GraphElement)
 	 * @generated
 	 */
 	protected CreateTypedEdgeCommand createCommand(EditDomain domain,
@@ -64,7 +67,8 @@ public class InheritanceEdgeCreationEditPolicy extends
 	}
 
 	/**
-	 * @see org.topcased.modeler.edit.policies.AbstractEdgeCreationEditPolicy#checkTargetForSource(org.topcased.modeler.di.model.GraphElement, org.topcased.modeler.di.model.GraphElement)
+	 * @see org.topcased.modeler.edit.policies.AbstractEdgeCreationEditPolicy#checkTargetForSource(org.topcased.modeler.di.model.GraphElement,
+	 *      org.topcased.modeler.di.model.GraphElement)
 	 * @generated NOT
 	 */
 	protected boolean checkTargetForSource(GraphElement source,
@@ -76,14 +80,13 @@ public class InheritanceEdgeCreationEditPolicy extends
 				&& targetObject instanceof ClassDefinition) {
 
 			// begin-addition
-			if (!checkSuperTypeConstraints(sourceObject, targetObject)) {
-				return false;
-			}
-			// end-addition
-
-			if (!sourceObject.equals(targetObject)) {
+			// Prevent a cyclic inheritance
+			if (!sourceObject.equals(targetObject)
+					&& checkSuperTypeUnicity(sourceObject, targetObject)
+					&& !ClassDefinitionHelper.isSuperClassOf((ClassDefinition) sourceObject, (ClassDefinition) targetObject)) {
 				return true;
 			}
+			// end-addition
 		}
 		return false;
 	}
@@ -97,7 +100,8 @@ public class InheritanceEdgeCreationEditPolicy extends
 	}
 
 	/**
-	 * @see org.topcased.modeler.edit.policies.AbstractEdgeCreationEditPolicy#getSourceTargetData(org.topcased.modeler.di.model.GraphElement, org.topcased.modeler.di.model.GraphElement)
+	 * @see org.topcased.modeler.edit.policies.AbstractEdgeCreationEditPolicy#getSourceTargetData(org.topcased.modeler.di.model.GraphElement,
+	 *      org.topcased.modeler.di.model.GraphElement)
 	 * @generated
 	 */
 	protected SourceTargetData getSourceTargetData(GraphElement source,
@@ -105,8 +109,8 @@ public class InheritanceEdgeCreationEditPolicy extends
 		EObject sourceObject = Utils.getElement(source);
 		EObject targetObject = Utils.getElement(target);
 
-		if (sourceObject instanceof fr.irisa.triskell.kermeta.language.structure.ClassDefinition
-				&& targetObject instanceof fr.irisa.triskell.kermeta.language.structure.ClassDefinition) {
+		if (sourceObject instanceof ClassDefinition
+				&& targetObject instanceof ClassDefinition) {
 			return new SourceTargetData(
 					false,
 					false,
@@ -118,29 +122,35 @@ public class InheritanceEdgeCreationEditPolicy extends
 	}
 
 	/**
-	 * Constraint : Inheritance relation between two classes must be Unique :
-	 * a class can only inherit once from another.
-	 * This constraint is defined in "fSuperType" property, as "isUnique=true" on it
-	 * (in textual checking : see KMT2KMPass3 in fr.irisa.triskell.kermeta.io project.)
+	 * Constraint : Inheritance relation between two classes must be Unique : a
+	 * class can only inherit once from another. This constraint is defined in
+	 * "SuperType" property, as "isUnique=true" on it (in textual checking : see
+	 * KMT2KMPass3 in fr.irisa.triskell.kermeta.io project.)
 	 * 
-	 * TODO This constraint should be generalizable to other properties. But yet it is very
-	 * specific to SuperType
-	 * @param sourceObject the inheriting classdefinition
-	 * @param targetObject the inherited classdefinition
-	 * @return true if targetObject is not yet a supertype of sourceObject, false otherwise. In particular, a "attribute"(reference,
-	 * or property...) must be uniquely represented in the ownedAttributes properties of a class.
+	 * TODO This constraint should be generalizable to other properties. But yet
+	 * it is very specific to SuperType
+	 * 
+	 * @param sourceObject
+	 *            the inheriting classdefinition
+	 * @param targetObject
+	 *            the inherited classdefinition
+	 * @return true if targetObject is not yet a supertype of sourceObject,
+	 *         false otherwise. In particular, a "attribute"(reference, or
+	 *         property...) must be uniquely represented in the ownedAttributes
+	 *         properties of a class.
 	 * @generated NOT
 	 */
-	protected boolean checkSuperTypeConstraints(EObject sourceObject,
+	protected boolean checkSuperTypeUnicity(EObject sourceObject,
 			EObject targetObject) {
 		Boolean isvalid = true;
 		// we know here that we handle ClassDefinitions, so hard cast
 		ClassDefinition sourceClassDef = (ClassDefinition) sourceObject;
 		ClassDefinition targetClassDef = (ClassDefinition) targetObject;
-		// is the inheritance relation unique? 
+		// is the inheritance relation unique?
 		if (StructurePackage.eINSTANCE.getClassDefinition_SuperType()
 				.isUnique()) {
-			// check if an inheritance relation always exists between the 2 given classes
+			// check if an inheritance relation always exists between the 2
+			// given classes
 			if (KermetaCommonUtils.isSuperType(sourceClassDef, targetClassDef))
 				isvalid = false;
 		}
