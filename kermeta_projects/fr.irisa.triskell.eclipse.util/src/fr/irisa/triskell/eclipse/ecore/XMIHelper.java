d7c7be25fda38df9a9aed2f15f742d28969c1503
ftanguy <ftanguy@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2007-04-04 13:26:43 +0000

    A basic helper to load and save properly.
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@3109 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: f442fd878fa752a765ae9211964b3443ae13cb5c

---
+++ kermeta_projects/fr.irisa.triskell.eclipse.util/src/fr/irisa/triskell/eclipse/ecore/XMIHelper.java
@@ -0,0 +1,97 @@
+package fr.irisa.triskell.eclipse.ecore;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+
+
+public class XMIHelper {
+
+	//////////////////////
+	//////////////////////
+	//		Saving		//
+	//////////////////////
+	//////////////////////
+	/**
+	 * 
+	 * @param fileName
+	 * @param filePath
+	 * @param root
+	 * @throws java.io.IOException
+	 */
+	static public void save(String fileName, String filePath, EObject root) throws java.io.IOException {
+		save(filePath + "/" + fileName, root);
+	}
+	
+	/**
+	 * 
+	 * @param absoluteFileName
+	 * @param root
+	 * @throws java.io.IOException
+	 */
+	static public void save(String absoluteFileName, EObject root) throws java.io.IOException {
+		save(URI.createFileURI(absoluteFileName), root);
+	}
+	
+	/**
+	 * 
+	 * @param fileURI
+	 * @param root
+	 * @throws java.io.IOException
+	 */
+	static public void save(URI fileURI, EObject root) throws java.io.IOException {
+		Resource res = new XMIResourceImpl(fileURI);
+		res.getContents().add(root);
+		res.save(null);		
+	}
+	//////////////////////////////
+	//////////////////////////////
+	//		End of Saving		//
+	//////////////////////////////
+	//////////////////////////////
+	
+	//////////////////////
+	//////////////////////
+	//		Loading		//
+	//////////////////////
+	//////////////////////
+	/**
+	 * 
+	 * @param fileName
+	 * @param filePath
+	 * @param root
+	 * @throws java.io.IOException
+	 */
+	static public Object load(String fileName, String filePath) throws java.io.IOException {
+		return load(filePath + "/" + fileName);
+	}
+	
+	/**
+	 * 
+	 * @param absoluteFileName
+	 * @return
+	 * @throws java.io.IOException
+	 */
+	static public Object load(String absoluteFileName) throws java.io.IOException {
+		return load(URI.createFileURI(absoluteFileName));
+	}
+	
+	/**
+	 * 
+	 * @param fileURI
+	 * @return
+	 * @throws java.io.IOException
+	 */
+	static public Object load(URI fileURI) throws java.io.IOException {
+		Resource resource = new XMIResourceImpl(fileURI);
+		resource.load(null);
+		return resource.getContents().get(0);
+	}
+	//////////////////////////////
+	//////////////////////////////
+	//		End of Loading		//
+	//////////////////////////////
+	//////////////////////////////
+	
+}
