04470f60135380b09a08903cccdbd014cba2a91b
Didier Vojtisek <didier.vojtisek@inria.fr>
2007-05-29 16:15:29 +0000

    added helper method for dealing with registered packages
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@3380 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 0ac03b0625dc10ac7112fd92ca77cc62502d8313

--- kermeta_projects/fr.irisa.triskell.eclipse.util/src/fr/irisa/triskell/eclipse/emf/EMFRegistryHelper.java
+++ kermeta_projects/fr.irisa.triskell.eclipse.util/src/fr/irisa/triskell/eclipse/emf/EMFRegistryHelper.java
@@ -1,4 +1,4 @@
-/* $Id: EMFRegistryHelper.java,v 1.1 2007-02-28 17:27:19 dvojtise Exp $
+/* $Id: EMFRegistryHelper.java,v 1.2 2007-05-29 16:15:29 dvojtise Exp $
  * Project   : Kermeta 
  * File      : EMFRegistryHelper.java
  * License   : EPL
@@ -11,12 +11,17 @@ package fr.irisa.triskell.eclipse.emf;
 
 import java.util.Iterator;
 
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EPackage.Registry;
+import org.eclipse.emf.ecore.impl.EPackageImpl;
 import org.eclipse.emf.common.util.URI;
 
 public class EMFRegistryHelper {
 
 	public static boolean isRegistered(URI uri){
+		return isRegistered(uri.toString());
+	}
+	public static boolean isRegistered(String uri){
 		Iterator it = Registry.INSTANCE.keySet().iterator();
 		while(it.hasNext()){
 			String regentry = (String)it.next();
@@ -24,5 +29,14 @@ public class EMFRegistryHelper {
 					return true;
 		}
 		return false;
+	}
+	
+	public static boolean isDynamicallyRegistered(String uri){
+		EPackage pack = (EPackage)Registry.INSTANCE.get(uri);
+		if(pack == null) return false;
+		if(pack.getClass() == EPackageImpl.class){ // if this is exactly an EPackage then this comes from a files, otherwise it comes from generated java
+			return true;
+		}
+		else return false;
 	}
 }
