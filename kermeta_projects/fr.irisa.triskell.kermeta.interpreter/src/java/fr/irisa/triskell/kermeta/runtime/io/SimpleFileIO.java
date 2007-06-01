638d328331e5ee35f62674fad68e628e606c2f92
jmottu <jmottu@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2007-06-01 16:06:57 +0000

    New kermeta method 	getAbsolutePathFolder(String):String it allows to get the complete path of a folder from a kermeta program.
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@3439 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: bb2d6ca510189e727c74ccbe54fbddb19df26590

--- kermeta_projects/fr.irisa.triskell.kermeta.interpreter/src/java/fr/irisa/triskell/kermeta/runtime/io/SimpleFileIO.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.interpreter/src/java/fr/irisa/triskell/kermeta/runtime/io/SimpleFileIO.java
@@ -1,8 +1,15 @@
 package fr.irisa.triskell.kermeta.runtime.io;
 
+import fr.irisa.triskell.eclipse.resources.ResourceHelper;
 import fr.irisa.triskell.kermeta.runtime.RuntimeObject;
 import fr.irisa.triskell.kermeta.runtime.basetypes.String;
 import java.io.*;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+
+
+
 
 public class SimpleFileIO {
 	
@@ -55,5 +62,33 @@ public class SimpleFileIO {
 		}
         RuntimeObject result = String.create(builder.toString(), filename.getFactory());
         return result;
+    }
+    
+    /**
+     * 
+     * @param foldername
+     * @return path of the current folder of the file calling this operation
+     */
+    public static RuntimeObject getAbsolutePathFolder(RuntimeObject foldername)
+    {   	
+    	IFile fileKermeta = ResourceHelper.getIFile(foldername.getFactory().getMemory().getUnit().getUri());
+    	RuntimeObject result = null;
+
+    	if (fileKermeta.exists()){
+    		java.lang.String pathFile = fileKermeta.getLocation().toString();
+        	java.lang.String pathFolder = pathFile.substring(0,pathFile.lastIndexOf("/")+1) + String.getValue(foldername);
+   		 	
+   		 	
+   		 	File folder = new File(pathFolder);
+   		 	if (folder.exists()){
+   		 		result = String.create(pathFolder, foldername.getFactory());
+   		 	}else{
+   		 		result = String.create("Cannot find path.", foldername.getFactory());
+   		 	}
+    	}else{
+    		result = String.create("Cannot find path.", foldername.getFactory());
+    	}
+    	
+		return result;
     }
 }
