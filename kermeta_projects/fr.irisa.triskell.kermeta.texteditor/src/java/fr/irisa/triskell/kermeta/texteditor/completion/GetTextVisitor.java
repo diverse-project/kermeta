514db4d5add7d72779cdccfdb717ad8f75b4d878
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2007-07-10 12:44:46 +0000

    solve ClassCastException between Class and PrimitiveType when we want to get a name
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@3632 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 163cc09af3f1f1c2b46239fb459188e02b180ce5

--- kermeta_projects/fr.irisa.triskell.kermeta.texteditor/src/java/fr/irisa/triskell/kermeta/texteditor/completion/GetTextVisitor.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.texteditor/src/java/fr/irisa/triskell/kermeta/texteditor/completion/GetTextVisitor.java
@@ -1,12 +1,12 @@
-/* $Id: GetTextVisitor.java,v 1.10 2007-06-27 14:43:05 cfaucher Exp $
-* Project : fr.irisa.triskell.kermeta.texteditor
-* File : GetTextVisitor.java
-* License : EPL
-* Copyright : IRISA / Universite de Rennes 1
-* ----------------------------------------------------------------------------
-* Creation date : 13 feb. 2005
-* Author : Franck Fleurey (ffleurey@irisa.fr)
-*/
+/* $Id: GetTextVisitor.java,v 1.11 2007-07-10 12:44:46 cfaucher Exp $
+ * Project : fr.irisa.triskell.kermeta.texteditor
+ * File : GetTextVisitor.java
+ * License : EPL
+ * Copyright : IRISA / Universite de Rennes 1
+ * ----------------------------------------------------------------------------
+ * Creation date : 13 feb. 2005
+ * Author : Franck Fleurey (ffleurey@irisa.fr)
+ */
 
 package fr.irisa.triskell.kermeta.texteditor.completion;
 
@@ -15,7 +15,7 @@ import java.util.Iterator;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 
-//import fr.irisa.triskell.kermeta.language.structure.FClass;
+// import fr.irisa.triskell.kermeta.language.structure.FClass;
 import fr.irisa.triskell.kermeta.language.structure.ClassDefinition;
 import fr.irisa.triskell.kermeta.language.structure.Constraint;
 import fr.irisa.triskell.kermeta.language.structure.Enumeration;
@@ -29,29 +29,32 @@ import fr.irisa.triskell.kermeta.language.structure.Parameter;
 import fr.irisa.triskell.kermeta.language.structure.PrimitiveType;
 import fr.irisa.triskell.kermeta.language.structure.ProductType;
 import fr.irisa.triskell.kermeta.language.structure.Property;
+import fr.irisa.triskell.kermeta.language.structure.Type;
 import fr.irisa.triskell.kermeta.language.structure.TypeVariable;
 import fr.irisa.triskell.kermeta.language.structure.TypeVariableBinding;
 import fr.irisa.triskell.kermeta.language.structure.VoidType;
+import fr.irisa.triskell.kermeta.modelhelper.TypeHelper;
 import fr.irisa.triskell.kermeta.visitor.KermetaOptimizedVisitor;
 
 /**
  * @author Franck Fleurey
  */
 public class GetTextVisitor extends KermetaOptimizedVisitor {
-	
-    //protected KermetaOutline outline;
-	
-	public GetTextVisitor(/*KermetaOutline outline*/) {
-	    //this.outline = outline;
-		//pp = new KM2KMTPrettyPrinter();
+
+	// protected KermetaOutline outline;
+
+	public GetTextVisitor(/* KermetaOutline outline */) {
+		// this.outline = outline;
+		// pp = new KM2KMTPrettyPrinter();
 	}
-	
+
 	/**
 	 * @see metacore.visitor.MetacoreVisitor#genericVisitChildren(org.eclipse.emf.ecore.EObject)
 	 */
 	public Object genericVisitChildren(EObject arg0) {
 		return null;
 	}
+
 	/**
 	 * @see metacore.visitor.MetacoreVisitor#visit(metacore.structure.ClassDefinition)
 	 */
@@ -63,49 +66,53 @@ public class GetTextVisitor extends KermetaOptimizedVisitor {
 			result += ">";
 		}
 		/*
-		if (node.getFSuperType().size() > 0) {
-			result += " -> ";
-			result += ppComaSeparatedNodes(node.getFSuperType());
-		}
-		*/
+		 * if (node.getFSuperType().size() > 0) { result += " -> "; result +=
+		 * ppComaSeparatedNodes(node.getFSuperType()); }
+		 */
 		return result;
 	}
-	
+
 	public String ppTypeVariableDeclaration(EList tparams) {
 		String result = "";
 		Iterator it = tparams.iterator();
 		while (it.hasNext()) {
-			TypeVariable node = (TypeVariable)it.next();
+			TypeVariable node = (TypeVariable) it.next();
 			result += node.getName();
-			if (node.getSupertype() != null) result += " : " + ((fr.irisa.triskell.kermeta.language.structure.Class)node.getSupertype()).getTypeDefinition().getName();
-			if (it.hasNext()) result +=  ", ";
+			if (node.getSupertype() != null) {
+				result += " : "	+ TypeHelper.getName((Type) node);
+			}
+			if (it.hasNext())
+				result += ", ";
 		}
 		return result;
 	}
-	
+
 	public Object visitTypeVariable(TypeVariable arg0) {
 		return arg0.getName();
 	}
+
 	/**
 	 * @see metacore.visitor.MetacoreVisitor#visit(metacore.structure.Constraint)
 	 */
 	public Object visitConstraint(Constraint node) {
-		String result = node.getName() + " : " + node.getStereotype().getName();		
+		String result = node.getName() + " : " + node.getStereotype().getName();
 		return result;
 	}
+
 	/**
 	 * @see metacore.visitor.MetacoreVisitor#visit(metacore.structure.Enumeration)
 	 */
 	public Object visitEnumeration(Enumeration arg0) {
 		return arg0.getName();
 	}
+
 	/**
 	 * @see metacore.visitor.MetacoreVisitor#visit(metacore.structure.EnumerationLiteral)
 	 */
 	public Object visitEnumerationLiteral(EnumerationLiteral arg0) {
 		return arg0.getName();
 	}
-	
+
 	/**
 	 * @see metacore.visitor.MetacoreVisitor#visit(metacore.structure.FOperation)
 	 */
@@ -119,31 +126,35 @@ public class GetTextVisitor extends KermetaOptimizedVisitor {
 		result += "(";
 		result += ppComaSeparatedNodes(node.getOwnedParameter());
 		result += ")";
-		if(node.getType() != null) {
+		if (node.getType() != null) {
 			result += " : " + ppTypeFromMultiplicityElement(node);
 		}
 		return result;
 	}
+
 	/**
 	 * @see metacore.visitor.MetacoreVisitor#visit(metacore.structure.Package)
 	 */
 	public Object visitPackage(Package arg0) {
 		return getQualifiedName(arg0);
 	}
+
 	/**
 	 * @see metacore.visitor.MetacoreVisitor#visit(metacore.structure.PrimitiveType)
 	 */
 	public Object visitPrimitiveType(PrimitiveType arg0) {
 		return arg0.getName();
 	}
+
 	/**
 	 * @see metacore.visitor.MetacoreVisitor#visit(metacore.structure.Property)
 	 */
 	public Object visitProperty(Property node) {
-		String result = node.getName() + " : " + ppTypeFromMultiplicityElement(node);
+		String result = node.getName() + " : "
+				+ ppTypeFromMultiplicityElement(node);
 		return result;
 	}
-	
+
 	/**
 	 * @see kermeta.visitor.MetacoreVisitor#visit(metacore.structure.ProductType)
 	 */
@@ -151,83 +162,91 @@ public class GetTextVisitor extends KermetaOptimizedVisitor {
 		String result = "[" + ppComaSeparatedNodes(node.getType()) + "]";
 		return result;
 	}
-	
+
 	/**
 	 * @see kermeta.visitor.MetacoreVisitor#visit(metacore.structure.FunctionType)
 	 */
 	public Object visitFunctionType(FunctionType node) {
-		return "< " + this.accept(node.getLeft()) + "->" + this.accept(node.getRight()) + " >";
+		return "< " + this.accept(node.getLeft()) + "->"
+				+ this.accept(node.getRight()) + " >";
 	}
-	
-	
+
 	/**
 	 * @see kermeta.visitor.MetacoreVisitor#visit(metacore.structure.VoidType)
 	 */
 	public Object visitVoidType(VoidType node) {
 		return "Void";
 	}
-	
+
 	/**
 	 * @see kermeta.visitor.MetacoreVisitor#visit(metacore.structure.FClass)
 	 */
-	public Object visitClass(fr.irisa.triskell.kermeta.language.structure.Class node) {
+	public Object visitClass(
+			fr.irisa.triskell.kermeta.language.structure.Class node) {
 		String result = node.getTypeDefinition().getName();
 		if (node.getTypeParamBinding().size() > 0) {
-			result += "<" + ppComaSeparatedNodes(node.getTypeParamBinding()) + ">";
+			result += "<" + ppComaSeparatedNodes(node.getTypeParamBinding())
+					+ ">";
 		}
 		return result;
 	}
-	
+
 	/**
 	 * @see kermeta.visitor.MetacoreVisitor#visit(metacore.structure.TypeVariableBinding)
 	 */
 	public Object visitTypeVariableBinding(TypeVariableBinding node) {
-	    if (node.getType() == null) return "!NULL!";
+		if (node.getType() == null)
+			return "!NULL!";
 		return this.accept(node.getType());
 	}
-	
+
 	/**
 	 * @see kermeta.visitor.MetacoreVisitor#visit(metacore.structure.Parameter)
 	 */
 	public Object visitParameter(Parameter node) {
 		return node.getName() + " : " + ppTypeFromMultiplicityElement(node);
 	}
-	
+
 	public String getQualifiedName(NamedElement element) {
-		if (element.eContainer() != null && element.eContainer() instanceof NamedElement)
-			return getQualifiedName( (NamedElement)element.eContainer() ) + "::" + element.getName();
-		else return element.getName();
+		if (element.eContainer() != null
+				&& element.eContainer() instanceof NamedElement)
+			return getQualifiedName((NamedElement) element.eContainer()) + "::"
+					+ element.getName();
+		else
+			return element.getName();
 	}
-	
+
 	public String ppComaSeparatedNodes(EList expressions) {
 		String result = "";
 		Iterator it = expressions.iterator();
-		while(it.hasNext()) {
-			EObject o = (EObject)it.next();
-			 result += this.accept(o);
-			if (it.hasNext()) result +=  ", ";
+		while (it.hasNext()) {
+			EObject o = (EObject) it.next();
+			result += this.accept(o);
+			if (it.hasNext())
+				result += ", ";
 		}
 		return result;
 	}
-	
+
 	public String ppTypeFromMultiplicityElement(MultiplicityElement elem) {
 		String result = "";
 		if (elem.getUpper() != 1) {
 			if (elem.isIsOrdered()) {
-				if (!elem.isIsUnique()) result +="seq ";
-			}
-			else {
-				if (elem.isIsUnique()) result +="set ";
-				else result +="bag ";
+				if (!elem.isIsUnique())
+					result += "seq ";
+			} else {
+				if (elem.isIsUnique())
+					result += "set ";
+				else
+					result += "bag ";
 			}
 		}
 		result += this.accept(elem.getType());
-			if (elem.getLower() != 0 || elem.getUpper() != 1) {
+		if (elem.getLower() != 0 || elem.getUpper() != 1) {
 			result += "[" + elem.getLower() + "..";
-			result += (elem.getUpper()<0)?"*":""+elem.getUpper();
+			result += (elem.getUpper() < 0) ? "*" : "" + elem.getUpper();
 			result += "]";
 		}
 		return result;
 	}
 }
-
