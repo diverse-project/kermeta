c62751a070c867d19733cf2f32cbc5e8c09c27e5
ftanguy <ftanguy@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2007-07-23 13:58:57 +0000

    Make the api for typechecking more simple.
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@3761 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 89938d4112c4dac40787a423cdede9295d3d5260

--- kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/org/kermeta/io/util2/KermetaUnitHelper.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/org/kermeta/io/util2/KermetaUnitHelper.java
@@ -92,7 +92,7 @@ public class KermetaUnitHelper {
 	 * @param content
 	 * @return
 	 */
-	private KermetaUnit internTypecheckKMTFile(String absoluteFileName, String content, IProgressMonitor monitor) {
+	private KermetaUnit internTypecheckFile(String absoluteFileName, String content, IProgressMonitor monitor) {
 		
 		//if ( Runtime.getRuntime().freeMemory() < 1000000 ) {
 		//	KermetaUnit.internalLog.info("Unloading all Kermeta Unit but the framework.km.");
@@ -162,69 +162,38 @@ public class KermetaUnitHelper {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		// If already parsed, do nothing
-	/*	if ( unit.isType_checked() ) {
-			System.out.println("***************************** No need to typecheck " + absoluteFileName + "*********************");	
-			return unit;
-		}
-
-		System.out.println("***************************** Typechecking " + absoluteFileName + "*********************");
-		
-		if ( unit != null ) {
-			// parsing
-			if ( content != null )	
-				unit.parseString(content);
-			//else if ( KermetaWorkspace.getInstance().getContent(unit.getUri()) != null )
-				//unit.parseString( KermetaWorkspace.getInstance().getContent(unit.getUri()) );
-			else
-				unit.parse();
-			
-			if ( monitor.isCanceled() )
-				return null;
-				
-			// loading
-		    if ( ! unit.messages.hasError() ) {
-		    	// No parsing errors, let us try to load.
-		    	unit.load();
-				if ( monitor.isCanceled() )
-					return null;
-		    }
-		    // typechecking
-		    if ( ! unit.messages.hasError() ) {
-		    	// No loading errors, let us try to type check.
-		    	unit.typeCheck( null );
-				if ( monitor.isCanceled() )
-					return null;
-		    }
-		}*/
 
 		System.out.println("***************************** End of typechecking " + absoluteFileName + "*********************");	
 		
 		//garbageCollect();
 		return unit;
 	}
-	
-	static public KermetaUnit typecheckKMTFile(IFile file, String content) {
-		return typecheckKMTFile(file, content, null);
+
+	static public KermetaUnit typecheckFile(IFile file) {
+		return typecheckFile(file, null, null);
 	}
 	
-	static public KermetaUnit typecheckKMTFile(IFile file, String content, IProgressMonitor monitor) {
+	static public KermetaUnit typecheckFile(IFile file, String content) {
+		return typecheckFile(file, content, null);
+	}
+	
+	static public KermetaUnit typecheckFile(IFile file, String content, IProgressMonitor monitor) {
 		if ( file.getLocation() == null )
 			return null;
 		if ( monitor == null )
 			monitor = new NullProgressMonitor();
-		return typecheckKMTFile("platform:/resource" + file.getFullPath().toString(), content, monitor);
+		return typecheckFile("platform:/resource" + file.getFullPath().toString(), content, monitor);
 	}
 	
-	static public KermetaUnit typecheckKMTFile(String absoluteFileName, String content) {
-		return instance.internTypecheckKMTFile(absoluteFileName, content, null);
+	static public KermetaUnit typecheckFile(String absoluteFileName, String content) {
+		return instance.internTypecheckFile(absoluteFileName, content, null);
 		//return __typecheckKMTFile(absoluteFileName, content);
 	}
 
-	static public KermetaUnit typecheckKMTFile(String absoluteFileName, String content, IProgressMonitor monitor) {
+	static public KermetaUnit typecheckFile(String absoluteFileName, String content, IProgressMonitor monitor) {
 		if ( monitor == null )
 			monitor = new NullProgressMonitor();
-		return instance.internTypecheckKMTFile(absoluteFileName, content, monitor);
+		return instance.internTypecheckFile(absoluteFileName, content, monitor);
 		//return __typecheckKMTFile(absoluteFileName, content);
 	}
 	
@@ -243,136 +212,7 @@ public class KermetaUnitHelper {
 	//////////////////////////////////////////////
 	//////////////////////////////////////////////
 	
-	
-	
-	
-	
-	
-	
-	
-	//////////////////////////////////
-	//////////////////////////////////
-	//		Loading Mechanism		//
-	//////////////////////////////////
-	//////////////////////////////////////////////////////////////////////////////////
-	//																				//
-	//	The loading mechanism works the same way as the typechecking mechanism.		//
-	//	Use case : when opening a kmt file in a editor, the file has already been	//
-	//	typechecked before, so we do not need to typecheck it again. Just a load.	//
-	//																				//
-	//////////////////////////////////////////////////////////////////////////////////
 
-/*	private class LoaderThread extends KermetaUnitThread {
-		
-		public LoaderThread(String absoluteFileName, String content) {
-			this.absoluteFileName = absoluteFileName;
-			this.content = content;
-		}
-		
-		public void run() {
-			unit = __loadKMTFile(absoluteFileName, content);		
-		}
-	}*/
-
-	/**
-	 * A table to be able to check if a typechecking process is running.
-	 */
-//	private Hashtable <String, LoaderThread> loadingThreads = new Hashtable <String, LoaderThread> ();
-	
-	/**
-	 * That is the method called by the thread.
-	 * @param absoluteFileName
-	 * @param content
-	 * @return
-	 */
-	/*static private KMTUnit __loadKMTFile (String absoluteFileName, String content ) {
-		unloadKermetaUnit( null );
-		
-		URI fileURI = URI.createFileURI(absoluteFileName);
-		KMTUnit unit = (KMTUnit) KermetaUnitFactory.getDefaultLoader().createKermetaUnit(fileURI.toString());
-		// If already parsed, do nothing
-		if ( unit.isType_checked() )
-			return unit;
-
-		if ( unit != null ) {
-			// parsing
-			if ( content != null )	
-				unit.parseString(content);
-			//else if ( KermetaWorkspace.getInstance().getContent(unit.getUri()) != null )
-				//unit.parseString( KermetaWorkspace.getInstance().getContent(unit.getUri()) );
-			else
-				unit.parse();
-			// loading
-		    if ( ! unit.messages.hasError() ) {
-		    	// No parsing errors, let us try to load.
-		    	unit.load();
-		    }
-
-		}
-		
-		garbageCollect();
-		
-		return unit;
-	}	*/
-	
-	/**
-	 * 
-	 * @param absoluteFileName
-	 * @param content
-	 * @return
-	 */
-	/*private KMTUnit internLoadKMTFile(String absoluteFileName, String content) {
-		KMTUnit result = null;
-		try {
-			LoaderThread thread;
-			synchronized (loadingThreads) {
-				thread = loadingThreads.get(absoluteFileName);
-				if ( thread == null ) {
-					thread = new LoaderThread(absoluteFileName, content);
-					loadingThreads.put(absoluteFileName, thread);
-				}
-			}
-			synchronized (thread) {
-				if ( ! thread.isAlive() ) 
-					thread.start();
-			}
-			try {
-				while ( thread.isAlive() ) {
-					thread.join();
-					result = thread.unit;
-				}
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} finally {
-			synchronized(loadingThreads) {
-				loadingThreads.remove(absoluteFileName);
-			}
-		}
-		return result;
-	}
-	
-	static private KMTUnit loadKMTFile(IFile file, String content) {
-		return loadKMTFile(file.getLocation().toString(), content);
-	}
-	
-	static private KMTUnit loadKMTFile(String absoluteFileName, String content) {
-		return instance.internLoadKMTFile(absoluteFileName, content);
-	}*/
-	//////////////////////////////////////////
-	//////////////////////////////////////////
-	//		End of Loading Mechanism		//
-	//////////////////////////////////////////
-	//////////////////////////////////////////
-	
-	
-	
-	
-	
-	
-	
-	
 	//////////////////////////////////////
 	//////////////////////////////////////
 	//		User friendly methods		//
@@ -386,12 +226,8 @@ public class KermetaUnitHelper {
 	 * @param kpmFile
 	 * @return The method returns the Kermeta unit used.
 	 */
-	static public KermetaUnit typecheckFile(IFile file) {
+/*	static public KermetaUnit typecheckFile(IFile file) {
 		return typecheckFile(file, null);
-	}
-	
-	/*static public KermetaUnit loadFile(IFile file) {
-		return loadFile(file, null);
 	}*/
 	
 	/**
@@ -402,7 +238,7 @@ public class KermetaUnitHelper {
 	 * @param content If specified, this content will be used for parsing otherwise content will be read from the file. Typechecking process is faster when content is providing (for instance the content of an editor's page).
 	 * @return The method returns the Kermeta unit used.
 	 */
-	static public KermetaUnit typecheckFile(IFile file, String content) {
+	/*static public KermetaUnit typecheckFile(IFile file, String content) {
 
 		KermetaUnit unit = null;
 		
@@ -416,9 +252,9 @@ public class KermetaUnitHelper {
 			//unit.load();
 		}
 		return unit;
-	}
+	}*/
 	
-	static public KermetaUnit typecheckFile(IFile file, String content, IProgressMonitor monitor) {
+	/*static public KermetaUnit typecheckFile(IFile file, String content, IProgressMonitor monitor) {
 
 		KermetaUnit unit = null;
 		
@@ -427,21 +263,22 @@ public class KermetaUnitHelper {
 		if ( extension.equals("kmt") ) {
 			unit = typecheckKMTFile(file, content, monitor);
 		} else if ( extension.equals("km") ) {
-			/*URI fileURI = URI.createFileURI(file.getLocation().toString());
+			unit = typecheckKMTFile(file, content, monitor);
+			URI fileURI = URI.createFileURI(file.getLocation().toString());
 			unit = (KermetaUnit) KermetaUnitFactory.getDefaultLoader().createKermetaUnit(fileURI.toString());
-			unit.load();*/
+			unit.load();
 		} else if (extension.equals("ecore") ) {
-			/*URI fileURI = URI.createFileURI(file.getLocation().toString());
+			URI fileURI = URI.createFileURI(file.getLocation().toString());
 			
 			Ecore2KM.isQuickFixEnabled = true;
 	        Ecore2KM.isMethodPropertyNameOverlapSafe = true;
 	        Ecore2KM.isMethodNameOverlapSafe = true;
 	        			
 			unit = KermetaUnitFactory.getDefaultLoader().createKermetaUnit(fileURI.toString());
-			unit.load();*/
+			unit.load();
 		}
 		return unit;
-	}
+	}*/
 	
 /*	static public KermetaUnit loadFile(IFile file, String content) {
 
