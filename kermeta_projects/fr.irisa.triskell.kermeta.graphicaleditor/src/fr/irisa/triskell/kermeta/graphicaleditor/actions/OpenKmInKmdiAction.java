bd5be8b2a9e20378928d1773f27bf602ca24a33e
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2007-07-26 16:07:01 +0000

    Done the Feature Request #1578: add a open with kermeta graphical editor directly on km files
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@3798 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 5fe313d71f954040af86c035f27109752ebc107a

---
+++ kermeta_projects/fr.irisa.triskell.kermeta.graphicaleditor/src/fr/irisa/triskell/kermeta/graphicaleditor/actions/OpenKmInKmdiAction.java
@@ -0,0 +1,100 @@
+/*$Id: OpenKmInKmdiAction.java,v 1.1 2007-07-26 16:07:01 cfaucher Exp $
+* Project : fr.irisa.triskell.kermeta
+* File : 	Ecore2KmAction.java
+* License : EPL
+* Copyright : IRISA / INRIA / Universite de Rennes 1
+* ----------------------------------------------------------------------------
+* Creation date : 11 janv. 07
+* Authors : cfaucher
+*/
+
+
+package fr.irisa.triskell.kermeta.graphicaleditor.actions;
+
+import java.util.Iterator;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.IEditorDescriptor;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
+import org.topcased.modeler.editor.Modeler;
+
+import fr.irisa.triskell.eclipse.resources.ResourceHelper;
+import fr.irisa.triskell.kermeta.graphicaleditor.editor.KmEditor;
+import fr.irisa.triskell.kermeta.graphicaleditor.wizards.NewKmDiagrams;;
+
+public class OpenKmInKmdiAction implements IObjectActionDelegate {
+
+	protected StructuredSelection currentSelection;
+    protected IFile kmfile;
+    
+	/**
+	 * Constructor for Ecore2KmAction.
+	 */
+	public OpenKmInKmdiAction() {
+		super();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		Shell shell = new Shell();
+	   
+    	if(kmfile.exists()) {
+    		IPath kmdiPath = kmfile.getLocation().removeFileExtension().addFileExtension("kmdi");
+    		IPath kmdiLocation = kmfile.getFullPath().removeFileExtension().addFileExtension("kmdi");
+    		if(kmdiPath.toFile().exists()) {
+    			IEditorDescriptor desc = PlatformUI.getWorkbench().getEditorRegistry().getDefaultEditor("test.kmdi");
+    			try {
+    				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().openEditor(
+    				      new FileEditorInput(ResourceHelper.getIFile(kmdiLocation.toString())),
+    				      desc.getId());
+    			} catch (PartInitException e) {
+    				// TODO Auto-generated catch block
+    				e.printStackTrace();
+    			};
+    		}
+    		else {
+        		NewKmDiagrams wizard =  new NewKmDiagrams();
+            	wizard.init(PlatformUI.getWorkbench(),currentSelection);
+            	WizardDialog wizDialog =  new org.eclipse.jface.wizard.WizardDialog(shell,wizard);
+            	wizDialog.open();
+        	}
+    	}
+    	
+	}
+
+    /**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		if (selection instanceof StructuredSelection)
+		{
+			// the se=lection should be a single *.ecore file
+			currentSelection = (StructuredSelection)selection;
+			Iterator it = currentSelection.iterator();
+
+			while(it.hasNext()) {
+				kmfile = (IFile)it.next();
+			}
+		}
+	}
+
+}
