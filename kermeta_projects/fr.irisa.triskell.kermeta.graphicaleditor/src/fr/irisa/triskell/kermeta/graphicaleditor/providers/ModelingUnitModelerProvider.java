ed52f1848b425b0a18d70f5de722402ba4c25aa6
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2007-08-23 07:57:55 +0000

    root object template becomes ModelingUnit instance of Package
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@4020 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 803ee691bc815180b7e6b68a2f302bcf38319616

---
+++ kermeta_projects/fr.irisa.triskell.kermeta.graphicaleditor/src/fr/irisa/triskell/kermeta/graphicaleditor/providers/ModelingUnitModelerProvider.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * $Id: ModelingUnitModelerProvider.java,v 1.1 2007-08-23 07:57:55 cfaucher Exp $
+ * License: EPL
+ * Copyright: IRISA / INRIA / Universite de Rennes 1
+ ******************************************************************************/
+package fr.irisa.triskell.kermeta.graphicaleditor.providers;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.edit.provider.ItemProviderAdapter;
+import org.topcased.modeler.providers.ILabelFeatureProvider;
+
+/**
+ * This is the item provider adpater for a {@link fr.irisa.triskell.kermeta.language.structure.ModelingUnit} object.
+ *
+ * @generated
+ */
+public class ModelingUnitModelerProvider extends ItemProviderAdapter implements
+		ILabelFeatureProvider {
+	/**
+	 * This constructs an instance from a factory and a notifier.
+	 *
+	 * @param adapterFactory the adapter factory
+	 * @generated
+	 */
+	public ModelingUnitModelerProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	/**
+	 * @see org.topcased.modeler.providers.ILabelFeatureProvider#getLabelFeature(java.lang.Object)
+	 * @generated
+	 */
+	public EAttribute getLabelFeature(Object object) {
+		return null;
+	}
+}
