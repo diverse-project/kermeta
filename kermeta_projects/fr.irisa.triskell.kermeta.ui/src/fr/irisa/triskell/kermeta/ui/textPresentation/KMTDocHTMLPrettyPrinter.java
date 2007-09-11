fe17d70756a443a2d7e6ddf2a093ccf67d30eb38
Didier Vojtisek <didier.vojtisek@inria.fr>
2007-09-11 12:29:51 +0000

    initial revision of a kermetadoc view
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@4160 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: ef0459682fdd745d78a9ad5c91b802265e9926e6

---
+++ kermeta_projects/fr.irisa.triskell.kermeta.ui/src/fr/irisa/triskell/kermeta/ui/textPresentation/KMTDocHTMLPrettyPrinter.java
@@ -0,0 +1,350 @@
+/* $Id: KMTDocHTMLPrettyPrinter.java,v 1.1 2007-09-11 12:29:51 dvojtise Exp $
+ * Project : fr.irisa.triskell.kermeta.touchnavigator
+ * File : TNHintHTMLPrettyPrinter.java
+ * License : EPL
+ * Copyright : IRISA / INRIA/ Universite de Rennes 1
+ * ----------------------------------------------------------------------------
+ * Creation date : 1 janv. 2006
+ * Authors : 
+ *     	dvojtise <dvojtise@irisa.fr>
+ */
+/**
+ * 
+ */
+package fr.irisa.triskell.kermeta.ui.textPresentation;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.kermeta.io.printer.KM2KMTPrettyPrinter;
+
+import fr.irisa.triskell.kermeta.ast.helper.KMTHelper;
+import fr.irisa.triskell.kermeta.language.structure.ClassDefinition;
+import fr.irisa.triskell.kermeta.language.structure.Operation;
+import fr.irisa.triskell.kermeta.language.structure.Package;
+import fr.irisa.triskell.kermeta.language.structure.Property;
+import fr.irisa.triskell.kermeta.language.structure.Type;
+import fr.irisa.triskell.kermeta.modelhelper.NamedElementHelper;
+
+/**
+ * Create a HTML presentation of the given Kermeta object for use in a documentation
+ * DVK : this class can really be improved for example :
+ * 	the fact that it inherit from KM2KMTPrettyPrinter is not really pertinent since each object has its 
+ * own presentation depending on the context in which it is called. a simplier structure is probably better
+ * in this situation  
+ */
+public class KMTDocHTMLPrettyPrinter extends KM2KMTPrettyPrinter{
+	/** if 0 do not show attributes and operations
+	 * if 1 show only attributes and operations of the current class
+	 * if >1 also shows attributes and operations of the inherited class (if classFlat == true)
+	 */
+	public int classShortLevel = 2;
+	/** shows or not the inherited classes */
+	public boolean classFlat = true;
+	
+	public String CR = "\n";
+	
+	public int currentClassShortLevel = 0;
+	
+	protected boolean mainClass = false;
+	
+	
+
+	public String getHTMLDoc(EObject node)
+	{
+		String result = "<pre>";
+		currentClassShortLevel = 0;
+		mainClass =  true;
+		if(node instanceof ClassDefinition)
+			result += htmlSummary((ClassDefinition)node);
+		if(node instanceof Package)
+			result += htmlSummary((Package)node);
+
+		if(node instanceof Operation)
+			result += htmlSummary((Operation)node);
+		mainClass =  false;
+		result+="</pre>";
+		return  fixPlatformURL(result);
+	}
+	
+	public String htmlSummary(Package node) {
+		StringBuffer result= new StringBuffer("");
+		result.append("<b>package</b> " + KMTHelper.getMangledIdentifier(node.getName()));
+		result.append("\n") ;
+		String tags = ppTags(node.getTag());
+		result.append(getPrefix() + tags);
+		if(tags.compareTo("")!=0) result.append("\n") ;
+		return result.toString();
+	}
+	
+	public String htmlSummary(ClassDefinition node) {
+
+		StringBuffer result= new StringBuffer("");
+		if(mainClass){
+			String tags = ppTags(node.getTag());
+			result.append(getPrefix() + tags);
+			if(tags.compareTo("")!=0) result.append("\n"); 
+		}
+		if(!classFlat && classShortLevel < 1) return result.toString();
+		result.append(getPrefix());
+		if (node.isIsAbstract()) result.append("<b>abstract</b> ");
+		result.append("<b>class</b> " + KMTHelper.getMangledIdentifier(node.getName()));
+		if (node.getTypeParameter().size() > 0) {
+			result.append("&lt;");
+			result.append(ppTypeVariableDeclaration(node.getTypeParameter()));
+			result.append("&gt;");
+		}
+		result.append("\n") ;
+		if (node.getSuperType().size() > 0) {
+			Vector<String> knowOperationNames = new Vector<String>();
+
+			// fill the knownOperationName list with those of the current node we don't care of the resulting list 
+			// EList e = getOperationsNotIn(node, knowOperationNames);
+			if(!classFlat)
+			{ // shows only the the directly inherited classes
+
+				result.append(" <b>inherits from</b>");
+				Iterator<Type> itSuperType = node.getSuperType().iterator();
+				while(itSuperType.hasNext()){
+					result.append( CR );
+					EObject o = (EObject)itSuperType.next();
+					result.append(getPrefix() + "<b>class</b> " + accept(o));
+				}
+			}
+			else{
+
+				pushPrefix();
+				// retreive direct super classes
+				// if short level > 1
+					// show attributes 
+					// show operations not redefined in the current class
+				result.append(" <b>inherits from</b>");
+				Iterator itSuperType = node.getSuperType().iterator();
+				while(itSuperType.hasNext()){
+					result.append( CR );
+					EObject o = (EObject)itSuperType.next();
+					result.append( getPrefix() + "<b>class</b> " + accept(o));
+					if(classShortLevel > 1 && o instanceof ClassDefinition){
+						ClassDefinition superNode = (ClassDefinition)o;
+						pushPrefix();
+						result.append( ppCRSeparatedNode(superNode.getOwnedAttribute()));
+						EList<Operation> el =  getOperationsNotIn(superNode, knowOperationNames);
+						result.append( ppCRSeparatedNode(el));
+						//result += ppCRSeparatedNode(superNode.getFOwnedOperation());
+						popPrefix();
+						result.append( getPrefix() + "}");
+					}
+				}
+				// retreive all indirect super classes
+				// if short level > 1
+					// show attributes 
+					// show operations not redefined in the current class or in the previously processed class
+
+				popPrefix();
+			}
+		}	
+		// if short level > 0 
+		if(classShortLevel > 0){
+			result.append( CR + getPrefix() + "{" + CR);
+			pushPrefix();
+			// show attributes with tags
+			result.append( ppCRSeparatedNode(node.getOwnedAttribute()));
+			// show operations with tags
+			result.append( ppCRSeparatedNode(node.getOwnedOperation()));
+			popPrefix();
+			result.append( getPrefix() + "}");		
+		}
+		
+		return result.toString();
+	}
+	
+	public String htmlSummary(Operation node) {
+		StringBuffer result= new StringBuffer("");
+		result.append("<b>operation</b> " + KMTHelper.getMangledIdentifier(node.getName()));
+		result.append("\n") ;
+		String tags = ppTags(node.getTag());
+		result.append(getPrefix() + tags);
+		if(tags.compareTo("")!=0) result.append("\n") ;
+		return result.toString();
+	}
+	
+	protected EList<Operation> getOperationsNotIn(ClassDefinition node, Vector<String> knownOperationNames)
+	{
+		EList<Operation> el =  new BasicEList<Operation>();
+		for(Operation o : node.getOwnedOperation()){
+			if(!(knownOperationNames.contains(o.getName())))
+			{
+				knownOperationNames.add(o.getName());
+				el.add(o);
+			}
+		}
+		return el;
+	}
+	
+	/* (non-Javadoc)
+	 * @see fr.irisa.triskell.kermeta.exporter.kmt.KM2KMTPrettyPrinter#visit(fr.irisa.triskell.kermeta.language.structure.ClassDefinition)
+	 */
+	public Object visit(ClassDefinition node) {
+		typedef = false;
+		String result="";
+		if(mainClass){
+			String tags = ppTags(node.getTag());
+			result = getPrefix() + tags;
+			if(tags.length() != 0) result += CR ;
+		}
+		if(!classFlat && currentClassShortLevel < classShortLevel) return result;
+		result += getPrefix();
+		if (node.isIsAbstract()) result += "<b>abstract</b> ";
+		result += "<b>class</b> " + KMTHelper.getMangledIdentifier(node.getName());
+		if (node.getTypeParameter().size() > 0) {
+			result += "&lt;";
+			result += ppTypeVariableDeclaration(node.getTypeParameter());
+			result += "&gt;";
+		}
+		if (node.getSuperType().size() > 0) {
+			result += CR + getPrefix() + "<b>inherits from</b> ";
+			Iterator itSuperType = node.getSuperType().iterator();
+			while(itSuperType.hasNext()){
+				if(!classFlat){
+					result += CR + getPrefix() + prefixTab + "<b>class</b> ";
+					EObject o = (EObject)itSuperType.next();
+					result += this.accept(o);
+				}
+				else{
+					result += CR;
+					EObject o = (EObject)itSuperType.next();
+					if(o instanceof fr.irisa.triskell.kermeta.language.structure.Class){
+						fr.irisa.triskell.kermeta.language.structure.Class aFClass = (fr.irisa.triskell.kermeta.language.structure.Class)o;
+						currentClassShortLevel++;
+						pushPrefix();
+						boolean previousMainClass = mainClass;
+						mainClass =  false;
+						result += this.accept(aFClass.getTypeDefinition());
+						mainClass = previousMainClass;
+						popPrefix();
+						currentClassShortLevel--;
+					}
+					else {
+						// not a class  => do not flatten inheritance
+						result += getPrefix() + "<b>class</b> " + this.accept(o);
+					}
+				}
+			}
+		}
+		if(currentClassShortLevel < classShortLevel){
+			result += CR + getPrefix() + "{" + CR;
+			pushPrefix();
+			result += ppCRSeparatedNode(node.getOwnedAttribute());
+			result += ppCRSeparatedNode(node.getOwnedOperation());
+			popPrefix();
+			result += getPrefix() + "}";		
+		}
+		
+		typedef = true;
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.irisa.triskell.kermeta.exporter.kmt.KM2KMTPrettyPrinter#ppSimplifiedProperty(fr.irisa.triskell.kermeta.language.structure.Property)
+	 */
+	public String ppSimplifiedProperty(Property node) {
+		//return super.ppSimplifiedProperty(node);
+		String result = "";
+		if(mainClass){
+			String tags = ppTags(node.getTag());
+			result = getPrefix() + tags;
+			if(tags.length() != 0) result += CR ;
+		}
+		if (node.isIsDerived()) result += "<b>property</b> ";
+		else if (node.isIsComposite()) result += "<b>attribute</b> ";
+		else result += "<b>reference</b> ";
+		if (node.isIsReadOnly()) result += "<b>readonly</b> ";
+		result += KMTHelper.getMangledIdentifier(node.getName()) + " : " + ppTypeFromMultiplicityElement(node);
+		if (node.getOpposite() != null) result += "#" + KMTHelper.getMangledIdentifier(node.getOpposite().getName());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.irisa.triskell.kermeta.exporter.kmt.KM2KMTPrettyPrinter#visit(fr.irisa.triskell.kermeta.language.structure.Operation)
+	 */
+	public Object visit(Operation node) {
+		//return super.visit(node);
+		String result = "";
+		if(mainClass){
+			//result = ppTags(node.getTag());
+			String tags = ppTags(node.getTag());
+			result = getPrefix() + tags;
+			if(tags.length() != 0) result += CR ;
+		}
+		if (node.isIsAbstract()) result += "<b>abstract</b> ";
+		if (node.getSuperOperation() != null) result += "<b>method</b> ";
+		else result += "<b>operation</b> ";
+		result += KMTHelper.getMangledIdentifier(node.getName());
+		if (node.getTypeParameter().size() > 0) {
+			result += "<";
+			result += ppTypeVariableDeclaration(node.getTypeParameter());
+			result += ">";
+		}
+		result += "(";
+		result += ppComaSeparatedNodes(node.getOwnedParameter());
+		result += ")";
+		if(node.getType() != null) {
+			result += " : " + ppTypeFromMultiplicityElement(node);
+		}
+	
+		if (node.getSuperOperation() != null) {
+			result += " from " + NamedElementHelper.getMangledQualifiedName(node.getSuperOperation().getOwningClass());
+		}
+		if (node.getRaisedException().size() > 0) {
+			result += " raises " + ppComaSeparatedNodes(node.getRaisedException());
+		}
+		
+		return result;
+	}
+
+	/**
+	 * replace the platfrom plugin url by the local file url 
+	 * 
+	 */
+	public String fixPlatformURL(String text){
+		String result;
+		
+		StringBuffer myStringBuffer = new StringBuffer();
+		Pattern myPattern = Pattern.compile("platform:/plugin/[^/]*/");
+		Matcher myMatcher = myPattern.matcher(text);
+		while (myMatcher.find()) {
+			try {
+				URL url = new URL(myMatcher.group());				
+				String  replacement = FileLocator.resolve(url).toString();
+			    myMatcher.appendReplacement(myStringBuffer, replacement);
+			} catch (Exception e) {
+			    myMatcher.appendReplacement(myStringBuffer, myMatcher.group());					
+			}
+		}
+		myMatcher.appendTail(myStringBuffer);
+	
+		result = myStringBuffer.toString();
+		
+		return result;
+	}
+	
+	/**
+	 * Constructor
+	 */
+	public KMTDocHTMLPrettyPrinter() {
+		super();
+		prefixTab = "   ";
+	}
+
+}
---
+++ kermeta_projects/fr.irisa.triskell.kermeta.ui/src/fr/irisa/triskell/kermeta/ui/views/KermetadocView.java
@@ -0,0 +1,325 @@
+/*$Id: KermetadocView.java,v 1.1 2007-09-11 12:29:51 dvojtise Exp $
+* Project : fr.irisa.triskell.kermeta.kpm
+* File : 	sdfg.java
+* License : EPL
+* Copyright : IRISA / INRIA / Universite de Rennes 1
+* ----------------------------------------------------------------------------
+* Creation date : Sep, 2007
+* Authors : 
+*	Didier Vojtisek
+*/
+package fr.irisa.triskell.kermeta.ui.views;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.resource.ColorRegistry;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.util.PropertyChangeEvent;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTError;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ViewPart;
+import org.kermeta.io.KermetaUnit;
+
+import fr.irisa.triskell.kermeta.texteditor.TexteditorPlugin;
+import fr.irisa.triskell.kermeta.texteditor.editors.KMTEditor;
+import fr.irisa.triskell.kermeta.texteditor.editors.KermetaEditorEventListener;
+import fr.irisa.triskell.kermeta.ui.textPresentation.KMTDocHTMLPrettyPrinter;
+
+/**
+ * View which shows Javadoc for a given Java element.
+ *
+ * FIXME: As of 3.0 selectAll() and getSelection() is not working
+ *			see https://bugs.eclipse.org/bugs/show_bug.cgi?id=63022
+ *
+ * @since 3.0
+ */
+public class KermetadocView extends ViewPart implements KermetaEditorEventListener {
+
+	/** The Browser widget */
+	private boolean fIsUsingBrowserWidget;
+	/** The HTML widget. */
+	private Browser fBrowser;
+	/** The text widget. */
+	private StyledText fText;
+	
+	
+	
+	private Shell shell;
+	
+	private Action actionAbout;
+		
+
+	private Object lastValidUpdate = null;
+	
+	private TexteditorPlugin textEditorPlugin = null;
+
+
+	KMTDocHTMLPrettyPrinter hintpp = new KMTDocHTMLPrettyPrinter();
+	
+	private String currentHTMLDocumentation =  "";
+		
+
+	/**
+	 * This is a callback that will allow us
+	 * to create the viewer and initialize it.
+	 */
+	public void createPartControl(Composite parent) {
+		/*viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		viewer.setContentProvider(new ViewContentProvider());
+		viewer.setLabelProvider(new ViewLabelProvider());
+		viewer.setSorter(new NameSorter());
+		viewer.setInput(getViewSite());
+		*/
+		
+		internalCreatePartControl(parent);
+		
+		makeActions();
+		hookContextMenu();
+		hookDoubleClickAction();
+		contributeToActionBars();
+
+		registerToTextEditor();
+	}
+	
+	protected void internalCreatePartControl(Composite parent) {
+		try {
+			fBrowser= new Browser(parent, SWT.NONE);
+			fIsUsingBrowserWidget= true;
+			
+			
+		} catch (SWTError er) {
+
+			/* The Browser widget throws an SWTError if it fails to
+			 * instantiate properly. Application code should catch
+			 * this SWTError and disable any feature requiring the
+			 * Browser widget.
+			 * Platform requirements for the SWT Browser widget are available
+			 * from the SWT FAQ web site.
+			 */
+
+		/*	IPreferenceStore store= JavaPlugin.getDefault().getPreferenceStore();
+			boolean doNotWarn= store.getBoolean(DO_NOT_WARN_PREFERENCE_KEY);
+			if (WARNING_DIALOG_ENABLED && !doNotWarn) {
+				String title= "KermetadocView_error_noBrowser_title";
+				String message= "KermetadocView_error_noBrowser_message";
+				String toggleMessage= "KermetadocView_error_noBrowser_doNotWarn";
+				MessageDialogWithToggle dialog= MessageDialogWithToggle.openError(parent.getShell(), title, message, toggleMessage, false, null, null); 
+				if (dialog.getReturnCode() == Window.OK)
+					store.setValue(DO_NOT_WARN_PREFERENCE_KEY, dialog.getToggleState());
+			}*/
+
+			fIsUsingBrowserWidget= false;
+		}
+
+		if (!fIsUsingBrowserWidget) {
+			fText= new StyledText(parent, SWT.V_SCROLL | SWT.H_SCROLL);
+			fText.setEditable(false);
+			
+		}
+		inititalizeColors();
+	}
+	protected Control getControl() {
+		if (fIsUsingBrowserWidget)
+			return fBrowser;
+		else
+			return fText;
+	}
+	
+	private void inititalizeColors() {
+		if (getSite().getShell().isDisposed())
+			return;
+
+		Display display= getSite().getShell().getDisplay();
+		if (display == null || display.isDisposed())
+			return;
+
+		getControl().setForeground(display.getSystemColor(SWT.COLOR_INFO_FOREGROUND));
+		
+		ColorRegistry registry= JFaceResources.getColorRegistry();
+		// same color as Javadoc
+		RGB fBackgroundColorRGB= registry.getRGB("org.eclipse.jdt.ui.JavadocView.backgroundColor");
+		Color bgColor;
+		if (fBackgroundColorRGB == null) {
+			bgColor= display.getSystemColor(SWT.COLOR_INFO_BACKGROUND);
+		} else {
+			bgColor= new Color(display, fBackgroundColorRGB);
+		}
+		
+		getControl().setBackground(bgColor);
+//		getControl().update();
+	}
+	
+	
+	private void hookContextMenu() {
+		MenuManager menuMgr = new MenuManager("#PopupMenu");
+		menuMgr.setRemoveAllWhenShown(true);
+		menuMgr.addMenuListener(new IMenuListener() {
+			public void menuAboutToShow(IMenuManager manager) {
+				KermetadocView.this.fillContextMenu(manager);
+			}
+		});
+		/*
+		Menu menu = menuMgr.createContextMenu(viewer.getControl());
+		viewer.getControl().setMenu(menu);
+		getSite().registerContextMenu(menuMgr, viewer);
+		*/
+	}
+	private void hookDoubleClickAction() {
+		/*viewer.addDoubleClickListener(new IDoubleClickListener() {
+			public void doubleClick(DoubleClickEvent event) {
+				doubleClickAction.run();
+			}
+		});*/
+		//showMessage("hookDoubleClickAction: Double-click detected");
+	}
+	
+	private void contributeToActionBars() {
+		IActionBars bars = getViewSite().getActionBars();
+		fillLocalPullDown(bars.getMenuManager());
+		fillLocalToolBar(bars.getToolBarManager());
+	}
+
+	private void fillLocalPullDown(IMenuManager manager) {
+		//manager.add(actionConfigure);
+		//manager.add(actionRestartSWTAWT);
+		//manager.add(new Separator());
+		manager.add(actionAbout);
+	}
+
+	private void fillContextMenu(IMenuManager manager) {
+		//manager.add(actionBack);
+		//manager.add(actionForward);
+		//manager.add(actionConfigure);
+		manager.add(actionAbout);
+		// Other plug-ins can contribute there actions here
+		//manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+	}
+	
+	private void fillLocalToolBar(IToolBarManager manager) {
+		//manager.add(actionBack);
+		//manager.add(actionForward);
+		// manager.add(actionConfigure);
+		// manager.add(actionAbout);
+	}
+
+	private void makeActions() {
+		
+		actionAbout = new Action() {
+			public void run() {
+				showMessage("Kermetadoc presents a simplified view of the documentation associated to your Kermeta model elements");
+			}
+		};
+		actionAbout.setText("About");
+		actionAbout.setToolTipText("About Kermetadoc view");
+		actionAbout.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
+				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));		
+		
+	}
+
+	/* The real work of doing the doc
+	 * 
+	 */
+	public void updateHTMLDocumentationFrom(Object n) {
+		
+		Boolean isValidUpdate = false;
+		String newHTMLDocumentation = "";
+		if(n != null && n != lastValidUpdate)
+			if (n instanceof EObject){						
+				newHTMLDocumentation = (String)hintpp.getHTMLDoc((EObject) n);
+				isValidUpdate = true;
+			}
+		if(isValidUpdate){
+			currentHTMLDocumentation = newHTMLDocumentation;
+			lastValidUpdate = n;
+			refresh();
+		}
+	}
+	
+	
+	private void showMessage(String message) {
+		MessageDialog.openInformation(
+			shell,
+			"Kermetadoc View",
+			message);
+	}
+	
+	/**
+	 * Passing the focus request to the viewer's control.
+	 */
+	public void setFocus() {
+		//viewer.getControl().setFocus();
+	}
+	
+	/**
+	 * @see KermetaEditorEventListener#textHoverCalled()
+	 */
+	public void textHoverCalled(fr.irisa.triskell.kermeta.language.structure.Object fobj) {
+		updateHTMLDocumentationFrom(fobj);
+	}
+	
+	/**
+	 * @see KermetaEditorEventListener#outlineSelectionChanged()
+	 */
+	public void outlineSelectionChanged(fr.irisa.triskell.kermeta.language.structure.Object fobj) {
+		updateHTMLDocumentationFrom(fobj);
+	}
+	/** 
+	 * Action when a kermetaEditor selection was changed
+	 * @see fr.irisa.triskell.kermeta.texteditor.editors.KermetaEditorEventListener#selectionChanged()
+	 */
+	public void selectionChanged() {
+		
+		
+	}
+	/** 
+	 * Action when a kermetaEditor unit was changed
+	 * @see fr.irisa.triskell.kermeta.texteditor.editors.KermetaEditorEventListener#unitChanged()
+	 */
+	public void unitGotFocus(KMTEditor editor) {
+		KermetaUnit currentUnit = editor.getMcunit();
+	}
+	
+	/**
+	 * Refreshes the view.
+	 */
+	private void refresh() {
+		registerToTextEditor();
+		
+		String docHtml= currentHTMLDocumentation;
+
+		if (fIsUsingBrowserWidget) {			
+			fBrowser.setText(docHtml);
+		} else {
+			fText.setText(docHtml);
+			
+		}
+	}
+	
+	protected void internalDispose(){
+		textEditorPlugin.unregisterListener(this);
+	}
+	
+	private void registerToTextEditor() {
+		if(textEditorPlugin == null) {
+			textEditorPlugin = TexteditorPlugin.getDefault();
+			textEditorPlugin.registerListener(this);
+		}
+		
+	}
+}
