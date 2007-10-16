c38882b9a6c2f4e1520ea7b4b5dbb5aa7afb91a4
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2007-10-16 09:20:20 +0000

    Add a initialisation step during the launching of the plugin
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@4333 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 1f920d43bf8a256dc9b43682529405f36a66bf7e

--- kermeta_projects/fr.irisa.triskell.kermeta.tests/src/fr/irisa/triskell/kermeta/tests/plugin/TestPlugin.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.tests/src/fr/irisa/triskell/kermeta/tests/plugin/TestPlugin.java
@@ -1,4 +1,4 @@
-/* $Id: TestPlugin.java,v 1.1 2007-09-10 08:35:16 cfaucher Exp $
+/* $Id: TestPlugin.java,v 1.2 2007-10-16 09:20:20 cfaucher Exp $
  * Project    : fr.irisa.triskell.kermeta.tests
  * File       : TestPlugin.java
  * License    : EPL
@@ -13,8 +13,18 @@
 
 package fr.irisa.triskell.kermeta.tests.plugin;
 
+import java.io.File;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.URIConverterImpl;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.kermeta.io.plugin.IOPlugin;
 import org.osgi.framework.BundleContext;
+
+import fr.irisa.triskell.kermeta.impl.KmPackageImpl;
+import fr.irisa.triskell.kermeta.modelhelper.URIMapUtil;
 
 
 /**
@@ -26,12 +36,18 @@ public class TestPlugin extends AbstractUIPlugin {
 	
 	public static final String PLUGIN_TESTS_PATH = "platform:/plugin/fr.irisa.triskell.kermeta.tests/";
 	
+	private static boolean INITIALIZED = false;
+	
 	/**
-	 * The constructor.
+	 * The constructor
 	 */
 	public TestPlugin() {
 		super();
 		plugin = this;
+		if ( ! INITIALIZED ) {
+			initialize();
+			INITIALIZED = true;
+		}
 	}
 
 	/**
@@ -53,7 +69,28 @@ public class TestPlugin extends AbstractUIPlugin {
 	 * Returns the shared instance.
 	 */
 	public static TestPlugin getDefault() {
+		if ( plugin == null ) 
+			plugin = new TestPlugin();
 		return plugin;
+	}
+	
+	private void initialize() {
+		if ( ! INITIALIZED ) {
+			
+			//kermetaUnitHelper = new org.kermeta.io.util2.KermetaUnitHelper( storer );
+			KmPackageImpl.init();
+			File file = new File(IOPlugin.URI_MAP);
+			if (file.exists()){
+				URIConverterImpl.URI_MAP.putAll(URIMapUtil.readMapFile(file));
+				System.out.println("URIMap read from " + file.toString());
+			}
+			else {
+				System.out.println("not able to read URIMap from " + file.toString());
+			}
+			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore",new XMIResourceFactoryImpl());
+			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("km",new XMIResourceFactoryImpl());	
+			
+		}
 	}
 	
 }
