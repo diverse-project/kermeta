74c3171c2f395ad83ee054b2aa7a9fd3374c7ca5
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2007-11-21 09:58:40 +0000

    comments, cleaning for preparating OpenEmbeDD release.
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@4508 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: da674fe2cf02752b61c7c1139bd566fd2f685df7

--- kermeta_projects/org.kermeta.compiler.generator/src/org/kermeta/compiler/generator/internal/generators/CompilerHelperGenerator.java
+++ kermeta_projects/org.kermeta.compiler.generator/src/org/kermeta/compiler/generator/internal/generators/CompilerHelperGenerator.java
@@ -43,12 +43,8 @@ import fr.irisa.triskell.kermeta.exporter.ecore.EcoreExporter;
 import fr.irisa.triskell.kermeta.language.structure.Tag;
 
 /**
- * This class is the Entry point of the generation for a Topcased editor defined
- * by a *.editorconfigurator file<br>
+ * This class is the Entry point of the generation for Kermeta compiler's helpers
  * 
- * Creation : 26 avril 2006
- * 
- * @author <a href="mailto:jacques.lescot@anyware-tech.com">Jacques LESCOT</a>
  */
 public class CompilerHelperGenerator extends AbstractGenerator {
 	/** ******************** */
@@ -60,57 +56,25 @@ public class CompilerHelperGenerator extends AbstractGenerator {
 
 	private static final String JAVA_LAUNCHER_LAUNCH = "templateHelper/JavaLauncher.launchjet";
 
+	// FIXME CF unused for the moment
 	private static final String KERMETA_LAUNCHER_LAUNCH = "templateHelper/KermetaLauncher.launchjet";
 
-	/*
-	 * private static final String IMAGE_REGISTRY_JAVA =
-	 * "templateEditor/ImageRegistry.javajet";
-	 * 
-	 * private static final String EDITOR_ACTION_BAR_CONTRIBUTOR_JAVA =
-	 * "templateEditor/actions/EditorActionBarContributor.javajet";
-	 * 
-	 * private static final String EDITOR_JAVA =
-	 * "templateEditor/editor/Editor.javajet";
-	 * 
-	 * private static final String MODELER_PROVIDER_JAVA =
-	 * "templateEditor/providers/ModelerProvider.javajet";
-	 * 
-	 * private static final String MODELER_PROVIDER_ADAPTER_FACTORY_JAVA =
-	 * "templateEditor/providers/ModelerProviderAdapterFactory.javajet";
-	 * 
-	 * private static final String NEW_DIAGRAMS_JAVA =
-	 * "templateEditor/wizards/NewDiagrams.javajet";
-	 * 
-	 * private static final String DIAGRAMS_PAGE_JAVA =
-	 * "templateEditor/wizards/DiagramsPage.javajet";
-	 * 
-	 * private static final String IMAGES_PROPERTIES =
-	 * "templateEditor/images.propertiesjet";
-	 * 
-	 * private static final String BUILD_PROPERTIES =
-	 * "templateEditor/build.propertiesjet";
-	 * 
-	 * private static final String PLUGIN_XML = "templateEditor/plugin.xmljet";
-	 * 
-	 * private static final String MANIFEST_MF =
-	 * "templateEditor/MANIFEST.MFjet";
-	 * 
-	 * private static final String PREFERENCE_PAGE_JAVA =
-	 * "templateEditor/preferences/PreferencePage.javajet";
-	 */
 
 	/** The GenModel object */
 	private GenModel configuration;
 
 	private KermetaUnit kmUnit;
+	// FIXME CF unused for the moment
 	private EcoreExporter km2ecoreGen;
 	private HelperModel helperModel;
 
 	/**
 	 * Constructor
 	 * 
-	 * @param conf
-	 *            The editor configuration to generate
+	 * @param conf the genmodel based on the generated Ecore
+	 * @param kmUnit
+	 * @param km2ecoreGen
+	 * @param helperModel
 	 */
 	public CompilerHelperGenerator(GenModel conf, KermetaUnit kmUnit,
 			EcoreExporter km2ecoreGen, HelperModel helperModel) {
@@ -121,7 +85,7 @@ public class CompilerHelperGenerator extends AbstractGenerator {
 	}
 
 	/**
-	 * Launch the generation for the current editor configuration
+	 * Launch the generation for the compilation
 	 * 
 	 * @param monitor
 	 *            the monitor for the work progression
@@ -141,8 +105,6 @@ public class CompilerHelperGenerator extends AbstractGenerator {
 		monitor.worked(1);
 
 		generateHelpers(configuration, pathProject, monitor);
-		// generateProviders(configuration, pathProject, monitor);
-		// generateEditor(configuration, pathProject, monitor);
 
 		if(this.helperModel!=null) {
 			generateHelperModel(helperModel, pathProject, monitor);
@@ -157,10 +119,7 @@ public class CompilerHelperGenerator extends AbstractGenerator {
 	protected JETEmitter createJETEmitter(String templateURI) {
 		return new DefaultJETEmitter(templateURI) {
 			public void initialize(Monitor monitor) throws JETException {
-				// The editorconfigurator plugin must be released to activate
-				// the
-				// generation.
-				// addVariable("EDITOR_CONF", "org.kermeta.compiler.helper");
+				// Further plugins must be released to activate the generation.
 				addVariable("KERMETA_ECLIPSE_HELPER",
 						"fr.irisa.triskell.eclipse.util");
 				addVariable("KERMETA_COMPILER_GENERATOR",
@@ -189,7 +148,9 @@ public class CompilerHelperGenerator extends AbstractGenerator {
 		
 		for(GenPackage genpack : configuration.getAllGenPackagesWithClassifiers()) {
 		
-		String projectName = EcoreHelper.getQualifiedName(genpack.getEcorePackage());/*"kermeta"; project.getName();*/
+		// Please do not remove this comment, old value of the project name variable:
+		// "kermeta"; project.getName();
+		String projectName = EcoreHelper.getQualifiedName(genpack.getEcorePackage());
 		IPath packagePath = new Path(projectName.replace('.', IPath.SEPARATOR));
 
 		for (int i = 1; i < packagePath.segmentCount() + 1; i++) {
@@ -235,156 +196,6 @@ public class CompilerHelperGenerator extends AbstractGenerator {
 		}
 	}
 
-	/**
-	 * Generate providers classes of the genModel (inside providers package)
-	 * 
-	 * @param conf
-	 * @param projectPath
-	 * @param monitor
-	 */
-	/*
-	 * private void generateProviders(EditorConfiguration conf, IPath
-	 * projectPath, IProgressMonitor monitor) { try { monitor.subTask("Providers
-	 * generation");
-	 * 
-	 * for (Iterator i =
-	 * conf.getGenModel().getAllGenPackagesWithClassifiers().iterator();
-	 * i.hasNext();) { GenPackage genPackage = (GenPackage) i.next();
-	 * 
-	 * generateModelerProviderAdapterFactory(conf, genPackage, projectPath);
-	 * monitor.worked(1);
-	 * 
-	 * for (Iterator j = genPackage.getGenClasses().iterator(); j.hasNext();) {
-	 * GenClass genClass = (GenClass) j.next(); generateModelerProvider(conf,
-	 * genClass, projectPath); monitor.worked(1); } } } catch (JETException e) {
-	 * GeneratorPlugin.log(e); GeneratorPlugin.displayDialog(null, "JETException :
-	 * an error occured during editor generation. See error logs for more
-	 * details.", IStatus.ERROR); } catch (CoreException e) {
-	 * GeneratorPlugin.log(e); GeneratorPlugin.displayDialog(null,
-	 * "CoreException : an error occured during providers generation. See error
-	 * logs for more details.", IStatus.ERROR); } }
-	 * 
-	 * private void generateEditor(EditorConfiguration conf, IPath projectPath,
-	 * IProgressMonitor monitor) { try {
-	 * 
-	 * monitor.subTask("Files creation");
-	 * 
-	 * generatePluginClass(conf, projectPath); generateImageRegistryClass(conf,
-	 * projectPath);
-	 * 
-	 * generatePluginXML(conf, projectPath); generateManifestMF(conf,
-	 * projectPath); generateImageProperties(conf, projectPath);
-	 * generateBuildProperties(conf, projectPath);
-	 * 
-	 * generateEditorActionBarContributorClass(conf, projectPath);
-	 * generateEditorClass(conf, projectPath); generatePreferencePage(conf,
-	 * projectPath);
-	 * 
-	 * monitor.worked(1);
-	 * 
-	 * generateNewDiagramsClass(conf, projectPath);
-	 * generateDiagramsPageClass(conf, projectPath);
-	 * 
-	 * monitor.worked(1); } catch (JETException e) { GeneratorPlugin.log(e);
-	 * GeneratorPlugin.displayDialog(null, "JETException : an error occured
-	 * during editor generation. See error logs for more details.",
-	 * IStatus.ERROR); } catch (CoreException e) { GeneratorPlugin.log(e);
-	 * GeneratorPlugin.displayDialog(null, "CoreException : an error occured
-	 * during editor generation. See error logs for more details.",
-	 * IStatus.ERROR); } }
-	 *  // ---------------------------------------- // The generations common to
-	 * the same editor // ---------------------------------------- private void
-	 * generatePluginClass(EditorConfiguration conf, IPath projectPath) throws
-	 * JETException, CoreException { applyTemplate(conf,
-	 * getTemplateURI(PLUGIN_JAVA), projectPath.append("/" + SOURCE_DIRECTORY +
-	 * "/" + conf.getProjectName().replace('.', '/') + "/" + conf.getPrefix() +
-	 * "Plugin.java"), conf.isForceOverwrite()); }
-	 * 
-	 * private void generateImageRegistryClass(EditorConfiguration conf, IPath
-	 * projectPath) throws JETException, CoreException { applyTemplate(conf,
-	 * getTemplateURI(IMAGE_REGISTRY_JAVA), projectPath.append("/" +
-	 * SOURCE_DIRECTORY + "/" + conf.getProjectName().replace('.', '/') + "/" +
-	 * conf.getPrefix() + "ImageRegistry.java"), conf.isForceOverwrite()); }
-	 * 
-	 * private void generateEditorActionBarContributorClass(EditorConfiguration
-	 * conf, IPath projectPath) throws JETException, CoreException {
-	 * applyTemplate(conf, getTemplateURI(EDITOR_ACTION_BAR_CONTRIBUTOR_JAVA),
-	 * projectPath.append("/src/" + conf.getProjectName().replace('.', '/') +
-	 * "/actions/" + conf.getPrefix() + "EditorActionBarContributor.java"),
-	 * conf.isForceOverwrite()); }
-	 * 
-	 * private void generateEditorClass(EditorConfiguration conf, IPath
-	 * projectPath) throws JETException, CoreException { applyTemplate(conf,
-	 * getTemplateURI(EDITOR_JAVA), projectPath.append("/" + SOURCE_DIRECTORY +
-	 * "/" + conf.getProjectName().replace('.', '/') + "/editor/" +
-	 * conf.getPrefix() + "Editor.java"), conf.isForceOverwrite()); }
-	 * 
-	 * private void generateNewDiagramsClass(EditorConfiguration conf, IPath
-	 * projectPath) throws JETException, CoreException { applyTemplate(conf,
-	 * getTemplateURI(NEW_DIAGRAMS_JAVA), projectPath.append("/" +
-	 * SOURCE_DIRECTORY + "/" + conf.getProjectName().replace('.', '/') +
-	 * "/wizards/New" + conf.getPrefix() + "Diagrams.java"),
-	 * conf.isForceOverwrite()); }
-	 * 
-	 * private void generateDiagramsPageClass(EditorConfiguration conf, IPath
-	 * projectPath) throws JETException, CoreException { applyTemplate(conf,
-	 * getTemplateURI(DIAGRAMS_PAGE_JAVA), projectPath.append("/" +
-	 * SOURCE_DIRECTORY + "/" + conf.getProjectName().replace('.', '/') +
-	 * "/wizards/" + conf.getPrefix() + "DiagramsPage.java"),
-	 * conf.isForceOverwrite()); }
-	 * 
-	 * private void generateImageProperties(EditorConfiguration conf, IPath
-	 * projectPath) throws JETException, CoreException { applyTemplate(conf,
-	 * getTemplateURI(IMAGES_PROPERTIES), projectPath.append("/" +
-	 * SOURCE_DIRECTORY + "/" + conf.getProjectName().replace('.', '/') +
-	 * "/images.properties"), conf.isForceOverwrite()); }
-	 * 
-	 * private void generatePluginXML(EditorConfiguration conf, IPath
-	 * projectPath) throws JETException, CoreException { applyTemplate(conf,
-	 * getTemplateURI(PLUGIN_XML), projectPath.append("/plugin.xml"),
-	 * conf.isForceOverwrite()); }
-	 * 
-	 * private void generateManifestMF(EditorConfiguration conf, IPath
-	 * projectPath) throws JETException, CoreException { applyTemplate(conf,
-	 * getTemplateURI(MANIFEST_MF), projectPath.append("/META-INF/MANIFEST.MF"),
-	 * conf.isForceOverwrite()); }
-	 * 
-	 * private void generateBuildProperties(EditorConfiguration conf, IPath
-	 * projectPath) throws JETException, CoreException { applyTemplate(conf,
-	 * getTemplateURI(BUILD_PROPERTIES),
-	 * projectPath.append("/build.properties"), conf.isForceOverwrite()); }
-	 *  // --------------------------------------------------------- // Generate
-	 * the classes for the providers //
-	 * ---------------------------------------------------------
-	 * 
-	 * private void generateModelerProviderAdapterFactory(EditorConfiguration
-	 * conf, GenPackage genPackage, IPath projectPath) throws JETException,
-	 * CoreException { Map map = new HashMap(); map.put("genPackage",
-	 * genPackage); map.put("editorConfiguration", conf); applyTemplate(map,
-	 * getTemplateURI(MODELER_PROVIDER_ADAPTER_FACTORY_JAVA),
-	 * projectPath.append("/" + SOURCE_DIRECTORY + "/" +
-	 * conf.getProjectName().replace('.', '/') + "/providers/" +
-	 * genPackage.getPrefix() + "ModelerProviderAdapterFactory.java"),
-	 * conf.isForceOverwrite()); }
-	 * 
-	 * private void generateModelerProvider(EditorConfiguration conf, GenClass
-	 * genClass, IPath projectPath) throws JETException, CoreException { Map map =
-	 * new HashMap(); map.put("genClass", genClass);
-	 * map.put("editorConfiguration", conf); applyTemplate(map,
-	 * getTemplateURI(MODELER_PROVIDER_JAVA), projectPath.append("/" +
-	 * SOURCE_DIRECTORY + "/" + conf.getProjectName().replace('.', '/') +
-	 * "/providers/" + genClass.getName() + "ModelerProvider.java"),
-	 * conf.isForceOverwrite()); }
-	 *  // --------------------------------------------------------- // Generate
-	 * the classes for the preferences //
-	 * --------------------------------------------------------- private void
-	 * generatePreferencePage(EditorConfiguration conf, IPath projectPath)
-	 * throws JETException, CoreException { applyTemplate(conf,
-	 * getTemplateURI(PREFERENCE_PAGE_JAVA), projectPath.append("/" +
-	 * SOURCE_DIRECTORY + "/" + conf.getProjectName().replace('.', '/') +
-	 * "/preferences/" + conf.getPrefix() + "PreferencePage.java"),
-	 * conf.isForceOverwrite()); }
-	 */
 
 	// ---------------------------------------------------------
 	// Generate the classes for the helpers
@@ -395,27 +206,8 @@ public class CompilerHelperGenerator extends AbstractGenerator {
 
 			monitor.subTask("Files creation");
 
-			//generateTestHelpers(conf, projectPath);
 			generateRunner(conf, projectPath);
 			generateLauncher(conf, projectPath);
-
-			/*
-			 * generatePluginClass(conf, projectPath);
-			 * generateImageRegistryClass(conf, projectPath);
-			 * 
-			 * generatePluginXML(conf, projectPath); generateManifestMF(conf,
-			 * projectPath); generateImageProperties(conf, projectPath);
-			 * generateBuildProperties(conf, projectPath);
-			 * 
-			 * generateEditorActionBarContributorClass(conf, projectPath);
-			 * generateEditorClass(conf, projectPath);
-			 * generatePreferencePage(conf, projectPath);
-			 * 
-			 * monitor.worked(1);
-			 * 
-			 * generateNewDiagramsClass(conf, projectPath);
-			 * generateDiagramsPageClass(conf, projectPath);
-			 */
 
 			monitor.worked(1);
 		} catch (JETException e) {
@@ -423,14 +215,14 @@ public class CompilerHelperGenerator extends AbstractGenerator {
 			GeneratorPlugin
 					.displayDialog(
 							null,
-							"JETException : an error occured during editor generation. See error logs for more details.",
+							"JETException : an error occured during helper generation. See error logs for more details.",
 							IStatus.ERROR);
 		} catch (CoreException e) {
 			GeneratorPlugin.log(e);
 			GeneratorPlugin
 					.displayDialog(
 							null,
-							"CoreException : an error occured during editor generation. See error logs for more details.",
+							"CoreException : an error occured during helper generation. See error logs for more details.",
 							IStatus.ERROR);
 		}
 	}
@@ -452,14 +244,14 @@ public class CompilerHelperGenerator extends AbstractGenerator {
 			GeneratorPlugin
 					.displayDialog(
 							null,
-							"JETException : an error occured during editor generation. See error logs for more details.",
+							"JETException : an error occured during helper generation. See error logs for more details.",
 							IStatus.ERROR);
 		} catch (CoreException e) {
 			GeneratorPlugin.log(e);
 			GeneratorPlugin
 					.displayDialog(
 							null,
-							"CoreException : an error occured during editor generation. See error logs for more details.",
+							"CoreException : an error occured during helper generation. See error logs for more details.",
 							IStatus.ERROR);
 		}
 	}
@@ -482,7 +274,6 @@ public class CompilerHelperGenerator extends AbstractGenerator {
 					.append("/" + SOURCE_DIRECTORY + "/" + helperMethod.getAllPackagesName().replace("::", "/") + "/helper/"
 							+ CodeGenUtil.capName(helperMethod.getParentMethodFromModel().getName()) + "_" + helperMethod.getId()
 							+ "_Helper.java"), configuration.isForceOverwrite());
-			System.out.println("tttttttttttttttoooooooooooo");
 			}
 		}
 	}
@@ -495,28 +286,6 @@ public class CompilerHelperGenerator extends AbstractGenerator {
 			for (EClassifier eClassifier : epackage.getEClassifiers()) {
 				if (eClassifier instanceof EClass) {
 					EClass eClass = (EClass) eClassifier;
-					/*
-					 * //for(EOperation eop : eClass.getEOperations()) { String
-					 * eClassQName =
-					 * EcoreHelper.getQualifiedName((ENamedElement) eClass);
-					 * 
-					 * TypeDefinition eClassTypeDef =
-					 * kmUnit.getTypeDefinitionByQualifiedName(eClassQName);
-					 * 
-					 * ClassDefinition cd = (ClassDefinition) eClassTypeDef;
-					 * for(Operation op : cd.getOwnedOperation()) {
-					 * 
-					 * if(op!=null && op instanceof Operation) { Type opType =
-					 * ((Operation) op).getType(); if( opType instanceof
-					 * VoidType || opType instanceof StringLiteral) { EOperation
-					 * eop = (EOperation)
-					 * km2ecoreGen.getKm2ecoremapping().get(op);
-					 * applyTemplate(eop, getTemplateURI(RUNNER_JAVA),
-					 * projectPath.append("/" + SOURCE_DIRECTORY + "/" +
-					 * "kermeta" + "/runner/" +
-					 * CodeGenUtil.capName(EcoreHelper.getQualifiedName((ENamedElement)
-					 * eop, "__")) + "_Runner.java"), conf.isForceOverwrite()); } } }
-					 */
 
 					for (EOperation eop : eClass.getEOperations()) {
 						if (isRunnable(eop)) {
@@ -575,15 +344,13 @@ public class CompilerHelperGenerator extends AbstractGenerator {
 	 * @return
 	 */
 	private boolean isRunnable(EOperation eop) {
-		boolean res = false;
 		for (EParameter eparam : eop.getEParameters()) {
 			if (eparam.getEType().getName().equals("String")) {
-				res = true;
+				return true;
 			} else {
 				return false;
 			}
 		}
-		res = true;
-		return res;
+		return true;
 	}
 }
