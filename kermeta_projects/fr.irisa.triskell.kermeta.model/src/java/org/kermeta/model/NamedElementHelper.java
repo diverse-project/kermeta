9ea71404b6a7d79e1164bd01d2ec40c3a2770db6
ftanguy <ftanguy@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2007-11-21 13:14:14 +0000

    Creating model helpers. We should move helpers from IO plugin to this one.
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@4511 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 53028201885f999ad8d7fae7023374ffc028daf3

---
+++ kermeta_projects/fr.irisa.triskell.kermeta.model/src/java/org/kermeta/model/NamedElementHelper.java
@@ -0,0 +1,45 @@
+
+
+/*$Id: NamedElementHelper.java,v 1.1 2007-11-21 13:14:14 ftanguy Exp $
+* Project : fr.irisa.triskell.kermeta.model
+* File : 	NamedElementHelper.java
+* License : EPL
+* Copyright : IRISA / INRIA / Universite de Rennes 1
+* ----------------------------------------------------------------------------
+* Creation date : 9 nov. 07
+* Authors : paco
+*/
+
+package org.kermeta.model;
+
+import fr.irisa.triskell.kermeta.language.structure.NamedElement;
+
+public class NamedElementHelper {
+
+	final static private String DEFAULT_SEPARATOR = "::";
+	
+	/**
+	 * 
+	 * @param element
+	 * @return
+	 */
+	static public String qualifiedName(NamedElement element) {
+		return qualifiedName(element, DEFAULT_SEPARATOR);
+	}
+	
+	/**
+	 * 
+	 * @param element
+	 * @param separator string that separates two elements' name
+	 * @return
+	 */
+	static public String qualifiedName(NamedElement element, String separator) {
+		if (element == null) return "";
+		if (element.eContainer() != null && element.eContainer() instanceof NamedElement)
+			return qualifiedName( (NamedElement)element.eContainer() ) + "::" + element.getName();
+		else return element.getName();	
+	}
+	
+}
+
+
