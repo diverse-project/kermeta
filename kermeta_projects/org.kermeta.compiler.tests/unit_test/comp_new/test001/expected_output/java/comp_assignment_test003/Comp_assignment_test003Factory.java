39303137eb5db10bbb53e3fdb5574048b07871bf
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2007-11-22 09:54:20 +0000

    initial revision
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@4532 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: d13af324e51e27c143e027d53993eccb41cc0175

---
+++ kermeta_projects/org.kermeta.compiler.tests/unit_test/comp_new/test001/expected_output/java/comp_assignment_test003/Comp_assignment_test003Factory.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright: IRISA/INRIA/Universite de Rennes 1 - License: EPL - Web site: http://www.kermeta.org
+ *
+ * $Id: Comp_assignment_test003Factory.java,v 1.1 2007-11-22 09:54:20 cfaucher Exp $
+ */
+package comp_assignment_test003;
+
+import org.eclipse.emf.ecore.EFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Factory</b> for the model.
+ * It provides a create method for each non-abstract class of the model.
+ * <!-- end-user-doc -->
+ * @see comp_assignment_test003.Comp_assignment_test003Package
+ * @generated
+ */
+public interface Comp_assignment_test003Factory extends EFactory
+{
+  /**
+   * The singleton instance of the factory.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  Comp_assignment_test003Factory eINSTANCE = comp_assignment_test003.impl.Comp_assignment_test003FactoryImpl.init();
+
+  /**
+   * Returns a new object of class '<em>Main</em>'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return a new object of class '<em>Main</em>'.
+   * @generated
+   */
+  Main createMain();
+
+  /**
+   * Returns a new object of class '<em>A</em>'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return a new object of class '<em>A</em>'.
+   * @generated
+   */
+  A createA();
+
+  /**
+   * Returns the package supported by this factory.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the package supported by this factory.
+   * @generated
+   */
+  Comp_assignment_test003Package getComp_assignment_test003Package();
+
+} //Comp_assignment_test003Factory
