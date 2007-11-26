f9d5ee92d81d8ce9ef91792a632296aa8e2c1c6f
Didier Vojtisek <didier.vojtisek@inria.fr>
2007-11-26 12:51:15 +0000

    automatically creates the runtime project (do not crash anymore for that reason)
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@4550 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: d273371f6972acbebcbc1a004ae20ccd899c3546

--- uml2_projects/org.kermeta.uml2.tests/src/org/kermeta/uml2/tests/loading/LoadingTestSuite.java
+++ uml2_projects/org.kermeta.uml2.tests/src/org/kermeta/uml2/tests/loading/LoadingTestSuite.java
@@ -1,10 +1,15 @@
-/* $Id: LoadingTestSuite.java,v 1.3 2007-10-30 14:17:53 ftanguy Exp $
- * Project : Kermeta.interpreter
- * File : JunitTestSuite.java
+/* $Id: LoadingTestSuite.java,v 1.4 2007-11-26 12:51:15 dvojtise Exp $
+ * Project : Kermeta MDK UML2
+ * File : LoadingTestSuite.java
  * License : EPL
  * Copyright : IRISA / INRIA / Universite de Rennes 1 
  */
 package org.kermeta.uml2.tests.loading;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -12,17 +17,28 @@ import junit.framework.TestSuite;
 import fr.irisa.triskell.kermeta.launcher.RunJunitFactory;
 
 /**
- * Interpreter test suite.
- * @author Franck Fleurey
+ * UML2 Loading test suite.
  */
 public class LoadingTestSuite extends TestSuite {
-
+	static final String RUNTIMEPROJECTNAME = "kermeta_mdk.uml2.tests";
+    
 	public static Test suite() {
 	    return new LoadingTestSuite();
 	}    
      
 	public LoadingTestSuite() {
 		super();
+		
+		// ensure projects are created in the workbench
+		IProject project =
+			ResourcesPlugin.getWorkspace().getRoot().getProject(RUNTIMEPROJECTNAME);
+		try{
+			project.create( new NullProgressMonitor() );		
+			project.open( new NullProgressMonitor() );
+		} 
+		catch (CoreException e) {
+			e.printStackTrace();
+		}
 
 /*** BEGIN GENERATED TESTS ***/
 		testWithFile("test/kmt_testcases","001_testLoadingBankModel.main.kmt" );
@@ -38,7 +54,7 @@ public class LoadingTestSuite extends TestSuite {
 		String uri = null;
 		uri = "platform:/plugin/org.kermeta.uml2.tests/" + dir + "/" + file;
 		
-		Test tests = new RunJunitFactory("platform:/resource/org.kermeta.mdk.tests/.bin").addTestsForUnit(uri);
+		Test tests = new RunJunitFactory("platform:/resource/"+RUNTIMEPROJECTNAME+"/.bin").addTestsForUnit(uri);
 		addTest( tests );
 	}
 	
