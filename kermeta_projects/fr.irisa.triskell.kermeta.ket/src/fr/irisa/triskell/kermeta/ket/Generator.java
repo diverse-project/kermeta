80c173897178950b0bdb7993ba63cbdbe6803317
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2007-12-03 13:44:22 +0000

    enable multiple selections
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@4644 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 37f41b41c6ed2a0dcbfe42fa0bcd7615502ef95f

--- kermeta_projects/fr.irisa.triskell.kermeta.ket/src/fr/irisa/triskell/kermeta/ket/Generator.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.ket/src/fr/irisa/triskell/kermeta/ket/Generator.java
@@ -1,4 +1,4 @@
-/* $Id: Generator.java,v 1.3 2007-09-05 15:16:01 cfaucher Exp $
+/* $Id: Generator.java,v 1.4 2007-12-03 13:44:22 cfaucher Exp $
  * Project    : fr.irisa.triskell.kermeta.ket
  * File       : Generator.java
  * License    : EPL
@@ -18,10 +18,13 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 
 import org.eclipse.core.filebuffers.FileBuffers;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
@@ -44,7 +47,17 @@ import fr.irisa.triskell.kermeta.ket.parser.KetParser;
 import fr.irisa.triskell.kermeta.ket.parser.KetTagLibrary;
 
 public class Generator implements IObjectActionDelegate{
- 
+
+	/**
+	 * Current selection of files
+	 */
+	private StructuredSelection currentSelection;
+	
+	/**
+	 * List of kmttffiles to compile into kmt
+	 */
+	private List<IFile> kmttfiles;
+	
 	private  final class NullTagLibraryResolver implements
 			ITagLibraryResolver {
 
@@ -75,28 +88,25 @@ public class Generator implements IObjectActionDelegate{
 		}
 
 	};
-	private StructuredSelection currentSelection;
-	private IFile kmttfile;
 
-	
-	
-	/**
-	 * @param args
-	 */
+
 	 public Generator(){
-		 
 	 }
 	 
-	 public void generate(){
+	 /**
+	  * 
+	  * @param kmttfile
+	  */
+	 public void generate( IFile kmttfile ){
 		 IJETParser p = new KetParser(nullTemplateResolver, new NullTagLibraryResolver() ,new HashMap());
-		 String outputURI = this.kmttfile.getLocation().removeFileExtension().addFileExtension("kmt").toOSString();
+		 String outputURI = kmttfile.getLocation().removeFileExtension().addFileExtension("kmt").toOSString();
 		 try {
-			FileBuffers.getTextFileBufferManager().connect(this.kmttfile.getLocation(),null);
+			FileBuffers.getTextFileBufferManager().connect(kmttfile.getLocation(),null);
 		} catch (CoreException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		 String s = FileBuffers.getTextFileBufferManager().getTextFileBuffer(this.kmttfile.getLocation()).getDocument().get();
+		 String s = FileBuffers.getTextFileBufferManager().getTextFileBuffer(kmttfile.getLocation()).getDocument().get();
 		 
 			 JETCompilationUnit cu = (JETCompilationUnit) p.parse(s.toCharArray());
 			// 
@@ -113,40 +123,47 @@ public class Generator implements IObjectActionDelegate{
 			 fo.close();
 			 
 	 }
-	 
 
-
-
+	/**
+	 * 
+	 */
 	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
 		// TODO Auto-generated method stub
 		
 	}
 
-
-
+	/**
+	 * 
+	 */
 	public void run(IAction action) {
 		Shell shell = new Shell();
-		this.generate();   
-		try {
-			this.kmttfile.getParent().refreshLocal(IResource.DEPTH_ONE, null);
-		} catch (CoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		for(IFile kmttfile : this.kmttfiles) {
+			generate(kmttfile);   
+			try {
+				kmttfile.getParent().refreshLocal(IResource.DEPTH_ONE, null);
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
 	}
 
-
-
+	/**
+	 * 
+	 */
 	public void selectionChanged(IAction action, ISelection selection) {
 		if (selection instanceof StructuredSelection)
 		{
-			// the selection should be a single *.kmtt file
+			kmttfiles = new ArrayList<IFile>();
+			// the selection should be a single *.kmtt file or multiple
 			currentSelection = (StructuredSelection)selection;
 			Iterator it = currentSelection.iterator();
 
 			while(it.hasNext()) {
-				kmttfile = (IFile)it.next();
+				IResource res = (IResource) it.next();
+				if( res instanceof IFile ) {
+					this.kmttfiles.add((IFile)res);
+				}
 			}
 
 		}
