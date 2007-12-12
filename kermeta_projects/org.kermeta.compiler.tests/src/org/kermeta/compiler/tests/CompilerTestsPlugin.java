78744656325f9b6e18415b79ed6fb4b82f67fbb9
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2007-12-12 13:49:28 +0000

    avoid many problems during the checking out of this project
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@4782 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 8f6f39b79c5bb9c35816b3044cea9dfe71d2b74a

---
+++ kermeta_projects/org.kermeta.compiler.tests/src/org/kermeta/compiler/tests/CompilerTestsPlugin.java
@@ -0,0 +1,21 @@
+/*$Id: CompilerTestsPlugin.java,v 1.1 2007-12-12 13:49:27 cfaucher Exp $
+* Project : org.kermeta.compiler.tests
+* File : 	CompilerTestsPlugin.java
+* License : EPL
+* Copyright : IRISA / INRIA / Universite de Rennes 1
+* ----------------------------------------------------------------------------
+* Creation date : 27 nov. 07
+* Authors : Cyril Faucher <cfaucher@irisa.fr>
+*/
+
+package org.kermeta.compiler.tests;
+
+import org.eclipse.core.runtime.Plugin;
+
+public class CompilerTestsPlugin extends Plugin {
+
+	public CompilerTestsPlugin() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
