232a96670053ad79a67e9c1ca29d8ccc8aef64c1
vmahe <vmahe@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2007-12-13 10:33:28 +0000

    adding former fr.irisa.triskell.uml2 tests
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@4801 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 9bb21d8ff83f7b9801f0f6ccbff4b6df90d682d3

---
+++ uml2_projects/org.kermeta.uml2.tests/src/org/kermeta/uml2/tests/GenerateJUnitTestSuite.java
@@ -0,0 +1,30 @@
+/* $Id: GenerateJUnitTestSuite.java,v 1.1 2007-12-13 10:33:28 vmahe Exp $
+ * Project : Kermeta.interpreter
+ * File : GenerateJUnitTestSuite.java
+ * License : GPL
+ * Copyright : IRISA / INRIA / Universite de Rennes 1
+ * ----------------------------------------------------------------------------
+ * Creation date : 16 févr. 2005
+ * Authors : 
+ * 		Franck FLEUREY (ffleurey@irisa.fr)
+ * Description :  	
+ * 	see class javadoc.	 
+ */
+package org.kermeta.uml2.tests;
+
+
+/**
+ * Generate or Regenerate the JunitTestSuite by looking for *.main.kmt files in the test folder.
+ * @author Franck Fleurey
+ */
+public class GenerateJUnitTestSuite extends kermeta_interpreter.jutils.GenerateJUnitTestSuite{
+	
+	public static void main(String[] args) {
+		String[] newfolders = {"test/kmt_testcases"} ;
+		String[] newfiletypes = {"kmt"};
+		folders = newfolders;
+		filetypes = newfiletypes;
+		junitfile = "src/org/kermeta/uml2/tests/loading/LoadingTestSuite.java";
+		genericmain(args);
+	}
+}
--- uml2_projects/org.kermeta.uml2.tests/src/org/kermeta/uml2/tests/loading/LoadingTestSuite.java
+++ uml2_projects/org.kermeta.uml2.tests/src/org/kermeta/uml2/tests/loading/LoadingTestSuite.java
@@ -1,4 +1,4 @@
-/* $Id: LoadingTestSuite.java,v 1.4 2007-11-26 12:51:15 dvojtise Exp $
+/* $Id: LoadingTestSuite.java,v 1.5 2007-12-13 10:33:28 vmahe Exp $
  * Project : Kermeta MDK UML2
  * File : LoadingTestSuite.java
  * License : EPL
@@ -42,10 +42,69 @@ public class LoadingTestSuite extends TestSuite {
 
 /*** BEGIN GENERATED TESTS ***/
 		testWithFile("test/kmt_testcases","001_testLoadingBankModel.main.kmt" );
+
 		testWithFile("test/kmt_testcases","002_testLoadingImprovedVehiclesModel.main.kmt" );
+
 		testWithFile("test/kmt_testcases","003_testLoadingGOFStatePatternModel.main.kmt" );
+
 		testWithFile("test/kmt_testcases","004_testLoadingSimulatorDesignModel.main.kmt" );
+
 		testWithFile("test/kmt_testcases","005_testLoadingSimpleBehavioredClassModel.main.kmt" );
+
+		testWithFile("test/kmt_testcases","101test_AbstractClass.main.kmt" );
+
+		testWithFile("test/kmt_testcases","102test_ClassInheritance.main.kmt" );
+
+		testWithFile("test/kmt_testcases","102test_Composition.main.kmt" );
+
+		testWithFile("test/kmt_testcases","103test_MultipleReference.main.kmt" );
+
+		testWithFile("test/kmt_testcases","103test_Reference.main.kmt" );
+
+		testWithFile("test/kmt_testcases","104test_OperationParameter.main.kmt" );
+
+		testWithFile("test/kmt_testcases","104test_OperationReturn.main.kmt" );
+
+		testWithFile("test/kmt_testcases","105test_ExternalInheritance.main.kmt" );
+
+		testWithFile("test/kmt_testcases","500test_Tool_Generalization.main.kmt" );
+
+		testWithFile("test/kmt_testcases","501test_Tool_Interface.main.kmt" );
+
+		testWithFile("test/kmt_testcases","501test_Tool_InterfaceAbstract.main.kmt" );
+
+		testWithFile("test/kmt_testcases","502test_Tool_Accessor.main.kmt" );
+
+		testWithFile("test/kmt_testcases","502test_Tool_Accessor_Public.main.kmt" );
+
+		testWithFile("test/kmt_testcases","503test_Tool_Transition.main.kmt" );
+
+		testWithFile("test/kmt_testcases","504test_Tool_Context4States.main.kmt" );
+
+		testWithFile("test/kmt_testcases","505test_Tool_Association.main.kmt" );
+
+		testWithFile("test/kmt_testcases","601test_Tool_GOFStatePattern_apply.main.kmt" );
+
+		testWithFile("test/kmt_testcases","601test_Tool_GOFStatePattern_visitors.main.kmt" );
+
+		testWithFile("test/kmt_testcases","701test_CloneTool_Package.main.kmt" );
+
+		testWithFile("test/kmt_testcases","702test_CloneTool_Class.main.kmt" );
+
+		testWithFile("test/kmt_testcases","703test_CloneTool_Operation.main.kmt" );
+
+		testWithFile("test/kmt_testcases","704test_CloneTool_Parameter.main.kmt" );
+
+		testWithFile("test/kmt_testcases","705test_CloneTool_Property.main.kmt" );
+
+		testWithFile("test/kmt_testcases","706test_CloneTool_ValueSpecification.main.kmt" );
+
+		testWithFile("test/kmt_testcases","707test_CloneTool_Association.main.kmt" );
+
+		testWithFile("test/kmt_testcases","708test_CloneTool_DataType.main.kmt" );
+
+		testWithFile("test/kmt_testcases","750test_CloneTool_StaticDiagram.main.kmt" );
+
 /*** END GENERATED TESTS ***/
 	}
 
