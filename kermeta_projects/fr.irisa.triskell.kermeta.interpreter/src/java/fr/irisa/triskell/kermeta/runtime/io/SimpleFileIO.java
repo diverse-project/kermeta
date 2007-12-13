348a15584ba2fed74376f1adb11138ec93376e5c
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2007-12-13 09:18:57 +0000

    Refactoring of the path management. The Eclipse platform:/resource/ path are taking into account.
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@4796 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 115237785fc8c1849258f9a96ca7cc2ee4ba0550

--- kermeta_projects/fr.irisa.triskell.kermeta.interpreter/src/java/fr/irisa/triskell/kermeta/runtime/io/SimpleFileIO.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.interpreter/src/java/fr/irisa/triskell/kermeta/runtime/io/SimpleFileIO.java
@@ -1,3 +1,18 @@
+/* $Id: SimpleFileIO.java,v 1.7 2007-12-13 09:18:57 cfaucher Exp $
+ * Project: Kermeta (First iteration)
+ * File: SimpleFileIO.java
+ * License: EPL
+ * Copyright: IRISA / INRIA / Universite de Rennes 1
+ * ----------------------------------------------------------------------------
+ * Creation date: March 1, 2007
+ * Authors:
+ * 		ffleurey
+ * 		jmottu
+ * 		ftanguy
+ * 		dvojtise
+ * 		cfaucher
+ */
+
 package fr.irisa.triskell.kermeta.runtime.io;
 
 import java.io.BufferedReader;
@@ -8,19 +23,20 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.NullProgressMonitor;
 
 import fr.irisa.triskell.eclipse.resources.ResourceHelper;
 import fr.irisa.triskell.kermeta.runtime.RuntimeObject;
 import fr.irisa.triskell.kermeta.runtime.basetypes.String;
 
-
-
-
 public class SimpleFileIO {
 	
 	public static RuntimeObject fileExists(RuntimeObject filename)
     {
-		java.lang.String fname = String.getValue(filename);
+		java.lang.String fname = getOSFileLocation(String.getValue(filename));
 		File file = new File(fname);
 		if (file.exists()) return filename.getFactory().getMemory().trueINSTANCE;
 		else return filename.getFactory().getMemory().falseINSTANCE;
@@ -28,7 +44,7 @@ public class SimpleFileIO {
 	
 	public static RuntimeObject fileIsDirectory(RuntimeObject filename)
     {
-		java.lang.String fname = String.getValue(filename);
+		java.lang.String fname = getOSFileLocation(String.getValue(filename));
 		File file = new File(fname);
 		if (file.isDirectory()) return filename.getFactory().getMemory().trueINSTANCE;
 		else return filename.getFactory().getMemory().falseINSTANCE;
@@ -41,29 +57,31 @@ public class SimpleFileIO {
         try
         {
         	/*
-        	 * 
-        	 * Getting the directory
-        	 * 
+        	 * Getting the directory        	 
         	 */
-        	java.lang.String folderPath = String.getValue(filename);
-        	//convert windows delimiter into /
-    		folderPath = folderPath.replaceAll("\\\\", "/");
-        	int i = folderPath.lastIndexOf("/");
-        	folderPath = folderPath.substring(0, i);
+        	java.lang.String filePath = getOSFileLocation(String.getValue(filename));
+        	
+        	int i = filePath.lastIndexOf("/");
+        	java.lang.String folderPath = filePath.substring(0, i);
         	
         	/*
-        	 * 
-        	 * 
         	 * Checking for its existency
-        	 * 
         	 */
         	File folder = new File( folderPath );
         	if ( ! folder.exists() )
         		folder.mkdirs();
         	
-        	FileWriter fw = new FileWriter(String.getValue(filename));
+        	FileWriter fw = new FileWriter( filePath );
             fw.write(String.getValue(text));
             fw.close();
+            
+            // Refresh the content of the folder that contains the created file
+        	try {
+            	int i_folder = String.getValue(filename).lastIndexOf("/");
+				ResourceHelper.getIFolder(String.getValue(filename).substring(0, i_folder)).refreshLocal(1, new NullProgressMonitor());
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
         }
         catch(IOException e) {
         	e.printStackTrace();
@@ -78,7 +96,7 @@ public class SimpleFileIO {
         java.lang.String ligne;
        	try {
        		//convert windows delimiter into /    		
-    		java.lang.String sfileName = String.getValue(filename).replaceAll("\\\\", "/");
+    		java.lang.String sfileName = getOSFileLocation(String.getValue(filename));
 			br = new BufferedReader(new FileReader(sfileName));
 			while((ligne = br.readLine()) != null) builder.append(ligne + "\n");
 			br.close();
@@ -91,6 +109,25 @@ public class SimpleFileIO {
 		}
         RuntimeObject result = String.create(builder.toString(), filename.getFactory());
         return result;
+    }
+    
+    /**
+     * 
+     * @param fileEclipsePath
+     * @return
+     */
+    private static java.lang.String getOSFileLocation(java.lang.String fileEclipsePath) {
+    	// FIXME CF Here we did the assumption that the path is absolute (towards the Eclipse workspace) !
+    	// We should here recalculate the path of the file if filePath is a relative path !
+    	java.lang.String filePath = ResourceHelper.cleanIfNecessaryPath(fileEclipsePath);
+
+    	IPath wkspace_path = ResourcesPlugin.getWorkspace().getRoot().getLocation();
+    	filePath = wkspace_path.toString() + filePath;
+    	
+    	//convert windows delimiter into /
+    	filePath = filePath.replaceAll("\\\\", "/");
+    	
+    	return filePath;
     }
     
     /**
