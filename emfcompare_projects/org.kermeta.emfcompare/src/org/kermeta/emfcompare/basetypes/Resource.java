08cd8f1bc561fe81bc14bc0c2b41830a64e2e325
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2007-12-14 12:46:38 +0000

    Initial revision
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@4829 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 3c5a2cebe279a51726798f329bac96acc8a9a862

---
+++ emfcompare_projects/org.kermeta.emfcompare/src/org/kermeta/emfcompare/basetypes/Resource.java
@@ -0,0 +1,163 @@
+/* $Id: Resource.java,v 1.1 2007-12-14 12:46:38 cfaucher Exp $
+ * Project   : org.kermeta.emfcompare
+ * File      : Resource.java
+ * License   : EPL
+ * Copyright : IRISA / INRIA / Universite de Rennes 1
+ * ----------------------------------------------------------------------------
+ * Creation date : Jul 6, 2005
+ * Authors       : zdrey
+ */
+package org.kermeta.emfcompare.basetypes;
+
+
+import java.io.IOException;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.compare.diff.generic.DiffMaker;
+import org.eclipse.emf.compare.diff.metamodel.DiffGroup;
+import org.eclipse.emf.compare.diff.metamodel.DiffModel;
+import org.eclipse.emf.compare.match.metamodel.MatchModel;
+import org.eclipse.emf.compare.match.statistic.DifferencesServices;
+import org.eclipse.emf.compare.util.ModelUtils;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.resource.impl.URIConverterImpl;
+
+import fr.irisa.triskell.eclipse.resources.ResourceHelper;
+import fr.irisa.triskell.kermeta.runtime.RuntimeObject;
+import fr.irisa.triskell.kermeta.runtime.basetypes.Boolean;
+import fr.irisa.triskell.kermeta.runtime.basetypes.String;
+import fr.irisa.triskell.kermeta.runtime.loader.RuntimeUnit;
+import fr.irisa.triskell.kermeta.runtime.loader.RuntimeUnitLoader;
+import fr.irisa.triskell.kermeta.runtime.loader.emf.EMFRuntimeUnit;
+
+/**
+ * This class is intended to "wrap" EMF Compare into kermeta.
+ */
+public class Resource extends fr.irisa.triskell.kermeta.runtime.basetypes.Resource {
+    
+    public static RuntimeObject getDiff(RuntimeObject self, RuntimeObject param0, RuntimeObject param1) {
+		
+		// Convert the 2 uris passed as arguments into String
+		java.lang.String leftModelPath = (java.lang.String) fr.irisa.triskell.kermeta.runtime.basetypes.String.getValue(self);
+		java.lang.String rightModelPath = (java.lang.String) fr.irisa.triskell.kermeta.runtime.basetypes.String.getValue(param0);
+		java.lang.String diffModelPath = (java.lang.String) fr.irisa.triskell.kermeta.runtime.basetypes.String.getValue(param1);
+		
+		try {
+			ModelUtils.save(getDiffModel(leftModelPath, rightModelPath), ResourceHelper.cleanIfNecessaryPath(diffModelPath));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return self.getFactory().getMemory().voidINSTANCE;
+	}
+	
+	public static RuntimeObject getMatch(RuntimeObject self, RuntimeObject param0, RuntimeObject param1) {
+		
+		// Convert the 2 uris passed as arguments into String
+		java.lang.String leftModelPath = (java.lang.String) fr.irisa.triskell.kermeta.runtime.basetypes.String.getValue(self);
+		java.lang.String rightModelPath = (java.lang.String) fr.irisa.triskell.kermeta.runtime.basetypes.String.getValue(param0);
+		java.lang.String matchModelPath = (java.lang.String) fr.irisa.triskell.kermeta.runtime.basetypes.String.getValue(param1);
+		
+		try {
+			ModelUtils.save(getMatchModel(leftModelPath, rightModelPath), ResourceHelper.cleanIfNecessaryPath(matchModelPath));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return self.getFactory().getMemory().voidINSTANCE;
+	}
+	
+	private static DiffModel getDiffModel(java.lang.String leftModelPath, java.lang.String rightModelPath) {
+		final DiffModel diff = new DiffMaker().doDiff(getMatchModel(leftModelPath, rightModelPath));
+		return diff;
+	}
+	
+	
+	private static MatchModel getMatchModel(java.lang.String leftModelPath, java.lang.String rightModelPath) {
+
+		try {
+			// Register the extensions, we are not the two files are the same extension, also we try to register the two extensions
+			EMFRuntimeUnit.registerEMFextensionToFactoryMap(leftModelPath);
+			EMFRuntimeUnit.registerEMFextensionToFactoryMap(rightModelPath);
+			
+			// Loads the two models passed as arguments
+			
+			ResourceSet resourceSet = new ResourceSetImpl();
+			
+	        URI left_u = URI.createURI(leftModelPath);
+	        left_u = new URIConverterImpl().normalize(left_u);
+	        org.eclipse.emf.ecore.resource.Resource left_resource = resourceSet.getResource(left_u, true);
+	        final EObject model1 = left_resource.getContents().get(0);
+	        
+	        URI right_u = URI.createURI(rightModelPath);
+	        right_u = new URIConverterImpl().normalize(right_u);
+	        org.eclipse.emf.ecore.resource.Resource right_resource = resourceSet.getResource(right_u, true);
+	        final EObject model2 = right_resource.getContents().get(0);
+			
+			// Creates the match then the diff model for those two models
+			final MatchModel match = new DifferencesServices().modelMatch(model1, model2, new NullProgressMonitor(), null);
+			
+			return match;
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	/**
+	 * 
+	 * @param result_dir
+	 * @param result_fileName
+	 */
+	public static RuntimeObject compare(RuntimeObject self, RuntimeObject param0) {
+
+		// Convert the 2 uris passed as arguments into String
+		java.lang.String leftModelPath = (java.lang.String) fr.irisa.triskell.kermeta.runtime.basetypes.String.getValue(self);
+		java.lang.String rightModelPath = (java.lang.String) fr.irisa.triskell.kermeta.runtime.basetypes.String.getValue(param0);
+		
+		boolean res = false;
+		
+		if(((DiffGroup) getDiffModel(leftModelPath, rightModelPath).getOwnedElements().get(0)).getSubchanges()> 0) {
+			res = false;
+		} else {
+			res = true;
+		}
+		
+		// Convert the result of the method into RuntimeObject
+		if(res) return self.getFactory().getMemory().trueINSTANCE;
+		else return self.getFactory().getMemory().falseINSTANCE;
+
+	}
+	
+	
+	/**
+	 * FIXME CF unused for the moment
+	 */
+    private static RuntimeObject saveInMemory(
+    		RuntimeObject self,
+    		RuntimeObject newUri, 
+    		RuntimeObject mmUri, 
+    		RuntimeObject resourceType, 
+    		RuntimeObject instances,
+    		RuntimeObject mustValidate)
+    {
+        // runtime unit handles the transformation Kermeta2EMFInstance
+        java.lang.String str_uri = String.getValue(newUri);
+        RuntimeUnit runtime_unit = RuntimeUnitLoader.getDefaultLoader().
+        	getConcreteFactory(String.getValue(resourceType)).
+        	createRuntimeUnit(str_uri, String.getValue(mmUri), instances);
+        runtime_unit.associatedResource = self;
+        runtime_unit.setMustValidate(Boolean.getValue(mustValidate));
+	    runtime_unit.save(str_uri);
+        return instances.getFactory().getMemory().voidINSTANCE;
+    }
+	
+}
