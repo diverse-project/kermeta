b2f56d150ea66166b52a92498ee1574a0875999e
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2008-01-17 16:24:53 +0000

    added useful static methods
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@5047 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: ab8a5f2140cdc8a70e7be63eb564c93e14a4c513

--- kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/modelhelper/ModelingUnitHelper.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/modelhelper/ModelingUnitHelper.java
@@ -1,13 +1,13 @@
 
 
-/*$Id: ModelingUnitHelper.java,v 1.2 2007-07-20 15:08:10 ftanguy Exp $
+/*$Id: ModelingUnitHelper.java,v 1.3 2008-01-17 16:24:53 cfaucher Exp $
 * Project : fr.irisa.triskell.kermeta.io
 * File : 	ModelingHelper.java
 * License : EPL
 * Copyright : IRISA / INRIA / Universite de Rennes 1
 * ----------------------------------------------------------------------------
 * Creation date : 11 juil. 07
-* Authors : paco
+* Authors : Francois Tanguy ftanguy@irisa.fr
 */
 
 package fr.irisa.triskell.kermeta.modelhelper;
@@ -21,10 +21,20 @@ import fr.irisa.triskell.kermeta.language.structure.Tag;
 
 public class ModelingUnitHelper {
 
+	/**
+	 * 
+	 * @param kermetaUnit
+	 * @return
+	 */
 	static public Tag getMainOperation(KermetaUnit kermetaUnit) {
 		return getMainOperation( kermetaUnit.getModelingUnit() );
 	}
 	
+	/**
+	 * 
+	 * @param modelingUnit
+	 * @return
+	 */
 	static public Tag getMainOperation(ModelingUnit modelingUnit) {
 		for ( Tag tag : (List<Tag>) modelingUnit.getOwnedTags() ) {
 			if ( (tag.getName() != null) && tag.getName().equals("mainOperation") )
@@ -33,14 +43,72 @@ public class ModelingUnitHelper {
 		return null;
 	}
 	
+	/**
+	 * 
+	 * @param kermetaUnit
+	 * @return
+	 */
+	static public String getMainOperationAsString(KermetaUnit kermetaUnit) {
+		if( getMainOperation(kermetaUnit) != null ) {
+			return getMainOperation(kermetaUnit).getValue();
+		}
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param modelingUnit
+	 * @return
+	 */
+	static public String getMainOperationAsString(ModelingUnit modelingUnit) {
+		if( getMainOperation(modelingUnit) != null ) {
+			return getMainOperation(modelingUnit).getValue();
+		}
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param kermetaUnit
+	 * @return
+	 */
 	static public Tag getMainClass(KermetaUnit kermetaUnit) {
 		return getMainClass( kermetaUnit.getModelingUnit() );
 	}
 	
+	/**
+	 * 
+	 * @param modelingUnit
+	 * @return
+	 */
 	static public Tag getMainClass(ModelingUnit modelingUnit) {
 		for ( Tag tag : (List<Tag>) modelingUnit.getOwnedTags() ) {
 			if ( (tag.getName() != null) && tag.getName().equals("mainClass") )
 				return tag;
+		}
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param kermetaUnit
+	 * @return
+	 */
+	static public String getMainClassAsString(KermetaUnit kermetaUnit) {
+		if( getMainClass(kermetaUnit) != null ) {
+			return getMainClass(kermetaUnit).getValue();
+		}
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param modelingUnit
+	 * @return
+	 */
+	static public String getMainClassAsString(ModelingUnit modelingUnit) {
+		if( getMainClass(modelingUnit) != null ) {
+			return getMainClass(modelingUnit).getValue();
 		}
 		return null;
 	}
