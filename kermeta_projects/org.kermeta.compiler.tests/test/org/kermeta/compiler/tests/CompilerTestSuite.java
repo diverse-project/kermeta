25c2c1a492fc741197b08fdf711fb4fd4e55c7b5
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2008-02-06 12:55:19 +0000

    comments
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@5241 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 081d2959fbcd067cdc69ec6f09fd99301e317da7

--- kermeta_projects/org.kermeta.compiler.tests/test/org/kermeta/compiler/tests/CompilerTestSuite.java
+++ kermeta_projects/org.kermeta.compiler.tests/test/org/kermeta/compiler/tests/CompilerTestSuite.java
@@ -1,13 +1,16 @@
 
 
-/*$Id: CompilerTestSuite.java,v 1.4 2008-02-06 12:39:18 cfaucher Exp $
+/*$Id: CompilerTestSuite.java,v 1.5 2008-02-06 12:55:19 cfaucher Exp $
 * Project : org.kermeta.compiler.tests
 * File : 	CompilerTestSuite.java
 * License : EPL
 * Copyright : IRISA / INRIA / Universite de Rennes 1
 * ----------------------------------------------------------------------------
 * Creation date : 30 janv. 08
-* Authors : paco
+* Authors : paco Francois Tanguy <ftanguy@irisa.fr>
+* 			Cyril Faucher <cfaucher@irisa.fr>
+* 
+* This class registers the test suite dedicated to the Kermeta compiler like the structure and behavior parts
 */
 
 package org.kermeta.compiler.tests;
