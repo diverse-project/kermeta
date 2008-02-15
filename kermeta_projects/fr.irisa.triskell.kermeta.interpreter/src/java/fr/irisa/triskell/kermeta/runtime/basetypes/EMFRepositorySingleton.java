df9d160fd8f34b91c615b2290432ad894c0e8d14
Didier Vojtisek <didier.vojtisek@inria.fr>
2008-02-15 14:23:56 +0000

    refactory in order to support UML loader without adding an UML dependency to Kermeta
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@5367 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: decca602bba6d19baa8bda2979aa19ab0cd1b6a9

---
+++ kermeta_projects/fr.irisa.triskell.kermeta.interpreter/src/java/fr/irisa/triskell/kermeta/runtime/basetypes/EMFRepositorySingleton.java
@@ -0,0 +1,109 @@
+/*$Id: EMFRepositorySingleton.java,v 1.1 2008-02-15 14:23:56 dvojtise Exp $
+* Project : fr.irisa.triskell.kermeta.interpreter
+* File : 	RepositorySingleton.java
+* License : EPL
+* Copyright : IRISA / INRIA 
+* ----------------------------------------------------------------------------
+* Creation date : 13 févr. 08
+* Authors : 
+*      Didier Vojtisek <dvojtise@irisa.fr>
+*/
+
+package fr.irisa.triskell.kermeta.runtime.basetypes;
+
+import java.util.ArrayList;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.URIConverter;
+import org.eclipse.emf.ecore.resource.impl.URIConverterImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+
+import fr.irisa.triskell.kermeta.runtime.RuntimeObject;
+import fr.irisa.triskell.kermeta.runtime.factory.RuntimeObjectFactory;
+
+/**
+ * Singleton that contains the actual code for Repository
+ * (necessary in order to provide dynamic binding and inheritance)
+ *
+ */
+public class EMFRepositorySingleton {
+
+	/* singleton implementation */
+	protected static EMFRepositorySingleton EMFRepositorySingletonInstance = new EMFRepositorySingleton();	
+	protected EMFRepositorySingleton(){		
+	}
+	public static EMFRepositorySingleton getSingleton(){
+		return EMFRepositorySingletonInstance;
+	}
+	
+	/**
+     * @param selfRO  - RO for repository
+     * @param uriRO   - RO for the uri of the resource to be created
+     * @param mmUriRO - RO for the uri of the metamodel of the resource to be created
+     * @return        - RO for the created resource, or void RO if creation failed
+     */
+    public RuntimeObject createResource(RuntimeObject selfRO, RuntimeObject uriRO, RuntimeObject mmUriRO) {
+    	// Default value for the resource RO to be returned
+    	RuntimeObject resRO = selfRO.getFactory().getMemory().voidINSTANCE;
+    	
+    	// Init repository RO
+    	Repository.initRepository(selfRO);
+    	
+    	// Creating EMF resource for the resource to be created
+    	java.lang.String file = fr.irisa.triskell.kermeta.runtime.basetypes.String.getValue(uriRO);
+    	
+    	java.lang.String unit_uri = selfRO.getFactory().getMemory().getUnit().getUri();
+    	java.lang.String unit_uripath = unit_uri.substring(0, unit_uri.lastIndexOf("/")+1);
+    	URI u = URI.createURI(file);
+    	if (u.isRelative()) {
+    		URIConverter c = new URIConverterImpl();
+    		u = u.resolve(c.normalize(URI.createURI(unit_uripath)));    			
+    	}
+    	
+    	if ( u.fileExtension() != null ) {
+    		
+    		Object o = Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().get(u.fileExtension());
+    		if ( o == null )
+    			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(u.fileExtension(), new XMIResourceFactoryImpl());
+    		
+    		/*if ( ! u.fileExtension().equals("ecore") && ! u.fileExtension().equals("km") )
+    			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+    					u.fileExtension(),
+    					new XMIResourceFactoryImpl()
+    			);*/
+    	}
+    	
+    	ResourceSet rSet = (ResourceSet) selfRO.getR2eEmfResourceset();
+    	Resource res = null;
+    	if (rSet != null)
+    		res = rSet.createResource(u);
+    	
+    	if(res != null) {
+        	// Create the resource RO
+        	resRO = createRuntimeObjectFromResource(res, selfRO, mmUriRO);
+        	
+        	// Insert created resource RO in the list of "resources" of the repository RO
+        	RuntimeObject resListRO = selfRO.getProperties().get("resources"); 
+        	ArrayList<RuntimeObject> l = fr.irisa.triskell.kermeta.runtime.basetypes.Collection.getArrayList(resListRO);
+        	l.add(resRO);
+    	}
+        
+    	return resRO;
+    }
+    
+    /**
+     * method used to factorize some code between this class and classes that refines it
+     * this method creates the concrete RuntimeObject for the resource (by default an EMFResource)
+     * @param emfRes
+     * @param repRO
+     * @param mmUriRO
+     * @return
+     */
+    protected RuntimeObject createRuntimeObjectFromResource(Resource emfRes, RuntimeObject repRO, RuntimeObject mmUriRO)
+    {
+    	return repRO.getFactory().createRuntimeObjectFromResource(emfRes, repRO, mmUriRO, RuntimeObjectFactory.EMFRESOURCE_QUALIFIED_NAME);
+    	
+    }
+}
--- kermeta_projects/fr.irisa.triskell.kermeta.interpreter/src/java/fr/irisa/triskell/kermeta/runtime/basetypes/Repository.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.interpreter/src/java/fr/irisa/triskell/kermeta/runtime/basetypes/Repository.java
@@ -1,4 +1,4 @@
-/* $Id: Repository.java,v 1.8 2007-12-13 08:29:27 ftanguy Exp $
+/* $Id: Repository.java,v 1.9 2008-02-15 14:23:56 dvojtise Exp $
  * Project   : Kermeta (First iteration)
  * File      : Repository.java
  * License   : EPL
@@ -10,15 +10,11 @@
 package fr.irisa.triskell.kermeta.runtime.basetypes;
 
 
-import java.util.ArrayList;
-
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.resource.impl.URIConverterImpl;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 
 import fr.irisa.triskell.kermeta.language.structure.GenericTypeDefinition;
 import fr.irisa.triskell.kermeta.language.structure.StructureFactory;
@@ -33,58 +29,15 @@ import fr.irisa.triskell.kermeta.runtime.RuntimeObjectImpl;
 public class Repository {
     
     /**
-     * @param selfRO  - RO for respository
+     * @param selfRO  - RO for repository
      * @param uriRO   - RO for the uri of the resource to be created
      * @param mmUriRO - RO for the uri of the metamodel of the resource to be created
      * @return        - RO for the created resource, or void RO if creation failed
      */
     public static RuntimeObject createResource(RuntimeObject selfRO, RuntimeObject uriRO, RuntimeObject mmUriRO) {
-    	// Default value for the resource RO to be returned
-    	RuntimeObject resRO = selfRO.getFactory().getMemory().voidINSTANCE;
     	
-    	// Init repository RO
-    	Repository.initRepository(selfRO);
-    	
-    	// Creating EMF resource for the resource to be created
-    	java.lang.String file = fr.irisa.triskell.kermeta.runtime.basetypes.String.getValue(uriRO);
-    	
-    	java.lang.String unit_uri = selfRO.getFactory().getMemory().getUnit().getUri();
-    	java.lang.String unit_uripath = unit_uri.substring(0, unit_uri.lastIndexOf("/")+1);
-    	URI u = URI.createURI(file);
-    	if (u.isRelative()) {
-    		URIConverter c = new URIConverterImpl();
-    		u = u.resolve(c.normalize(URI.createURI(unit_uripath)));    			
-    	}
-    	
-    	if ( u.fileExtension() != null ) {
-    		
-    		Object o = Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().get(u.fileExtension());
-    		if ( o == null )
-    			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(u.fileExtension(), new XMIResourceFactoryImpl());
-    		
-    		/*if ( ! u.fileExtension().equals("ecore") && ! u.fileExtension().equals("km") )
-    			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
-    					u.fileExtension(),
-    					new XMIResourceFactoryImpl()
-    			);*/
-    	}
-    	
-    	ResourceSet rSet = (ResourceSet) selfRO.getR2eEmfResourceset();
-    	Resource res = null;
-    	if (rSet != null)
-    		res = rSet.createResource(u);
-    	
-    	if(res != null) {
-        	// Create the resource RO
-        	resRO = selfRO.getFactory().createRuntimeObjectFromResource(res, selfRO, mmUriRO);
-        	
-        	// Insert created resource RO in the list of "resources" of the repository RO
-        	RuntimeObject resListRO = selfRO.getProperties().get("resources"); 
-        	ArrayList<RuntimeObject> l = fr.irisa.triskell.kermeta.runtime.basetypes.Collection.getArrayList(resListRO);
-        	l.add(resRO);
-    	}
-        
-    	return resRO;
+    	// default implemetation use EMF and the default singleton        
+    	return EMFRepositorySingleton.getSingleton().createResource(selfRO, uriRO, mmUriRO);
     }
 
 
