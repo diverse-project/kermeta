8f9108f1025f212400b489bcbb3fe2c53c393f43
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2008-02-18 08:49:27 +0000

    avoid the Kermeta build failed
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@5380 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 69ac136602222b24676818eab7828f8c23c8cc3d

--- samples.fsm_projects/fr.irisa.triskell.kermeta.samples.fsm.tests/src/fr/irisa/triskell/kermeta/samples/fsm/tests/launcher/Determinization.java
+++ samples.fsm_projects/fr.irisa.triskell.kermeta.samples.fsm.tests/src/fr/irisa/triskell/kermeta/samples/fsm/tests/launcher/Determinization.java
@@ -1,4 +1,4 @@
-/* $Id: Determinization.java,v 1.5 2008-01-08 09:19:42 dvojtise Exp $
+/* $Id: Determinization.java,v 1.6 2008-02-18 08:49:27 cfaucher Exp $
  * Project   : fr.irisa.triskell.kermeta.fsm.test
  * File      : Determinization.java
  * License   : EPL
@@ -9,15 +9,19 @@
  */
 package fr.irisa.triskell.kermeta.samples.fsm.tests.launcher;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Path;
 import org.junit.Assert;
 import org.junit.Test;
+import org.kermeta.interpreter.helper.RunnerHelper;
 
 import fr.irisa.triskell.eclipse.console.EclipseConsole;
 import fr.irisa.triskell.eclipse.console.IOConsole;
+import fr.irisa.triskell.kermeta.exceptions.NotRegisteredURIException;
+import fr.irisa.triskell.kermeta.exceptions.URIMalformedException;
 import fr.irisa.triskell.kermeta.launcher.KermetaInterpreter;
 import fr.irisa.triskell.kermeta.runtime.RuntimeObject;
 
@@ -32,18 +36,31 @@ public class Determinization {
 		
 		String file = "platform:/resource/fr.irisa.triskell.kermeta.samples.fsm.demo/launcher/determinization.kmt";
 		
-		KermetaInterpreter interpreter = new KermetaInterpreter(file, null, true);
-		IOConsole console = new EclipseConsole("Determinization");
-		
-		interpreter.setKStream(console);
-		ArrayList<RuntimeObject> parameters = new ArrayList<RuntimeObject> ();
-		parameters.add(fr.irisa.triskell.kermeta.runtime.basetypes.String.create(
-				"platform:/resource/fr.irisa.triskell.kermeta.samples.fsm.demo/models/sampletodeterminize.fsm", interpreter.getMemory().getROFactory()));
-		interpreter.setEntryParameters(parameters);
-		interpreter.launch();
-		
-		Assert.assertTrue( ResourcesPlugin.getWorkspace().getRoot().getFile( new Path("/fr.irisa.triskell.kermeta.samples.fsm.demo/models/sampletodeterminize.det.fsm") ).exists() );
-		
+		KermetaInterpreter interpreter;
+		try {
+			interpreter = new KermetaInterpreter(RunnerHelper.getKermetaUnitToExecute(file), null);
+			
+			IOConsole console = new EclipseConsole("Determinization");
+			
+			interpreter.setKStream(console);
+			ArrayList<RuntimeObject> parameters = new ArrayList<RuntimeObject> ();
+			parameters.add(fr.irisa.triskell.kermeta.runtime.basetypes.String.create(
+					"platform:/resource/fr.irisa.triskell.kermeta.samples.fsm.demo/models/sampletodeterminize.fsm", interpreter.getMemory().getROFactory()));
+			interpreter.setEntryParameters(parameters);
+			interpreter.launch();
+			
+			Assert.assertTrue( ResourcesPlugin.getWorkspace().getRoot().getFile( new Path("/fr.irisa.triskell.kermeta.samples.fsm.demo/models/sampletodeterminize.det.fsm") ).exists() );
+			
+		} catch (NotRegisteredURIException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URIMalformedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
