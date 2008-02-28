f1a76889070c5b03691c76931aa3eb346d5406b7
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2008-02-28 17:18:52 +0000

    initial revision
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@5518 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: b2422dfd5bd3188db4bec1fbb26c06be4f9e5271

---
+++ emfutils_projects/org.kermeta.emfutils.models.ui/src/org/kermeta/emfutils/models/ui/Activator.java
@@ -0,0 +1,62 @@
+/* $Id: Activator.java,v 1.1 2008-02-28 17:18:52 cfaucher Exp $
+ * Project    : org.kermeta.emfutils.models.ui
+ * File       : Activator.java
+ * License    : EPL
+ * Copyright  : IRISA / INRIA / Universite de Rennes 1
+ * -------------------------------------------------------------------
+ * Creation date : 28 feb. 2008
+ * Authors : 
+ *        Cyril Faucher <cfaucher@irisa.fr>
+ *
+ */
+
+package org.kermeta.emfutils.models.ui;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.kermeta.emfutils.models.ui";
+
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+}
---
+++ emfutils_projects/org.kermeta.emfutils.models.ui/src/org/kermeta/emfutils/models/ui/popup/actions/GetContainingRefAction.java
@@ -0,0 +1,83 @@
+/* $Id: GetContainingRefAction.java,v 1.1 2008-02-28 17:18:52 cfaucher Exp $
+ * Project    : org.kermeta.emfutils.models.ui
+ * File       : GetContainingRefAction.java
+ * License    : EPL
+ * Copyright  : IRISA / INRIA / Universite de Rennes 1
+ * -------------------------------------------------------------------
+ * Creation date : 28 feb. 2008
+ * Authors : 
+ *        Cyril Faucher <cfaucher@irisa.fr>
+ *
+ */
+
+package org.kermeta.emfutils.models.ui.popup.actions;
+
+import java.util.Iterator;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class GetContainingRefAction implements IObjectActionDelegate {
+
+	protected StructuredSelection currentSelection;
+	
+	protected EObject eObj;
+	
+	/**
+	 * Constructor for GetContainingRefAction.
+	 */
+	public GetContainingRefAction() {
+		super();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		Shell shell = new Shell();
+		
+		String text = "None containing reference.";
+		
+		if( eObj.eContainmentFeature() != null && eObj.eContainingFeature() != null ) {
+			text = "This node is contained through the reference: \n" +
+			" - " + eObj.eContainmentFeature().getName() + " (ContainmentFeature property)\n" + 
+			" - " + eObj.eContainingFeature().getName() + " (ContainingFeature property)";
+		}
+		
+		MessageDialog.openInformation(
+			shell,
+			"EMF Models Info by Kermeta",
+			text);
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		if (selection instanceof StructuredSelection) {
+			currentSelection = (StructuredSelection) selection;
+			
+			Iterator it = currentSelection.iterator();
+
+			while (it.hasNext()) {
+				Object obj = (Object) it.next();
+				if(obj instanceof EObject) {
+					eObj = (EObject) obj;
+				}
+			}
+		}
+	}
+}
