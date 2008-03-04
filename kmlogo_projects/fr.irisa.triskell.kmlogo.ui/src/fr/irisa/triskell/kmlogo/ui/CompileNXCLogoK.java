cb59b48f374dece718e9573ae2a189c78ef5cd5f
Didier Vojtisek <didier.vojtisek@inria.fr>
2008-03-04 13:07:32 +0000

    now works both in runtimeworkbench and in deployed mode
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@5567 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: e60dd8c8e942a3f858be4932a119c8decf1b06c7

--- kmlogo_projects/fr.irisa.triskell.kmlogo.ui/src/fr/irisa/triskell/kmlogo/ui/CompileNXCLogoK.java
+++ kmlogo_projects/fr.irisa.triskell.kmlogo.ui/src/fr/irisa/triskell/kmlogo/ui/CompileNXCLogoK.java
@@ -1,4 +1,4 @@
-/* $Id: CompileNXCLogoK.java,v 1.7 2008-03-04 11:12:33 dvojtise Exp $
+/* $Id: CompileNXCLogoK.java,v 1.8 2008-03-04 13:07:32 dvojtise Exp $
  * Project   : KmLogo
  * File      : CompileNXCLogoK.java
  * License   : EPL
@@ -12,13 +12,18 @@
 package fr.irisa.triskell.kmlogo.ui;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Platform;
 import org.kermeta.interpreter.helper.RunnerHelper;
 import org.kermeta.io.KermetaUnit;
+import org.osgi.framework.Bundle;
 
 import fr.irisa.triskell.eclipse.console.IOConsole;
 import fr.irisa.triskell.kermeta.exceptions.NotRegisteredURIException;
@@ -57,16 +62,30 @@ public class CompileNXCLogoK {
 		params.add(fr.irisa.triskell.kermeta.runtime.basetypes.String.create(out, inter.getMemory().getROFactory()));
 		params.add(fr.irisa.triskell.kermeta.runtime.basetypes.String.create(api, inter.getMemory().getROFactory()));
 		inter.setEntryParameters(params);
-		// And we launch the interpreter
-		
-		String jar = "platform:/plugin/fr.irisa.triskell.kmlogo.model.jar";
-		
-		//ResourcesPlugin.getPlugin().getDescriptor().getInstallURL()
 		
 		
-		URL[] urls = new URL[1];
-		urls[0] = new URL("file://" + Platform.resolve(Platform.getPlugin("fr.irisa.triskell.kmlogo.model").getDescriptor().getInstallURL()).getFile() + "bin");
-
+		// Add some URL to the classloader of the interpreter : needed if your code use some extra java classes (via extern for example)
+		// use a set for building the URL (in case some may fail due to malformed URL
+		// Note : URL must end with a / if this is a directory, if not, this is considered as a jar by the classloader
+		Set<URL> urlsSet = new LinkedHashSet<URL>();
+		// URL used when run in a runtimeworkbench, this allows to debug the plugin
+		safeAddURLAsString(urlsSet, "file://" + Platform.resolve(Platform.getPlugin("fr.irisa.triskell.kmlogo.model").getDescriptor().getInstallURL()).getFile() + "bin/");
+		// add this plugin as a deployed plugin
+		Bundle bundle = org.eclipse.core.runtime.Platform.getBundle("fr.irisa.triskell.kmlogo.model");
+		if(bundle != null){
+			urlsSet.add(FileLocator.resolve(bundle.getEntry("/")));
+			/*safeAddURLAsString(urlsSet,bundle.getLocation());*/
+			Activator.internalLog.debug("classloader updated with " +FileLocator.resolve(bundle.getEntry("/")));
+		}		
+		
+		
+		// convert the set into an array
+		URL[] urls = new URL[urlsSet.size()];
+		int i = 0;
+		for (URL url : urlsSet) {
+			urls[i] = url;
+			i++;
+		}
 		URLClassLoader cl = new URLClassLoader(urls, inter.getClass().getClassLoader());
 		Thread.currentThread().setContextClassLoader(cl);
 		inter.launch();
@@ -74,5 +93,20 @@ public class CompileNXCLogoK {
 
 	}
 
+	/**
+	 * add a new URL to the set
+	 * Doesn't fail if the URL is malformed, in that case, only a warning is raised, 
+	 * @param urlsSet : set that will contain the URL built
+	 * @param url : String of the URL to build
+	 */
+	private static void safeAddURLAsString(Set<URL> urlsSet, String url){
+		try{
+			urlsSet.add(new URL(url));
+		} catch (MalformedURLException e) {
+			Activator.logWarningMessage(
+					"problem adding an entry of the classpath, "
+					+ url + " cannot be added in classloader", e);
+		}
+	}
 
 }
