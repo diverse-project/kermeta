2169b02cd2e019a519c58d35ca339b11aa53c817
Didier Vojtisek <didier.vojtisek@inria.fr>
2008-03-04 10:57:02 +0000

    warning must go to warning in log4j
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@5564 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 6b6c60088dfc2443fc850365118006498a103a1e

--- kmlogo_projects/fr.irisa.triskell.kmlogo.ui/src/fr/irisa/triskell/kmlogo/ui/Activator.java
+++ kmlogo_projects/fr.irisa.triskell.kmlogo.ui/src/fr/irisa/triskell/kmlogo/ui/Activator.java
@@ -1,8 +1,13 @@
 package fr.irisa.triskell.kmlogo.ui;
 
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+
+import fr.irisa.triskell.kermeta.util.LogConfigurationHelper;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -12,6 +17,10 @@ public class Activator extends AbstractUIPlugin {
 	// The plug-in ID
 	public static final String PLUGIN_ID = "fr.irisa.triskell.kmlogo.ui";
 
+	// Log4j logger for this plugin
+	final static public Logger internalLog = LogConfigurationHelper.getLogger("KMLogoUI");
+
+	
 	// The shared instance
 	private static Activator plugin;
 	
@@ -57,5 +66,33 @@ public class Activator extends AbstractUIPlugin {
 	 */
 	public static ImageDescriptor getImageDescriptor(String path) {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+	
+	/**
+	 * This method logs an error message and an associated exception (as a trace)
+	 * It will post the message both in the ErrorLog view in Eclipse and in the Log4J
+	 * @param message String
+	 */
+	public static void logErrorMessage(String message, Throwable e) {
+		if (message == null)
+			message= "";
+		// eclipse logger
+		getDefault().getLog().log(new Status(IStatus.ERROR, PLUGIN_ID, IStatus.ERROR, message, e));
+		// log4j message
+		internalLog.error(message, e);
+	}
+	
+	/**
+	 * This method logs a warning message and an associated exception (as a trace)
+	 * It will post the message both in the ErrorLog view in Eclipse and in the Log4J
+	 * @param message String
+	 */
+	public static void logWarningMessage(String message, Throwable e) {
+		if (message == null)
+			message= "";
+		// eclipse logger
+		getDefault().getLog().log(new Status(IStatus.WARNING, PLUGIN_ID, IStatus.WARNING, message, e));
+		// log4j message
+		internalLog.warn(message, e);
 	}
 }
