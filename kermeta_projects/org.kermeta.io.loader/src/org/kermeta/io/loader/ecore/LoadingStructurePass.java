8eadb678c2842ed3a2d091a0436814942c1017af
ftanguy <ftanguy@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2008-03-05 07:52:42 +0000

    getting the resource that could be needed into the transformation
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@5578 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 8472b44eb43d2973f02d7b753710f9f496e3638c

--- kermeta_projects/org.kermeta.io.loader/src/org/kermeta/io/loader/ecore/LoadingStructurePass.java
+++ kermeta_projects/org.kermeta.io.loader/src/org/kermeta/io/loader/ecore/LoadingStructurePass.java
@@ -1,6 +1,6 @@
 
 
-/*$Id: LoadingStructurePass.java,v 1.2 2008-02-14 07:12:50 uid21732 Exp $
+/*$Id: LoadingStructurePass.java,v 1.3 2008-03-05 07:52:42 ftanguy Exp $
 * Project : org.kermeta.io.loader
 * File : 	LoadingStructurePass.java
 * License : EPL
@@ -17,16 +17,22 @@ import java.util.Map;
 import org.eclipse.emf.ecore.EObject;
 import org.kermeta.io.loader.action.EcoreLoadingAction;
 import org.kermeta.io.loader.datas.EcoreLoadingDatas;
+import org.eclipse.emf.ecore.resource.Resource;
 
 import fr.irisa.triskell.kermeta.loader.ecore.Ecore2KMPass1;
 
 public class LoadingStructurePass extends EcoreLoadingAction {
 
 	public void performAction(EcoreLoadingDatas datas, Map<?, ?> options) {
-		Ecore2KMPass1 pass = new Ecore2KMPass1( datas.getKermetaUnit(), datas.ecore2kmDatas, true, null, null );
+		Resource resource = null;
+		try {
+			resource = datas.contents.get(0).eResource();
+		} catch (NullPointerException e) {}
+		Ecore2KMPass1 pass = new Ecore2KMPass1( datas.getKermetaUnit(), datas.ecore2kmDatas, true, resource, null );
 		for ( EObject o : datas.contents ) {
 			pass.accept(o);
 		}
+		
 	}
 
 }
