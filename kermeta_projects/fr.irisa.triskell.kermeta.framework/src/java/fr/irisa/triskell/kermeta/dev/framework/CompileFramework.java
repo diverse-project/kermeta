be0ebbc43001428481143dbd8ddf5c05fb04a0b6
ftanguy <ftanguy@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2008-05-28 15:43:56 +0000

    generate the framework as ecore
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@6062 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 2ff021ce5e2962c1b3fd3dde35da44d0bf62a0e7

--- kermeta_projects/fr.irisa.triskell.kermeta.framework/src/java/fr/irisa/triskell/kermeta/dev/framework/CompileFramework.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.framework/src/java/fr/irisa/triskell/kermeta/dev/framework/CompileFramework.java
@@ -1,4 +1,4 @@
-/* $Id: CompileFramework.java,v 1.20 2008-05-28 09:20:45 ftanguy Exp $
+/* $Id: CompileFramework.java,v 1.21 2008-05-28 15:43:56 ftanguy Exp $
 * Project : fr.irisa.triskell.kermeta.framework
 * File : CompileFramework.java
 * License : EPL
@@ -20,6 +20,8 @@ import org.kermeta.merger.Merger;
 
 import fr.irisa.triskell.kermeta.exceptions.NotRegisteredURIException;
 import fr.irisa.triskell.kermeta.exceptions.URIMalformedException;
+import fr.irisa.triskell.kermeta.exporter.ecore.EcoreExporter;
+import fr.irisa.triskell.kermeta.exporter.ecore.ExporterOptions;
 import fr.irisa.triskell.kermeta.modelhelper.KermetaUnitHelper;
 
 
@@ -65,17 +67,17 @@ public class CompileFramework {
         	Set<KermetaUnit> l = new HashSet<KermetaUnit>();
         	l.add(kermetaUnit);
         	l.addAll( KermetaUnitHelper.getAllImportedKermetaUnits(kermetaUnit) );
-        	merger.process(l, "platform:/resource/fr.irisa.triskell.kermeta.framework/dist/framework.km", true, false);
+        	KermetaUnit mergedUnit = merger.process(l, "platform:/resource/fr.irisa.triskell.kermeta.framework/dist/framework.km", true, false);
         	System.out.println("Mergin and Saving done");
         	//System.out.println("SAVING IN KM...");
 //        	KmExporter exporter = new KmExporter();
 //        	exporter.export(kermetaUnit, dist_folder, true);
   //      	System.out.println("DONE");
 
-    /*    	System.out.println("SAVING IN ECORE...");
+        	System.out.println("SAVING IN ECORE...");
         	EcoreExporter exporter2 = new EcoreExporter();
-        	exporter2.export(kermetaUnit, dist_folder, ExporterOptions.getDefault());
-        	System.out.println("DONE");*/
+        	exporter2.export(mergedUnit, dist_folder, ExporterOptions.getDefault());
+        	System.out.println("DONE");
         }
     }
 }
