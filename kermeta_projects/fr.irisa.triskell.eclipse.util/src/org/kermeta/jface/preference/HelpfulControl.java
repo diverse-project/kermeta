a6a733600f509746cc7db84c3aacaf84239e93db
Didier Vojtisek <didier.vojtisek@inria.fr>
2008-05-30 15:54:02 +0000

    custom field for preference FieldEditor
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@6093 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: b90a0100875fd042aadc833a680873c1d8dff9df

---
+++ kermeta_projects/fr.irisa.triskell.eclipse.util/src/org/kermeta/jface/preference/HelpfulControl.java
@@ -0,0 +1,18 @@
+/*
+ Created on 27-May-2004
+ */
+package org.kermeta.jface.preference;
+
+//import java.io.*;
+//import java.util.*;
+
+
+/**
+ @author Daniel Winterstein
+ */
+public interface HelpfulControl {
+
+    public void setToolTipText(String text);
+
+    public void setHelp(String helpContextId);
+}
