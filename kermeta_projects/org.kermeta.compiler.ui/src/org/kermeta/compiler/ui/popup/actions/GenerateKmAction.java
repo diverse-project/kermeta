1ce58c905de35cab8da2018437d0ad68ac804a76
ftanguy <ftanguy@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2008-06-05 14:18:46 +0000

    Add an action to automatically generate a km for any kmt file in resource hierarchy
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@6130 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: a468f010fe3d04ae98574ad34031a3883ba4282b

---
+++ kermeta_projects/org.kermeta.compiler.ui/src/org/kermeta/compiler/ui/popup/actions/GenerateKmAction.java
@@ -0,0 +1,149 @@
+
+
+/*$Id: GenerateKmAction.java,v 1.1 2008-06-05 14:18:46 ftanguy Exp $
+* Project : org.kermeta.compiler.ui
+* File : 	GenerateKmAction.java
+* License : EPL
+* Copyright : IRISA / INRIA / Universite de Rennes 1
+* ----------------------------------------------------------------------------
+* Creation date : 19 mai 08
+* Authors : paco
+*/
+
+package org.kermeta.compiler.ui.popup.actions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceVisitor;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.TreePath;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.ui.IActionDelegate;
+import org.kermeta.io.KermetaUnit;
+import org.kermeta.io.loader.plugin.LoaderPlugin;
+import org.kermeta.loader.LoadingOptions;
+import org.kermeta.merger.Merger;
+
+import fr.irisa.triskell.kermeta.modelhelper.KermetaUnitHelper;
+
+public class GenerateKmAction implements IActionDelegate, IResourceVisitor {
+
+	private List<IFolder> _folders = new ArrayList<IFolder>();
+	
+	private List<IFile> _files = new ArrayList<IFile>();
+	
+	public GenerateKmAction() {
+	}
+
+	public void run(IAction action) {
+		final GenerateKmAction instance = this;
+		Job job = new Job("Generating KM Files") {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				try {
+					// Calculating the list of IFile
+					for ( IFolder f : _folders ) {
+						try {
+							f.accept(instance);
+						} catch (CoreException e) {
+							e.printStackTrace();
+						}	
+					}				
+					// Starting a task
+					monitor.beginTask("", _files.size());
+	
+					for ( IFile file : _files ) {
+						SubProgressMonitor subMonitor = new SubProgressMonitor(monitor, 1);
+						try {
+							subMonitor.beginTask(file.getFullPath().toString(), 1);
+							generateKm(file);
+							subMonitor.worked(1);
+						} catch (Exception e) {
+							e.printStackTrace();
+							subMonitor.done();
+						}
+					}
+				} finally {
+					monitor.done();
+					_folders.clear();
+					_files.clear();
+				}
+				return Status.OK_STATUS;
+			}
+		};
+		
+		job.schedule();
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		if ( selection instanceof TreeSelection ) {
+			TreeSelection s = (TreeSelection) selection;
+			for ( TreePath path : s.getPaths() )
+				_folders.add( (IFolder) path.getLastSegment() );
+		}
+	}
+
+	public boolean visit(IResource resource) throws CoreException {
+		boolean goOn = false;
+		switch( resource.getType() ) {
+		case IResource.FILE :
+			IFile file = (IFile) resource;
+			if ( file.getFileExtension().equals("kmt") )
+				_files.add( file );
+			break;
+			
+		case IResource.FOLDER :
+			goOn = true;
+			break;
+			
+		default :
+			break;
+		}
+		return goOn;
+	}
+
+	private void generateKm(IFile file) {
+		String uri = "platform:/resource" + file.getFullPath().toString();
+		String mergedURI = getURI(file);
+
+		HashMap<String, Object> options = new HashMap<String, Object>();
+		options.put( LoadingOptions.ECORE_QuickFixEnabled, true);
+		try {
+			KermetaUnit unit = LoaderPlugin.getDefault().load(uri, options);
+			HashSet<KermetaUnit> s = new HashSet<KermetaUnit>();
+			s.add(unit);
+			s.addAll( KermetaUnitHelper.getAllImportedKermetaUnits(unit) );
+			
+			Merger merger = new Merger();
+			merger.process(s, mergedURI, false, false );	
+			
+		} catch (Exception e) {
+			// We can catch any exception so that the execution will  not stop.
+			e.printStackTrace();
+		} finally {
+			LoaderPlugin.getDefault().unload( mergedURI );			
+		}
+	}
+	
+	private String getURI(IFile file) {
+		String uri = "platform:/resource" + file.getFullPath().toString();
+		int i = uri.lastIndexOf(".");
+		return uri.substring(0, i) + ".km";
+	}
+	
+}
+
+
