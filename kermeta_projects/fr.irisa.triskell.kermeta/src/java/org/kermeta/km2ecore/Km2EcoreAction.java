ed25cd2c3a981bf24b66e69f611e655f11c72686
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2008-06-05 13:36:33 +0000

    new version of the transformation
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@6127 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: e479763c319c73c241b86b5e7454e8962f2cd965

--- kermeta_projects/fr.irisa.triskell.kermeta/src/java/org/kermeta/km2ecore/Km2EcoreAction.java
+++ kermeta_projects/fr.irisa.triskell.kermeta/src/java/org/kermeta/km2ecore/Km2EcoreAction.java
@@ -1,6 +1,6 @@
 
 
-/*$Id: Km2EcoreAction.java,v 1.1 2008-05-28 15:35:54 ftanguy Exp $
+/*$Id: Km2EcoreAction.java,v 1.2 2008-06-05 13:36:33 cfaucher Exp $
 * Project : fr.irisa.triskell.kermeta
 * File : 	Action1.java
 * License : EPL
@@ -16,6 +16,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.TreeSelection;
@@ -41,23 +45,39 @@ public class Km2EcoreAction implements IObjectActionDelegate {
 	}
 
 	public void run(IAction action) {
-		Map<String, Object> options = new HashMap<String, Object>();
-		options.put(InterpreterOptions.MERGE, false);
-		options.put(InterpreterOptions.TYPE_CHECKED, true);
-		try {
-			Interpreter interpreter = new Interpreter("platform:/plugin/fr.irisa.triskell.kermeta/src/java/org/kermeta/km2ecore/StructureCompiler.km", InterpreterMode.RUN, options);
-			interpreter.setEntryPoint("kermeta::compiler::Main", "main");
-			String[] parameters = new String[3];
-			parameters[0] = "platform:/resource" + _file.getFullPath().toString();
-			parameters[1] = "";
-			parameters[2] = "platform:/resource" + _file.getFullPath().toString() + ".ecore";
-			interpreter.setParameters(parameters);
-			EclipseConsole console = new EclipseConsole("KM2Ecore");
-			interpreter.setStreams(console);
-			interpreter.launch();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		
+		Job job = new Job("km2ecore_in_kmt") {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+
+				/*************/
+				
+				Map<String, Object> options = new HashMap<String, Object>();
+				options.put(InterpreterOptions.MERGE, false);
+				options.put(InterpreterOptions.TYPE_CHECKED, true);
+				try {
+					Interpreter interpreter = new Interpreter("platform:/plugin/fr.irisa.triskell.kermeta/src/java/org/kermeta/km2ecore/StructureCompiler.km", InterpreterMode.RUN, options);
+					interpreter.setEntryPoint("kermeta::compiler::Main", "main_km2ecore_onlyEcore");
+					String[] parameters = new String[3];
+					parameters[0] = "platform:/resource" + _file.getFullPath().toString();
+					parameters[1] = "";
+					parameters[2] = "platform:/resource" + _file.getFullPath().toString() + ".ecore";
+					interpreter.setParameters(parameters);
+					EclipseConsole console = new EclipseConsole("KM2Ecore");
+					interpreter.setStreams(console);
+					interpreter.launch();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				/*************/
+				
+				return Status.OK_STATUS;
+			}
+		};
+		
+		job.schedule();
+		
 	}
 
 	public void selectionChanged(IAction action, ISelection selection) {
