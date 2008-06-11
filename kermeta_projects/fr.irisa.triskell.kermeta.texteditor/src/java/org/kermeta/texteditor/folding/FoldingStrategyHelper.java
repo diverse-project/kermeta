09bda9dfb478ebe48d70b798f90aff847eb0cb3c
ftanguy <ftanguy@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2008-06-11 14:36:02 +0000

    Reactivating the folding strategy
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@6173 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 9f72c98f2438dc3b27ceadcc6e08d330949496d5

--- kermeta_projects/fr.irisa.triskell.kermeta.texteditor/src/java/org/kermeta/texteditor/folding/FoldingStrategyHelper.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.texteditor/src/java/org/kermeta/texteditor/folding/FoldingStrategyHelper.java
@@ -1,6 +1,6 @@
 
 
-/*$Id: FoldingStrategyHelper.java,v 1.5 2008-05-28 09:25:06 ftanguy Exp $
+/*$Id: FoldingStrategyHelper.java,v 1.6 2008-06-11 14:36:02 ftanguy Exp $
 * Project : fr.irisa.triskell.kermeta.texteditor
 * File : 	FoldingStrategyHelper.java
 * License : EPL
@@ -12,19 +12,25 @@
 
 package org.kermeta.texteditor.folding;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jface.text.Position;
 import org.kermeta.texteditor.KermetaTextEditor;
 
 import antlr.RecognitionException;
 import antlr.TokenStreamException;
+import fr.irisa.triskell.kermeta.modelhelper.ASTHelper;
+import fr.irisa.triskell.kermeta.parser.gen.ast.CompUnit;
 
 public class FoldingStrategyHelper {
 
 	static public void updateFoldingPositions(KermetaTextEditor editor, String document) throws RecognitionException, TokenStreamException {
-		/*CompUnit compUnit = ASTHelper.parseString( document );
+		CompUnit compUnit = ASTHelper.parseString( document );
 		List<Position> positions = new ArrayList<Position> ();
 		FoldingVisitor visitor = new FoldingVisitor(positions);
 		compUnit.accept(visitor);
-		editor.updateFoldingStructure(positions);*/
+		editor.updateFoldingStructure(positions);
 	}
 	
 }
