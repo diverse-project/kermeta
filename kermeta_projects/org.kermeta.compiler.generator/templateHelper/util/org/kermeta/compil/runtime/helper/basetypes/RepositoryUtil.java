2794f11ae2caedba531d1e2953fbab5b16863cc5
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2008-07-31 07:35:16 +0000

    new impls
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@6518 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: f961038aaeba39b5ae44aebd8597f45628b593c9

---
+++ kermeta_projects/org.kermeta.compiler.generator/templateHelper/util/org/kermeta/compil/runtime/helper/basetypes/RepositoryUtil.java
@@ -0,0 +1,28 @@
+
+package org.kermeta.compil.runtime.helper.basetypes;
+
+/**
+ * This class is intended to "wrap" the repository management from kermeta.
+ */
+public class RepositoryUtil {
+    
+
+    /**
+     * @param selfRO - RO for respository
+     * @return       - RO for the created resource
+     */
+    public static void initRepository(kermeta.persistence.Repository self) {
+    }
+
+    
+    /**
+     * Allows to obtain a valid URI from various URI forms
+     * It takes into account relative path and tronsfor them into plaform:/plugin or platform:/resource path
+     * @param uriRO
+     * @return
+     */
+    public static java.lang.String normalizeUri(java.lang.String uri) {    	
+    	return null;
+    }
+    
+}
