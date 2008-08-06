8006784a5670d95d719928ec5e5bd492a00f6a23
Didier Vojtisek <didier.vojtisek@inria.fr>
2008-08-06 15:32:48 +0000

    added linux, win32 and macosx fragments
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@6572 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: d547c56d086a4155c90cbbb0e933eb5e1c2c407e

--- kmlogo_projects/org.kermeta.kmlogo.tutorial/src/org/kermeta/kmlogo/tutorial/wizards/KmLogoTutorialWizard.java
+++ kmlogo_projects/org.kermeta.kmlogo.tutorial/src/org/kermeta/kmlogo/tutorial/wizards/KmLogoTutorialWizard.java
@@ -1,4 +1,4 @@
-/* $Id: KmLogoTutorialWizard.java,v 1.2 2008-03-27 16:28:11 dvojtise Exp $
+/* $Id: KmLogoTutorialWizard.java,v 1.3 2008-08-06 15:32:48 dvojtise Exp $
  * Project    : org.kermeta.kmlogo.tutorial
  * File       : AbstractExampleWizard.java
  * License    : EPL
@@ -23,15 +23,18 @@ import fr.irisa.triskell.eclipse.wizard.AbstractExampleWizard;
 public class KmLogoTutorialWizard
 	extends AbstractExampleWizard {
 	
-	protected Collection getProjectDescriptors() {
+	protected Collection<ProjectDescriptor> getProjectDescriptors() {
 		
 		// We need the statements example to be unzipped along with the
 		// EMF library example model, edit and editor examples
-		List projects = new ArrayList(1);
+		List<ProjectDescriptor> projects = new ArrayList<ProjectDescriptor>(1);
 		projects.add(new ProjectDescriptor("org.kermeta.kmlogo.tutorial", "zips/fr.irisa.triskell.kmlogo.model.zip", "fr.irisa.triskell.kmlogo.model"));
 		projects.add(new ProjectDescriptor("org.kermeta.kmlogo.tutorial", "zips/fr.irisa.triskell.kmlogo.model.edit.zip", "fr.irisa.triskell.kmlogo.model.edit"));
 		projects.add(new ProjectDescriptor("org.kermeta.kmlogo.tutorial", "zips/fr.irisa.triskell.kmlogo.model.editor.zip", "fr.irisa.triskell.kmlogo.model.editor"));
 		projects.add(new ProjectDescriptor("org.kermeta.kmlogo.tutorial", "zips/fr.irisa.triskell.kmlogo.ui.zip", "fr.irisa.triskell.kmlogo.ui"));
+		projects.add(new ProjectDescriptor("org.kermeta.kmlogo.tutorial", "zips/fr.irisa.triskell.kmlogo.ui.win32.x86.zip", "fr.irisa.triskell.kmlogo.ui.win32.x86"));
+		projects.add(new ProjectDescriptor("org.kermeta.kmlogo.tutorial", "zips/fr.irisa.triskell.kmlogo.ui.linux.x86.zip", "fr.irisa.triskell.kmlogo.ui.linux.x86"));
+		projects.add(new ProjectDescriptor("org.kermeta.kmlogo.tutorial", "zips/fr.irisa.triskell.kmlogo.ui.macosx.zip", "fr.irisa.triskell.kmlogo.ui.macosx"));
 		projects.add(new ProjectDescriptor("org.kermeta.kmlogo.tutorial", "zips/org.kermeta.kmlogo.texteditor.zip", "org.kermeta.kmlogo.texteditor"));
 		return projects;
 	}
