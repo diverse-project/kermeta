1eb36cbc7132502af6003ee9be21cd0afc3a7cb9
Didier Vojtisek <didier.vojtisek@inria.fr>
2008-08-13 11:18:46 +0000

    split KermetaMarkersHelper in 2 in order to provide this service to other tool than kermeta
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@6598 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 6b13271158bfe4cf09c21e5a70791c1074553957

---
+++ kermeta_projects/fr.irisa.triskell.eclipse.util/src/fr/irisa/triskell/eclipse/resources/EclipseMarkerHelper.java
@@ -0,0 +1,142 @@
+/*$Id: EclipseMarkerHelper.java,v 1.1 2008-08-13 11:18:02 dvojtise Exp $
+* Project : fr.irisa.triskell.eclipse.util
+* File : 	EclipseMarkerHelper.java
+* License : EPL
+* Copyright : IRISA / INRIA 
+* ----------------------------------------------------------------------------
+* Creation date : 13 août 08
+* Authors : 
+*      Didier Vojtisek <dvojtise@irisa.fr>
+*/
+
+package fr.irisa.triskell.eclipse.resources;
+
+import java.util.HashMap;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.ui.texteditor.MarkerUtilities;
+
+
+public class EclipseMarkerHelper {
+	protected final static int LINE_MAX_SIZE = 80;
+	
+    /**
+     * The marker type : must correspond to the type that is
+     * declared in the extension "org.eclipse.core.resources.markers"
+     * in fr.irisa.triskell.kermeta/plugin.xml
+     *  (super type="org.eclipse.core.resources.problemmarker")
+     */
+    public static String getMarkerType() {
+    	return IMarker.PROBLEM;
+    }
+	    
+
+    public static void markError(IFile file, String message) throws CoreException {
+    	mark(file, message, IMarker.SEVERITY_ERROR, null, null );
+    }
+    
+    public static void markError(IFile file, String message, int charStart, int charStop) throws CoreException {
+    	mark(file, message, IMarker.SEVERITY_ERROR, charStart, charStop );
+    }
+    
+    public static void markWarning(IFile file, String message) throws CoreException {
+    	mark(file, message, IMarker.SEVERITY_WARNING, null, null );
+    }
+    
+    public static void markWarning(IFile file, String message, int charStart, int charStop) throws CoreException {
+    	mark(file, message, IMarker.SEVERITY_WARNING, charStart, charStop );
+    }
+    
+    public static void markInfo(IFile file, String message) throws CoreException {
+    	mark(file, message, IMarker.SEVERITY_INFO, null, null );
+    }
+    
+    public static void markInfo(IFile file, String message, int charStart, int charStop) throws CoreException {
+    	mark(file, message, IMarker.SEVERITY_INFO, charStart, charStop );
+    }
+    
+    protected static void mark(IFile file, String message, int severity, Integer charStart, Integer charStop) throws CoreException {
+     	HashMap<String, java.lang.Object> datas = new HashMap<String, java.lang.Object>();
+        datas.put( IMarker.MESSAGE, message );
+        datas.put( IMarker.SEVERITY, severity );
+        
+        if ( charStart != null )
+        	datas.put( IMarker.CHAR_START, charStart );
+        else
+        	datas.put( IMarker.CHAR_START, 0 );
+ 
+        if ( charStop != null )
+           	datas.put( IMarker.CHAR_END, charStop );
+       	else
+       		datas.put( IMarker.CHAR_END, 0 );
+        
+        MarkerUtilities.createMarker( file, datas, getMarkerType() );
+    }
+    
+   
+ 
+    
+    /** Split message so that it is displayed in many lines instead of only one */
+    protected static String formatMessage(String message)
+    {
+    	String result = "";
+    	int line_start=0; int line_end=0; String ret_char = "\n";
+    	if (message.length() >  LINE_MAX_SIZE )
+    	{
+    		int linenumber = message.length()/LINE_MAX_SIZE;
+    		for (int i=0; i<=linenumber; i++)
+    		{
+    			line_start = LINE_MAX_SIZE*i;
+    			line_end   = LINE_MAX_SIZE*(i+1);
+    			if (message.length()<line_end) {line_end = message.length();ret_char="";}
+    			result += message.substring(line_start, line_end) + ret_char ;
+    		}
+    	}
+    	else
+    		result = message;
+    	return result;
+    }
+	
+	
+    
+    /**
+     *  Clear previous marker in the given file; in the texteditor, markers
+     *  display messages/warning/errors icons at begining of lines in 
+     *  the text editors and underline elements that are concerned by those
+     *  messages (for example, invalid calls).
+     */
+    public static void clearMarkers(IFile file) {
+        try {
+            //file.deleteMarkers(getMarkerType(), true, 2);
+        	if ( file != null )
+        		file.deleteMarkers(getMarkerType(), true, IResource.DEPTH_INFINITE);
+        } catch(Exception ex) {
+            //ex.printStackTrace();
+        }
+    }
+  
+    static protected IMarker findMarker( IResource resource, String message ) throws CoreException {
+    	IMarker[] markers = resource.findMarkers(getMarkerType(), false, IResource.DEPTH_ZERO);
+    	IMarker foundMarker = null;
+    	for (int index = 0; index < markers.length; index++ ) {
+    		String currentMessage = ((String) markers[index].getAttribute("message")).replace("\n", "");
+    		if ( currentMessage.equals(message.replace("\n", "")) )
+    			foundMarker = markers[index];
+    	}
+    	return foundMarker;
+    }
+
+    static public void removeMarker( IResource resource, String message ) {
+		try {
+			if ( (resource != null) && resource.exists() ) {
+				IMarker marker = findMarker(resource, message);
+				if ( marker != null )
+					marker.delete();
+			}
+		} catch (CoreException e) {}
+    }
+
+}
--- kermeta_projects/fr.irisa.triskell.kermeta/src/java/fr/irisa/triskell/kermeta/resources/KermetaMarkersHelper.java
+++ kermeta_projects/fr.irisa.triskell.kermeta/src/java/fr/irisa/triskell/kermeta/resources/KermetaMarkersHelper.java
@@ -1,4 +1,4 @@
-/*$Id: KermetaMarkersHelper.java,v 1.14 2008-05-19 14:19:47 ftanguy Exp $
+/*$Id: KermetaMarkersHelper.java,v 1.15 2008-08-13 11:18:46 dvojtise Exp $
 * Project : fr.irisa.triskell.kermeta
 * File : 	KermetaMarkersHelper.java
 * License : EPL
@@ -9,20 +9,16 @@
 */
 package fr.irisa.triskell.kermeta.resources;
 
-import java.util.HashMap;
-
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IMarker;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.gymnast.runtime.core.ast.ASTNode;
-import org.eclipse.ui.texteditor.MarkerUtilities;
 import org.kermeta.io.ErrorMessage;
 import org.kermeta.io.KermetaUnit;
 import org.kermeta.io.Message;
 import org.kermeta.io.ParsingError;
 import org.kermeta.io.WarningMessage;
 
+import fr.irisa.triskell.eclipse.resources.EclipseMarkerHelper;
 import fr.irisa.triskell.kermeta.language.structure.Object;
 import fr.irisa.triskell.traceability.ModelReference;
 import fr.irisa.triskell.traceability.TextReference;
@@ -32,19 +28,8 @@ import fr.irisa.triskell.traceability.helper.ModelReferenceHelper;
  * This class provides helper functions to create and manage the Eclipse markers when used with Kermeta
  *
  */
-public class KermetaMarkersHelper {
+public class KermetaMarkersHelper extends EclipseMarkerHelper {
 
-	private final static int LINE_MAX_SIZE = 80;
-		
-    /**
-     * The marker type : must correspond to the type that is
-     * declared in the extension "org.eclipse.core.resources.markers"
-     * in fr.irisa.triskell.kermeta/plugin.xml
-     *  (super type="org.eclipse.core.resources.problemmarker")
-     */
-    public static String getMarkerType() {
-    	return IMarker.PROBLEM;
-    }
 	
     /**
      *  Create markers for showing to the user the elements that are erroneous, or
@@ -59,48 +44,7 @@ public class KermetaMarkersHelper {
 	    		createMarker(file, m);
     }
 
-    public static void markError(IFile file, String message) throws CoreException {
-    	mark(file, message, IMarker.SEVERITY_ERROR, null, null );
-    }
-    
-    private static void markError(IFile file, String message, int charStart, int charStop) throws CoreException {
-    	mark(file, message, IMarker.SEVERITY_ERROR, charStart, charStop );
-    }
-    
-    private static void markWarning(IFile file, String message) throws CoreException {
-    	mark(file, message, IMarker.SEVERITY_WARNING, null, null );
-    }
-    
-    private static void markWarning(IFile file, String message, int charStart, int charStop) throws CoreException {
-    	mark(file, message, IMarker.SEVERITY_WARNING, charStart, charStop );
-    }
-    
-    private static void markInfo(IFile file, String message) throws CoreException {
-    	mark(file, message, IMarker.SEVERITY_INFO, null, null );
-    }
-    
-    private static void markInfo(IFile file, String message, int charStart, int charStop) throws CoreException {
-    	mark(file, message, IMarker.SEVERITY_INFO, charStart, charStop );
-    }
-    
-    private static void mark(IFile file, String message, int severity, Integer charStart, Integer charStop) throws CoreException {
-     	HashMap<String, java.lang.Object> datas = new HashMap<String, java.lang.Object>();
-        datas.put( IMarker.MESSAGE, message );
-        datas.put( IMarker.SEVERITY, severity );
-        
-        if ( charStart != null )
-        	datas.put( IMarker.CHAR_START, charStart );
-        else
-        	datas.put( IMarker.CHAR_START, 0 );
  
-        if ( charStop != null )
-           	datas.put( IMarker.CHAR_END, charStop );
-       	else
-       		datas.put( IMarker.CHAR_END, 0 );
-        
-        MarkerUtilities.createMarker( file, datas, getMarkerType() );
-    }
-    
     private static void createParsingError(IFile file, String message, ParsingError error) throws CoreException {
 		int offset = error.getOffset();
 		int length = error.getLength();
@@ -191,64 +135,5 @@ public class KermetaMarkersHelper {
         }
     }
     
-    /** Split message so that it is displayed in many lines instead of only one */
-    private static String formatMessage(String message)
-    {
-    	String result = "";
-    	int line_start=0; int line_end=0; String ret_char = "\n";
-    	if (message.length() >  LINE_MAX_SIZE )
-    	{
-    		int linenumber = message.length()/LINE_MAX_SIZE;
-    		for (int i=0; i<=linenumber; i++)
-    		{
-    			line_start = LINE_MAX_SIZE*i;
-    			line_end   = LINE_MAX_SIZE*(i+1);
-    			if (message.length()<line_end) {line_end = message.length();ret_char="";}
-    			result += message.substring(line_start, line_end) + ret_char ;
-    		}
-    	}
-    	else
-    		result = message;
-    	return result;
-    }
-	
-	
-    
-    /**
-     *  Clear previous marker in the given file; in the texteditor, markers
-     *  display messages/warning/errors icons at begining of lines in 
-     *  the text editors and underline elements that are concerned by those
-     *  messages (for example, invalid calls).
-     */
-    public static void clearMarkers(IFile file) {
-        try {
-            //file.deleteMarkers(getMarkerType(), true, 2);
-        	if ( file != null )
-        		file.deleteMarkers(getMarkerType(), true, IResource.DEPTH_INFINITE);
-        } catch(Exception ex) {
-            //ex.printStackTrace();
-        }
-    }
-  
-    static private IMarker findMarker( IResource resource, String message ) throws CoreException {
-    	IMarker[] markers = resource.findMarkers(getMarkerType(), false, IResource.DEPTH_ZERO);
-    	IMarker foundMarker = null;
-    	for (int index = 0; index < markers.length; index++ ) {
-    		String currentMessage = ((String) markers[index].getAttribute("message")).replace("\n", "");
-    		if ( currentMessage.equals(message.replace("\n", "")) )
-    			foundMarker = markers[index];
-    	}
-    	return foundMarker;
-    }
-
-    static public void removeMarker( IResource resource, String message ) {
-		try {
-			if ( (resource != null) && resource.exists() ) {
-				IMarker marker = findMarker(resource, message);
-				if ( marker != null )
-					marker.delete();
-			}
-		} catch (CoreException e) {}
-    }
-    
+ 
 }
