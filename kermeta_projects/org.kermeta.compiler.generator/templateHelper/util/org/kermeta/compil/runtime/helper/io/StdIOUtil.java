47f4a8b967e100aceb5ae4e340a99f1f6dcd58b2
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2008-08-18 08:38:00 +0000

    extern impl
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@6621 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: be6c3ebfce83ce03e973238045e0bb6f27cf841a

--- kermeta_projects/org.kermeta.compiler.generator/templateHelper/util/org/kermeta/compil/runtime/helper/io/StdIOUtil.java
+++ kermeta_projects/org.kermeta.compiler.generator/templateHelper/util/org/kermeta/compil/runtime/helper/io/StdIOUtil.java
@@ -1,4 +1,4 @@
-/* $Id: StdIOUtil.java,v 1.1 2008-08-01 18:29:17 cfaucher Exp $
+/* $Id: StdIOUtil.java,v 1.2 2008-08-18 08:38:00 cfaucher Exp $
  * Project : Kermeta (First iteration)
  * File : Io.java
  * License : EPL
@@ -13,6 +13,10 @@
  */
 package org.kermeta.compil.runtime.helper.io;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 /**
  * Implementation of input and output methods (see io.kmt)
@@ -22,33 +26,43 @@ public class StdIOUtil {
 	// Implementation of method write called as :
 	// extern fr::irisa::triskell::kermeta::runtime::basetypes::Io.write(output)
 	public static void write(String output) {
-		//TODO
+		System.out.print(output);
 	}
-
+	
 	// Implementation of method writeln called as :
 	// extern fr::irisa::triskell::kermeta::runtime::basetypes::Io.writeln(output)
 	public static void writeln(java.lang.String output) {
-		//TODO
+		System.out.println(output);
 	}
 	
 	// Implementation of method error called as :
 	// extern fr::irisa::triskell::kermeta::runtime::basetypes::Io.error(output)
 	public static void error(java.lang.String output) {
-		//TODO
+		System.err.print(output);
 	}
 
 	// Implementation of method errorln called as :
 	// extern fr::irisa::triskell::kermeta::runtime::basetypes::Io.errorln(output)
 	public static void errorln(java.lang.String output) {
-		//TODO
+		System.err.println(output);
 	}
 	
 	// Implementation of method read called as :
 	// extern fr::irisa::triskell::kermeta::runtime::basetypes::Io.read(prompt)
-	public static java.lang.String read(java.lang.String prompt)
-	{
-		//TODO
-		return null;
+	public static java.lang.String read(java.lang.String prompt) {
+		System.out.println(prompt);
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = null;
+		
+		try {
+			str = br.readLine();
+		} catch (IOException ioe) {
+			System.out.println("IO error trying to read.");
+			System.exit(1);
+		}
+		
+		return str;
 	}
 	
 }
