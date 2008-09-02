1e9e2deb15e1865879404c1d365896f588acb4c6
cfaucher <cfaucher@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2008-09-02 13:20:18 +0000

    added convertAsEList
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@6752 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 4898ceab5059f8a1752ddf2d1f71ea21088587e6

--- kermeta_projects/org.kermeta.compiler.generator/templateHelper/util/org/kermeta/compil/runtime/helper/basetypes/CollectionUtil.java
+++ kermeta_projects/org.kermeta.compiler.generator/templateHelper/util/org/kermeta/compil/runtime/helper/basetypes/CollectionUtil.java
@@ -86,4 +86,29 @@ public class CollectionUtil<G> {
 		newSequence.setValues(l);
 		return newSequence;
 	}
+
+	public static <G> EList<G> convertAsEList(kermeta.standard.OrderedSet<G> col) {
+		EList<G> newEList = new BasicEList<G>(); 
+		newEList = col.getValues();
+		return newEList;
+	}
+	
+	public static <G> EList<G> convertAsEList(kermeta.standard.Set<G> col) {
+		EList<G> newEList = new BasicEList<G>(); 
+		newEList = col.getValues();
+		return newEList;
+	}
+	
+	public static <G> EList<G> convertAsEList(kermeta.standard.Sequence<G> col) {
+		EList<G> newEList = new BasicEList<G>(); 
+		newEList = col.getValues();
+		return newEList;
+	}
+	
+	public static <G> EList<G> convertAsEList(kermeta.standard.Bag<G> col) {
+		EList<G> newEList = new BasicEList<G>(); 
+		newEList = col.getValues();
+		return newEList;
+	}
+
 }
