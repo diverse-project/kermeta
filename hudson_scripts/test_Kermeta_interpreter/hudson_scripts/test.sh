82ffb2e9aa83e2aa4958476b37a5c1362931c7d6
Didier Vojtisek <didier.vojtisek@inria.fr>
2008-10-27 23:30:21 +0000

    *** empty log message ***
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@7046 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: a1487c1b343a8d80916d9e34697685b85d65d4d6

---
+++ hudson_scripts/test_Kermeta_interpreter/hudson_scripts/test.sh
@@ -0,0 +1,26 @@
+#!/bin/tcsh
+source /usr/local/env/envjava1.5
+#set ant environnment
+source /udd/triskell/bin/multiplatform/apache-ant-1.7.0/env_ant.sh
+#source /net/usr/local/env/envant
+#setenv ANT_HOME /udd/triskell/bin/multiplatform/apache-ant-1.5.4
+set path=( $ANT_HOME/bin $path )
+#setenv PATH "$PATH:$ANT_HOME/bin" 
+
+setenv ECLIPSE_HOME /local/daily_build/eclipse3.4_with_topcased/eclipse
+setenv BUILD_WORKSPACE $WORKSPACE/buildworkspace
+setenv EQUINOX_JAR org.eclipse.equinox.launcher_1.0.101.R34x_v20080819.jar
+
+
+echo "--------------------------------"
+echo " Launch interpreter tests "
+echo "--------------------------------"
+
+cd $WORKSPACE/src/plugins/fr.irisa.triskell.kermeta.tests/
+pwd
+
+setenv CLASSPATH  $ANT_HOME/lib/junit.jar:$ANT_HOME/lib/optional.jar
+#echo "CLASSPATH = $CLASSPATH"
+#which ant
+mkdir -p test/junit/reports
+ant -f test/junit/test_interpreter_build.xml -Declipse.home=$ECLIPSE_HOME -Dreport.dir=$REPORTDIR -Dcobertura.dir=/udd/triskell/bin/multiplatform/cobertura-1.7/ -Ddot.or.bin=@dot -Ddot.or.buildclass=@dot
