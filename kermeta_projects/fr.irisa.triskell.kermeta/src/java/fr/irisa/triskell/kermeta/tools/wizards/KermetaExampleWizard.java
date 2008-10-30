98b42ce85e4fd5465e36dcf4224074b592fbe7a2
Didier Vojtisek <didier.vojtisek@inria.fr>
2008-10-30 16:45:57 +0000

    use the common version of AbstractExampleWizard, not the copy ...
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@7084 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: cf9f1f51ff90e9e2201c5d4251309d4f242d647e

--- kermeta_projects/fr.irisa.triskell.kermeta/src/java/fr/irisa/triskell/kermeta/tools/wizards/KermetaExampleWizard.java
+++ kermeta_projects/fr.irisa.triskell.kermeta/src/java/fr/irisa/triskell/kermeta/tools/wizards/KermetaExampleWizard.java
@@ -1,14 +1,30 @@
+/* $Id: KermetaExampleWizard.java,v 1.2 2008-10-30 16:45:57 dvojtise Exp $
+ * Project    : fr.irisa.triskell.kermeta
+ * File       : KermetaExampleWizard.java
+ * License    : EPL
+ * Copyright  : IRISA / INRIA / Universite de Rennes 1
+ * -------------------------------------------------------------------
+ * Authors : 
+ *        	dvojtise <dvojtise@irisa.fr>
+ */
 package fr.irisa.triskell.kermeta.tools.wizards;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 
+import fr.irisa.triskell.eclipse.wizard.AbstractExampleWizard;
+import fr.irisa.triskell.eclipse.wizard.AbstractExampleWizard.ProjectDescriptor;
+import fr.irisa.triskell.kermeta.plugin.KermetaPlugin;
+
+
+@SuppressWarnings("unused")
 public class KermetaExampleWizard
 	extends AbstractExampleWizard {
 	
-	protected Collection getProjectDescriptors() {
+	protected Collection<ProjectDescriptor> getProjectDescriptors() {
 		
 		System.out.println("Debug - enter in the wizard");
 		// We need the statements example to be unzipped along with the
@@ -16,5 +32,11 @@ public class KermetaExampleWizard
 		List projects = new ArrayList(1);
 		projects.add(new ProjectDescriptor("fr.irisa.triskell.kermeta", "zips/fr.irisa.triskell.kermeta.samples.zip", "fr.irisa.triskell.kermeta.samples"));
 		return projects;
+	}
+
+	@Override
+	protected AbstractUIPlugin getContainerPlugin() {
+		
+		return KermetaPlugin.getDefault();
 	}
 }
