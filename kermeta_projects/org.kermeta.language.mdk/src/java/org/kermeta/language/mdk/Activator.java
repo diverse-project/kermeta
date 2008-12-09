de736bdc35fc898ec28dd65023b2a4aeff4c120e
Didier Vojtisek <didier.vojtisek@inria.fr>
2008-12-09 13:33:07 +0000

    initial revision of a plugin for extension of kermeta language itself It aims to gather utils for kermeta and useful tool (ex: kmtprettryprinter, fileio, math, hashtable, ...) which shouldn't be part of the main framework
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@7290 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 035bf2dd84cf64752df8cf03bb756167036feee7

---
+++ kermeta_projects/org.kermeta.language.mdk/src/java/org/kermeta/language/mdk/Activator.java
@@ -0,0 +1,59 @@
+/*$Id: Activator.java,v 1.1 2008-12-09 13:33:07 dvojtise Exp $
+* Project : org.kermeta.language.mdk
+* License : EPL
+* Copyright : IRISA / INRIA / Universite de Rennes 1
+* ----------------------------------------------------------------------------
+* Creation date : 09/12/2008
+* Authors :
+*		Didier Vojtisek
+*/
+package org.kermeta.language.mdk;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends Plugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.kermeta.language.mdk";
+
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+}
