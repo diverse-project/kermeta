7185db0066650e12f4b17991dfc16644fea88991
Olivier Barais <olivier.barais@gmail.com>
2009-03-20 00:01:49 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@7771 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: d12f05b1e2a3359e87791e3f49212d6ba1a57733

--- java_projects/fr.irisa.triskell.kermetaSpoonEMFBridge/src/fr/irisa/triskell/kermeta/runtime/loader/java5files/Java52RuntimeResource.java
+++ java_projects/fr.irisa.triskell.kermetaSpoonEMFBridge/src/fr/irisa/triskell/kermeta/runtime/loader/java5files/Java52RuntimeResource.java
@@ -1,6 +1,9 @@
 package fr.irisa.triskell.kermeta.runtime.loader.java5files;
 
-import spoonEFM.util.XMI2Java;
+import java.util.ArrayList;
+import java.util.List;
+
+import spoonemf.popup.actions.Java2XMIHelper;
 import fr.irisa.triskell.kermeta.runtime.RuntimeObject;
 import fr.irisa.triskell.kermeta.runtime.basetypes.String;
 
@@ -11,14 +14,25 @@ public class Java52RuntimeResource {
 
 	}
 	
+	public static void main( java.lang.String[] args) {
+	       java.lang.String xmi_uri = "/home/barais/test3.xmi";
+	      java.lang.String xmi_javaFolder = "/home/barais/workspaces/runtime-New_configuration(1)/Test/src/a/";
+	      List<java.lang.String> folders = new ArrayList<java.lang.String>();
+	      folders.add(xmi_javaFolder);
+	      Java2XMIHelper.eInstance.processJavaFolder(folders,xmi_uri);
+			
+		}
+	
 	public static void createResourcesFromJavaFolder(RuntimeObject self, RuntimeObject folderToParse, RuntimeObject outputXMIFiles){
-		XMI2Java.eInstance.processJavaFolder(String.getValue(folderToParse),String.getValue(outputXMIFiles));
+		List<java.lang.String> folders = new ArrayList<java.lang.String>();
+	      folders.add(String.getValue(folderToParse));
+	      Java2XMIHelper.eInstance.processJavaFolder(folders,String.getValue(outputXMIFiles));
 		
 	
 	}
 	
 	public static void LoadResourcesFromJavaXMI(RuntimeObject self, RuntimeObject  outputSpoonFolder , RuntimeObject inputXMIFiles){
-		XMI2Java.eInstance.processXMIFiles(String.getValue(outputSpoonFolder),String.getValue(inputXMIFiles));
+//		XMI2Java.eInstance.processXMIFiles(String.getValue(outputSpoonFolder),String.getValue(inputXMIFiles));
 	}
 
 	
