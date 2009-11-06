439db01734c34f163e094a8aab789c0757e82a6c
Didier Vojtisek <didier.vojtisek@inria.fr>
2009-11-06 15:50:44 +0000

    added support for compilation of kermeta code using this
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9057 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: a1a04a36b6bc97f962da0d61e50f2e1390705911

---
+++ uml2_projects/org.kermeta.uml2.profiles.model/templateHelper/util/org/kermeta/compil/runtime/helper/basetypes/UMLResource.java
@@ -0,0 +1,24 @@
+/* $Id: Runtime2EMF.java,v 1.79 2008-09-24 13:51:09 dvojtise Exp $
+ * Project   : Kermeta (First iteration)
+ * File      : Runtime2EMF.java
+ * License   : EPL
+ * Copyright : IRISA / INRIA / Universite de Rennes 1
+ * ----------------------------------------------------------------------------
+ * Creation date : nov 2009
+ * Authors       : dvojtise
+ */
+package org.kermeta.compil.runtime.helper.basetypes;
+
+import kermeta.persistence.EMFRepository;
+import kermeta.persistence.PersistenceFactory;
+import kermeta.persistence.Resource;
+
+public class UMLResource {
+	/**
+     * 
+     * 
+     */
+	public static void addUMLRuntimeUnitFactory() {
+		
+	}
+}
