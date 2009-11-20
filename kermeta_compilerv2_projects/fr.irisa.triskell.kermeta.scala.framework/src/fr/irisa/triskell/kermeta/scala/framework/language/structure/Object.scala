c9490469689e141a3a8928c3282f125573d74016
Olivier Barais <olivier.barais@gmail.com>
2009-11-20 17:41:39 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9138 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 3236c6917551f5e49ab021cacadc07b3aa92ecb4

--- kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.framework/src/fr/irisa/triskell/kermeta/scala/framework/language/structure/Object.scala
+++ kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.framework/src/fr/irisa/triskell/kermeta/scala/framework/language/structure/Object.scala
@@ -1,5 +1,5 @@
 package fr.irisa.triskell.kermeta.scala.framework.language.structure
 
 trait Object extends ObjectAspect{
-
+ 
 }
--- kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.framework/src/fr/irisa/triskell/kermeta/scala/framework/language/structure/ObjectAspect.scala
+++ kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.framework/src/fr/irisa/triskell/kermeta/scala/framework/language/structure/ObjectAspect.scala
@@ -3,7 +3,7 @@ package fr.irisa.triskell.kermeta.scala.framework.language.structure
 import org.eclipse.emf.ecore._
 import org.eclipse.emf.ecore.impl._
 
-trait ObjectAspect extends org.eclipse.emf.ecore.impl.EObjectImpl with EObject {
+trait ObjectAspect extends  EObject {
 	
 	def getMetaClass = this.eClass()
 	def classDefinition = this
