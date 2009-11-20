fa7a56a33651e8283edf36be41218f9585436c30
Olivier Barais <olivier.barais@gmail.com>
2009-11-20 12:41:59 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9127 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 80e876fa81ee2baf9728cb03074e1f4f5fa8eb77

--- kermeta_compilerv2_projects/CompiloKMV2/src/fr/irisa/triskell/kermeta/compilo/scala/rich/richAspect/CallFeatureAspect.scala
+++ kermeta_compilerv2_projects/CompiloKMV2/src/fr/irisa/triskell/kermeta/compilo/scala/rich/richAspect/CallFeatureAspect.scala
@@ -8,7 +8,7 @@ import fr.irisa.triskell.kermeta.language.structure._
 import fr.irisa.triskell.kermeta.language.behavior._
 import java.util._
 
-trait CallFeatureAspect extends EcoreRichAspectImplicit with ObjectAspect {
+trait CallFeatureAspect extends EcoreRichAspectImplicit with CallExpressionAspect {
 	
 	override def generateScalaCode(res : StringBuilder) : Unit = {
 		if ("new".equals(this.getName)){
