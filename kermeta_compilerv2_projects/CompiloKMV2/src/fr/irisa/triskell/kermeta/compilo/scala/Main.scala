f48cc669e223e40a131e94069d1760849f13d5d8
Olivier Barais <olivier.barais@gmail.com>
2009-11-24 19:58:47 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9205 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: fbc25db408d8b30133fcf8a999d4ea64040cf9eb

--- kermeta_compilerv2_projects/CompiloKMV2/src/fr/irisa/triskell/kermeta/compilo/scala/Main.scala
+++ kermeta_compilerv2_projects/CompiloKMV2/src/fr/irisa/triskell/kermeta/compilo/scala/Main.scala
@@ -1,33 +1,36 @@
 package fr.irisa.triskell.kermeta.compilo.scala
-
+  
 import fr.irisa.triskell.kermeta.compilo.scala.rich._
 import scala.collection.JavaConversions._
 import fr.irisa.triskell.kermeta.language._
 import fr.irisa.triskell.kermeta.language.structure._ 
 import fr.irisa.triskell.kermeta.language.behavior._
-import fr.irisa.triskell.kermeta.compilo.scala.loader._
+import fr.irisa.triskell.kermeta.compilo.scala.loader._  
 import fr.irisa.triskell.kermeta.compilo.scala.visitor._
 import fr.irisa.triskell.kermeta.compilo.scala.visitor.impl._
 
 object Main extends EcoreRichAspectImplicit {
   def main(args : Array[String]) : Unit = {
-		  
+	 
 	  var t: LoadModelHelper = new LoadModelHelper()  ;
-	  BehaviorPackage.eINSTANCE.setEFactoryInstance(new RichBehaviorFactoryImpl())
+	  
+	   BehaviorPackage.eINSTANCE.setEFactoryInstance(new RichBehaviorFactoryImpl())
 	  StructurePackage.eINSTANCE.setEFactoryInstance(new RichStructureFactoryImpl())
 		  
 	      
 	//var v : ModelingUnit = t.loadKM("tp1.km");  
 	//var v : ModelingUnit = t.loadKM("../compilerv2_test/tests/000HelloWorld.km");  
-//	var v : ModelingUnit = t.loadKM("../compilerv2_test/tests/001testAssignement2.km")
+	//var v : ModelingUnit = t.loadKM("../compilerv2_test/tests/001testAssignement2.km")
 	//var v : ModelingUnit = t.loadKM("../compilerv2_test/tests/001testAssignement.km") 
   	//var v : ModelingUnit = t.loadKM("../compilerv2_test/tests/002_testArithm.main.km") 
-//	var v : ModelingUnit = t.loadKM("../compilerv2_test/tests/003_testOpCall.main.km")
+//  	var v : ModelingUnit = t.loadKM("../compilerv2_test/tests/003_testOpCall.main.km")
 //  	var v : ModelingUnit = t.loadKM("../compilerv2_test/tests/004_testOpOpCall.main.km")
   	//var v : ModelingUnit = t.loadKM("../compilerv2_test/tests/005_testOpPkgCall.main.km")
- 	var v : ModelingUnit = t.loadKM("../compilerv2_test/tests/006_testClosure.km")
+  	//var v : ModelingUnit = t.loadKM("../compilerv2_test/tests/006_testClosure.km")
+  	
   	//var v : ModelingUnit = t.loadKM("../compilerv2_test/tests/010_testLoop.main.km")
-  //	var v : ModelingUnit = t.loadKM("../compilerv2_test/tests/011_testBenchmark.main.km")
+  	//var v : ModelingUnit = t.loadKM("../compilerv2_test/tests/011_testBenchmark.main.km")
+  	var v : ModelingUnit = t.loadKM("../compilerv2_test/testEcore/012_TypeFromEcore.km")
   	//var res = new StringBuilder
   	var visitorAspect = new ScalaAspectVisitor
   	v.accept(visitorAspect)
@@ -40,8 +43,5 @@ object Main extends EcoreRichAspectImplicit {
 	   
     //v.generateScalaCode(res)
     //Console.println(res.toString())
-	   
-	Util.threadExecutor.shutdown()
-	   
   }
 }
