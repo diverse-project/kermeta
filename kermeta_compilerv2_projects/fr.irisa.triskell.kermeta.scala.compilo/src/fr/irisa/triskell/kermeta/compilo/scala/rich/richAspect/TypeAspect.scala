3c007353673978a1aa7744ab0255d446e7e61dfa
Francois Fouquet <fouquet.f@gmail.com>
2009-12-01 15:46:50 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9291 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 625187c52adfc7c2baea848427c759f06dcb7505

---
+++ kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.compilo/src/fr/irisa/triskell/kermeta/compilo/scala/rich/richAspect/TypeAspect.scala
@@ -0,0 +1,9 @@
+package fr.irisa.triskell.kermeta.compilo.scala.rich.richAspect
+
+trait TypeAspect extends ObjectAspect {
+	
+	override def generateScalaCode(res : StringBuilder) : Unit = {
+		res append "/*TODO TYPE GENCODE */"
+	}
+
+}
