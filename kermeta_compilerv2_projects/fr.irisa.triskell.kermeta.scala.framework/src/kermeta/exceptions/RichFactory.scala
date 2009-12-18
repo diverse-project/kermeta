0a6b20fe6143ff436a193394d1e2a6e6dad6defc
Olivier Barais <olivier.barais@gmail.com>
2009-12-18 10:11:22 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9426 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: ebd9c459abc705a5897572944fb71efdc851b862

--- kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.framework/src/kermeta/exceptions/RichFactory.scala
+++ kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.framework/src/kermeta/exceptions/RichFactory.scala
@@ -2,6 +2,7 @@ package kermeta.exceptions
 object RichFactory {
  def createException : Exception = { new Throwable with Exception }
  def createConstraintViolatedInvException : Exception = { new Throwable with ConstraintViolatedInv  }
+ def createNotImplementedException : Exception = { new Throwable with NotImplementedException  }
  
 }
 
