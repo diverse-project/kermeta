56e8a72a1d79565ae3b487e8eadc776575f07b81
Francois Fouquet <fouquet.f@gmail.com>
2009-12-21 10:17:51 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9438 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 2da6f46440a63e81eb97bce7b8fda9383d2933c4

--- kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.framework/src/fr/irisa/triskell/kermeta/scala/framework/language/structure/ObjectAspect.scala
+++ kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.framework/src/fr/irisa/triskell/kermeta/scala/framework/language/structure/ObjectAspect.scala
@@ -10,7 +10,7 @@ trait ObjectAspect extends EObject with Contracted {
 	def classDefinition = this.asInstanceOf[EClass]
 	def ScalaclassDefinition = classDefinition
 	def typedefinition = this
-	def container = this.eContainer()
+	def container() = this.eContainer()
 	override def equals(o : Any) : Boolean = o == this /*TODO*/ 
 	def isNotEqual(o : Any) : Boolean = !equals(o)
 	def isKindOf(cl : EClass) : Boolean = this.eClass().equals(cl)
@@ -18,7 +18,7 @@ trait ObjectAspect extends EObject with Contracted {
 	def set(prop : EStructuralFeature,o : Any) = this.eSet(prop, o)
 	def isSet(prop : EStructuralFeature) =  this.eIsSet(prop)
 	def unset(prop : EStructuralFeature) = this.eUnset(prop)
-	def oid : Integer = this.hashCode() /*TODO*/
+	def oid() : Int = this.hashCode() /*TODO*/
 	override def toString : String = super.toString()//"["+this.eClass().getName()+":"+oid.toString()+"]"
 	//def isFrozen : Boolean  /*TODO*/
 	def isVoid() : Boolean = false
@@ -27,7 +27,6 @@ trait ObjectAspect extends EObject with Contracted {
 	//def freeze  /*TODO*/
 	//def asType(t : EClass) = this.asInstanceOf[t.getName]
 	//def isInstanceOf(t : EClass) = this.isInstanceOf(t.getMetaClass().getName)
-	
 	
 	
 	/* Contracted */
