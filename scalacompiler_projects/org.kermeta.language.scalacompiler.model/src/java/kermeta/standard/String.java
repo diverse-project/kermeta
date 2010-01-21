fac88d225aa137c210421fe879dec1d53f5794fa
Olivier Barais <olivier.barais@gmail.com>
2010-01-21 13:30:09 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9533 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 50cd53e662411109d19733a55b6051eb33e1b0e7

--- scalacompiler_projects/org.kermeta.language.scalacompiler.model/src/java/kermeta/standard/String.java
+++ scalacompiler_projects/org.kermeta.language.scalacompiler.model/src/java/kermeta/standard/String.java
@@ -11,4 +11,17 @@ public class String {
 	        	Integer.setValue(result, s.lastIndexOf(tok1));
 	        	return result; 
 	   }
+	   public static RuntimeObject insert(RuntimeObject string,RuntimeObject toInsert,RuntimeObject pos) {
+       	java.lang.String s = fr.irisa.triskell.kermeta.runtime.basetypes.String.getValue(string); 
+       	java.lang.String toI = fr.irisa.triskell.kermeta.runtime.basetypes.String.getValue(toInsert); 
+       	java.lang.Integer pos1 = fr.irisa.triskell.kermeta.runtime.basetypes.Integer.getValue(pos); 
+       	RuntimeObject result = string.getFactory().createObjectFromClassName("kermeta::standard::String");
+       	StringBuilder res = new StringBuilder(s);
+       	res =res.insert(pos1, toI);
+       	System.err.println(res.toString());
+       	fr.irisa.triskell.kermeta.runtime.basetypes.String.setValue(result, res.toString());
+      	return result; 
+  
+}
+
 }
