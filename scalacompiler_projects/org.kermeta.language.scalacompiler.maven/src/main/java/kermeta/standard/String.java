5f8d3c7c6f4ecf88f11c65d5a5944d9a0cb9aa23
Francois Fouquet <fouquet.f@gmail.com>
2010-01-25 18:21:36 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9568 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 0b7502c166394d4cbeab1494e3449ee856d511ca

---
+++ scalacompiler_projects/org.kermeta.language.scalacompiler.maven/src/main/java/kermeta/standard/String.java
@@ -0,0 +1,33 @@
+package kermeta.standard;
+
+import fr.irisa.triskell.kermeta.runtime.RuntimeObject;
+import fr.irisa.triskell.kermeta.runtime.basetypes.Integer;
+
+public class String {
+	   public static RuntimeObject lastIndexOf(RuntimeObject prompt, RuntimeObject tok) {
+	        	java.lang.String s = fr.irisa.triskell.kermeta.runtime.basetypes.String.getValue(prompt); 
+	        	java.lang.String tok1 = fr.irisa.triskell.kermeta.runtime.basetypes.String.getValue(tok); 
+	        	RuntimeObject result = prompt.getFactory().createObjectFromClassName("kermeta::standard::Integer");
+	        	Integer.setValue(result, s.lastIndexOf(tok1));
+	        	return result; 
+	   }
+	   
+		   public static RuntimeObject insert(RuntimeObject string,RuntimeObject toInsert,RuntimeObject pos) {
+		        	java.lang.String s = fr.irisa.triskell.kermeta.runtime.basetypes.String.getValue(string); 
+		        	java.lang.String toI = fr.irisa.triskell.kermeta.runtime.basetypes.String.getValue(toInsert); 
+		        	java.lang.Integer pos1 = fr.irisa.triskell.kermeta.runtime.basetypes.Integer.getValue(pos); 
+		        	RuntimeObject result = string.getFactory().createObjectFromClassName("kermeta::standard::String");
+		        	StringBuilder res = new StringBuilder(s);
+		        	res =res.insert(pos1, toI);
+		        	System.err.println(res.toString());
+		        	fr.irisa.triskell.kermeta.runtime.basetypes.String.setValue(result, res.toString());
+		       	return result; 
+		   
+	}
+
+
+}
+
+
+
+
