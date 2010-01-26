e49fa09c5590ff05b46a96ced354839bf446fbc2
mclavreu <mclavreu@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2010-01-26 16:19:59 +0000

    Commiting the whole set of testcases : moved to a new folder
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9653 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 44c97132e6db28f8aea809489b578fe65bc7bffe

--- scalacompiler_projects/org.kermeta.language.scalacompiler.maven/src/test/java/org/kermeta/compiler/test/TestCallExpr.java
+++ scalacompiler_projects/org.kermeta.language.scalacompiler.maven/src/test/java/org/kermeta/compiler/test/TestCallExpr.java
@@ -4,111 +4,142 @@ import org.junit.Test;
 
 public class TestCallExpr extends TestCaseRunnerPlatformDependant {
 
-	
 	@Test
 	public void testAssignment_int_401(){
-		process("testCases/Assignment_int_401.km");
+		process("testCases/CallExpressions/Assignment_int_401.km");
 	}
 	
 	@Test
 	public void testAssignment_bool_402(){
-		process("testCases/Assignment_bool_402.km");
+		process("testCases/CallExpressions/Assignment_bool_402.km");
 	}
 
 	@Test
 	public void testAssignment_string_403(){
-		process("testCases/Assignment_string_403.km");
+		process("testCases/CallExpressions/Assignment_string_403.km");
 	}
 	
 	@Test
 	public void testAssignment_negint_404(){
-		process("testCases/Assignment_negint_404.km");
+		process("testCases/CallExpressions/Assignment_negint_404.km");
 	}
 
 	@Test
 	public void testAssignment_classe_405(){
-		process("testCases/Assignment_classe_405.km");
+		process("testCases/CallExpressions/Assignment_classe_405.km");
 	}
 
 	@Test
 	public void testAssignment_minuszero_406(){
-		process("testCases/Assignment_minuszero_406.km");
+		process("testCases/CallExpressions/Assignment_minuszero_406.km");
 	}
 
 	@Test
 	public void testAssignment_cast_int_407(){
-		process("testCases/Assignment_cast_int_407.km");
+		process("testCases/CallExpressions/Assignment_cast_int_407.km");
 	}
-	
 	
 	@Test
 	public void testAssignment_cast_bool_408(){
-		process("testCases/Assignment_cast_bool_408.km");
+		process("testCases/CallExpressions/Assignment_cast_bool_408.km");
 	}
-	
 	
 	@Test
 	public void testAssignment_cast_string_409(){
-		process("testCases/Assignment_cast_string_409.km");
+		process("testCases/CallExpressions/Assignment_cast_string_409.km");
 	}
 	@Test
 	public void testAssignment_cast_classe_410(){
-		process("testCases/Assignment_cast_classe_410.km");
+		process("testCases/CallExpressions/Assignment_cast_classe_410.km");
 	}
 	
 	@Test
 	public void testCallSuperOperation_no_params_451(){
-		process("testCases/CallSuperOperation_no_params_451.km");
+		process("testCases/CallExpressions/CallSuperOperation_no_params_451.km");
 	}
 	
 	@Test
 	public void testCallSuperOperation_one_param_452(){
-		process("testCases/CallSuperOperation_one_param_452.km");
+		process("testCases/CallExpressions/CallSuperOperation_one_param_452.km");
 	}
 	
 	@Test
 	public void testCallSuperOperation_no_params_return_453(){
-		process("testCases/CallSuperOperation_no_params_return_453.km");
+		process("testCases/CallExpressions/CallSuperOperation_no_params_return_453.km");
 	}
 	
 	@Test
 	public void testCallResult_int_454(){
-		process("testCases/CallResult_int_454.km");
+		process("testCases/CallExpressions/CallResult_int_454.km");
 	}
 	
-	@Test
+	/*@Test
 	public void testCallValue_internal_op_455(){
 		process("testCases/CallValue_internal_op_455.km");
-	}
+	}*/
 	
 	@Test
 	public void testCallResult_String_456(){
-		process("testCases/CallResult_String_456.km");
+		process("testCases/CallExpressions/CallResult_String_456.km");
 	}
 	
 	@Test
 	public void testCallResult_Object_457(){
-		process("testCases/CallResult_Object_457.km");
+		process("testCases/CallExpressions/CallResult_Object_457.km");
 	}
 	
 	@Test
 	public void testCallFeature_new_458(){
-		process("testCases/CallFeature_new_458.km");
+		process("testCases/CallExpressions/CallFeature_new_458.km");
 	}
 	
 	@Test
 	public void testCallFeature_self_param_459(){
-		process("testCases/CallFeature_self_param_459.km");
+		process("testCases/CallExpressions/CallFeature_self_param_459.km");
 	}
-
+	
+	@Test
+	public void testCallFeature_op_class_no_params_460(){
+		process("testCases/CallExpressions/CallFeature_op_class_no_params_460.km");
+	}
+	
+	@Test
+	public void testCallFeature_op_class_params_461(){
+		process("testCases/CallExpressions/CallFeature_op_class_params_461.km");
+	}
+	
+	@Test
+	public void testCallFeature_op_class_params_diff_package_462(){
+		process("testCases/CallExpressions/CallFeature_op_class_params_diff_package_462.km");
+	}
+	
+	@Test
+	public void testCallFeature_attribute_string_same_class_463(){
+		process("testCases/CallExpressions/CallFeature_attribute_string_same_class_463.km");
+	}
+	
+	@Test
+	public void testCallFeature_attribute_Object_diff_class_464(){
+		process("testCases/CallExpressions/CallFeature_attribute_Object_diff_class_464.km");
+	}
+	
 	@Test
 	public void testCallFeature_self_operation_465(){
-		process("testCases/CallFeature_self_operation_465.km");
+		process("testCases/CallExpressions/CallFeature_self_operation_465.km");
 	}
 	
 	@Test
 	public void testCallFeature_obj_operation_operation_466(){
-		process("testCases/CallFeature_obj_operation_operation_466.km");
+		process("testCases/CallExpressions/CallFeature_obj_operation_operation_466.km");
 	}
 	
+	@Test
+	public void testCallFeature_multi_attributes_same_class_467(){
+		process("testCases/CallExpressions/CallFeature_multi_attributes_same_class_467.km");
+	}
+	
+	@Test
+	public void testCallFeature_plus_468(){
+		process("testCases/CallExpressions/CallFeature_plus_468.km");
+	}
 }
