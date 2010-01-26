5aa1876b452a9481dcff7158ce3dc28873da76ad
Arnaud Blouin <arnaud.blouin@irisa.fr>
2010-01-26 15:37:12 +0000

    tests
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9646 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 85f442c62c487480385ddada048532c5554883c3

--- scalacompiler_projects/org.kermeta.language.scalacompiler.maven/src/test/java/org/kermeta/compiler/test/TestConditionalExpr.java
+++ scalacompiler_projects/org.kermeta.language.scalacompiler.maven/src/test/java/org/kermeta/compiler/test/TestConditionalExpr.java
@@ -5,6 +5,21 @@ import org.junit.Test;
 public class TestConditionalExpr extends TestCaseRunnerSetup {
 	
 	@Test
+	public void testConditional_unary_300(){
+		process("testCases/conditional/conditional_unary_300.km");
+	}
+	
+	@Test
+	public void testConditional_2var_constVar_310(){
+		process("testCases/conditional/conditional_2var_constVar_310.km");
+	}
+	
+	@Test
+	public void testConditional_2var_varConst_310(){
+		process("testCases/conditional/conditional_2var_varConst_310.km");
+	}
+	
+	@Test
 	public void testConditional_2var_constConst_310(){
 		process("testCases/conditional/conditional_2var_constConst_310.km");
 	}
@@ -45,12 +60,12 @@ public class TestConditionalExpr extends TestCaseRunnerSetup {
 	}
 	
 	@Test
-	public void testConditional_unary_300(){
-		process("testCases/conditional/conditional_unary_300.km");
+	public void testLoop_350(){
+		process("testCases/conditional/loop_350.km");
 	}
 	
 	@Test
-	public void testLoop_350(){
+	public void testLoop_351(){
 		process("testCases/conditional/loop_350.km");
 	}
 }
