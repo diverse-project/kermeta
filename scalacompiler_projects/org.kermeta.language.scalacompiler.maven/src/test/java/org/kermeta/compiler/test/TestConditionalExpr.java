752c9fdb7b274bd11f31f87483c0116962b61726
bmorin <bmorin@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2010-01-26 14:41:34 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9629 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: eb29ed92de753afb4ec035e2319d9e53c94df4b3

--- scalacompiler_projects/org.kermeta.language.scalacompiler.maven/src/test/java/org/kermeta/compiler/test/TestConditionalExpr.java
+++ scalacompiler_projects/org.kermeta.language.scalacompiler.maven/src/test/java/org/kermeta/compiler/test/TestConditionalExpr.java
@@ -6,51 +6,51 @@ public class TestConditionalExpr extends TestCaseRunnerSetup {
 	
 	@Test
 	public void testConditional_2var_constConst_310(){
-		process("testCases/conditional_2var_constConst_310.km");
+		process("testCases/conditional/conditional_2var_constConst_310.km");
 	}
 	
 	@Test
 	public void testConditional_2var_opOp_310(){
-		process("testCases/conditional_2var_opOp_310.km");
+		process("testCases/conditional/conditional_2var_opOp_310.km");
 	}
 
 	@Test
 	public void testConditional_2var_varVar_310(){
-		process("testCases/conditional_2var_varVar_310.km");
+		process("testCases/conditional/conditional_2var_varVar_310.km");
 	}
 	
 	@Test
 	public void testConditional_2varOr_constConst_311(){
-		process("testCases/conditional_2varOr_constConst_311.km");
+		process("testCases/conditional/conditional_2varOr_constConst_311.km");
 	}	
 	
 	@Test
 	public void testConditional_3var_320(){
-		process("testCases/Conditional_3var_320.km");
+		process("testCases/conditional/Conditional_3var_320.km");
 	}
 	
 	@Test
 	public void testConditional_3var_321(){
-		process("testCases/Conditional_3var_321.km");
+		process("testCases/conditional/Conditional_3var_321.km");
 	}
 	
 	@Test
 	public void testConditional_asignment_330(){
-		process("testCases/conditional_asignment_330.km");
+		process("testCases/conditional/conditional_asignment_330.km");
 	}
 	
 	@Test
 	public void testConditional_nestedIf_340(){
-		process("testCases/conditional_nestedIf_340.km");
+		process("testCases/conditional/conditional_nestedIf_340.km");
 	}
 	
 	@Test
 	public void testConditional_unary_300(){
-		process("testCases/conditional_unary_300.km");
+		process("testCases/conditional/conditional_unary_300.km");
 	}
 	
 	@Test
 	public void testLoop_350(){
-		process("testCases/loop_350.km");
+		process("testCases/conditional/loop_350.km");
 	}
 }
