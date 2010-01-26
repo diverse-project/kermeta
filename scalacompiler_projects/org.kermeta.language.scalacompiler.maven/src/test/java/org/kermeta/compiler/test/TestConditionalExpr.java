68185b83f3ec7833ea5faa065160f4e22f24f022
bmorin <bmorin@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2010-01-26 14:38:09 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9627 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: a6a1f25390b8aceb1f9c10e46af060b5e1284125

--- scalacompiler_projects/org.kermeta.language.scalacompiler.maven/src/test/java/org/kermeta/compiler/test/TestConditionalExpr.java
+++ scalacompiler_projects/org.kermeta.language.scalacompiler.maven/src/test/java/org/kermeta/compiler/test/TestConditionalExpr.java
@@ -17,5 +17,40 @@ public class TestConditionalExpr extends TestCaseRunnerSetup {
 	@Test
 	public void testConditional_2var_varVar_310(){
 		process("testCases/conditional_2var_varVar_310.km");
+	}
+	
+	@Test
+	public void testConditional_2varOr_constConst_311(){
+		process("testCases/conditional_2varOr_constConst_311.km");
 	}	
+	
+	@Test
+	public void testConditional_3var_320(){
+		process("testCases/Conditional_3var_320.km");
+	}
+	
+	@Test
+	public void testConditional_3var_321(){
+		process("testCases/Conditional_3var_321.km");
+	}
+	
+	@Test
+	public void testConditional_asignment_330(){
+		process("testCases/conditional_asignment_330.km");
+	}
+	
+	@Test
+	public void testConditional_nestedIf_340(){
+		process("testCases/conditional_nestedIf_340.km");
+	}
+	
+	@Test
+	public void testConditional_unary_300(){
+		process("testCases/conditional_unary_300.km");
+	}
+	
+	@Test
+	public void testLoop_350(){
+		process("testCases/loop_350.km");
+	}
 }
