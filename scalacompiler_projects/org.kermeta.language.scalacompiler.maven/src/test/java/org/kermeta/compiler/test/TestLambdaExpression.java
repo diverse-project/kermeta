68b8bd5e6d3ef198be48853429a905e2903d0c69
jcadavid <jcadavid@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2010-01-27 14:50:25 +0000

    Five tests for Lambda expressions, of which one does not yet pass (forAll operation still does not compile properly)
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9696 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: fcb4e9513612a489db35d6811dda05768af8332e

--- scalacompiler_projects/org.kermeta.language.scalacompiler.maven/src/test/java/org/kermeta/compiler/test/TestLambdaExpression.java
+++ scalacompiler_projects/org.kermeta.language.scalacompiler.maven/src/test/java/org/kermeta/compiler/test/TestLambdaExpression.java
@@ -15,9 +15,26 @@ public class TestLambdaExpression extends TestCaseRunnerSetup {
 			// System.out.println(this.getClass().getResource("/testCases"));
 		 process("testCases/LambdaExpression_testCases/LambdaExpression_CollectionEach_501.km");
 	 }
+	 
 	 @Test
      public void testLambdaExpression_CollectionEach_502() {
 			// System.out.println(this.getClass().getResource("/testCases"));
 		 process("testCases/LambdaExpression_testCases/LambdaExpression_CollectionEach_502.km");
 	}
-}
+	 
+	 
+	 @Test
+     public void testLambdaExpression_CollectionSelect() {
+			// System.out.println(this.getClass().getResource("/testCases"));
+		 process("testCases/LambdaExpression_testCases/LambdaExpression_CollectionSelect_501.km");
+	}
+	
+	/*
+	 * This test does not pass. The forAll operation is not implemented in scala, and a workaround still hasn't yet been implemented.
+	 */
+	@Test
+    public void testLambdaExpression_CollectionForAll() {
+			// System.out.println(this.getClass().getResource("/testCases"));
+		 process("testCases/LambdaExpression_testCases/LambdaExpression_CollectionForAll_501.km");
+	}
+}	
