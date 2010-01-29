b64b0f151f272f78f360913f28a7bbadf3901715
jfalcou <jfalcou@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2010-01-29 15:40:32 +0000

    MarkerHelper for checkInvariant
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9725 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: aaa900e356db850e15abfcd7977cc5787fa3dce2

---
+++ kermeta_projects/org.kermeta.compiler.generator/templateHelper/util/org/kermeta/compil/runtime/helper/io/MarkerHelper.java
@@ -0,0 +1,28 @@
+/* $Id: MarkerHelper.java,v 1.6 2009-11-26 17:10:37 jfalcou Exp $
+ * Project : Kermeta (First iteration)
+ * File : MarkerHelper.java
+ * License : EPL
+ * Copyright : IRISA / Universite de Rennes 1
+ * ----------------------------------------------------------------------------
+ * Creation date : Mar 4, 2005
+ * Author : jfalcou
+ * Description : describe here file content
+ * TODO : 
+ * 	- write here your TODO actions
+ *  - ...
+ */ 
+
+package org.kermeta.compil.runtime.helper.io;
+public class MarkerHelper {
+
+	public static void markFile(String uri, String message) {
+		
+		//System.out.println("Marking file " + uri + " with message " + message);
+		
+	}
+	
+	public static void cleanFile(String uri) {
+		//System.out.println("Cleaning file " + uri);
+	}
+	
+}
