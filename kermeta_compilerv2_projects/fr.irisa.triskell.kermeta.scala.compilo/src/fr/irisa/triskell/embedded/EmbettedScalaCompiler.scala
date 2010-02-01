5ca4be118dd70bd970771fc7020b026b4b2e1111
Francois Fouquet <fouquet.f@gmail.com>
2010-02-01 16:07:19 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9750 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: f2fbdee5408a883f788f8ea50a3b7df664d6fd52

--- kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.compilo/src/fr/irisa/triskell/embedded/EmbettedScalaCompiler.scala
+++ kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.compilo/src/fr/irisa/triskell/embedded/EmbettedScalaCompiler.scala
@@ -1,10 +1,15 @@
 package fr.irisa.triskell.embedded
 
+import fr.irisa.triskell.kermeta.compilo.scala.LogAspect
 import java.io.File;
 
-object EmbettedScalaCompiler {
+object EmbettedScalaCompiler extends LogAspect {
 	
 	def compile(srcPATH : String, outputPATH : String, cleanOutput : Boolean, jars : List[String]) : Int = {
+		
+		var startTime = System.currentTimeMillis
+		log.info("Scala compilation step begin")
+		
 		/* Cleaning step */
 		if(cleanOutput){
 			InternalCompilerHelper.deleteDirRecursive(new File(outputPATH))
@@ -15,19 +20,18 @@ object EmbettedScalaCompiler {
 		/* Build class path */
 		
 		var classpath : StringBuilder = new StringBuilder("."+File.pathSeparator)
+		for(path <- jars) { classpath.append(path+File.pathSeparator) }
 		
-		classpath.append("/home/barais/workspaces/kermetaCompilerRuntime2/fr.irisa.triskell.kermeta.scala.compilo/target/classes:/home/barais/.m2/repository/org/scala-lang/scala-library/2.8.0.r18462-b20090811081019/scala-library-2.8.0.r18462-b20090811081019.jar:/home/barais/.m2/repository/fr/irisa/triskell/kermeta.model/1.3.0/kermeta.model-1.3.0.jar:/home/barais/.m2/repository/fr/irisa/triskell/kermeta.emf/2.5.0/kermeta.emf-2.5.0.jar:/home/barais/.m2/repository/fr/irisa/triskell/kermeta.emf.common/2.5.0/kermeta.emf.common-2.5.0.jar:/home/barais/.m2/repository/fr/irisa/triskell/kermeta.emf.ecore/2.5.0/kermeta.emf.ecore-2.5.0.jar:/home/barais/.m2/repository/fr/irisa/triskell/kermeta.emf.ecore.xmi/2.5.0/kermeta.emf.ecore.xmi-2.5.0.jar:/home/barais/workspaces/kermetaCompilerRuntime2/fr.irisa.triskell.kermeta.scala.framework/target/classes:/home/barais/.m2/repository/org/scala-lang/scala-library/2.8.0.Beta1-RC1/scala-library-2.8.0.Beta1-RC1.jar:/home/barais/workspaces/kermetaCompilerRuntime2/fr.irisa.triskell.kermeta.scala.loader/target/classes:/home/barais/.m2/repository/org/slf4j/slf4j-log4j12/1.5.9.RC1/slf4j-log4j12-1.5.9.RC1.jar:/home/barais/.m2/repository/org/slf4j/slf4j-api/1.5.9.RC1/slf4j-api-1.5.9.RC1.jar:/home/barais/.m2/repository/log4j/log4j/1.2.15/log4j-1.2.15.jar:/home/barais/.m2/repository/org/antlr/stringtemplate/3.2.1/stringtemplate-3.2.1.jar:/home/barais/.m2/repository/antlr/antlr/2.7.7/antlr-2.7.7.jar:/home/barais/.m2/repository/org/scala-lang/scala-compiler/2.8.0.r18462-b20090811081019/scala-compiler-2.8.0.r18462-b20090811081019.jar")
-		//classpath.append("/opt/eclipse/configuration/org.eclipse.osgi/bundles/275/1/.cp/lib/scala-library.jar:/opt/eclipse/configuration/org.eclipse.osgi/bundles/275/1/.cp/lib/scala-dbc.jar:/opt/eclipse/configuration/org.eclipse.osgi/bundles/275/1/.cp/lib/scala-swing.jar:/home/barais/.m2/repository/fr/irisa/triskell/kermeta.model/1.3.0/kermeta.model-1.3.0.jar:/home/barais/.m2/repository/fr/irisa/triskell/kermeta.emf/2.5.0/kermeta.emf-2.5.0.jar:/home/barais/.m2/repository/fr/irisa/triskell/kermeta.emf.common/2.5.0/kermeta.emf.common-2.5.0.jar:/home/barais/.m2/repository/fr/irisa/triskell/kermeta.emf.ecore/2.5.0/kermeta.emf.ecore-2.5.0.jar:/home/barais/.m2/repository/fr/irisa/triskell/kermeta.emf.ecore.xmi/2.5.0/kermeta.emf.ecore.xmi-2.5.0.jar:/home/barais/workspaces/kermetaCompilerRuntime2/fr.irisa.triskell.kermeta.scala.framework/target/classes:/home/barais/.m2/repository/org/slf4j/slf4j-log4j12/1.5.9.RC1/slf4j-log4j12-1.5.9.RC1.jar:/home/barais/.m2/repository/org/slf4j/slf4j-api/1.5.9.RC1/slf4j-api-1.5.9.RC1.jar:/home/barais/.m2/repository/log4j/log4j/1.2.15/log4j-1.2.15.jar:/home/barais/.m2/repository/org/antlr/stringtemplate/3.2.1/stringtemplate-3.2.1.jar:/home/barais/.m2/repository/antlr/antlr/2.7.7/antlr-2.7.7.jar")
-			//for(path <- jars) { println(path) ; if (!(path.contains("scala") && path.contains(".m2")) ) classpath.append(path+File.pathSeparator) }
-		println(classpath)
-		var compilParams = List( "-d",outputPATH,"-classpath",classpath.toString) ++ listSrcFiles
- 		System.setProperty("scala.home", "");
-		System.setProperty("user.home", "/home/barais");
-
+		var compilParams = List("-d",outputPATH,"-classpath",classpath.toString) ++ listSrcFiles
+		
 		/* Compilation step */
 		scala.tools.nsc.Main.process(compilParams.toArray)
+		
+		var endTime= System.currentTimeMillis() - startTime
+		log.info("Scala compilation step complete in "+(endTime)+" millisecondes ")
+		
 		return if (scala.tools.nsc.Main.reporter.hasErrors) 1 else 0
-	}  
-	 
+	}
+	
 
 }
