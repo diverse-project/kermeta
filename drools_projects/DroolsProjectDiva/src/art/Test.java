27a6294a889ce143614133ab91657b61e1a48c7f
Olivier Barais <olivier.barais@gmail.com>
2010-02-10 12:15:04 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9889 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 0a37f3a658251e86027b9d4d1c77534f893a82d0

--- drools_projects/DroolsProjectDiva/src/art/Test.java
+++ drools_projects/DroolsProjectDiva/src/art/Test.java
@@ -1,5 +1,6 @@
 package art;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 import main.TreeIterable;
@@ -13,6 +14,7 @@ import org.drools.io.ResourceFactory;
 import org.drools.runtime.StatelessKnowledgeSession;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
@@ -23,35 +25,57 @@ public class Test {
 
 	
 	private static System s;
+	private org.eclipse.emf.ecore.resource.Resource resource;
+	private KnowledgeBase kbase;
 
 
 	public static void main(String[] args) {
 		
 		Test t = new Test();
-		s = t.loadArtModel("model1.art");
-		t.process();
+		s = t.loadArtModel("/home/barais/workspaces/divaDrools/DroolsGenerator/base/thales.base.art");
+		long start = java.lang.System.currentTimeMillis();
+		t.init();
+		t.process("1.drl");
+		t.process("2.drl");
+		t.process("3.drl");
+		t.process("4.drl");
+		t.process("5.drl");
+		t.process("6.drl");
+		long end = java.lang.System.currentTimeMillis();
+		java.lang.System.err.println("Execution time was "+(end-start)+" ms.");
+		
+		t.saveArtModel("/home/barais/workspaces/divaDrools/DroolsGenerator/base/thales.base1.art",t.resource);
 		
 	}
 	
-	void process(){
+	void init(){
+		
+		kbase = KnowledgeBaseFactory.newKnowledgeBase();
+	}
+	
+	void process(String drl){
 		KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
 		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
-		kbuilder.add( ResourceFactory.newClassPathResource( "JoinPoint6.drl", getClass() ),
+		kbuilder.add( ResourceFactory.newClassPathResource( drl, getClass() ),
 		              ResourceType.DRL );
 		if ( kbuilder.hasErrors() ) {
 			java.lang.System.err.println( kbuilder.getErrors().toString() );
 		}
+		kbase.getKnowledgePackages().clear();
 		kbase.addKnowledgePackages( kbuilder.getKnowledgePackages() );
+		
+		
 		StatelessKnowledgeSession ksession = kbase.newStatelessKnowledgeSession();
 		
 		java.util.List<HashMap<String,EObject>> globalList = new java.util.ArrayList<HashMap<String,EObject>>( );
 		ksession.setGlobal("list", globalList);
+		java.util.Map uniqueobjects = new java.util.HashMap( );
+		ksession.setGlobal("uniqueobjects", uniqueobjects);
 		
 		
 		
 		
 		
-		long start = java.lang.System.currentTimeMillis();
 
 		/*List<EObject> allcontents = new ArrayList<EObject>();
 		Iterator<EObject> objs = s.eResource().getAllContents();
@@ -61,14 +85,8 @@ public class Test {
 			
 		}*/
 		
-		ksession.execute(new TreeIterable<EObject>(s.eResource().getAllContents()));
-		long end = java.lang.System.currentTimeMillis();
-		java.lang.System.err.println("Execution time was "+(end-start)+" ms.");
+		ksession.execute(new TreeIterable<EObject>(resource.getAllContents()));
 		java.lang.System.err.println("Nombre de résultat trouvé "  + globalList.size());
-		for (HashMap<String,EObject> map : globalList){
-			java.lang.System.out.println("i1 = " +((ComponentInstance)map.get("i1")).getName());
-			java.lang.System.out.println("i2 = " + ((ComponentInstance)map.get("i2")).getName());
-		}
 		
 		
 		
@@ -93,11 +111,42 @@ public class Test {
 		rs.getPackageRegistry().put(art.ArtPackage.eNS_URI, art.ArtPackage.eINSTANCE);
 		
 		URI uri1  = URI.createURI(uri);//.replace("platform:/resource/",EcorePackages.workspaceURI).replace("platform:/plugin/",EcorePackages.pluginURI ));
-		org.eclipse.emf.ecore.resource.Resource resource = rs.getResource(uri1,true);
+		resource = rs.getResource(uri1,true);
 		
 		System s = (System) resource.getContents().get(0);
 		return s;
 
 
 	}
+	
+	void saveArtModel(String uri, Resource r){
+		ResourceSetImpl rs = new ResourceSetImpl(); 
+		
+		//EcorePackageImpl.init();
+		org.eclipse.emf.ecore.resource.Resource.Factory.Registry f = rs.getResourceFactoryRegistry();
+		
+		java.util.Map<String,Object> m = f.getExtensionToFactoryMap();
+		//f.get
+		
+		m.put("ecore",new EcoreResourceFactoryImpl());
+		m.put("*",new XMIResourceFactoryImpl());
+		
+		
+		rs.getPackageRegistry().put(art.ArtPackage.eNS_URI, art.ArtPackage.eINSTANCE);
+		
+		URI uri1  = URI.createURI(uri);//.replace("platform:/resource/",EcorePackages.workspaceURI).replace("platform:/plugin/",EcorePackages.pluginURI ));
+		
+		r.setURI(uri1);
+		
+		try {
+			r.save(new HashMap());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+
+	}
+
 }
