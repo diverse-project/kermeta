3ad251e0bb3a284d90ed2c48583866f98bdca614
marieg <marieg@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2010-02-11 16:39:01 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@9970 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: f33b8d91d4faaeb5ddf4b28e32d6c6a95ffa2b05

---
+++ samples.fsm_projects/fr.irisa.triskell.kermeta.samples.fsm.gmf.diagram/src/fsm/diagram/preferences/DiagramGeneralPreferencePage.java
@@ -0,0 +1,19 @@
+package fsm.diagram.preferences;
+
+import org.eclipse.gmf.runtime.diagram.ui.preferences.DiagramsPreferencePage;
+
+import fsm.diagram.part.FsmDiagramEditorPlugin;
+
+/**
+ * @generated
+ */
+public class DiagramGeneralPreferencePage extends DiagramsPreferencePage {
+
+	/**
+	 * @generated
+	 */
+	public DiagramGeneralPreferencePage() {
+		setPreferenceStore(FsmDiagramEditorPlugin.getInstance()
+				.getPreferenceStore());
+	}
+}
---
+++ samples.fsm_projects/fr.irisa.triskell.kermeta.samples.fsm.gmf.diagram/src/fsm/diagram/preferences/DiagramPrintingPreferencePage.java
@@ -0,0 +1,19 @@
+package fsm.diagram.preferences;
+
+import org.eclipse.gmf.runtime.diagram.ui.preferences.PrintingPreferencePage;
+
+import fsm.diagram.part.FsmDiagramEditorPlugin;
+
+/**
+ * @generated
+ */
+public class DiagramPrintingPreferencePage extends PrintingPreferencePage {
+
+	/**
+	 * @generated
+	 */
+	public DiagramPrintingPreferencePage() {
+		setPreferenceStore(FsmDiagramEditorPlugin.getInstance()
+				.getPreferenceStore());
+	}
+}
