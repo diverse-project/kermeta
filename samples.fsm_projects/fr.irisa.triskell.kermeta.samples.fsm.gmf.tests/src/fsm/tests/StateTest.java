dbf847750aa70953337f7ddc034bbbb2d138bfad
marieg <marieg@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2010-02-12 09:52:48 +0000

    Tests for GMF Diagram Editor
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10051 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 528bd9767442f890b65628af8240344f333f6af9

---
+++ samples.fsm_projects/fr.irisa.triskell.kermeta.samples.fsm.gmf.tests/src/fsm/tests/StateTest.java
@@ -0,0 +1,93 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package fsm.tests;
+
+import fsm.FsmDemoFactory;
+import fsm.State;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>State</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class StateTest extends TestCase {
+
+	/**
+	 * The fixture for this State test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected State fixture = null;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(StateTest.class);
+	}
+
+	/**
+	 * Constructs a new State test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public StateTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this State test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(State fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this State test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected State getFixture() {
+		return fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(FsmDemoFactory.eINSTANCE.createState());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //StateTest
