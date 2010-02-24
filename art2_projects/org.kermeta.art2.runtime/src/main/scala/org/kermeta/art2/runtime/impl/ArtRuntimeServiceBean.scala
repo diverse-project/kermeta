e762b563cca64bd2f947a2286e665f7250130660
Francois Fouquet <fouquet.f@gmail.com>
2010-02-24 12:08:03 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10184 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: bbbcde0463c25b57c2e57ee89eb137f8a770c900

---
+++ art2_projects/org.kermeta.art2.runtime/src/main/scala/org/kermeta/art2/runtime/impl/ArtRuntimeServiceBean.scala
@@ -0,0 +1,73 @@
+
+package org.kermeta.art2.runtime.impl
+
+import org.kermeta.art2.runtime.ArtRuntimeService
+import java.util.Properties
+import org.kermeta.artKomparator.LauncherAspect
+
+class ArtRuntimeServiceBean extends ArtRuntimeService {
+	
+	var actualLaunch : org.kermeta.artKomparator.LauncherAspect = null
+	
+	var dictionary : Properties = null
+	def setDictionary(prop : Properties) = dictionary = prop
+	def getDictionary : Properties = dictionary
+	
+	def update(url : String) = {
+		actualLaunch.launchFromURL(url)
+	}
+	
+	
+	def init() = {		
+		var testtemp = classOf[org.eclipse.emf.ecore.impl.EObjectImpl]
+		var testtemp2 = classOf[org.kermeta.artKomparator.RichLauncher]
+		println(testtemp)
+		println(testtemp2)
+		
+	{
+	//println(classOf[org.eclipse.emf.ecore.impl.EcorePackageImpl].getDeclaredConstructors.size)
+		
+	var ctab  = classOf[org.eclipse.emf.ecore.impl.EcorePackageImpl].getDeclaredConstructors
+	var c = ctab(0)
+	c.setAccessible(true);
+	var pack : org.eclipse.emf.ecore.impl.EcorePackageImpl =  c.newInstance().asInstanceOf[org.eclipse.emf.ecore.impl.EcorePackageImpl]
+	pack.setEFactoryInstance(org.eclipse.emf.ecoreScalaAspect.RichFactory)
+ 	var f : java.lang.reflect.Field = classOf[org.eclipse.emf.ecore.impl.EPackageImpl].getDeclaredField("ecoreFactory")
+	f.setAccessible(true)
+	f.set(pack, org.eclipse.emf.ecoreScalaAspect.RichFactory)
+	org.eclipse.emf.ecore.EPackage.Registry.INSTANCE.put(org.eclipse.emf.ecore.EcorePackage.eNS_URI, pack)
+	kermeta.persistence.EcorePackages.getPacks().put(org.eclipse.emf.ecore.EcorePackage.eNS_URI, pack)
+	org.eclipse.emf.ecore.impl.EcorePackageImpl.init
+}
+
+{
+	var pack : art2.Art2Package = art2.impl.Art2PackageImpl.init
+	org.eclipse.emf.ecore.EPackage.Registry.INSTANCE.put(art2.Art2Package.eNS_URI, pack)
+	kermeta.persistence.EcorePackages.getPacks().put(art2.Art2Package.eNS_URI, pack)
+	pack.setEFactoryInstance(art2ScalaAspect.RichFactory)
+}
+
+/*
+{
+	var pack : fr.irisa.triskell.kermeta.language.structure.StructurePackage = fr.irisa.triskell.kermeta.language.structure.impl.StructurePackageImpl.init
+	org.eclipse.emf.ecore.EPackage.Registry.INSTANCE.put(fr.irisa.triskell.kermeta.language.structure.StructurePackage.eNS_URI, pack)
+	kermeta.persistence.EcorePackages.getPacks().put(fr.irisa.triskell.kermeta.language.structure.StructurePackage.eNS_URI, pack)
+	pack.setEFactoryInstance(fr.irisa.triskell.kermeta.language.structureScalaAspect.RichFactory)
+}
+
+
+{
+	var pack : fr.irisa.triskell.kermeta.language.behavior.BehaviorPackage = fr.irisa.triskell.kermeta.language.behavior.impl.BehaviorPackageImpl.init
+	org.eclipse.emf.ecore.EPackage.Registry.INSTANCE.put(fr.irisa.triskell.kermeta.language.behavior.BehaviorPackage.eNS_URI, pack)
+	kermeta.persistence.EcorePackages.getPacks().put(fr.irisa.triskell.kermeta.language.behavior.BehaviorPackage.eNS_URI, pack)
+	pack.setEFactoryInstance(fr.irisa.triskell.kermeta.language.behaviorScalaAspect.RichFactory)
+}	
+*/
+		actualLaunch = org.kermeta.artKomparator.RichFactory.createLauncher.asInstanceOf[LauncherAspect]
+		actualLaunch.KersetNodeName(dictionary.getProperty("node.name"))
+		actualLaunch.KersetHotdeployPATH(dictionary.getProperty("deploy.path"))//"/Users/ffouquet/Servers/apache-felix-karaf-1.2.0/deploy")
+
+	}//fin init
+	
+
+}
