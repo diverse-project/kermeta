a577d75b9137d6da9d6fd665e7817969b66bc00c
bmorin <bmorin@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2010-02-25 13:16:58 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10224 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: ccafe7d4373e9a57ec02d50ff9c3ce399d226138

--- drools_projects/org.kermeta.smartadapters.drools5.diva/src/org/kermeta/smartadapters/drools/utils/AspectComparator.java
+++ drools_projects/org.kermeta.smartadapters.drools5.diva/src/org/kermeta/smartadapters/drools/utils/AspectComparator.java
@@ -6,15 +6,25 @@ public class AspectComparator implements Comparator<String> {
 
 	@Override
 	public int compare(String arg0, String arg1) {
-		int position0 = Integer.parseInt(arg0.split("_")[0].split("compiled")[1].substring(1));
-		int position1 = Integer.parseInt(arg1.split("_")[0].split("compiled")[1].substring(1));
+		
+		String tempArg0 = arg0.split("_")[arg0.split("_").length-2];
+		String tempArg1 = arg1.split("_")[arg1.split("_").length-2]; 
+		
+		//System.out.println(tempArg0);
+		//System.out.println(tempArg1);
+		
+		//System.out.println(tempArg0.substring(tempArg0.length()-2, tempArg0.length()));
+		//System.out.println(tempArg1.substring(tempArg1.length()-2, tempArg1.length()));
+		
+		int position0 = Integer.parseInt(tempArg0.substring(tempArg0.length()-2, tempArg0.length()));
+		int position1 = Integer.parseInt(tempArg1.substring(tempArg1.length()-2, tempArg1.length()));
 		
 		if(position0 > position1)
 			return 1;
 		else if(position0 < position1)
 			return -1;
 		else
-			if (!arg0.split("_")[1].equals(arg1.split("_")[1]))
+			if (!arg0.equals(arg1))
 				return 1;
 			else
 				return 0;
