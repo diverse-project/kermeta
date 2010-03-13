648928030de92857ad0d4adb7afe04c7aa0650bc
Olivier Barais <olivier.barais@gmail.com>
2010-03-13 20:53:35 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10462 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 14ad0fcaa1ead7255ba9cc37d3d79434b65eb5a7

--- kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.compilo/src/fr/irisa/triskell/kermeta/compilo/scala/Compiler.scala
+++ kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.compilo/src/fr/irisa/triskell/kermeta/compilo/scala/Compiler.scala
@@ -1,49 +1,63 @@
 package fr.irisa.triskell.kermeta.compilo.scala
 
 import fr.irisa.triskell.kermeta.compilo.scala.rich._
+import kermeta.utils.ReflexivityLoader
 import scala.collection.JavaConversions._
 import fr.irisa.triskell.kermeta.language._
 import fr.irisa.triskell.kermeta.language.structure._
 import fr.irisa.triskell.kermeta.language.behavior._
 import fr.irisa.triskell.kermeta.compilo.scala.loader._
 import fr.irisa.triskell.kermeta.compilo.scala.visitor._
+import fr.irisa.triskell.kermeta.compilo.scala.visitor.impl.VisitorAsyncUtility
 import fr.irisa.triskell.kermeta.compilo.scala.visitor.impl._
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.Executors
+import java.io.File
 import java.util.concurrent.ExecutorService
 
 class Compiler extends RichAspectImplicit with LogAspect {
 
-	def compile(url : String){
-		log.debug("Cleaning Output Step")  
-		Util.cleanFolder(GlobalConfiguration.outputFolder)
-		/* Init Factory Step */
-		var t: LoadModelHelper = new LoadModelHelper()
-		BehaviorPackage.eINSTANCE.setEFactoryInstance(new RichBehaviorFactoryImpl())
-		StructurePackage.eINSTANCE.setEFactoryInstance(new RichStructureFactoryImpl())
-		Util.threadExecutor = Executors.newCachedThreadPool() /* Init new Thread Pool */
+    def compile(url : String){
+        log.debug("Cleaning Output Step")
+        Util.cleanFolder(GlobalConfiguration.outputFolder)
+        /* Init Factory Step */
+        var t: LoadModelHelper = new LoadModelHelper()
+        BehaviorPackage.eINSTANCE.setEFactoryInstance(new RichBehaviorFactoryImpl())
+        StructurePackage.eINSTANCE.setEFactoryInstance(new RichStructureFactoryImpl())
+        Util.threadExecutor = Executors.newCachedThreadPool() /* Init new Thread Pool */
 
-		/* Loading Model KM Step */
-		var startTime = System.currentTimeMillis
-		var v : ModelingUnit = t.loadKM(url) /* Load KM Model */
-	  	var midTime= System.currentTimeMillis() - startTime
-	  	log.info("Loading KM model step complete in "+(midTime)+" millisecondes ")
-		startTime = System.currentTimeMillis
-		/* Target Model Aspect Generation */
-		var visitorAspect = new ScalaAspectVisitor
+        /* Loading Model KM Step */
+        var startTime = System.currentTimeMillis
+        var v : ModelingUnit = t.loadKM(url) /* Load KM Model */
 
-		var futur = VisitorAsyncUtility.runAfterCallback(v,visitorAspect)
-		/* Utility Files & Factory Generation */
-		var visitorImplicitFactory = new ScalaFactoryAndImplicitVisitor
-		VisitorAsyncUtility.runAfter(v,visitorImplicitFactory)
+        println(GlobalConfiguration.outputFolder)
+        var fi  = new File(url);
+        var fo =  new File(GlobalConfiguration.outputFolder +File.separator+".."+File.separator + "resources").getCanonicalFile
+        fo.mkdir
+        fo = new File(GlobalConfiguration.outputFolder  +File.separator+".."+File.separator + "resources"+File.separator +"Reflexivity.km" ).getCanonicalFile
+        if (fo.exists)
+            fo.delete
 
-		/* Synchronisation Step */
-		futur.get /* Waiting for ScalaAspectVisitor finish submit subtask before close pool ()  */
-		Util.threadExecutor.shutdown /* Send ended signal to pool */
-		Util.threadExecutor.awaitTermination(600,TimeUnit.SECONDS) /* Waiting for all tasks finished */
-		/* End step */
-		var endTime= System.currentTimeMillis() - startTime
-		log.info("Compilation step complete in "+(endTime)+" millisecondes ")
-	}
+        fo.createNewFile
+        ReflexivityLoader.copyFile(fi, fo);
+        var midTime= System.currentTimeMillis() - startTime
+        log.info("Loading KM model step complete in "+(midTime)+" millisecondes ")
+        startTime = System.currentTimeMillis
+        /* Target Model Aspect Generation */
+        var visitorAspect = new ScalaAspectVisitor
+
+        var futur = VisitorAsyncUtility.runAfterCallback(v,visitorAspect)
+        /* Utility Files & Factory Generation */
+        var visitorImplicitFactory = new ScalaFactoryAndImplicitVisitor
+        VisitorAsyncUtility.runAfter(v,visitorImplicitFactory)
+
+        /* Synchronisation Step */
+        futur.get /* Waiting for ScalaAspectVisitor finish submit subtask before close pool ()  */
+        Util.threadExecutor.shutdown /* Send ended signal to pool */
+        Util.threadExecutor.awaitTermination(600,TimeUnit.SECONDS) /* Waiting for all tasks finished */
+        /* End step */
+        var endTime= System.currentTimeMillis() - startTime
+        log.info("Compilation step complete in "+(endTime)+" millisecondes ")
+    }
 
 }
