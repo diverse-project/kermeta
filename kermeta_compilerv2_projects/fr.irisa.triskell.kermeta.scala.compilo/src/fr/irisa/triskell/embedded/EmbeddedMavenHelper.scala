9e1cd8e4fd24b5043c603941d6ea7d0d22779815
Olivier Barais <olivier.barais@gmail.com>
2010-03-18 10:50:49 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10502 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 5bc9d402cc18fd486d729147cfa9a7b19f03b2ee

--- kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.compilo/src/fr/irisa/triskell/embedded/EmbeddedMavenHelper.scala
+++ kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.compilo/src/fr/irisa/triskell/embedded/EmbeddedMavenHelper.scala
@@ -5,35 +5,28 @@
 
 package fr.irisa.triskell.embedded
 
-import com.sun.xml.internal.stream.buffer.sax.Properties
 import fr.irisa.triskell.kermeta.compilo.scala.GlobalConfiguration
 import java.io.FileWriter
 //import org.apache.maven.embedder.DefaultConfiguration
 import java.io.OutputStream
 import java.io.PrintStream
-import org.apache.maven.artifact.repository.ArtifactRepository
 import org.apache.maven.artifact.repository.metadata.DefaultRepositoryMetadataManager
 import org.apache.maven.embedder.DefaultConfiguration
 import org.apache.maven.embedder.MavenEmbedder
 import org.apache.maven.embedder.MavenEmbedderConsoleLogger
-import org.apache.maven.embedder.MavenEmbedderFileLogger
 import org.apache.maven.execution.DefaultMavenExecutionRequest
 import org.apache.maven.execution.MavenExecutionRequest
-import org.apache.maven.monitor.event.DefaultEventMonitor
-import org.apache.maven.profiles.ProfileManager
-import org.apache.maven.project.MavenProject
-import org.apache.maven.settings.Settings
 import scala.collection.JavaConversions._
-import sun.awt.dnd.SunDropTargetContextPeer.EventDispatcher
 
 object EmbeddedMavenHelper {
 
-    def run(createPackage : Boolean,standalone : Boolean,exec : Boolean,additionalClasspath : List[String], outputstream : OutputStream) = {
+    def run(clean : Boolean, createPackage : Boolean,standalone : Boolean,exec : Boolean,additionalClasspath : List[String], outputstream : OutputStream) = {
 
         var goals = new scala.collection.mutable.ArrayBuffer[String]
-        goals.add("clean")
-        goals.add("scala:compile")
-    
+        if (clean){
+            goals.add("clean")
+            goals.add("scala:compile")
+        }
         if(createPackage){
 
             goals.add("package")
