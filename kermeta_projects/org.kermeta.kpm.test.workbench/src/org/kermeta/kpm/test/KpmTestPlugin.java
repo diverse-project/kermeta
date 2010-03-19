3a5910605b32f716e6a56e41a1b3b1d5a8abe7cd
Didier Vojtisek <didier.vojtisek@inria.fr>
2010-03-19 13:15:51 +0000

    now use slf4j as logging facade allows to have less dependency to eclipse
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10525 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 956aa511e7f938091e3055638d187d469d1bf4a6

--- kermeta_projects/org.kermeta.kpm.test.workbench/src/org/kermeta/kpm/test/KpmTestPlugin.java
+++ kermeta_projects/org.kermeta.kpm.test.workbench/src/org/kermeta/kpm/test/KpmTestPlugin.java
@@ -9,10 +9,10 @@
 */
 package org.kermeta.kpm.test;
 
-import org.apache.commons.logging.Log;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.kermeta.log4j.util.LogConfigurationHelper;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -26,7 +26,7 @@ public class KpmTestPlugin extends AbstractUIPlugin {
 	private static KpmTestPlugin plugin;
 	
 	// Log4j logger for this plugin
-	final static public Log internalLog = LogConfigurationHelper.getLogger("KpmTestPlugin");
+	final static public Logger internalLog = LoggerFactory.getLogger("KpmTestPlugin");
 	
 	/**
 	 * The constructor
