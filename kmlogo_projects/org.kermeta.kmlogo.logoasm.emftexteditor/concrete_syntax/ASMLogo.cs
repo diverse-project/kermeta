4adc8b494eb7bcc8b4c17e215539a11e3cde69aa
hrambelo <hrambelo@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2010-03-19 14:37:21 +0000

    hrambelo :
    Logo grmmar refactor according to new ASMLogo.ecore
    
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10531 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: a7127c109b93e2bb154d10eeff09f0f112109995

--- kmlogo_projects/org.kermeta.kmlogo.logoasm.emftexteditor/concrete_syntax/ASMLogo.cs
+++ kmlogo_projects/org.kermeta.kmlogo.logoasm.emftexteditor/concrete_syntax/ASMLogo.cs
@@ -1,95 +1,131 @@
 SYNTAXDEF logo
 FOR <http://www.kermeta.org/kmLogo> <platform:/resource/org.kermeta.kmlogo.logoasm.model/model/ASMLogo.genmodel>
-START org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Block
-
+START ASM.LogoProgram
 
 IMPORTS{
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM:<http://www.kermeta.org/kmLogo/ASM> <platform:/resource/org.kermeta.kmlogo.logoasm.model/model/ASMLogo.genmodel>
+	ASM:<http://www.kermeta.org/kmLogo/ASM> <platform:/resource/org.kermeta.kmlogo.logoasm.model/model/ASMLogo.genmodel>
 }
-
 
 OPTIONS {
 	overrideEditorConfiguration ="false";
 }
 
-
 TOKENS{
 	DEFINE INTEGER$('-')?('1'..'9')('0'..'9')*|'0'$;
 	DEFINE FLOAT$('-')?(('1'..'9') ('0'..'9')* | '0') '.' ('0'..'9')+ $;
+	DEFINE SL_COMMENT$('//'(~('\n'|'\r'))*) | (';'(~('\n'|'\r'))*) $;
+	DEFINE ML_COMMENT$'/*'.*'*/'$;
 }
 
 TOKENSTYLES{
-		"FLOAT"  COLOR #007F55, BOLD;
-		"INTEGER" COLOR #007F55, BOLD;
-		"TEXT" COLOR #666666, BOLD ;
-		"Forward" COLOR #CC8000, ITALIC;
-		"FORWARD" COLOR #CC8000, ITALIC;
-		"fd" COLOR #CC8000, ITALIC;
-		"Right" COLOR #CC8000, ITALIC;
-		"RIGHT" COLOR #CC8000, ITALIC;
-		"rt" COLOR #CC8000, ITALIC;
-		"Left" COLOR #CC8000, ITALIC;
-		"LEFT" COLOR #CC8000, ITALIC;
-		"lt" COLOR #CC8000, ITALIC;
-		"Back" COLOR #CC8000, ITALIC;
-		"BACK" COLOR #CC8000, ITALIC;
-		"PenDown" COLOR #A22000, BOLD;
-		"PENDOWN" COLOR #A22000, BOLD;
-		"PenUp" COLOR #A22000, BOLD;
-		"PENUP" COLOR #A22000, BOLD;
-		"Clear" COLOR #0055bb, ITALIC;
-		"CLEAR" COLOR #0055bb, ITALIC;	
-} 
+	"SL_COMMENT" COLOR #348017;
+	"ML_COMMENT" COLOR #348017;
+	"FLOAT" COLOR #007F55, BOLD;
+	"INTEGER" COLOR #007F55, BOLD;
+	"TEXT" COLOR #666666, BOLD;
+	"Forward" COLOR #CC8000, ITALIC;
+	"FORWARD" COLOR #CC8000, ITALIC;
+	"FD" COLOR #CC8000, ITALIC;
+	"fd" COLOR #CC8000, ITALIC;
+	"forward" COLOR #CC8000, ITALIC;
+	"Right" COLOR #CC8000, ITALIC;
+	"RIGHT" COLOR #CC8000, ITALIC;
+	"rt" COLOR #CC8000, ITALIC;
+	"RT" COLOR #CC8000, ITALIC;
+	"right" COLOR #CC8000, ITALIC;
+	"Left" COLOR #CC8000, ITALIC;
+	"LEFT" COLOR #CC8000, ITALIC;
+	"lt" COLOR #CC8000, ITALIC;
+	"LT" COLOR #CC8000, ITALIC;
+	"left" COLOR #CC8000, ITALIC;
+	"Back" COLOR #CC8000, ITALIC;
+	"BACK" COLOR #CC8000, ITALIC;
+	"bk" COLOR #CC8000, ITALIC;
+	"BK" COLOR #CC8000, ITALIC;
+	"back" COLOR #CC8000, ITALIC;
+	"PenDown" COLOR #A22000, BOLD;
+	"PENDOWN" COLOR #A22000, BOLD;
+	"pd" COLOR #A22000, BOLD;
+	"PD" COLOR #A22000, BOLD;
+	"PenUp" COLOR #A22000, BOLD;
+	"PENUP" COLOR #A22000, BOLD;
+	"pu" COLOR #A22000, BOLD;
+	"PU" COLOR #A22000, BOLD;
+	"penUp" COLOR #A22000, BOLD;
+	"Clear" COLOR #0055bb, ITALIC;
+	"CLEAR" COLOR #0055bb, ITALIC;
+	"cs" COLOR #0055bb, ITALIC;
+	"CLEARSCREEN" COLOR #0055bb, ITALIC;
+	"clearScreen" COLOR #0055bb, ITALIC;
+	"ClearScreen" COLOR #0055bb, ITALIC;
+	"CS" COLOR #0055bb, ITALIC;
+	"clear" COLOR #0055bb, ITALIC;
+}
+
 RULES{
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Back::= ("Back"|"BACK") steps ;
+	ASM.LogoProgram::=	(instructions !0  )* ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Forward::=( "Forward" | "fd" | "FORWARD" ) steps ;
+	ASM.Back::=		( "Back" | "BACK" | "bk" | "BK" | "back" )steps  ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Left::= ("Left"|"lt"|"LEFT") angle  ;
+	ASM.Forward::=	( "Forward" | "fd" | "FORWARD" | "FD" | "forward" )steps  ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Right::=( "Right" | "rt" | "RIGHT"  )angle  ;
+	ASM.Left::=		( "Left" | "lt" | "LEFT" | "LT" | "left" )angle  ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.PenDown::= ("PenDown"|"PENDOWN")  ;
+	ASM.Right::=	( "Right" | "rt" | "RIGHT" | "RT" | "right" )angle  ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.PenUp::= ("PenUp"|"PENUP")  ;
+	ASM.PenDown::=	( "PenDown" | "PENDOWN" | "PD" | "pd" | "penDown" ) ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Clear::= ("Clear"|"CLEAR" ) ;
+	ASM.PenUp::=	( "PenUp" | "PENUP" | "pu" | "PU" | "penUp" ) ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Constant::=integerValue[INTEGER] ;
+	ASM.Clear::=	( "Clear" | "CLEAR" | "cs" | "CLEARSCREEN" | "clearScreen" | "ClearScreen" | "CS" | "clear" ) ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.ProcCall::= ("$")?declaration[] ("(" actualArgs? (","actualArgs)* ")")? ;
+	ASM.Constant::=	integerValue[INTEGER] ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.ProcDeclaration::= ("to"|"TO") #0 name[](args)*block  ("end"|"END") ;
+	ASM.ProcCall::=	( "$"  )?declaration[]( "(" actualArgs ( "," actualArgs  )* ")"  )? ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Block::= ("{"|"[") (!1 instructions  )*!0  ("}"|"]") ;
+	ASM.ProcDeclaration::=	( "to" | "TO" | "To" )#0 name[](args  )*  block ( "end" | "END" | "End" ) ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.If::= (
-						(("If"|"IF") condition !0 ("then"|"THEN") thenPart !0 ( ("else"|"ELSE") elsePart !0 )?)|
-						(("ifelse"|"IFELSE") condition !0 thenPart !0 elsePart)
-						) ;
+	ASM.Block ::= (!1 	instructions:ASM.If |instructions:ASM.Right |instructions:ASM.Left |instructions:ASM.Back |
+						instructions:ASM.Forward |instructions:ASM.PenDown |instructions:ASM.PenUp |instructions:ASM.Clear |
+						instructions:ASM.ProcCall |instructions:ASM.Repeat |instructions:ASM.While	)
+						  
+					((!1instructions:ASM.If |instructions:ASM.Right |instructions:ASM.Left |instructions:ASM.Back |
+						instructions:ASM.Forward |instructions:ASM.PenDown |instructions:ASM.PenUp |instructions:ASM.Clear |
+						instructions:ASM.ProcCall |instructions:ASM.Repeat |instructions:ASM.While ))* !0 
+						
+				| "["(!1instructions:ASM.If |instructions:ASM.Right |instructions:ASM.Left |instructions:ASM.Back |
+						instructions:ASM.Forward |instructions:ASM.PenDown |instructions:ASM.PenUp |instructions:ASM.Clear |
+						instructions:ASM.ProcCall |instructions:ASM.Repeat |instructions:ASM.While )? 
+					
+					(!1	instructions:ASM.If |instructions:ASM.Right |instructions:ASM.Left |instructions:ASM.Back |
+						instructions:ASM.Forward |instructions:ASM.PenDown |instructions:ASM.PenUp |instructions:ASM.Clear |
+						instructions:ASM.ProcCall |instructions:ASM.Repeat |instructions:ASM.While )* "]" !0  ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.ControlStructure::=condition  ;
+	ASM.If::= 	(( "If" | "IF" | "if" ) condition !0 ( "then" | "THEN" | "Then" ) thenPart !0 ( ("Else"|"else"|"ELSE") elsePart !0 )?)|
+				(("Ifelse" | "IFELSE" | "ifelse" ) condition !0 thenPart !0 elsePart) ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Repeat::= ("Repeat"|"REPEAT") #0 condition !1 block  ;
+	ASM.ControlStructure::=	condition  ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.While::=("While"|"WHILE") #0 condition !1 block  ;
+	ASM.Repeat::=	( "Repeat" | "REPEAT" | "repeat") #0 condition !1 block  ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Parameter::= ":" #0 name[] ;
+	ASM.While::=	( "While" | "WHILE" | "while") #0 condition !1 block  ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.ParameterCall::= ":" #0 parameter[] ;
+	ASM.Parameter::= ":" #0 name[] ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Plus::= (("(" lhs  "+" rhs  ")" )|("+(" lhs "," rhs ")")) ;
+	ASM.ParameterCall::= ":" #0 parameter[] ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Minus::= (("(" lhs  "-" rhs  ")" )|("-(" lhs "," rhs ")")) ;
+	ASM.Plus::=		(("(" lhs  "+" rhs  ")" )|("+""(" lhs "," rhs ")")) ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Mult::= (("(" lhs  "*" rhs  ")" )|("*(" lhs "," rhs ")")) ;
+	ASM.Minus::= 	(("(" lhs  "-" rhs  ")" )|("-""(" lhs "," rhs ")")) ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Div::= (("(" lhs  "/" rhs  ")")|("/(" lhs "," rhs ")")) ;
+	ASM.Mult::= 	(("(" lhs  "*" rhs  ")" )|("*""(" lhs "," rhs ")")) ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Equals::= (("(" lhs  "=" rhs  ")" )|("=(" lhs "," rhs ")")) ;
+	ASM.Div::= 		(("(" lhs  "/" rhs  ")")|("/""(" lhs "," rhs ")"))  ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Greater::= (("(" lhs  ">" rhs  ")" )|(">(" lhs "," rhs ")")) ;
+	ASM.Equals::= 	(("(" lhs  "=" rhs  ")" )|("=""(" lhs "," rhs ")")) ;
 	
-	org.kermeta.kmlogo.logoasm.model.kmLogo.ASM.Lower::= (("(" lhs  "<" rhs  ")" )|("<(" lhs "," rhs ")"))  ;
+	ASM.Greater::= 	(("(" lhs  ">" rhs  ")" )|(">""(" lhs "," rhs ")")) ;
+	
+	ASM.Lower::= 	(("(" lhs  "<" rhs  ")" )|("<""(" lhs "," rhs ")")) ;
 }
