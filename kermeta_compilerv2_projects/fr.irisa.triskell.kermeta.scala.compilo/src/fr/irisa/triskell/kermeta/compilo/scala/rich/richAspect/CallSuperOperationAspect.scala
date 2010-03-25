3f2271d1d8fafba14e515c07a88d0477e71853b2
Francois Fouquet <fouquet.f@gmail.com>
2010-03-25 14:39:31 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10599 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 2356f2dac981db13556a6f48b80b99e5f4205ec4

--- kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.compilo/src/fr/irisa/triskell/kermeta/compilo/scala/rich/richAspect/CallSuperOperationAspect.scala
+++ kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.compilo/src/fr/irisa/triskell/kermeta/compilo/scala/rich/richAspect/CallSuperOperationAspect.scala
@@ -11,25 +11,43 @@ import org.eclipse.emf.ecore.EObject
 
 trait CallSuperOperationAspect extends RichAspectImplicit with ObjectAspect with LogAspect {
 	
-	override def generateScalaCode(res : StringBuilder) : Unit = {
-		log.debug("CallSuperOperation={}",this.getName())
-		/* SEARCH FOR CONTAINER OPERATION */
-		var actualEObject : EObject = this
-		while(!actualEObject.eClass().getName().equals("Operation")){
-			log.debug("Search For super Operation {}",actualEObject.eClass.getName)
-			actualEObject = actualEObject.eContainer()
-		}
-		log.debug("Operation Found {}",actualEObject)
-		var actualOperation = actualEObject.asInstanceOf[Operation]
-		/* Out EObject Iinstance of Operaiton */
-		
-		var superClassFromName = actualOperation.getSuperOperation().getOwningClass.getName
-		
-		res.append("super["+superClassFromName+"Aspect]")
-		res.append("."+actualOperation.getName())
-		res.append("(")
-		this.getParameters().foreach(par => par.generateScalaCode(res))
-		res.append(")")
-	}
+  override def generateScalaCode(res : StringBuilder) : Unit = {
+    log.debug("CallSuperOperation={}",this.getName())
+    /* SEARCH FOR CONTAINER OPERATION */
+    var actualEObject : EObject = this
+    while(!actualEObject.eClass().getName().equals("Operation")){
+      log.debug("Search For super Operation {}",actualEObject.eClass.getName)
+      actualEObject = actualEObject.eContainer()
+    }
+    log.debug("Operation Found {}",actualEObject)
+    var actualOperation = actualEObject.asInstanceOf[Operation]
+    /* Out EObject Iinstance of Operaiton */
+
+    var superClassFromName = actualOperation.getSuperOperation().getOwningClass.getName
+    var classFrom = actualOperation.getOwningClass
+
+    //SEARCH THE FIRST LEVEL SUPER TYPE WHERE OPERATION COMING FROM
+    var superTrait = classFrom.getSuperType.filter({s=>isSubTypeOf(s, superClassFromName)}).head
+                
+    res.append("super["+superTrait.asInstanceOf[Class].getTypeDefinition.asInstanceOf[ClassDefinition].getName+"Aspect]")
+    res.append("."+actualOperation.getName())
+    res.append("(")
+                
+    this.getParameters().foreach(par => par.generateScalaCode(res))
+    res.append(")")
+  }
+
+
+  def isSubTypeOf(typ : Type ,superClassName : String) : Boolean = {
+    //log.error("ECHOECHO "+typ.getQualifiedNameCompilo + " "+superClassName)
+    if(typ.asInstanceOf[Class].getTypeDefinition.asInstanceOf[ClassDefinition].getName.equals(superClassName)){
+      return true
+    } else {
+      
+      typ.asInstanceOf[Class].getTypeDefinition.asInstanceOf[ClassDefinition].getSuperType.exists({s=>isSubTypeOf(s, superClassName) } )
+    }
+  }
 
 }
+
+
