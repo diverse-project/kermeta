65f46ecc3f2a083c8663f9f08e4c53ba3bae91b0
Arnaud Blouin <arnaud.blouin@irisa.fr>
2010-04-07 16:52:31 +0000

    Added: a scroller to be able to scroll on panels.
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10661 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: b910bb7aecaad8d31eb48e7db62e3cc898c070a3

--- ki_projects/org.kermeta.ki.malai/src/org/kermeta/ki/malai/widget/Panel.java
+++ ki_projects/org.kermeta.ki.malai/src/org/kermeta/ki/malai/widget/Panel.java
@@ -1,13 +1,17 @@
 package org.kermeta.ki.malai.widget;
 
 import java.awt.Component;
+import java.awt.Container;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 
 import org.kermeta.ki.malai.interaction.eventWrapper.EventManagerWrapper;
 import org.kermeta.ki.malai.kermetaMap.RuntimeObject2JavaMap;
 
 import fr.irisa.triskell.kermeta.runtime.RuntimeObject;
+import fr.irisa.triskell.kermeta.runtime.basetypes.Integer;
 
 
 public abstract class Panel 
@@ -35,5 +39,78 @@ public abstract class Panel
 			((JPanel)panel).add((Component)comp);
 		
 		return panelRO.getFactory().getMemory().voidINSTANCE; 
+	}
+	
+	
+	
+	
+	public static RuntimeObject hasScrollbars(RuntimeObject panelRO) {
+		boolean ok = hasScrollbars((JPanel) panelRO.getUserData());
+		
+		return ok ? panelRO.getFactory().getMemory().trueINSTANCE :  panelRO.getFactory().getMemory().falseINSTANCE;
+	}
+	
+	
+	
+	
+	
+	public static RuntimeObject isVerticalScrollbarVisible(RuntimeObject panelRO) {
+		JScrollBar sb = getScrollbar((JPanel) panelRO.getUserData(), true);
+		
+		return sb==null || ! sb.isVisible() ? panelRO.getFactory().getMemory().falseINSTANCE :  
+							panelRO.getFactory().getMemory().trueINSTANCE;
+	}
+	
+	
+	
+	
+	public static RuntimeObject isHorizontalScrollbarVisible(RuntimeObject panelRO) {
+		JScrollBar sb = getScrollbar((JPanel) panelRO.getUserData(), false);
+		
+		return sb==null || ! sb.isVisible() ? panelRO.getFactory().getMemory().falseINSTANCE :  
+							panelRO.getFactory().getMemory().trueINSTANCE;
+	}
+	
+	
+	
+	
+	public static RuntimeObject scrollVertically(RuntimeObject panelRO, RuntimeObject incrementR0) {
+		scroll(panelRO, incrementR0, true);
+		return panelRO.getFactory().getMemory().voidINSTANCE;
+	}
+	
+	
+	
+	public static RuntimeObject scrollHorizontally(RuntimeObject panelRO, RuntimeObject incrementR0) {
+		scroll(panelRO, incrementR0, false);
+		return panelRO.getFactory().getMemory().voidINSTANCE;
+	}
+	
+	
+	
+	public static void scroll(RuntimeObject panelRO, RuntimeObject incrementR0, boolean vertical) {
+		JScrollBar bar = getScrollbar((JPanel) panelRO.getUserData(), vertical);
+		int increment  = Integer.getValue(incrementR0);
+		
+		if(bar!=null && bar.isVisible())
+			bar.setValue(bar.getValue() + increment);
+	}
+	
+	
+	
+	public static JScrollBar getScrollbar(JPanel panel, boolean vertical) {
+		if(hasScrollbars(panel)) {
+			JScrollPane sp = (JScrollPane)panel.getParent().getParent();
+			
+			return vertical ? sp.getVerticalScrollBar() : sp.getHorizontalScrollBar();
+		}
+			
+		return null;
+	}
+	
+	
+	public static boolean hasScrollbars(JPanel panel) {
+		Container container = panel.getParent();
+		return container!=null && container.getParent() instanceof JScrollPane;
 	}
 }
