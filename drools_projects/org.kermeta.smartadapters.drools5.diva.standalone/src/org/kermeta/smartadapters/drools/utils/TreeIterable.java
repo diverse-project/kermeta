e53ec1919e5dfae4316b738a764fd417c81cedbc
bmorin <bmorin@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2010-04-09 16:25:08 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10697 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 7f8a37b2a73169feaf11232972cf42a68c03436e

--- drools_projects/org.kermeta.smartadapters.drools5.diva.standalone/src/org/kermeta/smartadapters/drools/utils/TreeIterable.java
+++ drools_projects/org.kermeta.smartadapters.drools5.diva.standalone/src/org/kermeta/smartadapters/drools/utils/TreeIterable.java
@@ -5,14 +5,12 @@ import java.util.Iterator;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 
-@SuppressWarnings("hiding")
 public class TreeIterable<EObject> implements Iterable<EObject>{
 	TreeIterator<EObject> wrapper;
 	public TreeIterable(TreeIterator<EObject> o ) {
 		wrapper = o;
 	}
 	
-	@Override
 	public Iterator<EObject> iterator() {
 		return wrapper;
 	}
