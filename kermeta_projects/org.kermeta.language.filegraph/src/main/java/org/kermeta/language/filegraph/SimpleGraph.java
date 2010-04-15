e9fe602f5f37164e7433f898d076e96f38578c4a
Didier Vojtisek <didier.vojtisek@inria.fr>
2010-04-15 07:33:55 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10762 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 08600228ec10966bd58e8443fa54add7e971287e

--- kermeta_projects/org.kermeta.language.filegraph/src/main/java/org/kermeta/language/filegraph/SimpleGraph.java
+++ kermeta_projects/org.kermeta.language.filegraph/src/main/java/org/kermeta/language/filegraph/SimpleGraph.java
@@ -12,5 +12,23 @@ package org.kermeta.language.filegraph;
 
 public class SimpleGraph {
 
+	/**
+	 * Root node of the graph
+	 */
+	protected GraphNode rootNode;
+
+	/**
+	 * @return the rootNode
+	 */
+	public GraphNode getRootNode() {
+		return rootNode;
+	}
+
+	/**
+	 * @param rootNode the rootNode to set
+	 */
+	public void setRootNode(GraphNode rootNode) {
+		this.rootNode = rootNode;
+	}
 	
 }
