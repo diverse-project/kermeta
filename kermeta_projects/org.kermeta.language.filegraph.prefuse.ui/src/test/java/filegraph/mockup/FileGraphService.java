25499b439ea3a1b5929692b37ab9983ebbfd05e5
Didier Vojtisek <didier.vojtisek@inria.fr>
2010-04-16 07:46:56 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10782 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 0329444b329d3baef69a0b7273ded77507e309b7

---
+++ kermeta_projects/org.kermeta.language.filegraph.prefuse.ui/src/test/java/filegraph/mockup/FileGraphService.java
@@ -0,0 +1,75 @@
+/*$Id: $
+* Project : org.kermeta.language.filegraph
+* File : 	FileGraph.java
+* License : EPL
+* Copyright : IRISA / INRIA / Universite de Rennes 1
+* ----------------------------------------------------------------------------
+* Creation date : 15 avr. 2010
+* Authors : Didier Vojtisek
+*/
+
+package filegraph.mockup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kermeta.language.filegraph.CycleGraph;
+import org.kermeta.language.filegraph.GraphNode;
+import org.kermeta.language.filegraph.IFileGraphService;
+import org.kermeta.language.filegraph.SimpleGraph;
+
+public class FileGraphService implements IFileGraphService{
+
+	public CycleGraph getCycleGraph(String rootFileURI) {
+		CycleGraph result = new CycleGraph();
+		GraphNode rootNode = new GraphNode();
+		rootNode.setName("root");
+		for ( Integer i=0; i<4; ++i ) {
+			GraphNode n = new GraphNode();
+			n.setName(i.toString());
+			rootNode.getDirectReferences().add(n);
+			for ( Integer i2=0; i2<5; ++i2 ) {
+				GraphNode n2 = new GraphNode();
+				n2.setName(i.toString()+"_"+i2.toString());
+				n.getDirectReferences().add(n2);
+				
+		    }
+			// add a cycle
+			for ( Integer i2=0; i2<5; ++i2 ) {
+				if(i2 == 4)
+					n.getDirectReferences().get(i2).getDirectReferences().add(n.getDirectReferences().get(0));
+				else
+					n.getDirectReferences().get(i2).getDirectReferences().add(n.getDirectReferences().get(i2+1));				
+		    }
+			List<GraphNode> cycle = new ArrayList<GraphNode>();
+			cycle.addAll(n.getDirectReferences());
+			result.getCycles().add(cycle);
+	    }
+		result.setRootNode(rootNode);
+		return result;
+	}
+
+	public SimpleGraph getSimpleGraph(String rootFileURI) {
+		SimpleGraph result = new SimpleGraph();
+		GraphNode rootNode = new GraphNode();
+		rootNode.setName("root");
+		for ( Integer i=0; i<4; ++i ) {
+			GraphNode n = new GraphNode();
+			n.setName(i.toString());
+			rootNode.getDirectReferences().add(n);
+			for ( Integer i2=0; i2<5; ++i2 ) {
+				GraphNode n2 = new GraphNode();
+				n2.setName(i.toString()+"_"+i2.toString());
+				n.getDirectReferences().add(n2);
+		    }
+	    }
+		result.setRootNode(rootNode);
+		return result;
+	}
+
+	public List<String> getAllRequired(String rootFileURI) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
