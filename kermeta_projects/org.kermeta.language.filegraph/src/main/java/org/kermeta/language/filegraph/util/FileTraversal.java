6aa3e50dfe921248930f6c67b90c2742f2cde981
hrambelo <hrambelo@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2010-04-18 18:53:29 +0000

    hrambelo :
    deal with require on 2 lines
    
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10836 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 4c91b1c84d823cd9a65a8e9f1d997ee8035ec790

--- kermeta_projects/org.kermeta.language.filegraph/src/main/java/org/kermeta/language/filegraph/util/FileTraversal.java
+++ kermeta_projects/org.kermeta.language.filegraph/src/main/java/org/kermeta/language/filegraph/util/FileTraversal.java
@@ -36,6 +36,7 @@ public class FileTraversal {
 					 new InputStreamReader(converter.createInputStream(_uri)));
 
 			boolean inBlockComment = false;
+			boolean splitOn2lines = false ;
 			while ((sCurrentLine = br.readLine()) != null) {
 				boolean inLineCommment = false;
 				StringTokenizer st = new StringTokenizer(sCurrentLine);
@@ -45,18 +46,33 @@ public class FileTraversal {
 						inLineCommment = true;
 					if(token.startsWith("/*"))
 						inBlockComment =  true;
-					if(token.endsWith("*/"))
+					if(token.endsWith("*/")){
 						inBlockComment =  false;
+						//go to next line
+						continue;
+					}
 					if(!inLineCommment && !inBlockComment){
+						// deal with require split on 2 lines
+						if (!splitOn2lines){
 						// ignore commented tokens
-						if(token.equals("require")){
-							// next token is our string 
-							// TODO deal with require split on 2 lines ?
-							String rawUri = st.nextToken();
-							if (rawUri != null){
-								String currUri = rawUri.replaceAll("\"", "");
-								requires.add(currUri);
+							if(token.equals("require")){
+								// next token is our string 
+								// TODO deal with require split on 2 lines ?
+								
+								if (st.hasMoreTokens()){
+									String rawUri = st.nextToken();
+									String currUri = rawUri.replaceAll("\"", "");
+									requires.add(currUri);
+								}else {
+									splitOn2lines = true;
+								}
 							}
+						}else{
+							//the token is our string
+							//we assume that the parser has already raised an error if it is a void require
+							String currUri = token.replaceAll("\"", "");
+							requires.add(currUri);
+							splitOn2lines = false;
 						}
 					}
 			    }
