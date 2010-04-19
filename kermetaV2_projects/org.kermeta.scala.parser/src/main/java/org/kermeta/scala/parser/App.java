291cdea7d6ed7380def4e38f4a73ad159cc53b0b
Francois Fouquet <fouquet.f@gmail.com>
2010-04-19 08:51:01 +0000

    Parser Project creation
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10839 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 66baf29befa531d07acbc568b7ab17be6755d16a

---
+++ kermetaV2_projects/org.kermeta.scala.parser/src/main/java/org/kermeta/scala/parser/App.java
@@ -0,0 +1,13 @@
+package org.kermeta.scala.parser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
