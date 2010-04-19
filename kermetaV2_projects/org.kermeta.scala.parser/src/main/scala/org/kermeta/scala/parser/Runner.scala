74f3fab1f1d37fd571ae35f4a0b34333452ac772
Francois Fouquet <fouquet.f@gmail.com>
2010-04-19 09:41:28 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10847 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 131a198cb42e9410305d226d26a58703e1a7e3af

---
+++ kermetaV2_projects/org.kermeta.scala.parser/src/main/scala/org/kermeta/scala/parser/Runner.scala
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.kermeta.scala.parser
+
+object Runner{
+
+  def main(args: Array[String]): Unit = {
+    Parser.parse
+  }
+
+
+}
