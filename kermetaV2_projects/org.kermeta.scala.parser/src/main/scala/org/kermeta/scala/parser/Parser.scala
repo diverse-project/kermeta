ac742babe402fc0419a7d1a131a45ccc59d50d3c
Francois Fouquet <fouquet.f@gmail.com>
2010-04-21 15:01:58 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10905 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 851dbccd1dfbefb005006d1ca80e01b9f1cc0919

--- kermetaV2_projects/org.kermeta.scala.parser/src/main/scala/org/kermeta/scala/parser/Parser.scala
+++ kermetaV2_projects/org.kermeta.scala.parser/src/main/scala/org/kermeta/scala/parser/Parser.scala
@@ -31,18 +31,25 @@ object Parser extends StandardTokenParsers {
 
   def program = scompUnit+
   def scompUnit = (packageDecl|importStmts|usingStmts|topLevelDecl)
-  def packageDecl = ("package" ~ expr ~ ";" ^^ { case _ ~ p ~ _ => {
-          var newp =StructureFactory.eINSTANCE.createPackage
-          newp.setName(p)
-          newp
-        }})
+  def packageDecl = "package" ~ expr ~ ";" ^^ { case _ ~ p ~ _ => {
+        var newp =StructureFactory.eINSTANCE.createPackage
+        newp.setName(p)
+        newp
+      }}
   def importStmts = importStmt+
-  def importStmt = "require" <~ expr ^^ { case e => println(e);e }
-  def usingStmts = "using" ~> ident ~ packageQualifiedName
+  def importStmt = "require" <~ expr ^^ { case e =>
+      var newo =StructureFactory.eINSTANCE.createRequire
+      newo.setUri(e.toString)
+      newo
+  }
+  def usingStmts = "using" ~> ident ~ packageQualifiedName ^^ { case id ~ name =>
+      var newo =StructureFactory.eINSTANCE.createUsing
+      newo.setQualifiedName(id.toString+"::"+name.toString)
+  }
   def packageQualifiedName =  ( "::" ~ ident )*
 //def topLevelDecls = (topLevelDecl)
   def topLevelDecl : Parser[Any] = (annotation | annotableElement)+
-  def annotation : Parser[Tag] = "@" ~> ident ~ stringLit ^^ { case id1 ~ st1 =>
+  private def annotation : Parser[Tag] = "@" ~> ident ~ stringLit ^^ { case id1 ~ st1 =>
       var newo =StructureFactory.eINSTANCE.createTag
       newo.setName(id1.toString)
       newo.setValue(st1.toString)
@@ -56,16 +63,51 @@ object Parser extends StandardTokenParsers {
    def enumDecl = ("")
    def dataTypeDecl = ("")*/
 
-  private def classDecl = aspectModifier ~ abstractModifier ~ "class" ~ ident ~ "{" ~ classMemberDecls ~ "}" ^^ { case aspectM ~ abstractM ~ _ ~ id1 ~ _ ~ _ ~ _ =>
+  private def classDecl = aspectModifier ~ abstractModifier ~ "class" ~ ident ~ "{" ~ classMemberDecls ~ "}" ^^ { case aspectM ~ abstractM ~ _ ~ id1 ~ _ ~ members ~ _ =>
+      var newo =StructureFactory.eINSTANCE.createClassDefinition
+      newo.setName(id1.toString)
+      aspectM match {
+        case Some(_) =>  newo.setIsAspect(true)
+        case None => newo.setIsAspect(false)
+      }
+      abstractM match {
+        case Some(_) =>  newo.setIsAbstract(true)
+        case None => newo.setIsAbstract(false)
+      }
 
+      //TODO remplir class definition
+      members.foreach{member => {
+          member match {
+            case m : Constraint => newo.getInv.add(m)
+            case m : Operation => newo.getOwnedOperation.add(m)
+            case _ => println("class def add new member type")
+          }
+        }}
+      newo
   }
+
   private def classMemberDecls = annotableClassMemberDecl +
-  def annotableClassMemberDecl = ( (annotation?) ~ classMemberDecl)
+  private def annotableClassMemberDecl = (annotation?) ~ classMemberDecl ^^ { case e ~ e1 =>
+      e match {
+        case Some(_) => println("TODO ADD ANNOTATION")
+        case None => //NOTHING TO DO
+      }
+      e1
+  }
   def classMemberDecl = ( invariant | operation | property ) //attribute | reference | operation ;
 
-  def invariant = ("inv" ~> expr )
+  def invariant = ("inv" ~> expr ) ^^ { case expr =>
+      var newo =StructureFactory.eINSTANCE.createConstraint
+      //TODO
+      newo
+  }
   def operation = operationKind ~ ident ~ "(" ~ ")" ~ ":" ~ ident ~ "is" ~ operationBody ^^ { case opkind ~ opName ~ _ ~ _  ~ _ ~ id2 ~ _ ~ body =>
       var newo =StructureFactory.eINSTANCE.createOperation
+
+      opkind match {
+        case "operation" => println("oper")
+        case "method" => println("method")
+      }
       newo.setName(opName)
       newo
   }
@@ -86,11 +128,21 @@ object Parser extends StandardTokenParsers {
    def fConditional = ""
    def fRaiseException = ""
    */
-  def fVariableDecl : Parser[Any] = ("var" ~ ident ~ ":" ~ ident ~ packageQualifiedName ^^ { case _ ~ id1 ~ _ ~ id2  => println("echo") }  )
-  def fAssignement = ( ident ~ ":=" ~ fStatement)
+  def fVariableDecl : Parser[Any] = "var" ~> ident ~ ":" ~ ident ~ packageQualifiedName ^^ { case id1 ~ _ ~ id2 ~ id3  =>
+      var newo = BehaviorFactory.eINSTANCE.createVariableDecl
+      newo.setIdentifier(id1.toString)
+      newo
+  }
+  def fAssignement = ( ident ~ ":=" ~ fStatement) ^^ { case id ~ _ ~ statment =>
+      var newo = BehaviorFactory.eINSTANCE.createAssignment
+      //newo.setTarget(ident)
+      //newo.setValue(fStatement)
+      newo
+  }
 
   def fBlock = "do" ~> fExpressionLst ~ (fRescueLst?) <~ "end" ^^ { case expL ~ rescueL =>
       var newo = BehaviorFactory.eINSTANCE.createBlock
+      
       newo
   }
   def fRescueLst = ident
