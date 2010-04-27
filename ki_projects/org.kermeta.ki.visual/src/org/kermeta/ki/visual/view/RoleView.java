75cd8366c90edc9abf3c3364f944e0fb76392920
Arnaud Blouin <arnaud.blouin@irisa.fr>
2010-04-27 12:56:18 +0000

    improving layout of roles.
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@10970 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 4182202c7b25132add23352fe0ec03197c394df7

--- ki_projects/org.kermeta.ki.visual/src/org/kermeta/ki/visual/view/RoleView.java
+++ ki_projects/org.kermeta.ki.visual/src/org/kermeta/ki/visual/view/RoleView.java
@@ -1,6 +1,7 @@
 package org.kermeta.ki.visual.view;
 
 import java.awt.Graphics2D;
+import java.awt.font.TextLayout;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 
@@ -47,12 +48,49 @@ public class RoleView {
 	
 	
 	public void paint(Graphics2D g) {
-		if(view.isVisible()) {
-			Point2D.Double pt = source ? view.pointSrc : view.pointTar;
-			Line2D.Double line = view.getPerpendicularLine(pt);
-			Point2D.Double[] pts =  LinkView.findPoints(line, pt.x, pt.y, 20.);
+		if(view.isVisible() && name!=null && card!=null && (name.length()>0 || card.length()>0)) {
+			Point2D.Double pt 		= source ? view.pointTar : view.pointSrc;
+			Line2D.Double line 		= view.getPerpendicularLine(pt);
+			Point2D.Double[] pts 	= LinkView.findPoints(line, pt.x, pt.y, 20.);
 			
 			if(pts!=null && pts.length>1) {
+				double xMin	= Math.min(pts[0].x, pts[1].x);
+				double yMin	= Math.min(pts[0].y, pts[1].y);
+				double gap = 10.;
+				
+				if(LinkView.equals(pts[0].x, pts[1].x, 20))
+					if(xMin>(view.pointSrc.x-15) && xMin>(view.pointTar.x-15)) {
+						double w1 = new TextLayout(name, view.entitySrc.font, EntityView.FONT_RENDER_CONT).getBounds().getWidth();
+						double w2 = new TextLayout(card, view.entitySrc.font, EntityView.FONT_RENDER_CONT).getBounds().getWidth();
+						double maxWidth = Math.max(w1, w2);
+						
+						pts[0].x -= maxWidth + gap;
+						pts[1].x -= maxWidth + gap;
+					}
+					else {
+						pts[0].x += gap;
+						pts[1].x += gap;
+					}
+				
+				if(LinkView.equals(pts[0].y, pts[1].y, 20)) {
+					pts[0].x -= gap;
+					pts[1].x -= gap;
+					
+					if(yMin<(view.pointSrc.y+15) && yMin<(view.pointTar.y+15)) {
+						double h1 = new TextLayout(name, view.entitySrc.font, EntityView.FONT_RENDER_CONT).getBounds().getHeight();
+						double h2 = new TextLayout(card, view.entitySrc.font, EntityView.FONT_RENDER_CONT).getBounds().getHeight();
+						double maxHeight = Math.max(h1, h2);
+						
+						pts[0].y += maxHeight + gap;
+						pts[1].y += maxHeight + gap;
+					}
+					else {
+						pts[0].y -= gap;
+						pts[1].y -= gap;
+					}
+				}
+				
+				
 				g.drawString(name, (int)pts[0].x, (int)pts[0].y);
 				g.drawString(card, (int)pts[1].x, (int)pts[1].y);
 			}
