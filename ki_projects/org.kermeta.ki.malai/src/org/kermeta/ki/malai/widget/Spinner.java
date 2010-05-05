e45dd5fb08d57e64e689a4ee762dcd017244b74d
Arnaud Blouin <arnaud.blouin@irisa.fr>
2010-05-05 16:31:16 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@11101 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 7915575474df68e78cc94e53ee07563e754432f4

---
+++ ki_projects/org.kermeta.ki.malai/src/org/kermeta/ki/malai/widget/Spinner.java
@@ -0,0 +1,27 @@
+package org.kermeta.ki.malai.widget;
+
+import java.awt.Dimension;
+
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+
+import org.kermeta.ki.malai.interaction.eventWrapper.EventManagerWrapper;
+import org.kermeta.ki.malai.kermetaMap.RuntimeObject2JavaMap;
+
+import fr.irisa.triskell.kermeta.runtime.RuntimeObject;
+
+public abstract class Spinner {
+	public static RuntimeObject initialise(RuntimeObject spinnerRO, RuntimeObject eventManagerRO) {
+		final EventManagerWrapper emw = (EventManagerWrapper) eventManagerRO.getUserData();
+		final JSpinner spinner = new JSpinner(new SpinnerNumberModel(0, 0, 10000,1));
+		
+		spinner.setEditor(new JSpinner.NumberEditor(spinner, "0"));
+		spinner.setMaximumSize(new Dimension(50, 30));
+		
+		spinnerRO.setUserData(spinner);
+		RuntimeObject2JavaMap.MAP.put(spinner, spinnerRO);
+		emw.attachTo(spinner);
+		
+		return spinnerRO.getFactory().getMemory().voidINSTANCE; 
+	}
+}
