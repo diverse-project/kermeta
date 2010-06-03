27db16f371572264d635bf1870de83dddf7d5c5c
Francois Fouquet <fouquet.f@gmail.com>
2010-06-03 09:52:46 +0000

    Project Creation - Merge from POC
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@11431 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 7cb256ff51a8db71cc78080a62baf1ed6b46550f

---
+++ art2_projects/art2.ui.editor/src/main/java/org/kermeta/art2/ui/editor/ComponentTypePalette.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.kermeta.art2.ui.editor;
+
+/**
+ *
+ * @author ffouquet
+ */
+public class ComponentTypePalette {
+
+}
