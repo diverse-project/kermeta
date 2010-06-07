e646df0a81ce2e1fc19a491cf4e10aea7f71c06b
Francois Fouquet <fouquet.f@gmail.com>
2010-06-07 15:03:34 +0000

    POC Project creation for ART2
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@11463 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: dd470b8406f581ace48aae09721fedef632cc3c2

---
+++ art2_projects/org.kermeta.art2.entimid.fakeStuff/src/main/java/org/entimid/fakeStuff/devices/FakeSimpleLight.java
@@ -0,0 +1,99 @@
+package org.entimid.fakeStuff.devices;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.logging.Logger;
+
+import javax.swing.JFrame;
+import org.entimid.framework.SimpleActionService;
+import org.kermeta.art2.annotation.ComponentType;
+import org.kermeta.art2.annotation.Port;
+import org.kermeta.art2.annotation.ProvidedPort;
+import org.kermeta.art2.annotation.Provides;
+import org.kermeta.art2.annotation.Start;
+import org.kermeta.art2.annotation.Stop;
+import org.kermeta.art2.framework.AbstractComponentType;
+
+/**
+ *
+ * @author ffouquet
+ */
+@Provides({
+    @ProvidedPort(name="on",className=SimpleActionService.class),
+    @ProvidedPort(name="off",className=SimpleActionService.class)
+})
+@ComponentType
+public class FakeSimpleLight extends AbstractComponentType {
+	
+
+    @Start
+    public void init() {
+        logger = Logger.getLogger(this.getClass().getName());
+        frame = new MyFrame(Color.RED);
+        frame.setVisible(true);
+        state = false;
+    }
+
+    @Stop
+    public void stop() {
+        frame.dispose();
+        frame = null;
+    }
+
+    @Port(name="on",method="process")
+    public void lightUp(){
+        frame.setColor(Color.green);
+        state = true;
+    }
+
+    @Port(name="off",method="process")
+    public void lightOff(){
+        frame.setColor(Color.red);
+        state = false;
+    }
+
+    private static final int FRAME_WIDTH = 300;
+    private static final int FRAME_HEIGHT = 300;
+    private static Logger logger;
+    private MyFrame frame;
+    private Boolean state = false;
+
+    public Boolean getState() {
+        return state;
+    }
+
+    public void setState(Boolean state) {
+        this.state = state;
+    }
+
+    private static class MyFrame extends JFrame {
+
+        private Color c;
+
+        public MyFrame(Color c) {
+            super("Couleur " + c.toString());
+            this.c = c;
+            setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
+            pack();
+        }
+
+        public void paint(Graphics g) {
+            if (g instanceof Graphics2D) {
+                Graphics2D g2d = Graphics2D.class.cast(g);
+                g2d.setColor(c);
+                g2d.fillOval(0, 0, FRAME_WIDTH, FRAME_HEIGHT);
+            } else {
+                logger.warning("Graphics are not 2D. Instance of:" + g.getClass());
+            }
+        }
+
+        public final void setColor(Color c) {
+            this.c = c;
+            repaint();
+            Logger.getLogger(this.getName()).info("SetColor "+c.toString());
+        }
+    }
+
+}
