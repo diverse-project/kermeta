ae78318dd74780340f93b00ae66ce5e2c6e08152
Francois Fouquet <fouquet.f@gmail.com>
2010-06-07 14:58:38 +0000

    Beta 2 - Model Construction by Dnd Model Merge
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@11460 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: d841d7e97b6fbd65630f9e02ba8de7c4a8d25de1

---
+++ art2_projects/org.kermeta.art2.runtime/src/main/scala/org/kermeta/art2/runtime/ArtRuntimeService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.kermeta.art2.runtime;
+
+/**
+ *
+ * @author ffouquet
+ */
+public interface ArtRuntimeService {
+
+    	public void update(String url);
+
+        public void updateFromContent(String body);
+
+}
