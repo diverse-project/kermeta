e68e54f979c3ada3770ec04437a80be463c34e83
Olivier Barais <olivier.barais@gmail.com>
2010-06-09 14:33:04 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@11483 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: e92cde848c44d6b48d32128eb59beb8ab9493146

--- kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.compilo/src/fr/irisa/triskell/embedded/POMGeneratorHelper.scala
+++ kermeta_compilerv2_projects/fr.irisa.triskell.kermeta.scala.compilo/src/fr/irisa/triskell/embedded/POMGeneratorHelper.scala
@@ -215,7 +215,7 @@ object POMGeneratorHelper {
     
 
         dependencies.add(this.createDependency("org.scala-lang", "scala-library", "2.8.0.RC1"))
-        dependencies.add(createDependency("fr.irisa.triskell", "kermeta.model", "1.3.0"));
+        dependencies.add(createDependency("org.kermeta", "kermeta.model", "1.3.0"));
         dependencies.add(createDependency("org.eclipse", "emf", "3.5.0"));
         dependencies.add(createDependency("fr.irisa.triskell", "kermeta.scala.framework", "0.0.3"));
         /* INIT LOCAL VARIABLE DEPENDENCY */
