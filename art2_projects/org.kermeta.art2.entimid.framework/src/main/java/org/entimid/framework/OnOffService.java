1da964f1baf463d60a6d80422bd2d68e88bf8ee1
Francois Fouquet <fouquet.f@gmail.com>
2010-06-25 09:38:13 +0000

    POC Entimid Framework
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@11621 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: ffb0f3284e3154a6757f95c2556b5dfa0b14e4f7

---
+++ art2_projects/org.kermeta.art2.entimid.framework/src/main/java/org/entimid/framework/OnOffService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.entimid.framework;
+
+/**
+ *
+ * @author ffouquet
+ */
+public interface OnOffService {
+
+    public void on();
+
+    public void off();
+
+}
---
+++ art2_projects/org.kermeta.art2.entimid.framework/src/main/java/org/entimid/framework/SimpleActionService.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.entimid.framework;
+
+/**
+ *
+ * @author ffouquet
+ */
+public interface SimpleActionService {
+
+    public void process();
+
+}
