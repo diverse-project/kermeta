bc3bf4b589e528d8b5a43ddf26aeb722dc8f8bce
Francois Fouquet <fouquet.f@gmail.com>
2010-07-02 06:50:38 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@11667 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: df53c2e12c01baf5054d05ff911a7092a45e7128

--- art2_projects/org.kermeta.art2.entimid.fakeStuff/hello.scala
+++
@@ -1,7 +0,0 @@
-package org.entimid.fakeStuff.devices;
-import org.kermeta.art2.framework.AbstractPort;
-import org.entimid.framework.SimpleActionService;
-public class FakeSimpleLightPORTPROXYon extends AbstractPort implements org.entimid.framework.SimpleActionService {
-public void process(){
-((org.entimid.fakeStuff.devices.FakeSimpleLight)getComponent()).lightUp();}
-}
