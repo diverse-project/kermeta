2768a9cec3a39c396b8fae01796bc93d91a93441
Francois Fouquet <fouquet.f@gmail.com>
2010-07-26 13:14:03 +0000

    Project creation
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@11865 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: faa564680277781bcd29ec79bfb45cd8f4bc8d93

---
+++ art2_projects/art2.componentType.parent/src/main/java/org/kermeta/art2/componenttype/parent/App.java
@@ -0,0 +1,13 @@
+package org.kermeta.art2.componenttype.parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
