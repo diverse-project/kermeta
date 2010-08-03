d49ab41da75a0b8e5eb919b901eb43f579773c8a
Francois Fouquet <fouquet.f@gmail.com>
2010-08-03 12:00:14 +0000

    Project creation
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@12005 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 20fc0c5908e67b22e15e004d6064c6ca79f2299e

---
+++ art2_projects/org.kermeta.art2.kompare/src/main/scala/org/kermeta/art2/kompare/Art2KompareBean.scala
@@ -0,0 +1,157 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.kermeta.art2.kompare
+
+import org.kermeta.art2._
+import org.kermeta.art2adaptation._
+import scala.collection.JavaConversions._
+import org.kermeta.art2.framework.aspects.Art2Aspects._
+
+class Art2KompareBean extends org.kermeta.art2.api.service.core.kompare.ModelKompareService{
+
+  def kompare(actualModel : ContainerRoot,targetModel:ContainerRoot,nodeName : String) : AdaptationModel = {
+
+    var adaptationModel = org.kermeta.art2adaptation.Art2adaptationFactory.eINSTANCE.createAdaptationModel
+    //STEP 0 - FOUND LOCAL NODE
+    var actualLocalNode = actualModel.getNodes.find{c=> c.getName==nodeName }
+    var updateLocalNode = targetModel.getNodes.find{c=> c.getName==nodeName }
+    updateLocalNode match {
+      case Some(uln)=> {
+          actualLocalNode match {
+            case Some(aln)=> updateNode(aln,uln) //UPDATE
+            case None=> initNode(uln)
+          }
+        }
+      case None=> {
+          actualLocalNode match {
+            case Some(aln) => stopNode(aln)
+            case None =>  println("ART2 Stange Error !!!");adaptationModel
+          }
+        }
+    }
+  }
+
+
+  def stopNode(node:ContainerNode):AdaptationModel={
+    var adaptationModel = org.kermeta.art2adaptation.Art2adaptationFactory.eINSTANCE.createAdaptationModel
+    println("STOP NODE "+node.getName)
+    node.getComponentTypes.foreach{ct=>
+      var rmctcmd = Art2adaptationFactory.eINSTANCE.createRemoveComponentType
+      rmctcmd.setRef(ct)
+      adaptationModel.getAdaptations.add(rmctcmd)
+    }
+    node.getComponents.foreach({c =>
+        var rmccmd = Art2adaptationFactory.eINSTANCE.createRemoveComponentInstance
+        rmccmd.setRef(c)
+        adaptationModel.getAdaptations.add(rmccmd)
+      })
+    adaptationModel
+  }
+
+
+  def initNode(node:ContainerNode):AdaptationModel= {
+    var adaptationModel = org.kermeta.art2adaptation.Art2adaptationFactory.eINSTANCE.createAdaptationModel
+    println("INIT NODE "+node.getName)
+    //UPDATE ALL COMPONENT TYPE
+    node.getComponentTypes.foreach{ct=>
+      var newctcmd = Art2adaptationFactory.eINSTANCE.createAddComponentType
+      newctcmd.setRef(ct)
+      adaptationModel.getAdaptations.add(newctcmd)
+    }
+    node.getComponents.foreach({c =>
+        var newccmd = Art2adaptationFactory.eINSTANCE.createAddComponentInstance
+        newccmd.setRef(c)
+        adaptationModel.getAdaptations.add(newccmd)
+      })
+    adaptationModel
+  }
+
+  def updateNode(actualNode : ContainerNode,updateNode : ContainerNode) : AdaptationModel = {
+    var adaptationModel = org.kermeta.art2adaptation.Art2adaptationFactory.eINSTANCE.createAdaptationModel
+    println("INIT NODE "+actualNode.getName)
+    //ComponentType Step
+    updateNode.getComponentTypes.foreach{uct=>
+      actualNode.getComponentTypes.find({act=> act.isModelEquals(uct) }) match {
+        case Some(ct)=> //OK
+        case None => {
+            var ctcmd = Art2adaptationFactory.eINSTANCE.createAddComponentType
+            ctcmd.setRef(uct)
+            adaptationModel.getAdaptations.add(ctcmd)
+          }
+      }
+    }
+    actualNode.getComponentTypes.foreach{act=>
+      updateNode.getComponentTypes.find({uct=> uct.isModelEquals(act) }) match {
+        case Some(ct)=> //OK
+        case None => {
+            var ctcmd = Art2adaptationFactory.eINSTANCE.createRemoveComponentType
+            ctcmd.setRef(act)
+            adaptationModel.getAdaptations.add(ctcmd)
+          }
+      }
+    }
+    //ComponentInstance Step
+    updateNode.getComponents.foreach{uc=>
+      actualNode.getComponents.find({ac=> ac.isModelEquals(uc) }) match {
+        case Some(c)=>{
+            //OK CHECK FOR UPDATE
+
+            //TODO CHECK UPDATE
+          }
+        case None => {
+            var ccmd = Art2adaptationFactory.eINSTANCE.createAddComponentInstance
+            ccmd.setRef(uc)
+            adaptationModel.getAdaptations.add(ccmd)
+          }
+      }
+    }
+    actualNode.getComponents.foreach{ac=>
+      updateNode.getComponents.find({uc=> uc.isModelEquals(ac) }) match {
+        case Some(c)=> //OK
+        case None => {
+            var ccmd = Art2adaptationFactory.eINSTANCE.createRemoveComponentInstance
+            ccmd.setRef(ac)
+            adaptationModel.getAdaptations.add(ccmd)
+          }
+      }
+    }
+    adaptationModel
+  }
+
+  
+
+  /*
+
+
+   var runtimeComponentType : Set<ComponentType> init Set<ComponentType>.new
+   var alreadyTestedUpdateComponentType : Set<ComponentType> init Set<ComponentType>.new
+
+
+
+   //COMPONENT INSTANCE STEP
+   runtimeNode.components.each{ rc |
+   var similarComponent : ComponentInstance init updateNode.components.detect{uc| uc.isEquals(rc) }
+   if(similarComponent == void)
+   then result.add(factory.createCommandRemoveComponentInstance(rc))
+   else
+   if(rc.isUpdated(similarComponent))
+   then result.add(factory.createCommandUpdateComponentInstance(similarComponent))
+   end
+   end
+   }
+
+   updateNode.components.each{ uc |
+   var similarComponent : ComponentInstance init runtimeNode.components.detect{rc| rc.isEquals(uc) }
+   if(similarComponent == void)
+   then result.add(factory.createCommandAddComponentInstance(uc))
+   end
+   }
+   end
+
+   */
+
+
+}
