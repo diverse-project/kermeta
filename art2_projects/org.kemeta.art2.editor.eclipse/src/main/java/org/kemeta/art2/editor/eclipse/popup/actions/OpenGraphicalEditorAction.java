3718603a7ddd62b51e929286ad6b8a796386fe65
Didier Vojtisek <didier.vojtisek@inria.fr>
2010-08-03 09:04:08 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@11982 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 027a765b7cab6a1d9b0f23a034634b3240c45008

---
+++ art2_projects/org.kemeta.art2.editor.eclipse/src/main/java/org/kemeta/art2/editor/eclipse/popup/actions/OpenGraphicalEditorAction.java
@@ -0,0 +1,44 @@
+package org.kemeta.art2.editor.eclipse.popup.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class OpenGraphicalEditorAction implements IObjectActionDelegate {
+
+	private Shell shell;
+	
+	/**
+	 * Constructor for Action1.
+	 */
+	public OpenGraphicalEditorAction() {
+		super();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		shell = targetPart.getSite().getShell();
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		MessageDialog.openInformation(
+			shell,
+			"ART2 :: Eclipse :: graphical editor",
+			"Open in Graphical editor was executed.");
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+}
