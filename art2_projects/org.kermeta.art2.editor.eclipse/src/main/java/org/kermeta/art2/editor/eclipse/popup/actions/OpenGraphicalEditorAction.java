b6c017677f523a8be410297a99555d4e7904d9d0
Didier Vojtisek <didier.vojtisek@inria.fr>
2010-08-04 10:32:07 +0000

    open on a file
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@12034 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 067e6f87aafe4531c9bdfa600b1e6733f2917f0a

--- art2_projects/org.kermeta.art2.editor.eclipse/src/main/java/org/kermeta/art2/editor/eclipse/popup/actions/OpenGraphicalEditorAction.java
+++ art2_projects/org.kermeta.art2.editor.eclipse/src/main/java/org/kermeta/art2/editor/eclipse/popup/actions/OpenGraphicalEditorAction.java
@@ -3,16 +3,27 @@
  * Copyright  : IRISA / INRIA / Universite de Rennes 1 */
 package org.kermeta.art2.editor.eclipse.popup.actions;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.util.Iterator;
+
+import javax.swing.JFrame;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.kermeta.art2.ui.editor.Art2Editor;
 
 public class OpenGraphicalEditorAction implements IObjectActionDelegate {
 
 	private Shell shell;
+	protected IFile selectedIFile=null;
 	
 	/**
 	 * Constructor for Action1.
@@ -32,16 +43,38 @@ public class OpenGraphicalEditorAction implements IObjectActionDelegate {
 	 * @see IActionDelegate#run(IAction)
 	 */
 	public void run(IAction action) {
-		MessageDialog.openInformation(
-			shell,
-			"ART2 :: Eclipse :: graphical editor",
-			"Open in Graphical editor was executed.");
+		
+		JFrame jframe = new JFrame("Art2 UI Tester");
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.setPreferredSize(new Dimension(800, 600));
+
+        Art2Editor artpanel = new Art2Editor();
+        jframe.add(artpanel.getPanel(),BorderLayout.CENTER);
+
+        //System.out.println(selectedIFile.getLocation().toFile().getPath());
+        artpanel.loadModel("file://"+selectedIFile.getLocation().toFile().getPath());
+
+
+        jframe.pack();
+        jframe.setVisible(true);
 	}
 
 	/**
 	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
 	 */
 	public void selectionChanged(IAction action, ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			for (Iterator<?> it = ((IStructuredSelection) selection).iterator(); it
+					.hasNext();) {
+				Object element = it.next();
+				if (element instanceof IFile) {
+					selectedIFile = (IFile) element;
+				} else if (element instanceof IAdaptable) {
+					selectedIFile = (IFile) ((IAdaptable) element)
+							.getAdapter(IFile.class);
+				}
+			}
+		}
 	}
 
 }
