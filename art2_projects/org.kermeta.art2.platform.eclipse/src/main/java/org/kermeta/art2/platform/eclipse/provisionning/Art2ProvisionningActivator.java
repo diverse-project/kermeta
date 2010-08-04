8f9d86d9bcb7da8001acd79f3abfbc93b0ba542f
Didier Vojtisek <didier.vojtisek@inria.fr>
2010-08-04 10:04:59 +0000

    looks for the pax.jar directly in the bundle
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@12033 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: b4824b668a114382953387a28d0f693954db4d55

--- art2_projects/org.kermeta.art2.platform.eclipse/src/main/java/org/kermeta/art2/platform/eclipse/provisionning/Art2ProvisionningActivator.java
+++ art2_projects/org.kermeta.art2.platform.eclipse/src/main/java/org/kermeta/art2/platform/eclipse/provisionning/Art2ProvisionningActivator.java
@@ -27,15 +27,15 @@ public class Art2ProvisionningActivator implements BundleActivator {
     @Override
     public void start(BundleContext context) throws Exception {
         
-        URL bundleRootURL = context.getBundle().getEntry("config/Pax");
+        URL bundleRootURL = context.getBundle().getEntry("pax-url-mvn-1.1.3.jar");
         URL pluginUrl = FileLocator.resolve(bundleRootURL);
 
-        Scanner sc = new Scanner(new File(pluginUrl.getPath()));
-        while (sc.hasNext()) {
-            String deployUri = sc.nextLine();
-            Bundle b = context.installBundle(deployUri);
-            b.start();
-        }
+        //Scanner sc = new Scanner(new File(pluginUrl.getPath()));
+        //while (sc.hasNext()) {
+            String deployPaxUri = "file://" + pluginUrl.getPath();// sc.nextLine();
+            Bundle paxBundle = context.installBundle(deployPaxUri);
+            paxBundle.start();
+        //}
 
         URL baseConfig = context.getBundle().getEntry("config");
         URL baseConfigUrl = FileLocator.resolve(baseConfig);
@@ -43,14 +43,18 @@ public class Art2ProvisionningActivator implements BundleActivator {
 
         File[] configFile = directory.listFiles();
         for (int i = 0; i < configFile.length; i++) {
-            File cFile = new File(configFile[i].getAbsolutePath());
-            Scanner csc = new Scanner(cFile);
-            while (csc.hasNext()) {
-            	
-                String deployUri = csc.nextLine();
-                Bundle b = context.installBundle(deployUri);
-                b.start();
-            }
+        	if(!configFile[i].getName().equals(".svn")){
+	            File cFile = new File(configFile[i].getAbsolutePath());
+	            if(cFile.isFile()){
+		            Scanner csc = new Scanner(cFile);
+		            while (csc.hasNext()) {
+		            	
+		                String deployUri = csc.nextLine();
+		                Bundle b = context.installBundle(deployUri);
+		                b.start();
+		            }
+	            }
+        	}
         }
 
 
