195e412acef97f6af48e634544568eee16b9f1a6
Francois Fouquet <fouquet.f@gmail.com>
2010-08-06 07:32:12 +0000

    Add Save strategy
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@12085 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 4f27ad1e71bd77bdf736163917811893692cc4ca

--- art2_projects/org.kermeta.art2.deploy.osgi/src/main/scala/org/kermeta/art2/adaptation/deploy/osgi/command/AddThirdPartyCommand.scala
+++ art2_projects/org.kermeta.art2.deploy.osgi/src/main/scala/org/kermeta/art2/adaptation/deploy/osgi/command/AddThirdPartyCommand.scala
@@ -11,26 +11,13 @@ import org.kermeta.art2.adaptation.deploy.osgi.context.Art2OSGiBundle
 
 case class AddThirdPartyCommand(ct : DeployUnit, ctx : Art2DeployManager)  extends PrimitiveCommand {
 
-  def buildQuery(du : DeployUnit) : String = {
-    var query = new StringBuilder
-    query.append("mvn:")
-    query.append(du.getGroupName)
-    query.append("/")
-    query.append(du.getUnitName)
-    du.getVersion match {
-      case "default"=>
-      case ""=>
-      case _ => query.append("/");query.append(du.getVersion)
-    }
-    query.toString
-  }
 
   var lastExecutionBundle : Option[org.osgi.framework.Bundle] = None
   def execute() : Boolean= {
     println("CMD ADD CT EXECUTION");
     /* Actually deploy only bundle from library  */
     try{
-      lastExecutionBundle = Some(ctx.bundleContext.installBundle(buildQuery(ct)));
+      lastExecutionBundle = Some(ctx.bundleContext.installBundle(ct.getUrl));
       ctx.bundleMapping.append(Art2OSGiBundle(ct,ct.getName,ct.getClass,lastExecutionBundle.get))
       lastExecutionBundle.get.start
       true
