0aeea5bfbcd73008f75cb89153ab96dcac4071a4
Francois Fouquet <fouquet.f@gmail.com>
2010-08-06 07:08:25 +0000

    Add Save strategy
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@12069 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 243bb4bab888ccb065ac984a39c77113feb8dd56

---
+++ art2_projects/org.kermeta.art2.annotation/src/main/java/org/kermeta/art2/annotation/ThirdParty.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kermeta.art2.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ *
+ * @author ffouquet
+ */
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThirdParty {
+    String name();
+    String url();
+}
