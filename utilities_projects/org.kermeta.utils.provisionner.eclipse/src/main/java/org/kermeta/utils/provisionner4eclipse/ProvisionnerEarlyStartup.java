4695ddf9a14c4db10dfeb7a9de38a8e0d1a8b3af
Didier Vojtisek <didier.vojtisek@inria.fr>
2010-08-10 15:13:56 +0000

    refactored plugin name added dependency to equinox.preferences
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@12137 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 410208ecbe5e4eb2a73780da171241bc6a836c32

---
+++ utilities_projects/org.kermeta.utils.provisionner.eclipse/src/main/java/org/kermeta/utils/provisionner4eclipse/ProvisionnerEarlyStartup.java
@@ -0,0 +1,35 @@
+/*$Id$
+* Project : org.kermeta.utils.provisionner.eclipse
+* License : EPL
+* Copyright : IRISA / INRIA / Universite de Rennes 1
+* ----------------------------------------------------------------------------
+* Creation date : 2010
+* Authors : 
+*			Didier Vojtisek <didier.vojtisek@inria.fr>
+*/
+package org.kermeta.utils.provisionner4eclipse;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.ui.IStartup;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleException;
+
+public class ProvisionnerEarlyStartup implements IStartup{
+
+	@Override
+	public void earlyStartup() {
+		Activator.getDefault();
+		Bundle paxURLbundle = Platform.getBundle("org.ops4j.pax.url.mvn");
+		try {
+			paxURLbundle.start();
+		} catch (BundleException e) {
+			Activator.getDefault().getLog().log(new Status(IStatus.WARNING, Activator.PLUGIN_ID, 0, "failed to start org.ops4j.pax.url.mvn. Maven URI won't be available", e));
+		}
+	}
+
+}
