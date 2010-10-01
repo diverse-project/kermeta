311ac06e18c4bef4c7d7850f4b622d653c22dd03
Francois Fouquet <fouquet.f@gmail.com>
2010-10-01 09:05:13 +0000

    Fixe Art2 Channel cluster
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@12741 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 87b15232cad14dfb99006fe0dd6a218e61605064

--- art2_projects/org.kermeta.art2.framework/src/main/java/org/kermeta/art2/framework/ChannelFragment.java
+++ art2_projects/org.kermeta.art2.framework/src/main/java/org/kermeta/art2/framework/ChannelFragment.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.kermeta.art2.framework;
 
 import org.kermeta.art2.framework.message.Art2Message;
@@ -15,4 +14,9 @@ public interface ChannelFragment {
 
     public Object dispatch(Art2Message msg);
 
+    public java.util.List<Art2Port> getBindedPorts();
+
+    public java.util.List<Art2ChannelFragment> getOtherFragments();
+
+    public Object forward(Art2Actor delegate, Art2Message msg);
 }
