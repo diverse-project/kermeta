b424ded992668ebbf99ee9cdc42cf6375f327804
Didier Vojtisek <didier.vojtisek@inria.fr>
2010-10-05 07:48:48 +0000

    imrpove start in order to make sure that the required bundles are corectly started before installing our own bundles
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@12776 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 1f89408fceac56876fde070c82d21f5167f307dd

--- utilities_projects/org.kermeta.utils.provisionner4eclipse/src/main/java/org/kermeta/utils/provisionner4eclipse/ProvisionnerEarlyStartup.java
+++ utilities_projects/org.kermeta.utils.provisionner4eclipse/src/main/java/org/kermeta/utils/provisionner4eclipse/ProvisionnerEarlyStartup.java
@@ -9,27 +9,14 @@
 */
 package org.kermeta.utils.provisionner4eclipse;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.IStartup;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleException;
 
 public class ProvisionnerEarlyStartup implements IStartup{
 
 	@Override
 	public void earlyStartup() {
-		Activator.getDefault();
-		Bundle paxURLbundle = Platform.getBundle("org.ops4j.pax.url.mvn");
-		try {
-			paxURLbundle.start();
-		} catch (BundleException e) {
-			Activator.getDefault().getLog().log(new Status(IStatus.WARNING, Activator.PLUGIN_ID, 0, "failed to start org.ops4j.pax.url.mvn. Maven URI won't be available", e));
-		}
+		// simply start this bundle
+		Activator.getDefault();		
 	}
 
 }
