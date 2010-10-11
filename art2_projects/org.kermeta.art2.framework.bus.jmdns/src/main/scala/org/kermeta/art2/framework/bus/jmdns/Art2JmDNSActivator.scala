b4dc6f166cfb0e9911b82a809376ed86411ee3d9
Francois Fouquet <fouquet.f@gmail.com>
2010-10-11 17:20:07 +0000

    Add remote Service API
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@12925 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: f45e13859510335f1e224d088ab8287dcf9765e0

--- art2_projects/org.kermeta.art2.framework.bus.jmdns/src/main/scala/org/kermeta/art2/framework/bus/jmdns/Art2JmDNSActivator.scala
+++ art2_projects/org.kermeta.art2.framework.bus.jmdns/src/main/scala/org/kermeta/art2/framework/bus/jmdns/Art2JmDNSActivator.scala
@@ -6,26 +6,47 @@
 package org.kermeta.art2.framework.bus.jmdns
 
 import org.kermeta.art2.api.service.core.handler.Art2ModelHandlerService
+import org.kermeta.art2.api.service.remote.Art2Dispatcher
+import org.kermeta.art2.api.service.remote.Art2ModelDispatcher
 import org.osgi.framework.BundleActivator
 import org.osgi.framework.BundleContext
 import org.osgi.util.tracker.ServiceTracker
+import org.kermeta.art2.api.configuration.ConfigConstants
 
 class Art2JmDNSActivator extends BundleActivator {
 
 //  var bean : Art2KompareBean = null
   var modelHandlerServiceTracker : ServiceTracker = null
+  var modelDispatchServiceTracker : ServiceTracker = null
+  var msgDispatcherServiceTracker : ServiceTracker = null
   var art2discovery : Art2DiscoveryActor = null
 
 
   def start(bc : BundleContext){
 
     modelHandlerServiceTracker = new ServiceTracker(bc,classOf[Art2ModelHandlerService].getName,null)
+
+    modelDispatchServiceTracker = new ServiceTracker(bc,classOf[Art2ModelDispatcher].getName,null)
+    msgDispatcherServiceTracker = new ServiceTracker(bc,classOf[Art2Dispatcher].getName,null)
+
+
     new Thread {
       override def run(){
         modelHandlerServiceTracker.open
+        modelDispatchServiceTracker.open
+        msgDispatcherServiceTracker.open
+
         var mhandler = modelHandlerServiceTracker.waitForService(10000).asInstanceOf[Art2ModelHandlerService]
 
-        art2discovery = new Art2DiscoveryActor(30,mhandler,8082,8081)
+        modelDispatchServiceTracker.waitForService(10000)
+        msgDispatcherServiceTracker.waitForService(10000)
+
+        var modelPort = modelDispatchServiceTracker.getServiceReference.getProperty(ConfigConstants.ART2_NODE_MODELSYNC_PORT.getValue).toString
+        var msgPort = msgDispatcherServiceTracker.getServiceReference.getProperty(ConfigConstants.ART2_NODE_DISPATCHER_PORT.getValue).toString
+
+     
+
+        art2discovery = new Art2DiscoveryActor(30,mhandler,Integer.parseInt(msgPort),Integer.parseInt(modelPort))
         art2discovery.start
 
       }
@@ -34,6 +55,8 @@ class Art2JmDNSActivator extends BundleActivator {
 
   def stop(bc : BundleContext){
     modelHandlerServiceTracker.close
+    modelDispatchServiceTracker.close
+    msgDispatcherServiceTracker.close
     art2discovery.stop
   }
 }
