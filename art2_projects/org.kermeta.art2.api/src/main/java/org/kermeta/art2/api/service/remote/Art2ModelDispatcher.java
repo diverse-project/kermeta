888888e076b33769ff84f6b8a57939aa79fceb72
Francois Fouquet <fouquet.f@gmail.com>
2010-10-11 16:36:13 +0000

    Add remote Service API
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@12922 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 1766825e81df654b083d86edc9741b8653a3d99b

---
+++ art2_projects/org.kermeta.art2.api/src/main/java/org/kermeta/art2/api/service/remote/Art2ModelDispatcher.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.kermeta.art2.api.service.remote;
+
+/**
+ *
+ * @author ffouquet
+ */
+public interface Art2ModelDispatcher {
+
+}
