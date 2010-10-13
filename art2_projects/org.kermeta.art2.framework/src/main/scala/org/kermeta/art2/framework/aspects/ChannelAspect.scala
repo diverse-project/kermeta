57d75d23cd5be8d4bec3b30da37d5dcc6d84ab20
Francois Fouquet <fouquet.f@gmail.com>
2010-10-13 07:19:44 +0000

    Merger Fix
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@12964 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 0916482ca10359ba01b4f496a9665db27c00190a

--- art2_projects/org.kermeta.art2.framework/src/main/scala/org/kermeta/art2/framework/aspects/ChannelAspect.scala
+++ art2_projects/org.kermeta.art2.framework/src/main/scala/org/kermeta/art2/framework/aspects/ChannelAspect.scala
@@ -11,6 +11,13 @@ import Art2Aspects._
 
 case class ChannelAspect(cself : Channel) {
 
+  def usedByNode(nodeName:String) : Boolean = {
+    cself.eContainer.asInstanceOf[ContainerRoot].getMBindings.find(mb=> mb.getHub == cself && mb.getPort.eContainer.eContainer.asInstanceOf[ContainerNode].getName == nodeName  ) match {
+      case None => false
+      case Some(b)=> true
+    }
+  }
+
   def getOtherFragment(nodeName : String) : List[String] = {
     var result : List[String] = List()
     cself.eContainer.asInstanceOf[ContainerRoot].getMBindings.filter(mb=> mb.getHub == cself && mb.getPort.eContainer.eContainer.asInstanceOf[ContainerNode].getName != nodeName  ).foreach{
