a2e2ac6b6cf82f168bdcdfd3cb503558227ea690
Arnaud Blouin <arnaud.blouin@irisa.fr>
2010-10-18 20:03:52 +0000

    fixed: must update the metamodel view after its creation.
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@13043 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: a569d460a903a46042a3e57e3ffb5170088e085b

--- ki_projects/org.kermeta.ki.visual.concretePres/src/org/kermeta/ki/diagram/test/DiagramTest.java
+++ ki_projects/org.kermeta.ki.visual.concretePres/src/org/kermeta/ki/diagram/test/DiagramTest.java
@@ -1,11 +1,16 @@
 package org.kermeta.ki.diagram.test;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JToggleButton;
+import javax.swing.UIManager;
 
 import org.kermeta.ki.diagram.view.impl.DiagramView;
+import org.kermeta.ki.diagram.view.interfaces.IEntityView;
 import org.kermeta.ki.diagram.view.interfaces.IRelationView;
 import org.kermeta.ki.kompren.view.ClassDiagramView;
 import org.kermeta.ki.kompren.view.ClassModelBasicStrategy;
@@ -17,14 +22,23 @@ import org.kermeta.ki.kompren.view.RoleView.Cardinality;
 public class DiagramTest {
 
 	public static void main(String[] args) {
+		try{ UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel"); } 
+		catch(Exception e) { /* */ }
+		
 		JFrame frame 			= new JFrame();
 		JToggleButton handB 	= new JToggleButton();
-		DiagramView diag		= createBasicClassDiagram();
+		DiagramView diag		= createEcoreClassDiagram();
 		diag.getHand().setHandButton(handB);
 		handB.setSelected(true);
 		
-		diag.setPreferredSize(new Dimension(1000, 800));
-		frame.getContentPane().add(diag);
+		frame.getContentPane().setLayout(new BorderLayout());
+		JScrollPane sp = diag.getScrollPane();
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		int width  = (int)(screen.width*0.85);
+		int height = (int)(screen.height*0.8);
+		sp.setPreferredSize(new Dimension(width, height));
+		
+		frame.getContentPane().add(sp, BorderLayout.CENTER);
 		frame.setLocation(100, 100);
 		frame.setVisible(true);
 		frame.pack();
@@ -35,12 +49,24 @@ public class DiagramTest {
 	
 	
 	public static DiagramView createEcoreClassDiagram() {
-		ClassDiagramView diag = new ClassDiagramView(true);
-		ClassView eclass = new ClassView("EClass");
-		IRelationView rel  = new RelationClassView(eclass, eclass, true, true, null, "supertypes", null, Cardinality.ONE_MULTI);
+		ClassDiagramView diag 	= new ClassDiagramView(true);
+		ClassView eclass 		= (ClassView) diag.addEntity("EClass", -1, false);
+//		IRelationView rel  		= new RelationClassView(eclass, eclass, true, true, null, "supertypes", null, Cardinality.ONE_MULTI);
+		IRelationView rel  		= diag.addRelation(eclass, eclass, true, true, null, "supertypes", null, "0..*", -1);
 		
-		diag.addEntity(eclass);
-		diag.addRelation(rel);
+		eclass.addAttribute("toto", "fdsfsdfsdff");
+		eclass.addAttribute("toto", "fdsfsdfsdff");
+		eclass.addAttribute("toto", "fdsfsdfsdff");
+		eclass.addAttribute("toto", "fdsfsdfsdff");
+		eclass.addAttribute("toto", "fdsfsdfsdff");
+		eclass.addAttribute("toto", "fdsfsdfsdff");
+		eclass.addAttribute("toto", "fdsfsdfsdff");
+		eclass.addAttribute("toto", "fdsfsdfsdff");
+		eclass.addAttribute("toto", "fdsfsdfsdff");
+		eclass.addAttribute("toto", "fdsfsdfsdff");
+		eclass.addAttribute("toto", "fdsfsdfsdff");
+		eclass.update();
+//		diag.addRelation(rel);
 		
 		diag.setLayoutStrategy(new ClassModelBasicStrategy(diag));
 		diag.updateLayout();
@@ -53,7 +79,7 @@ public class DiagramTest {
 	public static DiagramView createBasicClassDiagram() {
 		ClassDiagramView diag = new ClassDiagramView(true);
 		
-		ClassView class1   = new ClassView("Class1");
+		ClassView class1   = (ClassView) diag.addEntity("Class1", -1, false);
 		ClassView class2   = new ClassView("Class2");
 		ClassView class3   = new ClassView("Class3");
 		IRelationView rel  = new RelationClassView(class1, class2, true, true, null, "role2", Cardinality.ONE, Cardinality.ONE_MULTI);
@@ -61,7 +87,6 @@ public class DiagramTest {
 		IRelationView rel3 = new InheritanceView(class3, class1);
 		IRelationView rel4 = new RelationClassView(class1, class1, true, true, null, "role3", Cardinality.ONE, Cardinality.ZERO_ONE);
 
-		diag.addEntity(class1);
 		diag.addEntity(class2);
 		diag.addEntity(class3);
 		diag.addRelation(rel);
