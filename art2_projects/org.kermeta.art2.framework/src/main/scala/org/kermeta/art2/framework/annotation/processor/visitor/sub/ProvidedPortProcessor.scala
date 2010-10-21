abc7d6685eff867a2ebe8db0dee1ebb1da37807a
Francois Fouquet <fouquet.f@gmail.com>
2010-10-21 09:17:38 +0000

    Add Library Annotation
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@13081 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: bb15fbc5b6520f2967bff2ad3b83efabb71d5610

--- art2_projects/org.kermeta.art2.framework/src/main/scala/org/kermeta/art2/framework/annotation/processor/visitor/sub/ProvidedPortProcessor.scala
+++ art2_projects/org.kermeta.art2.framework/src/main/scala/org/kermeta/art2/framework/annotation/processor/visitor/sub/ProvidedPortProcessor.scala
@@ -19,7 +19,7 @@ trait ProvidedPortProcessor {
     if(classdef.getAnnotation(classOf[org.kermeta.art2.annotation.Provides]) != null){
       classdef.getAnnotation(classOf[org.kermeta.art2.annotation.Provides]).value.foreach{req=>
 
-        var portAll = componentType.getRequired ++ componentType.getProvided
+        var portAll : List[org.kermeta.art2.PortTypeRef] = componentType.getRequired.toList ++ componentType.getProvided.toList
         portAll.find(alR=> alR.getName == req.name) match {
           case None => {
               var ptreqREF = Art2Factory.eINSTANCE.createPortTypeRef
