0f3b24fe5c2f63eadddd9ac130af9d6fa647481a
Francois Fouquet <fouquet.f@gmail.com>
2010-10-26 16:40:18 +0000

    Primitive Type Fix
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@13176 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 7bb6260985bac6c404595f0cb6a9a705c0f7d33b

---
+++ art2_projects/org.kermeta.art2.framework/src/main/scala/org/kermeta/art2/framework/annotation/processor/visitor/DataTypeVisitor.scala
@@ -0,0 +1,100 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.kermeta.art2.framework.annotation.processor.visitor
+
+import org.kermeta.art2.TypedElement
+import com.sun.mirror.`type`.AnnotationType
+import com.sun.mirror.`type`.ArrayType
+import com.sun.mirror.`type`.ClassType
+import com.sun.mirror.`type`.DeclaredType
+import com.sun.mirror.`type`.EnumType
+import com.sun.mirror.`type`.InterfaceType
+import com.sun.mirror.`type`.PrimitiveType
+import com.sun.mirror.`type`.ReferenceType
+import com.sun.mirror.`type`.TypeMirror
+import com.sun.mirror.`type`.TypeVariable
+import com.sun.mirror.`type`.VoidType
+import com.sun.mirror.`type`.WildcardType
+import com.sun.mirror.`type`.PrimitiveType
+import com.sun.mirror.util.TypeVisitor
+import org.kermeta.art2.Art2Factory
+import org.kermeta.art2.framework.Art2Utility
+import scala.collection.JavaConversions._
+
+class DataTypeVisitor extends TypeVisitor {
+
+  var dataType = Art2Factory.eINSTANCE.createTypedElement();
+  def getDataType():TypedElement={return dataType}
+
+  def visitTypeMirror(t:TypeMirror)= {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  def visitPrimitiveType(t:PrimitiveType)= {
+    t.getKind match {
+      case PrimitiveType.Kind.BOOLEAN => dataType.setName(scala.Boolean.getClass.getName)
+      case PrimitiveType.Kind.BYTE => dataType.setName(scala.Byte.getClass.getName)
+      case PrimitiveType.Kind.CHAR => dataType.setName(scala.Char.getClass.getName)
+      case PrimitiveType.Kind.DOUBLE => dataType.setName(scala.Double.getClass.getName)
+      case PrimitiveType.Kind.FLOAT => dataType.setName(scala.Float.getClass.getName)
+      case PrimitiveType.Kind.INT => dataType.setName(scala.Int.getClass.getName)
+      case PrimitiveType.Kind.LONG => dataType.setName(scala.Long.getClass.getName)
+      case PrimitiveType.Kind.SHORT => dataType.setName(scala.Short.getClass.getName)
+    }
+  }
+
+  def visitVoidType(t:VoidType)= {
+    dataType.setName("void");
+  }
+
+  def visitReferenceType(t:ReferenceType)= {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  def visitDeclaredType(t:DeclaredType)= {
+    dataType.setName(t.getDeclaration().getQualifiedName());
+    System.out.println(dataType.getName());
+  }
+
+  def visitClassType(t:ClassType)= {
+    dataType.setName(t.getDeclaration().getQualifiedName());
+    t.getActualTypeArguments().foreach{tm=>
+      var dtv = new DataTypeVisitor();
+      tm.accept(dtv);
+      dataType.getGenericTypes().add(Art2Utility.getOraddDataType(dtv.getDataType()));
+    }
+
+  }
+
+  def visitEnumType(t:EnumType)= {
+    dataType.setName(t.getDeclaration().getQualifiedName())
+  }
+
+  def visitInterfaceType(t:InterfaceType)= {
+    dataType.setName(t.getDeclaration().getQualifiedName());
+    t.getActualTypeArguments().foreach{tm=>
+      var dtv = new DataTypeVisitor();
+      tm.accept(dtv);
+      dataType.getGenericTypes().add(Art2Utility.getOraddDataType(dtv.getDataType()));
+    }
+  }
+
+  def visitAnnotationType(t:AnnotationType) ={
+    dataType.setName(t.getDeclaration().getQualifiedName());
+  }
+
+  def visitArrayType(t:ArrayType) ={
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  def visitTypeVariable(t:TypeVariable)= {
+    dataType.setName(t.getDeclaration().getSimpleName());
+  }
+
+  def visitWildcardType(t:WildcardType)= {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+}
