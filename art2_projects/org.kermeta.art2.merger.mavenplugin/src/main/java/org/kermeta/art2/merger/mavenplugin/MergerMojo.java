6163fd3a9ded07f669f93f85f60814a12e99ed8e
Francois Fouquet <fouquet.f@gmail.com>
2010-11-03 06:51:19 +0000

    Add Model Directory gesture
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@13273 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: f35c771cb09e33f5210843bb5dc69cb3a3858f86

--- art2_projects/org.kermeta.art2.merger.mavenplugin/src/main/java/org/kermeta/art2/merger/mavenplugin/MergerMojo.java
+++ art2_projects/org.kermeta.art2.merger.mavenplugin/src/main/java/org/kermeta/art2/merger/mavenplugin/MergerMojo.java
@@ -10,7 +10,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
@@ -54,7 +56,6 @@ public class MergerMojo extends AbstractMojo {
      * @required
      */
     protected org.apache.maven.artifact.repository.ArtifactRepository local;
-
     /**
      * POM
      *
@@ -73,13 +74,38 @@ public class MergerMojo extends AbstractMojo {
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+
+        if (model.isDirectory()) {
+            for (File f : scanForArt2(model)) {
+                mergeFile(f);
+            }
+
+        } else {
+            mergeFile(model);
+        }
+    }
+
+    private List<File> scanForArt2(File modelDir) {
+        List<File> models = new ArrayList();
+        for (File f : modelDir.listFiles()) {
+            if (f.getName().endsWith(".art2")) {
+                models.add(f);
+            }
+        }
+        return models;
+    }
+
+    private void mergeFile(File modelInput) {
+
+        this.getLog().info("Process Art2 File => "+modelInput);
+
         //CREATE NEW MERGER INSTANCE
         Art2MergerComponent merger = new Art2MergerComponent();
 
         //LOAD PREVIOUS MODEL OR CREATE ONE
         ContainerRoot root = null;
-        if (model.isFile()) {
-            root = Art2XmiHelper.load(model.getAbsolutePath());
+        if (modelInput.isFile()) {
+            root = Art2XmiHelper.load(modelInput.getAbsolutePath());
         } else {
             this.getLog().warn("Model File Empty, creating one !");
             root = Art2Factory.eINSTANCE.createContainerRoot();
@@ -104,13 +130,12 @@ public class MergerMojo extends AbstractMojo {
                     merger.merge(root, nroot);
 
                     //CREATE TEMP FILE FROM ACTUAL MODEL
-                    Art2XmiHelper.save(URI.create(model.getAbsolutePath()).toString(), root);
+                    Art2XmiHelper.save(URI.create(modelInput.getAbsolutePath()).toString(), root);
                 }
             } catch (IOException ex) {
                 this.getLog().error(ex);
             }
         }
-
 
     }
 
@@ -129,6 +154,6 @@ public class MergerMojo extends AbstractMojo {
         out.flush();
         out.close();
 
-        return "file://"+temp.getAbsolutePath().toString();
+        return "file://" + temp.getAbsolutePath().toString();
     }
 }
