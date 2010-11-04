720a59b1e7a27dc8d48beeaefcb46a3c438a77fc
Francois Fouquet <fouquet.f@gmail.com>
2010-11-04 18:12:53 +0000

    Add Update Method
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@13298 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 0ca64ca6219601e4f9cc119e3c724cf678ad0ec6

---
+++ art2_projects/org.kermeta.art2.annotation/src/main/java/org/kermeta/art2/annotation/Update.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.kermeta.art2.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author ffouquet
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.METHOD})
+public @interface Update {
+
+}
