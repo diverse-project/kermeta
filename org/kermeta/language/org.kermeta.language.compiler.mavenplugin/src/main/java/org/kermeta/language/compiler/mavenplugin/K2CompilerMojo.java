8efa1bc2662a57bff71231fffc46c309ce9bb5a4
Francois Fouquet <fouquet.f@gmail.com>
2010-11-10 14:16:03 +0000

    Project creation
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@13426 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: ef1dd419acb0a77ae6731d876261c6a0606947e1

---
+++ org/kermeta/language/org.kermeta.language.compiler.mavenplugin/src/main/java/org/kermeta/language/compiler/mavenplugin/K2CompilerMojo.java
@@ -0,0 +1,87 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kermeta.language.compiler.mavenplugin;
+
+import fr.irisa.triskell.kermeta.compilo.scala.Compiler;
+import fr.irisa.triskell.kermeta.compilo.scala.GlobalConfiguration;
+import java.io.File;
+import java.util.Properties;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+
+/**
+ *
+ * @author ffouque
+ *
+ * @goal generate
+ * @phase generate-sources
+ * @requiresDependencyResolution compile
+ * @author <a href="mailto:ffouquet@irisa.fr">Fouquet François</a>
+ * @version $Id$
+ *
+ */
+public class K2CompilerMojo extends AbstractMojo {
+
+    /**
+     * List of Remote Repositories used by the resolver
+     *
+     * @parameter expression="${project.remoteArtifactRepositories}"
+     * @readonly
+     * @required
+     */
+    protected java.util.List remoteRepos;
+    /**
+     * Location of the local repository.
+     *
+     * @parameter expression="${localRepository}"
+     * @readonly
+     * @required
+     */
+    protected org.apache.maven.artifact.repository.ArtifactRepository local;
+    /**
+     * POM
+     *
+     * @parameter expression="${project}"
+     * @readonly
+     * @required
+     */
+    protected MavenProject project;
+    /**
+     * Ecore file
+     *
+     * @parameter
+     * @required
+     */
+    private File model;
+    /**
+     * Output Directory file
+     *
+     * @parameter expression="${project.build.directory}/generated-sources/kermeta"
+     * @readonly
+     * @required
+     */
+    private File output;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+        Compiler compilo = new fr.irisa.triskell.kermeta.compilo.scala.Compiler();
+
+        this.getLog().info(output.getAbsolutePath());
+
+        GlobalConfiguration.outputFolder_$eq(output.getAbsolutePath());
+        GlobalConfiguration.frameworkGeneratedPackageName_$eq("ScalaImplicit.generated");
+        GlobalConfiguration.props_$eq(new Properties());
+        GlobalConfiguration.props().setProperty("use.default.aspect.uml", "false");
+        GlobalConfiguration.props().setProperty("use.default.aspect.ecore", "false");
+        GlobalConfiguration.props().setProperty("use.default.aspect.km", "false");
+
+
+
+        compilo.compile(model.getAbsolutePath().toString());
+
+    }
+}
