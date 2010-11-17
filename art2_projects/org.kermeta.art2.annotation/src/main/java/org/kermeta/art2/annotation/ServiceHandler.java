c04d69df5c7aec8555c00cc697f1353b73d69f00
Francois Fouquet <fouquet.f@gmail.com>
2010-11-17 13:14:10 +0000

    Add Service Handler annotation
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@13534 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: a056b61c3d67634cf597d1beb85827ec1c260654

--- art2_projects/org.kermeta.art2.annotation/src/main/java/org/kermeta/art2/annotation/ServiceHandler.java
+++ art2_projects/org.kermeta.art2.annotation/src/main/java/org/kermeta/art2/annotation/ServiceHandler.java
@@ -5,9 +5,16 @@
 
 package org.kermeta.art2.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  *
  * @author ffouquet
  */
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.FIELD})
 public @interface ServiceHandler {
 }
