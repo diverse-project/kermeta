3ac39d7a75e3d378694f0123ae16cabb43653d49
bouhours <bouhours@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2010-11-25 10:23:28 +0000

    target must not be in the svn !
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@13639 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 30c8097cdd6541438b74627de4fa1217edbef6f3

--- org/kermeta/language/org.kermeta.language.KWF/target/generated-sources/apt/org/kermeta/language/kwf/art2/impl/Art2ComponentKWFPORTkevent.java
+++
@@ -1,7 +0,0 @@
-package org.kermeta.language.kwf.art2.impl;
-import org.kermeta.art2.framework.AbstractPort;
-import org.kermeta.language.api.port.PortKEvent;
-public class Art2ComponentKWFPORTkevent extends AbstractPort implements org.kermeta.language.api.port.PortKEvent {
-public void getKEvent(org.kermeta.language.api.kevent.KEvent arg0){
-((org.kermeta.language.kwf.art2.impl.Art2ComponentKWF)getComponent()).getKEvent(arg0);}
-}
