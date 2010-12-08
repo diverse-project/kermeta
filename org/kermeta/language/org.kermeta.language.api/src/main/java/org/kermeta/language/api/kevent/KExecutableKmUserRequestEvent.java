dc9dc1cef67acf860815971c08f20b840a9381b9
Didier Vojtisek <didier.vojtisek@inria.fr>
2010-12-08 11:14:02 +0000

    added an event for user request (used to separate GUI from real work)
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@13794 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 34e8c4eb0e6366730b889699956057e3d569eeb4

---
+++ org/kermeta/language/org.kermeta.language.api/src/main/java/org/kermeta/language/api/kevent/KExecutableKmUserRequestEvent.java
@@ -0,0 +1,45 @@
+/* $Id:$ 
+ * Creation : 7 dec. 2010
+ * Licence  : EPL 
+ * Copyright: IRISA/INRIA
+ * Authors  : 
+ *            Didier Vojtisek <didier.vojtisek@inria.fr>
+ */
+package org.kermeta.language.api.kevent;
+
+/** 
+ * Event that capture the User request for an executable Km
+ * 
+ */
+public class KExecutableKmUserRequestEvent extends KEvent {
+
+	/**
+	 * URI of the input description that will be processed
+	 * This can be either :
+	 * - a folder
+	 * - a kp file (kermeta project file)
+	 *
+	 */
+    protected String inputDescriptionURI = "";
+    
+    /**
+     * URI of where the result will be saved
+     */
+    protected String outputURI = "";
+
+	public String getInputDescriptionURI() {
+		return inputDescriptionURI;
+	}
+
+	public void setInputDescriptionURI(String inputDescriptionURI) {
+		this.inputDescriptionURI = inputDescriptionURI;
+	}
+
+	public String getOutputURI() {
+		return outputURI;
+	}
+
+	public void setOutputURI(String outputURI) {
+		this.outputURI = outputURI;
+	}
+}
