bb047a6301e245baf5f0ddb160b629926eb2a947
bouhours <bouhours@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2010-12-21 21:09:13 +0000

    some elements for static setter, but missing main code
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@13949 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: b6713e3016ba83a5d1dbd12d2a8129f195bc5800

--- org/kermeta/language/org.kermeta.language.resolver/src/main/java/org/kermeta/language/resolver/art2/impl/Art2ComponentKmResolver.java
+++ org/kermeta/language/org.kermeta.language.resolver/src/main/java/org/kermeta/language/resolver/art2/impl/Art2ComponentKmResolver.java
@@ -11,6 +11,16 @@
 package org.kermeta.language.resolver.art2.impl;
 
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.kermeta.art2.annotation.ComponentType;
 import org.kermeta.art2.annotation.Library;
 import org.kermeta.art2.annotation.Port;
@@ -70,7 +80,11 @@ public class Art2ComponentKmResolver extends AbstractComponentType implements Po
     	
     	MainRunner.init();
     	Resolver resolver = org.kermeta.language.resolver.RichFactory.createResolver();
-    	resolver.resolveFromModelingUnit(mu);
+    	try {
+			resolver.resolveFromModelingUnit(enforceAspect(mu));
+		} catch (IOException e) {
+			logger.error(e.getMessage(),e);
+		}
     	
     	logger.debug("ModelingUnit resolved..." );
     	logger.debug("Static setting ModelingUnit..." );
@@ -97,6 +111,39 @@ public class Art2ComponentKmResolver extends AbstractComponentType implements Po
 
     public Bundle getBundle() {
         return bundle;
+    }
+    
+    protected ModelingUnit enforceAspect(ModelingUnit mu) throws IOException{
+    	if(! (mu instanceof ScalaAspect.org.kermeta.language.structure.ModelingUnitAspect)){
+	    	ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+	    	URI uri = URI.createURI(mu.getNamespacePrefix()+"."+mu.getName() + ".km_in_memory");
+	    	Map<String, String> options = null;
+	    	if(mu.eResource() != null){
+		    	uri = mu.eResource().getURI();
+	    	}
+	    	else{
+	    		// let's suppose that the ModelingUnit contains everything (otherwise we would have to look for references and save them too ...)
+	    		ResourceSet resourceSet = new ResourceSetImpl();
+	    		Resource.Factory.Registry f = resourceSet.getResourceFactoryRegistry();
+	    		Map<String,Object> m = f.getExtensionToFactoryMap();
+	    		m.put("km_in_memory",new XMIResourceFactoryImpl());
+				Resource resource = resourceSet.createResource(uri);
+				resource.getContents().add(mu);
+	    	}
+			mu.eResource().save(outputStream, options);
+
+	    	ResourceSet resourceSet = new ResourceSetImpl();
+    		Resource.Factory.Registry f = resourceSet.getResourceFactoryRegistry();
+    		Map<String,Object> m = f.getExtensionToFactoryMap();
+    		m.put("*",new XMIResourceFactoryImpl());
+			Resource resource = resourceSet.createResource(uri);
+			resource.load(new ByteArrayInputStream(outputStream.toByteArray()), options);
+			// let's suppose the ModelingUnit is the first element in the root
+			return (ModelingUnit)resource.getContents().get(0);
+    	}
+    	else{
+    		return mu;
+    	}
     }
     
 }
