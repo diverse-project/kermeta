b9239e2235903f00e5349cb830e57b7e8f406bf4
Francois Fouquet <fouquet.f@gmail.com>
2011-01-03 14:11:23 +0000

    Wrap Scala version
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@13984 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 2bb565ae9fa945a1a202d74a773933e427704bdf

---
+++ org/kermeta/language/org.kermeta.language.library.core/src/main/scala/kermeta/io/FileIO.scala
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kermeta.io
+
+object FileIO {
+
+  def writeTextFile(filename : String, text : String) = {
+
+  }
+  def writeTextFileWithEncoding(filename : String, text : String, encoding : String) = {
+
+  }
+  def readTextFile(filename : String) : String = {
+    "not implemented"
+  }
+
+}
---
+++ org/kermeta/language/org.kermeta.language.library.core/src/main/scala/kermeta/io/StdIO.scala
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kermeta.io
+
+object StdIO {
+
+  def write(`object` : String) = {print(`object`)}
+  def writeln(`object` : String) ={println(`object`)}
+  def error(`object` : String) = {print(`object`)}
+  def errorln(`object` : String)={println(`object`)}
+  def read(prompt : String) : String ={"not implement"}
+
+}
