0fee9072b7b20021913c45bc422287742d1a58fb
Francois Fouquet <fouquet.f@gmail.com>
2011-01-04 12:12:10 +0000

    Refactor fCall to remove left recursion
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@13993 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: cab85a75d3614bdf319ddb6f36f1b85527ac6d13

---
+++ org/kermeta/language/org.kermeta.language.api/src/main/java/org/kermeta/language/api/port/PortKmCompiler.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.kermeta.language.api.port;
+
+import org.kermeta.language.structure.ModelingUnit;
+
+/**
+ *
+ * @author ffouquet
+ */
+public interface PortKmCompiler {
+
+    public void compile(ModelingUnit model,String outputPath);
+
+}
