6b0ce45fc17cb0104ad5b51fcb06e3b5682fe1a7
Didier Vojtisek <didier.vojtisek@inria.fr>
2011-02-09 12:17:20 +0000

    do not commit target
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@14154 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: bf1df3a09a85d81f1fdc18193e49cf4ced115de1

--- org/kermeta/language/org.kermeta.language.simple.assembly.services.art2/target/generated-sources/apt/org/kermeta/language/simple/assembly/services/art2/impl/Art2ComponentSimpleAssemblyPORTlog.scala
+++
@@ -1,8 +0,0 @@
-package org.kermeta.language.simple.assembly.services.art2.impl
-import org.kermeta.art2.framework.port._
-import scala.{Unit=>void}
-class Art2ComponentSimpleAssemblyPORTlog() extends org.kermeta.art2.framework.MessagePort with Art2RequiredPort {
-def getName : String = "log"
-def process(o : Object) = {this ! o}
-def getInOut = false
-}
