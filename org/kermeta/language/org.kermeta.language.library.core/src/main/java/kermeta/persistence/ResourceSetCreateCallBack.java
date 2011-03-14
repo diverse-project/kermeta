fa2dfe8398d808e6eedf376855c54d2d00c28ccc
Olivier Barais <olivier.barais@gmail.com>
2011-03-14 15:35:07 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@14629 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 74fcfce78bb10101775ab3e85953d81c891c8e71

---
+++ org/kermeta/language/org.kermeta.language.library.core/src/main/java/kermeta/persistence/ResourceSetCreateCallBack.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kermeta.persistence;
+
+/**
+ *
+ * @author barais
+ */
+public interface ResourceSetCreateCallBack {
+
+    public void init( org.eclipse.emf.ecore.resource.ResourceSet rs);
+    
+}
---
+++ org/kermeta/language/org.kermeta.language.library.core/src/main/java/org/kermeta/console/Console.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.kermeta.console;
+
+/**
+ *
+ * @author barais
+ */
+public interface Console {
+    
+    public void writeln(String message);
+    public void write(String message);
+    public void errorln(String message);
+    public String readln();
+    public void changeColor(java.awt.Color c);
+    
+    
+
+}
