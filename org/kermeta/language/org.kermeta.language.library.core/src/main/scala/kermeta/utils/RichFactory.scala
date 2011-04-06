db91d4e204dff0c0af3c9d5f7f9d7a0a20ad9a6d
Olivier Barais <olivier.barais@gmail.com>
2011-04-06 05:11:43 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@14782 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: c531ca687864994a2575405a4b8be38a6685cf65

--- org/kermeta/language/org.kermeta.language.library.core/src/main/scala/kermeta/utils/RichFactory.scala
+++
@@ -1,8 +0,0 @@
-package kermeta.utils
-
-object RichFactory {
-	
-	def createStringBuffer = new StringBuilder
-	def createHashtable[K,T]() = new java.util.HashMap[K,T]()
-                  def createStack[K] = new java.util.Stack[K]
-}
