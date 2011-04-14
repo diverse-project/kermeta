8edd19ce26113b95158cc37659f6a45597ece043
bouhours <bouhours@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2011-04-14 12:46:39 +0000

    API changing
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@14881 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: fed9e9f329698aafcf9bed154666b642689f569c

--- org/kermeta/language/org.kermeta.language.loader.kmt.scala.api/src/main/java/org/kermeta/language/loader/kmt/scala/api/KMTparser.java
+++ org/kermeta/language/org.kermeta.language.loader.kmt.scala.api/src/main/java/org/kermeta/language/loader/kmt/scala/api/KMTparser.java
@@ -1,9 +1,10 @@
 package org.kermeta.language.loader.kmt.scala.api;
 
 import org.kermeta.language.structure.ModelingUnit;
+import org.kermeta.utils.messagingsystem.api.MessagingSystem;
 
 public interface KMTparser {
 	
-	public ModelingUnit load(String uri, String optionalContent);
+	public ModelingUnit load(String uri, String optionalContent, MessagingSystem logger);
 
 }
