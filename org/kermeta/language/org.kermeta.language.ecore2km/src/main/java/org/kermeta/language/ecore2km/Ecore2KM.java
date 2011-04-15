d9a772e04d88fef97818ab096b07d2d1fa25df50
Didier Vojtisek <didier.vojtisek@inria.fr>
2011-04-15 16:05:34 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@14930 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 9c8758102bafac7d6d820b248a189605fa570215

---
+++ org/kermeta/language/org.kermeta.language.ecore2km/src/main/java/org/kermeta/language/ecore2km/Ecore2KM.java
@@ -0,0 +1,42 @@
+/* $Id: $
+ * Project    : org.kermeta.language.ecore2km
+ * License    : EPL
+ * Copyright  : IRISA / INRIA / Universite de Rennes 1
+ * -------------------------------------------------------------------
+ * Creation date : 25 nov. 2010
+ * Authors : 
+ *            Haja Rambelontsalama <hajanirina-johary.rambelontsalama@inria.fr>
+ *            Arnaud Blouin
+ */
+package org.kermeta.language.ecore2km;
+
+
+/*import org.kermeta.language.api.messaging.UnifiedMessageFactory;
+import org.kermeta.language.api.port.PortEcore2Km;
+import org.kermeta.language.structure.ModelingUnit;
+import org.osgi.framework.Bundle;
+import org.kermeta.language.language.ecore2kmrunner.MainRunner;
+import org.eclipse.emf.ecore.EPackage;
+import org.kermeta.language.ecore2km.Ecore2km;
+*/
+public class Ecore2KM  implements org.kermeta.language.ecore2km.api.Ecore2KM {
+
+  
+    
+    
+    
+
+    
+   
+    public ModelingUnit convertPackage(final EPackage rootPackage, final String namespacePrefix) {
+        // call the init in order to make sure that the registry is correctly set
+        // TODO look how to not put duplicates in the eclipse registry when run in eclipse work
+        //((org.eclipse.emf.ecore.EcoreFactoryWrapper)(org.eclipse.emf.ecore.EcoreFactory.eINSTANCE)).setWrap((org.eclipse.emf.ecore.EcoreFactory)ScalaAspect.org.eclipse.emf.ecore.RichFactory.createEFactory());
+        MainRunner.init();
+
+        Ecore2km converter = org.kermeta.language.ecore2km.RichFactory.createEcore2km();
+        return converter.convert(rootPackage, namespacePrefix);
+    }
+    
+           
+}
