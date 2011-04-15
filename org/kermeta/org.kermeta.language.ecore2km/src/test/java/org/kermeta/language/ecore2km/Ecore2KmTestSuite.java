c908164aa7895760b17430a4dfbdf910a1f1681d
Didier Vojtisek <didier.vojtisek@inria.fr>
2011-04-15 13:20:59 +0000

    creating art2 independent version
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@14913 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 1a663b3439ac13487b964885dd72711fbc48cb64

---
+++ org/kermeta/org.kermeta.language.ecore2km/src/test/java/org/kermeta/language/ecore2km/Ecore2KmTestSuite.java
@@ -0,0 +1,57 @@
+/*
+ * License : EPL
+ * Copyright : IRISA / INRIA / Universite de Rennes 1
+ * ----------------------------------------------------------------------------
+ * Creation date : January 2011
+ * Authors :
+ *			Arnaud Blouin
+ */
+package org.kermeta.language.ecore2km;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.xml.namespace.XMLNamespacePackage;
+import org.kermeta.language.api.port.PortEcore2Km;
+import org.kermeta.language.api.tests.factory.PortAbstractFactory;
+import org.kermeta.language.api.tests.port.PortEcore2KmTestSuite;
+import org.kermeta.language.ecore2km.art2.impl.Art2ComponentEcore2Km;
+import org.kermeta.language.language.ecore2kmrunner.MainRunner;
+
+/**
+ * Test suite for Ecore2Km port
+ */
+public class Ecore2KmTestSuite extends TestSuite {
+
+    public static Test suite() {
+        ((org.eclipse.emf.ecore.EcoreFactoryWrapper)(org.eclipse.emf.ecore.EcoreFactory.eINSTANCE)).setWrap(org.kermeta.language.language.ecore2km.org.eclipse.emf.ecore.RichFactory$.MODULE$) ;
+        MainRunner.init();
+        XMLNamespacePackage einstance = org.eclipse.emf.ecore.xml.namespace.XMLNamespacePackage.eINSTANCE;
+    	EPackage pkg = EcorePackage.eINSTANCE;
+
+        PortEcore2KmTestSuite.portEcore2Kmfactory = new PortAbstractFactory<PortEcore2Km>(){
+            @Override
+            public PortEcore2Km create() throws IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException, InvocationTargetException {
+                Art2ComponentEcore2Km comp = new Art2ComponentEcore2Km();
+                comp.simulatedStart(comp.getClass().getCanonicalName());
+                return comp;
+            }
+        };
+        File dir1 = new File (".");
+        try {
+            System.out.println("outputFolder ? : " + dir1.getCanonicalPath());
+        } catch (IOException ex) {
+            Logger.getLogger(Ecore2KmTestSuite.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return PortEcore2KmTestSuite.suite();
+    }
+}
