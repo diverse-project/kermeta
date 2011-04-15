b61d61a2580fca9427f82460feae5df8c9e6edce
Didier Vojtisek <didier.vojtisek@inria.fr>
2011-04-15 16:00:24 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@14929 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 474c5b57ab70f08155e97dbc5d1024a109980272

---
+++ org/kermeta/language/org.kermeta.language.ecore2km.api/src/main/java/org/kermeta/language/ecore2km/api/Ecore2KM.java
@@ -0,0 +1,24 @@
+/*$Id:  $
+* License : EPL
+* Copyright : IRISA / INRIA 
+* ----------------------------------------------------------------------------
+* Creation date : 15 avr. 2011
+* Authors : 
+*      Didier Vojtisek <didier.vojtisek@inria.fr>
+*/
+package org.kermeta.language.ecore2km.api;
+
+import org.eclipse.emf.ecore.EPackage;
+import org.kermeta.language.structure.ModelingUnit;
+
+
+public interface Ecore2KM {
+	
+	/**
+	 * Converts the given ecore package into a kermeta modeling unit.
+	 * @param rootPackage The package to convert.
+	 * @param namespacePrefix The namespace prefix to set to the generated modeling unit.
+	 * @return The created modeling unit.
+	 */
+	ModelingUnit convertPackage(final EPackage rootPackage, final String namespacePrefix); 
+}
