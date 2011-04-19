13a1cc181c69dedb42d033a8bc2e1e91f3c2e93f
Didier Vojtisek <didier.vojtisek@inria.fr>
2011-04-19 13:17:27 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@14973 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 5c5f20255e9ebb388ff04c08be7dd31c9808a01f

---
+++ org/kermeta/kp/org.kermeta.kp.loader.kp.api/src/main/java/org/kermeta/kp/loader/kp/api/KpLoader.java
@@ -0,0 +1,22 @@
+/* $Id: $
+ * Project    : org.kermeta.kp.loader.kp
+ * License    : EPL
+ * Copyright  : IRISA / INRIA / Universite de Rennes 1
+ * -------------------------------------------------------------------
+ * Creation date : 22 févr. 2011
+ * Authors : 
+ *            Haja Rambelontsalama <hajanirina-johary.rambelontsalama@inria.fr>
+ */
+
+package org.kermeta.kp.loader.kp.api;
+
+import org.kermeta.kp.KermetaProject;
+
+public interface KpLoader {
+
+	/**
+	 * @param uriKpResource
+	 */
+	public KermetaProject loadKp(String uriKpResource);
+	
+}
