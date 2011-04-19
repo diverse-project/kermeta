c7d691ca7051655b248b4c703e5c9978163ee437
Didier Vojtisek <didier.vojtisek@inria.fr>
2011-04-19 12:00:24 +0000

    use systemservices.api
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@14957 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: d41f5b4268fe68de68cbb2a0997fa58ca97688c0

--- org/kermeta/language/org.kermeta.language.loader.kmt.scala.api/src/main/java/org/kermeta/language/loader/kmt/scala/api/KMTparser.java
+++ org/kermeta/language/org.kermeta.language.loader.kmt.scala.api/src/main/java/org/kermeta/language/loader/kmt/scala/api/KMTparser.java
@@ -1,7 +1,7 @@
 package org.kermeta.language.loader.kmt.scala.api;
 
 import org.kermeta.language.structure.ModelingUnit;
-import org.kermeta.utils.messagingsystem.api.MessagingSystem;
+import org.kermeta.utils.systemservices.api.messaging.MessagingSystem;
 
 public interface KMTparser {
 	
