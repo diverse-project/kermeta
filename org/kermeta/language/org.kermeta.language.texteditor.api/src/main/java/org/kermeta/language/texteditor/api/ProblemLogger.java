51e4231dd5b6ebea2585fee13abb827e9128de00
bouhours <bouhours@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2011-04-20 14:02:03 +0000

    Share project "org.kermeta.language.texteditor.api" into "svn+ssh://scm.gforge.inria.fr/svn/kermeta"
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15027 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 5b6e617289baacfbde88f3d91cdd2857aa0293c5

---
+++ org/kermeta/language/org.kermeta.language.texteditor.api/src/main/java/org/kermeta/language/texteditor/api/ProblemLogger.java
@@ -0,0 +1,7 @@
+package org.kermeta.language.texteditor.api;
+
+import org.kermeta.utils.systemservices.api.messaging.MessagingSystem;
+
+public abstract class ProblemLogger extends MessagingSystem {
+
+}
