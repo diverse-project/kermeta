85a199cec677f96478222552d95f5aeb05431cf1
bouhours <bouhours@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2011-04-28 09:33:40 +0000

    Builder API
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15091 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 4acfe90852ea9b52423acb08b7841280980fccea

---
+++ org/kermeta/language/org.kermeta.language.builder.api/src/main/java/org/kermeta/language/builder/api/Builder.java
@@ -0,0 +1,14 @@
+package org.kermeta.language.builder.api;
+
+import org.eclipse.core.resources.IResource;
+import org.kermeta.language.structure.ModelingUnit;
+
+public abstract class Builder {
+	
+	protected Builder(){
+		super();
+	}
+	
+	public abstract ModelingUnit parseSpecificFile(IResource toParse, boolean inModification);
+
+}
