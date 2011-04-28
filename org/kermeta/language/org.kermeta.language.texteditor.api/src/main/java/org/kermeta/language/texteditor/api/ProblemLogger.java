c83c63d858585013aa26ca5b7868fd8abd655880
bouhours <bouhours@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2011-04-28 08:25:58 +0000

    useless
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15088 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 9cf3124fb701106412988c58f7872684a67ba480

--- org/kermeta/language/org.kermeta.language.texteditor.api/src/main/java/org/kermeta/language/texteditor/api/ProblemLogger.java
+++
@@ -1,7 +0,0 @@
-package org.kermeta.language.texteditor.api;
-
-import org.kermeta.utils.systemservices.api.messaging.MessagingSystem;
-
-public abstract class ProblemLogger extends MessagingSystem {
-
-}
