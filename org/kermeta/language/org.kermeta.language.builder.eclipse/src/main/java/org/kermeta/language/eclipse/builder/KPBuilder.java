3d5a15df3339592c0be46e8236ca6089460d433a
bouhours <bouhours@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2011-04-29 09:33:25 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15123 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 7a65f20826bcbaa7dd3ca6d1347ec9ad4382b15d

--- org/kermeta/language/org.kermeta.language.builder.eclipse/src/main/java/org/kermeta/language/eclipse/builder/KPBuilder.java
+++ org/kermeta/language/org.kermeta.language.builder.eclipse/src/main/java/org/kermeta/language/eclipse/builder/KPBuilder.java
@@ -21,6 +21,7 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.kermeta.kp.compiler.commandline.KermetaCompiler;
+import org.kermeta.utils.helpers.FileHelpers;
 import org.kermeta.utils.systemservices.api.impl.StdioSimpleMessagingSystem;
 import org.kermeta.utils.systemservices.api.messaging.MessagingSystem.Kind;
 
@@ -59,13 +60,12 @@ public class KPBuilder {
 				try {
 					// currently don't know how to selectively put and flush markers, need to find a way to create dynamically markers or add an attribute in it ?
 					// or calculate where the marker may be put and remove only those ones ?
-					Activator.getDefault().getMessaggingSystem().flushProblem(Activator.PLUGIN_ID);
 					String kpFileURL = kpProjectFile.getRawLocation().toString();
 					File f = new File(kpFileURL);
 					String projectUri = f.getParentFile().getCanonicalPath();
 					String outputFolder = projectUri+"/target";
 					KermetaCompiler compiler = new KermetaCompiler(false, Activator.getDefault().getMessaggingSystem(), true,outputFolder );
-				
+					Activator.getDefault().getMessaggingSystem().flushAllProblems(FileHelpers.StringToURL(kpFileURL));				
 					compiler.kp2bytecode(kpFileURL, outputFolder,outputFolder, new ArrayList<String>(),true);
 				} catch (IOException e) {
 					Activator.getDefault().getMessaggingSystem().log(Kind.DevERROR,"builder failed", this.getClass().getName(), e);
