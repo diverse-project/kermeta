fe2b9e284b89defd353ff51e5e45784fe4c73cfd
Didier Vojtisek <didier.vojtisek@inria.fr>
2011-05-04 12:11:29 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15182 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: eeae77dd5d09d0c3badfa8041ad5531e26903e59

--- org/kermeta/language/org.kermeta.language.ecore2km/src/main/java/org/kermeta/language/ecore2km/Ecore2KMImpl4Eclipse.java
+++ org/kermeta/language/org.kermeta.language.ecore2km/src/main/java/org/kermeta/language/ecore2km/Ecore2KMImpl4Eclipse.java
@@ -20,29 +20,16 @@ public class Ecore2KMImpl4Eclipse  implements org.kermeta.language.ecore2km.api.
   
     
     public Ecore2KMImpl4Eclipse(){
-    	//org.kermeta.language.ecore2km.internal.Init4Eclipse.init();
-    	//((org.eclipse.emf.ecore.EcoreFactoryWrapper) org.eclipse.emf.ecore.EcoreFactory.eINSTANCE).setWrap(org.kermeta.language.language.ecore2km.org.eclipse.emf.ecore.KerRichFactory$.MODULE$);
     	org.kermeta.language.language.ecore2kmrunner.MainRunner.init4eclipse();
-    	//org.kermeta.language.language.ecore2kmrunner.MainRunner.init();
     }
     
 
     
    
     public ModelingUnit convertPackage(final EPackage rootPackage, final String namespacePrefix) {
-        // call the init in order to make sure that the registry is correctly set
-        // TODO look how to not put duplicates in the eclipse registry when run in eclipse work
-    	/*org.eclipse.emf.ecore.impl.EcorePackageImpl pack =  new org.eclipse.emf.ecore.impl.EcorePackageImpl();// with org.kermeta.language.language.ecore2km.org.eclipse.emf.ecore.EPackageAspect with fr.irisa.triskell.kermeta.language.structureScalaAspect.aspect.DefaultObjectImplementation
-    	org.eclipse.emf.ecore.EPackage.Registry.INSTANCE.put(org.eclipse.emf.ecore.EcorePackage.eNS_URI,pack);
-    	pack.setEFactoryInstance(org.kermeta.language.language.ecore2km.org.eclipse.emf.ecore.KerRichFactory$.MODULE$);
-    	java.lang.reflect.Field f = 
-     	var f : java.lang.reflect.Field = classOf[org.eclipse.emf.ecore.impl.EPackageImpl].getDeclaredField("ecoreFactory")
-    	f.setAccessible(true);
-    	*/
-    	//org.kermeta.language.ecore2km.internal.Init4Eclipse.init();
-    	//((org.eclipse.emf.ecore.EcoreFactoryWrapper) org.eclipse.emf.ecore.EcoreFactory.eINSTANCE).setWrap(org.kermeta.language.language.ecore2km.org.eclipse.emf.ecore.KerRichFactory$.MODULE$);
+        
     	org.kermeta.language.language.ecore2kmrunner.MainRunner.init4eclipse();
-    	//org.kermeta.language.language.ecore2kmrunner.MainRunner.init();
+    	
 
         org.kermeta.language.ecore2km.Ecore2km converter = org.kermeta.language.ecore2km.KerRichFactory.createEcore2km();
         return converter.convert(rootPackage, namespacePrefix);
