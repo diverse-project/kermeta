46851aa76ccb71521fd3bc430963449c3745598e
bouhours <bouhours@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2011-06-23 08:26:34 +0000

    Adding of an Activator. Now mandatory for kermeta project. Modification of compilo versions.
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15270 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: b186904fc2453b7f3adecb0d42b21814b73fd1e8

--- org/kermeta/language/org.kermeta.language.checker/src/main/java/org/kermeta/language/checker/CheckerImpl.java
+++ org/kermeta/language/org.kermeta.language.checker/src/main/java/org/kermeta/language/checker/CheckerImpl.java
@@ -3,6 +3,7 @@ package org.kermeta.language.checker;
 import org.kermeta.diagnostic.DiagnosticModel;
 import org.kermeta.language.checker.api.Checker;
 import org.kermeta.language.checker.api.CheckerScope;
+import org.kermeta.language.checker.internal.Activator;
 import org.kermeta.language.structure.ModelingUnit;
 import org.kermeta.utils.systemservices.api.messaging.MessagingSystem;
 
@@ -15,6 +16,8 @@ public class CheckerImpl implements Checker {
 		logger.error("Checking modeling unit for scope " + scope.toString(), "checkermessagegroup", null);
 		
 		org.kermeta.language.language.checkerrunner.MainRunner.init();
+		
+		Activator.getDefault().reflexivityLoaderContext();
 		
 		org.kermeta.language.checker.Checker checker = org.kermeta.language.checker.KerRichFactory.createChecker();
 		
