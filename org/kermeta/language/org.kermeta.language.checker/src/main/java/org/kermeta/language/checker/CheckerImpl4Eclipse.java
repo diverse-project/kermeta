e5af3788d08c935a6ef054b5dd154983ff055804
bouhours <bouhours@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2011-06-24 06:29:33 +0000

    New constructor for eclipse
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15311 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 44d319b36df829b5f80322c8ef68814ac4fda6d6

--- org/kermeta/language/org.kermeta.language.checker/src/main/java/org/kermeta/language/checker/CheckerImpl4Eclipse.java
+++ org/kermeta/language/org.kermeta.language.checker/src/main/java/org/kermeta/language/checker/CheckerImpl4Eclipse.java
@@ -7,15 +7,16 @@ import org.kermeta.language.checker.internal.Activator;
 import org.kermeta.language.structure.ModelingUnit;
 import org.kermeta.utils.systemservices.api.messaging.MessagingSystem;
 
-public class CheckerImpl implements Checker {
+public class CheckerImpl4Eclipse implements Checker {
 	
-	public CheckerImpl() {
-		org.kermeta.language.language.checkerrunner.MainRunner.init();
+	public CheckerImpl4Eclipse() {
+		org.kermeta.language.language.checkerrunner.MainRunner.init4eclipse();
+		Activator.getDefault().reflexivityLoaderContext();
 	}
 
 	public DiagnosticModel check(ModelingUnit mu, CheckerScope scope,
 			String optionalContent, MessagingSystem logger) {
-		
+			
 		org.kermeta.language.checker.Checker checker = org.kermeta.language.checker.KerRichFactory.createChecker();
 				
 		DiagnosticModel result = checker.checkObject(mu, scope.toString());
