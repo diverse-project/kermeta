ee144681e70e28128176ff8f1ea473b42314f340
bouhours <bouhours@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2011-06-26 16:09:58 +0000

    new API
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15339 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: d290e5b9c2efd7ab38360b8836dd5ecf5c63365c

--- org/kermeta/language/org.kermeta.language.loader.kmt.scala.api/src/main/java/org/kermeta/language/loader/kmt/scala/api/Lexer.java
+++ org/kermeta/language/org.kermeta.language.loader.kmt.scala.api/src/main/java/org/kermeta/language/loader/kmt/scala/api/Lexer.java
@@ -3,5 +3,7 @@ package org.kermeta.language.loader.kmt.scala.api;
 public interface Lexer {
 	
 	public java.util.List<IKToken> lex(String content);
+	public java.util.List<String> getReservedWords();
+	public java.util.List<String> getDelimiters();
 
 }
