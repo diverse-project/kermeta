ba5c3aba5a0fa5560ee77dc3ebc1949ff372d168
bouhours <bouhours@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2011-06-26 16:12:23 +0000

    API for autocompletion
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15344 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: fd6bcd1185947076e0b3bc505d51df5c101ef28d

---
+++ org/kermeta/language/org.kermeta.language.autocompletion.api/src/main/java/org/kermeta/language/autocompletion/api/Autocompletion.java
@@ -0,0 +1,11 @@
+package org.kermeta.language.autocompletion.api;
+
+import java.util.List;
+
+import org.kermeta.language.structure.ModelingUnit;
+
+public interface Autocompletion {
+	
+	public List<String> getAllVariableDecl();
+
+}
