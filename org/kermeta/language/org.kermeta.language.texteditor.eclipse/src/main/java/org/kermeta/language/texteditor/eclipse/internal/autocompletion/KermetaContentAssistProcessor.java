c56c3f4ebeb47859ecf50c153cb6eb33eef158d7
bouhours <bouhours@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2011-06-27 16:29:53 +0000

    Improve of the autocompletion
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15375 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: b59e75e74b53e88f2097b0d22361f4e3f8c494f2

--- org/kermeta/language/org.kermeta.language.texteditor.eclipse/src/main/java/org/kermeta/language/texteditor/eclipse/internal/autocompletion/KermetaContentAssistProcessor.java
+++ org/kermeta/language/org.kermeta.language.texteditor.eclipse/src/main/java/org/kermeta/language/texteditor/eclipse/internal/autocompletion/KermetaContentAssistProcessor.java
@@ -10,6 +10,7 @@
 
 package org.kermeta.language.texteditor.eclipse.internal.autocompletion;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,14 +25,18 @@ import org.kermeta.language.autocompletion.AutocompletionImpl;
 import org.kermeta.language.autocompletion.api.Autocompletion;
 import org.kermeta.language.builder.eclipse.KermetaBuilder;
 import org.kermeta.language.loader.kmt.scala.Lexer;
+import org.kermeta.language.loader.kmt.scala.api.IKToken;
+import org.kermeta.language.structure.ModelingUnit;
 import org.kermeta.language.texteditor.eclipse.internal.Activator;
 import org.kermeta.language.texteditor.eclipse.internal.KermetaEditor;
 import org.kermeta.language.texteditor.eclipse.internal.autocompletion.KermetaImage;
+import org.kermeta.utils.systemservices.api.messaging.MessagingSystem;
 
 public class KermetaContentAssistProcessor implements IContentAssistProcessor {
-	
+
 	KermetaEditor editor;
-	
+	private org.kermeta.language.loader.kmt.scala.api.Lexer theLexer = new Lexer();
+
 	public KermetaContentAssistProcessor(KermetaEditor editor) {
 		this.editor = editor;
 	}
@@ -40,11 +45,11 @@ public class KermetaContentAssistProcessor implements IContentAssistProcessor {
 	public ICompletionProposal[] computeCompletionProposals(ITextViewer viewer,	int documentOffset) {
 		// Retrieve current document
 		IDocument doc = viewer.getDocument();
-		
+
 		ArrayList<KermetaCompletionProposal> propList = new ArrayList<KermetaCompletionProposal>();
 
 		// Retrieve qualifier
-		String qualifier = getQualifier(doc, documentOffset);
+		List<IKToken> qualifier = getQualifier(doc, documentOffset);
 
 		// Compute completion proposals
 		computeProposals(qualifier, documentOffset, propList);
@@ -59,7 +64,7 @@ public class KermetaContentAssistProcessor implements IContentAssistProcessor {
 		return proposals; 
 	}
 
-	private String getQualifier(IDocument doc, int documentOffset) {
+	private List<IKToken> getQualifier(IDocument doc, int documentOffset) {
 
 		// Use string buffer to collect characters
 		StringBuffer currentLine = new StringBuffer();
@@ -72,79 +77,98 @@ public class KermetaContentAssistProcessor implements IContentAssistProcessor {
 		} catch (BadLocationException e1) {}
 
 		currentLine = currentLine.reverse();
-			
-		return currentLine.toString().trim();	
+
+		List<IKToken> temp = theLexer.lex(currentLine.toString().trim());
+
+		if (temp.size() == 0) {
+			temp.add(new IKToken() {			
+				@Override
+				public Integer getOffset() {
+					return null;
+				}			
+				@Override
+				public Integer getLength() {
+					return null;
+				}
+			});
+		}
+
+		return temp;	
 	}
-	
-	private boolean isTerminatedbyDelimiter(String qualifier,List<String> delimiters) {
-		for (String aDelimiter : delimiters) {
-			if (qualifier.endsWith(aDelimiter)) {
-				return true;
-			}
+
+	private boolean isTerminatedbyKeyword(List<IKToken> qualifier) {
+		if (qualifier.get(qualifier.size()-1).getClass().getSimpleName().equals("Keyword")) {
+			return true;
 		}
 		return false;
 	}
-	
-	private String getLastQualifier(String qualifier) {
-		String[] explode = qualifier.split("[ \t\n\f\r]");
-		return explode[explode.length-1];
-		
+
+	private String getDelimiter(List<IKToken> qualifier) {
+		int i = qualifier.size();
+		while (--i >= 0) {
+			if (qualifier.get(i).getClass().getSimpleName().equals("Delimiter")) {
+				return qualifier.get(i).toString();
+			}
+		}
+
+		return "";
 	}
-	
-	private void computeProposals(String qualifier, int documentOffset, List<KermetaCompletionProposal> propList) { 
-		   int qlen = qualifier.length();
-		   
-		   org.kermeta.language.loader.kmt.scala.api.Lexer theLexer = new Lexer(); 
-		   List<String> reserved = theLexer.getReservedWords();
-		   List<String> delimiters = theLexer.getDelimiters();
-		   List<String> theVariables = null;
-		   List<String> theOperations = null;
 
-		   if (editor.getFile() != null) {	   
-			   String kpIdentifier = KermetaBuilder.getDefault().findKPidentifierFromKMT(editor.getFile());
-			   
-			   if (KermetaBuilder.getDefault().getKpLastModelingunit(kpIdentifier) != null) {
-				   Autocompletion myAutocompletion = new AutocompletionImpl(Activator.getDefault().getMessaggingSystem(),KermetaBuilder.getDefault().getKpLastModelingunit(kpIdentifier));		   
-				   theVariables = myAutocompletion.getAllVariableDecl();
-				   theOperations = myAutocompletion.getAllOperations();
-			   }
-		   }
-		   
-		   if (isTerminatedbyDelimiter(qualifier, delimiters)) {
-			   //In this case, the last character is a delimiter
-		   } else {
-			   if (qlen > 0) {
-				   String lastQualifier = getLastQualifier(qualifier);
-				   for (String oneWord : reserved) {
-					   if (oneWord.startsWith(lastQualifier)) {
-					         int cursor = oneWord.length();
-					         KermetaCompletionProposal proposal = new KermetaCompletionProposal(oneWord, documentOffset - qlen, qlen, cursor, KermetaImage.getImage("/icons/kermeta.gif"));
+	private String getLastIdentifier(List<IKToken> qualifier) {
+		if (qualifier.get(qualifier.size()-1).getClass().getSimpleName().equals("Identifier")) {
+			return qualifier.get(qualifier.size()-1).toString();
+		}
 
-					         // and add to result list
-					         propList.add(proposal);
-					   }
-				   }
-			   } else {
-				   for (String oneWord : reserved) {
-				         int cursor = oneWord.length();
-				         KermetaCompletionProposal proposal = new KermetaCompletionProposal(oneWord, documentOffset - qlen, qlen, cursor, KermetaImage.getImage("/icons/kermeta.gif"));
+		return "";
+	}
 
-				         // and add to result list
-				         propList.add(proposal);
-				   }
-			   }
-		   }
-		   
-		   if (theVariables != null) {
-			   for (String aVariable : theVariables) {
-				   int cursor = aVariable.length();
-				   KermetaCompletionProposal proposal = new KermetaCompletionProposal(aVariable, documentOffset - qlen, qlen, cursor, KermetaImage.getImage("/icons/specific/VariableDecl.gif"));
+	private String getLastKeywordk(List<IKToken> qualifier) {
+		int i = qualifier.size();
+		while (--i >= 0) {
+			if (qualifier.get(i).getClass().getSimpleName().equals("Keyword")) {
+				return qualifier.get(i).toString();
+			}
+		}
 
-				   propList.add(proposal);
-			   }
-		   }
-		   
-		   /*if (theOperations != null) {
+		return "";
+	}
+
+
+	private void computeProposals(List<IKToken> qualifier, int documentOffset, List<KermetaCompletionProposal> propList) { 
+
+		int qlen = getLastIdentifier(qualifier).length();
+
+		List<String> reserved = theLexer.getReservedWords();   
+		List<String> theVariables = null;
+		List<String> theOperations = null;
+		List<String> theClassDefinition = null;
+
+		if (editor.getFile() != null) {	   
+			String kpIdentifier = KermetaBuilder.getDefault().findKPidentifierFromKMT(editor.getFile());
+
+			if (KermetaBuilder.getDefault().getKpLastModelingunit(kpIdentifier) != null) {
+				Autocompletion myAutocompletion = new AutocompletionImpl(Activator.getDefault().getMessaggingSystem(),KermetaBuilder.getDefault().getKpLastModelingunit(kpIdentifier));		   
+				theVariables = myAutocompletion.getAllVariableDecl();
+				theOperations = myAutocompletion.getAllOperations();
+				theClassDefinition = myAutocompletion.getAllClassDefinition();
+			}
+		}
+
+		if (getDelimiter(qualifier).equals("::") || getDelimiter(qualifier).equals(":")) {
+			//Proposal of ClassDef
+			proposeClassDefinition(qualifier, documentOffset, propList, qlen, theClassDefinition);
+		} else if (! isTerminatedbyKeyword(qualifier)) {
+			//Proposal of Keywords and Variables
+			proposeVariable(qualifier, documentOffset, propList, qlen, theVariables);
+			proposeKeywords(qualifier, documentOffset, propList, qlen, reserved);
+		} else {
+			if (getLastKeywordk(qualifier).equals("reference")) {
+				//Proposal of ClassDef
+				proposeClassDefinition(qualifier, documentOffset, propList, qlen, theClassDefinition);
+			}
+		}
+
+		/*if (theOperations != null) {
 			   for (String anOperation : theOperations) {
 				   int cursor = anOperation.length();
 				   KermetaCompletionProposal proposal = new KermetaCompletionProposal(anOperation, documentOffset - qlen, qlen, cursor, KermetaImage.getImage("/icons/green/operation.png"));
@@ -152,8 +176,55 @@ public class KermetaContentAssistProcessor implements IContentAssistProcessor {
 				   propList.add(proposal);
 			   }
 		   }*/
+	}
+
+	private void proposeKeywords(List<IKToken> qualifier, int documentOffset, List<KermetaCompletionProposal> propList, int qlen, List<String> reserved) {
+		String lastQualifier = getLastIdentifier(qualifier);
+		
+		for (String oneWord : reserved) {
+			int cursor = oneWord.length();
+			if (lastQualifier.length() > 0) {
+				if (oneWord.startsWith(lastQualifier)) {
+					propList.add(new KermetaCompletionProposal(oneWord, documentOffset - qlen, qlen, cursor, KermetaImage.getImage("/icons/kermeta.gif")));
+				}
+			} else {
+				propList.add(new KermetaCompletionProposal(oneWord, documentOffset - qlen, qlen, cursor, KermetaImage.getImage("/icons/kermeta.gif")));
+			}
 		}
-	
+	}
+
+	private void proposeVariable(List<IKToken> qualifier, int documentOffset, List<KermetaCompletionProposal> propList, int qlen, List<String> theVariables) {
+		String lastQualifier = getLastIdentifier(qualifier);
+
+		if (theVariables != null) {
+			for (String aVariable : theVariables) {
+				int cursor = aVariable.length();
+				if (lastQualifier.length() > 0) {
+					if (aVariable.startsWith(lastQualifier)) {
+						propList.add(new KermetaCompletionProposal(aVariable, documentOffset - qlen, qlen, cursor, KermetaImage.getImage("/icons/specific/VariableDecl.gif")));
+					}
+				} else {
+					propList.add(new KermetaCompletionProposal(aVariable, documentOffset - qlen, qlen, cursor, KermetaImage.getImage("/icons/specific/VariableDecl.gif")));
+				}
+			}
+		}
+	}
+
+	private void proposeClassDefinition(List<IKToken> qualifier, int documentOffset, List<KermetaCompletionProposal> propList, int qlen, List<String> theClassDefinition) {
+		String lastQualifier = getLastIdentifier(qualifier);
+
+		for (String aClassDef : theClassDefinition) {
+			int cursor = aClassDef.length();
+			if (lastQualifier.length() > 0) {
+				if (aClassDef.startsWith(lastQualifier)) {
+					propList.add(new KermetaCompletionProposal(aClassDef, documentOffset - qlen, qlen, cursor, KermetaImage.getImage("/icons/specific/ClassDefinition.gif")));
+				}					   
+			} else {			   
+				propList.add(new KermetaCompletionProposal(aClassDef, documentOffset - qlen, qlen, cursor, KermetaImage.getImage("/icons/specific/ClassDefinition.gif")));
+			}
+		}
+	}
+
 	@Override
 	public IContextInformation[] computeContextInformation(ITextViewer arg0,
 			int arg1) {
