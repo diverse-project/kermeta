fcd02a07e968fb7b935ef4464a395cce827fd822
bouhours <bouhours@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2011-07-08 14:34:26 +0000

    start of pointing
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15500 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 144fc5bad8e5f37e2415d0e51fcbe86c87678911

--- org/kermeta/language/org.kermeta.language.autocompletion.api/src/main/java/org/kermeta/language/autocompletion/api/Autocompletion.java
+++ org/kermeta/language/org.kermeta.language.autocompletion.api/src/main/java/org/kermeta/language/autocompletion/api/Autocompletion.java
@@ -1,13 +1,15 @@
 package org.kermeta.language.autocompletion.api;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public interface Autocompletion {
 	
 	public List<String> getAllVariableDecl();
-	public List<String> getAllOperations();
 	public List<String> getAllClassDefinition(String thePackages);
 	public List<String> getAllPackages();
 	public List<String> getSubPackages(String thePackages);
+	public HashMap<String,ArrayList<String>> getCallExpression(ArrayList<String> theIdentifiers,String thePackage);
 
 }
