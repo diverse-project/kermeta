8fc2f9149fe6207191697742a380720a0eb021f2
Olivier Barais <olivier.barais@gmail.com>
2011-07-11 11:52:39 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15508 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: ae877321f435dc8e5d4ad697d6eea7d7748679ce

--- org/kermeta/language/org.kermeta.language.loader.kmt.scala/src/main/scala/org/kermeta/language/loader/kmt/scala/internal/parser/sub/KLiteralParser.scala
+++ org/kermeta/language/org.kermeta.language.loader.kmt.scala/src/main/scala/org/kermeta/language/loader/kmt/scala/internal/parser/sub/KLiteralParser.scala
@@ -22,7 +22,7 @@ import scala.collection.JavaConversions._
  */
 trait KLiteralParser extends KAbstractParser {
 
-  def fLiteral : Parser[Expression] = (fVoidLiteral|fSuperLiteral|fSelfLiteral|fStringLiteral|fBooleanLiteral|fNumericLiteral)
+  def fLiteral : Parser[Expression] = (fVoidLiteral|fSuperLiteral|fSelfLiteral|fStringLiteral|fBooleanLiteral|fNumericLiteral|fresultLiteral)
   def fBooleanLiteral : Parser[Expression] = ("true" ^^^ {
       var newo = BehaviorFactory.eINSTANCE.createBooleanLiteral;newo.setValue(true)
       newo
@@ -33,6 +33,7 @@ trait KLiteralParser extends KAbstractParser {
   private def fSelfLiteral : Parser[Expression] = ( "self" ) ^^^ { BehaviorFactory.eINSTANCE.createSelfExpression() }
   private def fSuperLiteral : Parser[Expression] = ( "super" ) ^^^ { BehaviorFactory.eINSTANCE.createCallSuperOperation() }
   private def fVoidLiteral : Parser[Expression] = ( "Void" ) ^^^ { BehaviorFactory.eINSTANCE.createVoidLiteral }
+  private def fresultLiteral : Parser[Expression] = ( "result" ) ^^^ { BehaviorFactory.eINSTANCE.createCallResult }
   private def fStringLiteral : Parser[Expression] = ( stringLit ^^ { case e => var newo =BehaviorFactory.eINSTANCE.createStringLiteral;newo.setValue(e.toString);newo  } )
   private def fNumericLiteral : Parser[Expression] = ( numericLit ^^ { case e => var newo =BehaviorFactory.eINSTANCE.createIntegerLiteral;newo.setValue(e.toInt);newo  } )
 
