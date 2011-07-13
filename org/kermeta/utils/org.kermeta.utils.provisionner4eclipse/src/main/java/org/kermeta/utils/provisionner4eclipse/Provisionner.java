566c829a42abee3e218c1d2e52a648eac9ffb368
Didier Vojtisek <didier.vojtisek@inria.fr>
2011-07-13 14:45:46 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15520 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: c7cb625d33232291d6aa7d2370b0c7faf52fb6d7

---
+++ org/kermeta/utils/org.kermeta.utils.provisionner4eclipse/src/main/java/org/kermeta/utils/provisionner4eclipse/Provisionner.java
@@ -0,0 +1,65 @@
+/*$Id$
+* Project : org.kermeta.utils.provisionner.eclipse
+* License : EPL
+* Copyright : IRISA / INRIA / Universite de Rennes 1
+* ----------------------------------------------------------------------------
+* Creation date : 10 août 2010
+* Authors : 
+*			Didier Vojtisek <didier.vojtisek@inria.fr>
+*/
+package org.kermeta.utils.provisionner4eclipse;
+
+import java.util.ArrayList;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.core.runtime.Status;
+import org.kermeta.utils.provisionner4eclipse.preferences.PreferenceConstants;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
+public class Provisionner {
+	public void provisionFromPreferences(){
+		String bundleList = Activator.getDefault().getPreferenceStore().getString(PreferenceConstants.P_BUNDLE_URI_LIST);
+		String[] bundleRawURIs = bundleList.split("\n");
+		ArrayList<IStatus> statusChildren = new ArrayList<IStatus>();
+		boolean hasStartError = false;
+		for (int i = 0; i < bundleRawURIs.length; i++) {
+			String bundleURI = bundleRawURIs[i].trim();
+			if(!bundleURI.startsWith("#") && !bundleURI.isEmpty()){
+				
+				try{
+					BundleContext context = Activator.getDefault().getBundle().getBundleContext();
+					Bundle bundle = context.installBundle(bundleURI);
+					try{
+						bundle.start();
+						statusChildren.add(new Status(IStatus.INFO, Activator.PLUGIN_ID, 0, bundleURI + " installed and started ",null));
+					}
+					catch (Exception e) {
+						statusChildren.add(new Status(IStatus.ERROR, Activator.PLUGIN_ID, 0, "failed to start " + bundleURI, e));
+						hasStartError = true;
+					}
+				}
+				
+				catch (Exception e) {
+					if(e.getClass().getName().equals("org.eclipse.osgi.framework.internal.core.Framework$DuplicateBundleException")){
+						statusChildren.add(new Status(IStatus.WARNING, Activator.PLUGIN_ID, 0, "ingored installation of already installed bundle " + bundleURI, e));
+					}
+					else{
+						statusChildren.add(new Status(IStatus.ERROR, Activator.PLUGIN_ID, 0, "failed to install " + bundleURI, e));
+					}
+					hasStartError = true;
+				}
+				
+			}
+		}
+		
+		String message= hasStartError? "One or more bundle failed to start or install":"All bundles have successfully started";
+		MultiStatus mstatus = new MultiStatus(Activator.PLUGIN_ID, 0, message, null);
+		for (IStatus iStatus : statusChildren) {
+			mstatus.add(iStatus);
+		}
+		Activator.getDefault().getLog().log(mstatus);						
+		
+	}
+}
---
+++ org/kermeta/utils/org.kermeta.utils.provisionner4eclipse/src/main/java/org/kermeta/utils/provisionner4eclipse/preferences/BundleProvisionnerPreferencePage.java
@@ -0,0 +1,79 @@
+/*$Id$
+* Project : org.kermeta.utils.provisionner.eclipse
+* License : EPL
+* Copyright : IRISA / INRIA / Universite de Rennes 1
+* ----------------------------------------------------------------------------
+* Creation date : 2010
+* Authors : 
+*			Didier Vojtisek <didier.vojtisek@inria.fr>
+*/
+package org.kermeta.utils.provisionner4eclipse.preferences;
+
+import org.eclipse.jface.preference.*;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import org.kermeta.utilities.eclipse.jface.preference.HelpfulTextFieldEditor;
+import org.kermeta.utils.provisionner4eclipse.Activator;
+import org.kermeta.utils.provisionner4eclipse.Provisionner;
+
+/**
+ * This class represents a preference page that
+ * is contributed to the Preferences dialog. By 
+ * subclassing <samp>FieldEditorPreferencePage</samp>, we
+ * can use the field support built into JFace that allows
+ * us to create a page that is small and knows how to 
+ * save, restore and apply itself.
+ * <p>
+ * This page is used to modify preferences only. They
+ * are stored in the preference store that belongs to
+ * the main plug-in class. That way, preferences can
+ * be accessed directly via the preference store.
+ */
+
+public class BundleProvisionnerPreferencePage
+	extends FieldEditorPreferencePage
+	implements IWorkbenchPreferencePage {
+
+	public BundleProvisionnerPreferencePage() {
+		super(GRID);
+		setPreferenceStore(Activator.getDefault().getPreferenceStore());
+		setDescription("Additional Eclipse platform dynamic provisionning system: \n"+"" +
+				"This dynamicaly adds bundles from external sources.\n"	+
+				"Notes and current limitations in this experimental version: \n"+
+				"- removed entries won't be unloaded dynamically, \n  a restart of Eclipse is required for that."+
+				"- the order is important, it main fail to start a bundle if \n  a required bundles isn't present before.");
+	}
+	
+	/**
+	 * Creates the field editors. Field editors are abstractions of
+	 * the common GUI blocks needed to manipulate various types
+	 * of preferences. Each field editor knows how to save and
+	 * restore itself.
+	 */
+	public void createFieldEditors() {
+		HelpfulTextFieldEditor headerFE = new HelpfulTextFieldEditor(PreferenceConstants.P_BUNDLE_URI_LIST, 
+				"&Bundle URI List (one per line):\n", 
+				getFieldEditorParent());		
+		Font font = new Font(this.getShell().getDisplay(), "Courier", 8, SWT.NORMAL);;
+		headerFE.getTextControl(getFieldEditorParent()).setFont(font);
+		headerFE.setToolTipText("Add these bundles to your current Eclipse installation.\n");
+		addField(headerFE);
+		//addField(
+		//	new StringFieldEditor(PreferenceConstants.P_BUNDLE_URI_LIST, "Bundle URI List:", getFieldEditorParent()));
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
+	 */
+	public void init(IWorkbench workbench) {
+	}
+
+	@Override
+	protected void performApply() {
+		super.performApply();
+		new Provisionner().provisionFromPreferences();
+	}
+	
+}
