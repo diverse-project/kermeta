73182c1e76a99b0ba3c24e4118be2410c1f59850
Didier Vojtisek <didier.vojtisek@inria.fr>
2011-07-13 14:48:41 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15521 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: f548146b48ae7ada4829f9e465d259ab8207d0a6

---
+++ org/kermeta/utils/org.kermeta.utilities.eclipse.jface/src/main/java/org/kermeta/utilities/eclipse/jface/preference/HelpfulControl.java
@@ -0,0 +1,12 @@
+/*
+ Created on 27-May-2004
+ */
+package org.kermeta.utilities.eclipse.jface.preference;
+
+
+public interface HelpfulControl {
+
+    public void setToolTipText(String text);
+
+    public void setHelp(String helpContextId);
+}
