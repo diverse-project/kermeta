c134831845ff4d55692646a83044bd0a642ae1fc
Didier Vojtisek <didier.vojtisek@inria.fr>
2011-07-13 08:46:46 +0000

    added log message group for resolver
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15512 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 0a6585e600d4aec12937b4287c67593df17fe48d

--- org/kermeta/language/org.kermeta.language.resolver.api/src/main/java/org/kermeta/language/resolver/api/KmResolver.java
+++ org/kermeta/language/org.kermeta.language.resolver.api/src/main/java/org/kermeta/language/resolver/api/KmResolver.java
@@ -14,7 +14,8 @@ import org.kermeta.language.structure.ModelingUnit;
  ** This service offers to resolve and to set static types of a ModelingUnit
  */
 public interface KmResolver {
-
+	
+	public final static String LOG_MESSAGE_GROUP = "org.kermeta.language.resolver";
 	public ErrorProneResult<ModelingUnit> resolve(ModelingUnit mu);
 	public ErrorProneResult<ModelingUnit> doResolving(ModelingUnit mu);
     public ErrorProneResult<ModelingUnit> doStaticSetting(ModelingUnit mu);
