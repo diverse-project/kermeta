7032472214e71c50d7bcbd3165fb46d2dc9480cb
mclavreu <mclavreu@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2011-07-21 14:59:41 +0000

    Implementing a lookup for the method visit Workaround for the Kompilerv2 init4eclipse modification of the system factories to access a visit method that is not a direct member of the current Rich object that we manipulate
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15621 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: cd3b25a48aae6dc789bacc1ae76c475c92afd589

--- kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/loader/ecore/ecore2km/EcoreVisitor.java
+++ kermeta_projects/fr.irisa.triskell.kermeta.io/src/java/fr/irisa/triskell/kermeta/loader/ecore/ecore2km/EcoreVisitor.java
@@ -14,7 +14,11 @@
 package fr.irisa.triskell.kermeta.loader.ecore.ecore2km;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EAnnotation;
@@ -32,6 +36,7 @@ import org.eclipse.emf.ecore.EParameter;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.ETypeParameter;
 import org.eclipse.emf.ecore.impl.EStringToStringMapEntryImpl;
+import org.kermeta.io.plugin.IOPlugin;
 
 
 /**
@@ -40,6 +45,12 @@ import org.eclipse.emf.ecore.impl.EStringToStringMapEntryImpl;
  * Distributed under the terms of the EPL license
  */
 public class EcoreVisitor {
+	
+	// Maps for keeping data about the hierarchy of objects
+	// and data about the visit methods already found
+	Map<Class<?>,Set<Class<?>>> hierarchies = new HashMap<Class<?>, Set<Class<?>>>();
+	Map<Class<?>,Class<?>> parents = new HashMap<Class<?>,Class<?>>();
+	Map<Class<?>,Method> visits = new HashMap<Class<?>,Method>();
 
 			// This is a generic visit method.
 			public Object genericVisitChildren(EObject node) {
@@ -67,17 +78,39 @@ public class EcoreVisitor {
 						cname = "org.eclipse.emf.ecore.impl.EPackageImpl";
 					} */
 					
-//					|| 
-//					 cname.equals("org.eclipse.uml2.uml.internal.impl.UMLPackageImpl")
+		//					|| 
+		//					 cname.equals("org.eclipse.uml2.uml.internal.impl.UMLPackageImpl")
 					
 					//if ( ! cname.equals("org.eclipse.emf.ecore.impl.EStringToStringMapEntryImpl") )
 						// Special case for EStringToStringMapEntry which does not have an EMF interface.
 						//cname = cname.substring(0, cname.length()-4).replaceAll(".impl", "").replaceAll(".internal", "");					
 					
 					Class<?> c = Class.forName(cname, true, node.getClass().getClassLoader());
-					Method m =  getMethod( c );
-					if ( m == null )
-						throw new NoSuchMethodException("No method visit found for " + c.toString() );
+					Method m = null;
+					// Look for the right method to invoke
+					// First check if the method is accessible for the current type
+					Class<?>[] ptypes = new Class[1];
+					try{
+						ptypes[0] = c;
+						m = this.getClass().getMethod("visit",ptypes);
+					} catch (NoSuchMethodException e){
+						//We voluntarily ignore this exception
+						IOPlugin.internalLog.debug("[EcoreVisitor] No method visit found for " + c.toString());
+					}
+					if (m == null){
+						// Trying to get the method from a previous run
+						if (visits.containsKey(c)){
+							m = visits.get(c);
+						} else {
+						// Browse the object hierarchy
+							hierarchies.clear();
+							//getHierarchies(c);
+							buildHierarchy(c);
+							m =  getMethod( c );
+							visits.put(c,m);
+						}
+					} else
+						throw new NoSuchMethodException("[EcoreVisitor] No method visit found for " + c.toString() );
 					
 					Object[] params = new Object[1];
 					params[0] = node;
@@ -101,23 +134,108 @@ public class EcoreVisitor {
 					this.accept(o);
 				}
 			}
-
-			
+		
+			/**
+			 * Looks for the visit method in the hierarchy of cl
+			 * @param cl the parameter of the visit method that we are
+			 * looking for
+			 */
 			private Method getMethod(Class<?> cl) throws NoSuchMethodException {
 				Method m = null;
-				Class<?>[] ptypes = new Class[1];
-				try {
-					ptypes[0] = cl;
-					m = this.getClass().getMethod("visit", ptypes);
-				} catch (NoSuchMethodException e) {}
-				
-				Class<?>[] interfaces = cl.getInterfaces();
-				int index = 0;
-				while ( m == null ) {
-					m = getMethod( interfaces[index++] );
+				// List of candidate methods
+				Map<Method,Class<?>> methods = new HashMap<Method,Class<?>>();
+				// Building the list of all types in hierarchy
+				Set<Class<?>> listClasses = new HashSet<Class<?>>();
+				listClasses.add(cl);
+				for (Class<?> i : hierarchies.keySet()) {
+					listClasses.add(i);
+					listClasses.addAll(hierarchies.get(i));
 				}
-				
+				methods = lookForMethodInInterfaces(listClasses);
+				// From the set of candidate method, find the right one
+				m = selectMethodInHierarchy(methods);
 				return m;
+			}
+			
+			/**
+			 * Select one of the candidate method owned by the leaf
+			 * class in the hierarchy of classes
+			 * @param methods lists the candidates methods
+			 */
+			private Method selectMethodInHierarchy(Map<Method,Class<?>> methods) {
+				// Retrieves the type of the parameter of the first method
+				// for comparing types
+				Method m = ((Method)methods.keySet().toArray()[0]);
+				Class<?> lowerClassInHierarchy = m.getParameterTypes()[0];
+				for (Method method : methods.keySet()) {
+					if (method.getParameterTypes().length != 0){
+						// Find the lowest type in the hierarchy
+						for (Class<?> c: method.getParameterTypes()) {
+							if (isParent(lowerClassInHierarchy,c)){
+								parents.put(lowerClassInHierarchy,c);
+								lowerClassInHierarchy = c;
+								m = method;
+							}
+						}
+					}
+				}
+				return m;
+			}
+			
+			/**
+			 * Checks if a type is a supertype of another class
+			 * @param c is the possible parent
+			 * @param c2 is the possible child
+			 */
+			public boolean isParent(Class<?> c,Class<?> c2){
+				// Test already run
+				if ((parents.get(c)!= null) && parents.get(c).equals(c2))
+					return true;
+				if (c.equals(c2))
+					return true;
+				// if c is a direct parent of c2
+				if (hierarchies.get(c2).contains(c))
+					return true;
+				else {
+					// looking among indirect parents
+					boolean b = false;
+					for (Class<?> i : hierarchies.get(c2)) {
+						b = b || isParent(c,i);
+					}
+					return b;
+				}
+			}
+		
+			/**
+			 * Browse the hierarchy to find candidate methods
+			 * @param classes types of the hierarchy
+			 */
+			public Map<Method,Class<?>> lookForMethodInInterfaces(Set<Class<?>> classes){
+				Map<Method,Class<?>> m = new HashMap<Method,Class<?>>();
+				for (Class<?> cl : classes) {
+					Class<?>[] ptypes = new Class[1];
+					try{
+						ptypes[0] = cl;
+						m.put(this.getClass().getMethod("visit",ptypes),cl);
+					} catch (NoSuchMethodException e){
+						m.putAll(lookForMethodInInterfaces(hierarchies.get(cl)));
+					}
+				}
+				return m;
+			}
+			
+			/**
+			 * Builds the hierarchy of types for a given type
+			 * @param cl is the class from which we build the hierarchy
+			 */
+			public void buildHierarchy(Class<?> cl){
+				Set<Class<?>> supInterfaces = new HashSet<Class<?>>();
+				for (Class<?> i : cl.getInterfaces()) {
+					supInterfaces.add(i);
+					if (!hierarchies.containsKey(i))
+						buildHierarchy(i);
+				}
+				hierarchies.put(cl,supInterfaces);
 			}
 			
 			
