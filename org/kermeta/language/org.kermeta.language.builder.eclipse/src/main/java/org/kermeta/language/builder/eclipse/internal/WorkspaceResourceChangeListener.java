0a0431bd0385701c466d96c3915cab77777c7ded
bouhours <bouhours@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2011-07-26 15:52:59 +0000

    Nature management
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15669 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: d96fee1a88f07508ffaf77c10a5b62cf77c15e34

--- org/kermeta/language/org.kermeta.language.builder.eclipse/src/main/java/org/kermeta/language/builder/eclipse/internal/WorkspaceResourceChangeListener.java
+++ org/kermeta/language/org.kermeta.language.builder.eclipse/src/main/java/org/kermeta/language/builder/eclipse/internal/WorkspaceResourceChangeListener.java
@@ -1,17 +1,18 @@
 /*$Id:  $
-* License : EPL
-* Copyright : IRISA / INRIA 
-* ----------------------------------------------------------------------------
-* Creation date : 21 avr. 2011
-* Authors : 
-*      Didier Vojtisek <didier.vojtisek@inria.fr>
-*/
+ * License : EPL
+ * Copyright : IRISA / INRIA 
+ * ----------------------------------------------------------------------------
+ * Creation date : 21 avr. 2011
+ * Authors : 
+ *      Didier Vojtisek <didier.vojtisek@inria.fr>
+ */
 package org.kermeta.language.builder.eclipse.internal;
 
 import java.io.IOException;
 
 import org.eclipse.core.internal.resources.Workspace;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
@@ -28,103 +29,123 @@ import org.kermeta.language.builder.eclipse.KermetaBuilder;
 import org.kermeta.utils.systemservices.api.messaging.MessagingSystem.Kind;
 
 /**
- * This class is used to handle Workspace level events
- * It must do has less as possible concrete work, the real work is done in a separate jobs
- * it deals with add/remove of kp file
- * 		for each kp files, a specific KermetaKPResourceChangeListener is registered  
+ * This class is used to handle Workspace level events It must do has less as
+ * possible concrete work, the real work is done in a separate jobs it deals
+ * with add/remove of kp file for each kp files, a specific
+ * KermetaKPResourceChangeListener is registered
  */
-public class WorkspaceResourceChangeListener implements IResourceChangeListener{
+public class WorkspaceResourceChangeListener implements IResourceChangeListener {
 
 	public static final String KP_FILE_EXTENSION = "kp";
 	private KermetaBuilder kermetaBuilder = KermetaBuilder.getDefault();
-	
+
 	public WorkspaceResourceChangeListener() {
 		super();
 		// initialize builders
-		Workspace ws = (Workspace)ResourcesPlugin.getWorkspace();
+		Workspace ws = (Workspace) ResourcesPlugin.getWorkspace();
 		try {
 			ws.getRoot().accept(new KPBuilderInitializerResourceVisitor());
 		} catch (CoreException e) {
-			Activator.getDefault().getMessaggingSystem().log(Kind.DevERROR, "failed to initialize builders ", this.getClass().getName(), e);		
+			Activator.getDefault().getMessaggingSystem().log(Kind.DevERROR, "failed to initialize builders ", this.getClass().getName(), e);
 		}
 	}
 
-
 	class KPFileDeltaVisitor implements IResourceDeltaVisitor {
-		
+
 		public boolean visit(IResourceDelta delta) throws CoreException {
 			// Flag stating whether to continue the visit or not.
 			boolean processResourceChildren = true;
 			IResource resource = delta.getResource();
-			switch( delta.getResource().getType() ) {		
+			switch (delta.getResource().getType()) {
 			// Only handling project changes.
-			case IResource.PROJECT :
-				// maybe we should deal with project open/close ?
+			case IResource.PROJECT:
+				try {
+					if (resource.getProject() != null) {
+						if (resource.getProject().hasNature(org.kermeta.language.texteditor.eclipse.nature.Activator.NATURE_ID)) {
+							//do something to autobuild the projet ?
+						}
+					}
+				}  catch (CoreException e) {}
 				break;
 
-			case IResource.FILE :
-				if(resource.getFileExtension().equals(KP_FILE_EXTENSION)){
+			case IResource.FILE:
+				if (resource.getFileExtension().equals(KP_FILE_EXTENSION)) {
 					KPBuilder builder = null;
 					switch (delta.getKind()) {
 					case IResourceDelta.ADDED:
-						// add a new visitor dedicated to this Kermeta project file
-						Activator.getDefault().getMessaggingSystem().log(Kind.DevDEBUG, "adding builder for "+resource.getFullPath(), this.getClass().getName());
-						final KPBuilder aBuilder = new KPBuilder((IFile) resource);
-						
-						kermetaBuilder.kpBuilders.put(kermetaBuilder.generateIdentifier(resource),aBuilder);
-						
-						Job job = new Job("Kermeta builder initializer for "+aBuilder.getKpProjectFile().getRawLocation()) {
-							protected IStatus run(IProgressMonitor monitor) {
-								aBuilder.compile();
-								return Status.OK_STATUS;
-					        }
-					    };
-					    job.setPriority(Job.LONG);
-					    job.schedule();	
-						
+						try {
+							if (resource.getProject() != null) {
+								if (resource.getProject().hasNature(org.kermeta.language.texteditor.eclipse.nature.Activator.NATURE_ID)) {
+									createBuilder(resource);
+								}
+							}
+						} catch (CoreException e) {}
 						break;
 					case IResourceDelta.REMOVED:
 						// handle removed resource
-						Activator.getDefault().getMessaggingSystem().log(Kind.DevDEBUG, "removing builder for "+resource.getFullPath(), this.getClass().getName());
-						if(kermetaBuilder != null){
+						Activator.getDefault().getMessaggingSystem().log(Kind.DevDEBUG, "removing builder for " + resource.getFullPath(), this.getClass().getName());
+						if (kermetaBuilder != null) {
 							kermetaBuilder.kpBuilders.remove(kermetaBuilder.generateIdentifier(resource));
-						}
-						else{
-							Activator.getDefault().getMessaggingSystem().log(Kind.DevERROR, "failed to remove builder for "+resource.getFullPath(), this.getClass().getName());							
+						} else {
+							Activator.getDefault().getMessaggingSystem().log(Kind.DevERROR, "failed to remove builder for " + resource.getFullPath(), this.getClass().getName());
 						}
 						break;
 					case IResourceDelta.CHANGED:
-						 if ((delta.getFlags() & IResourceDelta.CONTENT) == 0)
-							  // we care only about content change
-			                  return false;
-						Activator.getDefault().getMessaggingSystem().log(Kind.DevDEBUG, "refreshing builder for "+resource.getFullPath(), this.getClass().getName());
-						builder = kermetaBuilder.kpBuilders.get(kermetaBuilder.generateIdentifier(resource));
-						if(builder != null){
-							try {
-								builder.refreshFileIndex();
-								builder.compile();
-							} catch (IOException e) {
-								Activator.getDefault().getMessaggingSystem().log(Kind.DevERROR, "failed to refresh builder for "+resource.getFullPath(), this.getClass().getName());
+						if ((delta.getFlags() & IResourceDelta.CONTENT) == 0)
+							// we care only about content change
+							return false;
+						try {
+							if (resource.getProject() != null) {
+								if (resource.getProject().hasNature(org.kermeta.language.texteditor.eclipse.nature.Activator.NATURE_ID)) {
+									Activator.getDefault().getMessaggingSystem().log(Kind.DevDEBUG, "refreshing builder for " + resource.getFullPath(), this.getClass().getName());
+									builder = kermetaBuilder.kpBuilders.get(kermetaBuilder.generateIdentifier(resource));
+									if (builder != null) {
+										try {
+											builder.refreshFileIndex();
+											builder.compile();
+										} catch (IOException e) {
+											Activator.getDefault().getMessaggingSystem().log(Kind.DevERROR, "failed to refresh builder for " + resource.getFullPath(), this.getClass().getName());
+										}
+									} else {
+										createBuilder(resource);
+									}
+
+								}
 							}
-						}
-						else{
-							Activator.getDefault().getMessaggingSystem().log(Kind.DevERROR, "failed to refresh builder for "+resource.getFullPath(), this.getClass().getName());							
-						}
+						} catch (CoreException e) {}
 						break;
 					}
 				}
 				processResourceChildren = false;
 				break;
-								
-			default :
+
+			default:
 				break;
 			}
-			
-			//return true to continue visiting children.
+
+			// return true to continue visiting children.
 			return processResourceChildren;
 		}
+
+		private void createBuilder(IResource resource) {
+			// add a new visitor dedicated to this
+			// Kermeta project file
+			Activator.getDefault().getMessaggingSystem().log(Kind.DevDEBUG, "adding builder for " + resource.getFullPath(), this.getClass().getName());
+			final KPBuilder aBuilder = new KPBuilder((IFile) resource);
+
+			kermetaBuilder.kpBuilders.put(kermetaBuilder.generateIdentifier(resource), aBuilder);
+
+			Job job = new Job("Kermeta builder initializer for " + aBuilder.getKpProjectFile().getRawLocation()) {
+				protected IStatus run(IProgressMonitor monitor) {
+					aBuilder.compile();
+					return Status.OK_STATUS;
+				}
+			};
+			job.setPriority(Job.LONG);
+			job.schedule();
+		}
 	}
-	
+
 	/**
 	 * Initialize the builders by looking for kp files in the workbench
 	 */
@@ -132,44 +153,52 @@ public class WorkspaceResourceChangeListener implements IResourceChangeListener{
 		public boolean visit(IResource resource) {
 			// Flag stating whether to continue the visit or not.
 			boolean processResourceChildren = true;
-			switch( resource.getType() ) {
-			case IResource.PROJECT :
-				// ignore closed projects
-				
-				break;
-			case IResource.FILE :
-				if (resource.getFileExtension()!=null) {
-					if(resource.getFileExtension().equals(KP_FILE_EXTENSION)){
-						// ignore kp in target folder
-						if(!resource.getProjectRelativePath().segments()[0].equals("target")){
-							Activator.getDefault().getMessaggingSystem().log(Kind.DevDEBUG, "adding builder for "+resource.getFullPath(), this.getClass().getName());
-							final KPBuilder aBuilder = new KPBuilder((IFile) resource);
-							kermetaBuilder.kpBuilders.put(kermetaBuilder.generateIdentifier(resource),aBuilder);
-							Job job = new Job("Kermeta builder initializer for "+aBuilder.getKpProjectFile().getRawLocation()) {
-								protected IStatus run(IProgressMonitor monitor) {
-									aBuilder.compile();
-									return Status.OK_STATUS;
-						        }
-						    };
-						    job.setPriority(Job.LONG);
-						    job.schedule();						
+			try {
+				if (resource.getProject() != null) {
+					if (resource.getProject().hasNature(org.kermeta.language.texteditor.eclipse.nature.Activator.NATURE_ID)) {
+						switch (resource.getType()) {
+						case IResource.PROJECT:
+							// ignore closed projects
+
+							break;
+						case IResource.FILE:
+							if (resource.getFileExtension() != null) {
+								if (resource.getFileExtension().equals(KP_FILE_EXTENSION)) {
+									// ignore kp in target folder
+									if (!resource.getProjectRelativePath().segments()[0].equals("target")) {
+										Activator.getDefault().getMessaggingSystem().log(Kind.DevDEBUG, "adding builder for " + resource.getFullPath(), this.getClass().getName());
+										final KPBuilder aBuilder = new KPBuilder((IFile) resource);
+										kermetaBuilder.kpBuilders.put(kermetaBuilder.generateIdentifier(resource), aBuilder);
+										Job job = new Job("Kermeta builder initializer for " + aBuilder.getKpProjectFile().getRawLocation()) {
+											protected IStatus run(IProgressMonitor monitor) {
+												aBuilder.compile();
+												return Status.OK_STATUS;
+											}
+										};
+										job.setPriority(Job.LONG);
+										job.schedule();
+									}
+								}
+							}
+							processResourceChildren = false;
+							break;
+
+						default:
+							break;
 						}
 					}
 				}
-				processResourceChildren = false;
-				break;
-								
-			default :
-				break;
+			} catch (CoreException e) {
 			}
+
 			return processResourceChildren;
 		}
 	}
-	
+
 	@Override
 	public void resourceChanged(IResourceChangeEvent event) {
 		final IResourceDelta delta = event.getDelta();
-		if ( delta != null ){
+		if (delta != null) {
 			Job job = new Job("Kermeta builder root job") {
 				protected IStatus run(IProgressMonitor monitor) {
 					// see if the event need a refresh of the kpBuilders
@@ -179,15 +208,15 @@ public class WorkspaceResourceChangeListener implements IResourceChangeListener{
 						Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, IStatus.ERROR, "failed to process delta with KPFileDeltaVisitor", e));
 					}
 					/* see if the event is related to one of our builders */
-					for(KPBuilder builder : kermetaBuilder.kpBuilders.values()){
+					for (KPBuilder builder : kermetaBuilder.kpBuilders.values()) {
 						builder.conditionalBuild(delta);
 					}
-		            return Status.OK_STATUS;
-		        }
-		     };
-		    job.setPriority(Job.SHORT);
-		    job.schedule(); // start as soon as possible
-			
+					return Status.OK_STATUS;
+				}
+			};
+			job.setPriority(Job.SHORT);
+			job.schedule(); // start as soon as possible
+
 		}
 	}
 }
