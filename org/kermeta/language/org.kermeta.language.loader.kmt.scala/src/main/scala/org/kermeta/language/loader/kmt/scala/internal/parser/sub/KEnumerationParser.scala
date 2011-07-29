181389fa3b674cd301e1331eca3a0f1e47f78cc4
Arnaud Blouin <arnaud.blouin@irisa.fr>
2011-07-29 12:40:13 +0000

    Enumeration supported by the kmt parser.
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15740 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: eb223a45236b370a9098137f93eae23102af0cd7

---
+++ org/kermeta/language/org.kermeta.language.loader.kmt.scala/src/main/scala/org/kermeta/language/loader/kmt/scala/internal/parser/sub/KEnumerationParser.scala
@@ -0,0 +1,35 @@
+/* $Id: KEnumerationParser.scala 14357 2011-07-29 14:17:16Z $
+ * Project : org.kermeta.language.loader.scalaKMT
+ * License : EPL
+ * Copyright : IRISA / INRIA/ Universite de Rennes 1
+ * ----------------------------------------------------------------------------
+ * Creation date : 2011
+ * Authors :
+ * 		Arnaud Blouin
+ */
+
+package org.kermeta.language.loader.kmt.scala.internal.parser.sub
+
+import org.kermeta.language.structure._
+import scala.collection.JavaConversions._
+
+
+trait KEnumerationParser extends KAbstractParser {
+  def enumDecl : Parser[Enumeration] = ("enumeration"|"enum") ~ ident ~ "{" ~ rep(enumLiteral) ~ "}" ^^ {
+      case  _ ~ name ~ _ ~ listLiterals ~ _ =>
+
+    var newEnum : Enumeration = StructureFactory.eINSTANCE.createEnumeration
+    newEnum.setName(name)
+
+    listLiterals.foreach{ name =>
+      var newLiteral = StructureFactory.eINSTANCE.createEnumerationLiteral()
+      newLiteral.setName(name)
+      newEnum.getOwnedLiteral.add(newLiteral)
+    }
+
+    newEnum
+  }
+
+
+  def enumLiteral = ident <~ ";"
+}
