8d591534d4e20d3ffd7a41c68f021d609befaadd
bouhours <bouhours@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2011-08-05 11:27:10 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@15854 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 7165dae2142c7040ed9578d3ed11ac610b5801f6

---
+++ org/kermeta/kp/org.kermeta.kp.launcher.eclipse/src/main/java/org/kermeta/kp/launcher/eclipse/internal/BuildKP.java
@@ -0,0 +1,35 @@
+package org.kermeta.kp.launcher.eclipse.internal;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.kermeta.language.builder.eclipse.KermetaBuilder;
+
+public class BuildKP implements IObjectActionDelegate {
+
+	private ISelection selection;
+	
+	@Override
+	public void setActivePart(IAction arg0, IWorkbenchPart arg1) {
+	}
+
+	@Override
+	public void run(IAction arg0) {
+		if (selection instanceof IStructuredSelection) {
+			Object oneElement = ((IStructuredSelection)selection).getFirstElement();
+			if (oneElement instanceof IFile) {
+				KermetaBuilder.getDefault().buildFromKP(KermetaBuilder.getDefault().generateIdentifier((IFile) oneElement));				
+			}
+		}
+	}
+
+	@Override
+	public void selectionChanged(IAction arg0, ISelection selection) {
+		this.selection = selection;		
+	}
+
+}
---
+++ org/kermeta/kp/org.kermeta.kp.launcher.eclipse/src/main/java/org/kermeta/kp/launcher/eclipse/internal/KermetaTabGroup.java
@@ -0,0 +1,17 @@
+package org.kermeta.kp.launcher.eclipse.internal;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+
+public class KermetaTabGroup extends AbstractLaunchConfigurationTabGroup{
+
+	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] { 
+				new KermetaTabConfiguration()
+		};
+		setTabs(tabs);
+		
+	}
+
+}
