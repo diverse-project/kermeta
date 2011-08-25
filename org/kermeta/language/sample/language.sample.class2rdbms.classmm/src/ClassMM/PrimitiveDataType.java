ac7ca6f21fe076c51406bae745359910b9fdb373
Didier Vojtisek <didier.vojtisek@inria.fr>
2011-08-25 15:13:38 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@16030 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: f7a6fa957f943cccbeaf47a8b077e062bec310af

---
+++ org/kermeta/language/sample/language.sample.class2rdbms.classmm/src/ClassMM/PrimitiveDataType.java
@@ -0,0 +1,21 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package ClassMM;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Primitive Data Type</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see ClassMM.ClassMMPackage#getPrimitiveDataType()
+ * @model
+ * @generated
+ */
+public interface PrimitiveDataType extends Classifier {
+} // PrimitiveDataType
