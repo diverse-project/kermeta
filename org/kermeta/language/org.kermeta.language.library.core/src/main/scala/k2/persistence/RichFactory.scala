4fe5d55fb3f51fc0f3011043e0309b5c920924c7
Didier Vojtisek <didier.vojtisek@inria.fr>
2011-09-08 14:34:51 +0000

    add XMIResourceFactory by default for this EMF ResourceSet
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@16155 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 513ade57e9487320add754f74b58f8a358040240

--- org/kermeta/language/org.kermeta.language.library.core/src/main/scala/k2/persistence/RichFactory.scala
+++ org/kermeta/language/org.kermeta.language.library.core/src/main/scala/k2/persistence/RichFactory.scala
@@ -1,6 +1,12 @@
 package k2.persistence
 object KerRichFactory {
- def createResourceSet : org.eclipse.emf.ecore.resource.ResourceSet = { new org.eclipse.emf.ecore.resource.impl.ResourceSetImpl }
+  //def createResourceSet : org.eclipse.emf.ecore.resource.ResourceSet = { new org.eclipse.emf.ecore.resource.impl.ResourceSetImpl }
+  def createResourceSet : org.eclipse.emf.ecore.resource.ResourceSet = {
+    val rs = new org.eclipse.emf.ecore.resource.impl.ResourceSetImpl
+    // by default knows about xmi
+    rs.getResourceFactoryRegistry.getExtensionToFactoryMap.put("*",new org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl())
+    rs
+  }
 }
 
  
