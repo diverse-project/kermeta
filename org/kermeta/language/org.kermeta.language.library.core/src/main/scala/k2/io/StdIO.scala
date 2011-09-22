20c1898654bfbb6b06974b7ffc6396c496c5fe74
Didier Vojtisek <didier.vojtisek@inria.fr>
2011-09-22 08:54:01 +0000

    by default use a real System.in err out the MessagingSystem is optional and used when deployed in a more complex environment (ex: bundle)
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@16385 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: bbfcb339963dd63f1073e86d701b8460e59792f0

--- org/kermeta/language/org.kermeta.language.library.core/src/main/scala/k2/io/StdIO.scala
+++ org/kermeta/language/org.kermeta.language.library.core/src/main/scala/k2/io/StdIO.scala
@@ -8,40 +8,59 @@ package k2.io
 
 class StdIOClass extends k2.standard.EObjectImplForPrimitive {
 
-    // default messaging system goes to stdio
-    var _messagingSystem : _root_.org.kermeta.utils.systemservices.api.messaging.MessagingSystem = new _root_.org.kermeta.utils.systemservices.api.impl.StdioSimpleMessagingSystem
-    def messagingSystem_=(arg : _root_.org.kermeta.utils.systemservices.api.messaging.MessagingSystem) = {_messagingSystem = arg}
+    // by default no messaging system, use System in, out, err
+    // in more complex environment, one can use _root_.org.kermeta.utils.systemservices.api.impl.StdioSimpleMessagingSystem
+    var _messagingSystem : _root_.org.kermeta.utils.systemservices.api.messaging.MessagingSystem  = null
+    def messagingSystem_=(arg : _root_.org.kermeta.utils.systemservices.api.messaging.MessagingSystem) = {
+      _messagingSystem = arg
+    }
 
     // writebuffer is used to make sure we create an info message only when a \n is reached or another messages needs it to be flushed
     var _writeBuffer : StringBuffer = new  StringBuffer
     def write(obj : AnyRef) : Unit = {
-        val message : String = obj.toString
+      if(_messagingSystem != null){
+         val message : String = obj.toString
         _writeBuffer.synchronized{         // basic way to handle buffer, flush if ends with \n
             _writeBuffer.append(message)
             if(message.endsWith("\n")){
                 flushBuffers()
             }
         }
+      }
+      else{
+        System.out.print(obj.toString)
+      }
     }
 
     def errorln(obj : AnyRef) : Unit = {
+      if(_messagingSystem != null){
         flushBuffers()
         _messagingSystem.error(obj.toString,"")
+      }
+      else{
+        System.err.println(obj.toString)
+      }
     }
 
 
     var _errorBuffer : StringBuffer = new  StringBuffer
     def error(obj : AnyRef) : Unit = {
-      val message : String = obj.toString
-      _errorBuffer.synchronized{         // basic way to handle error buffer, wait for
-        _errorBuffer.append(message)
-        if(message.endsWith("\n")){
-            flushBuffers()
+      if(_messagingSystem != null){
+        val message : String = obj.toString
+        _errorBuffer.synchronized{         // basic way to handle error buffer, wait for
+          _errorBuffer.append(message)
+          if(message.endsWith("\n")){
+              flushBuffers()
+          }
         }
+      }
+      else{
+        System.err.print(obj.toString)
       }
     }
 
     def flushBuffers() : Unit = {
+
       _errorBuffer.synchronized{         // basic way to handle error buffer,
           if(_errorBuffer.length() != 0){
               _messagingSystem.error(_errorBuffer.toString,"")
@@ -57,12 +76,26 @@ class StdIOClass extends k2.standard.EObjectImplForPrimitive {
     }
 
     def writeln(obj : AnyRef) : Unit = {
-        flushBuffers()
-        _messagingSystem.info(obj.toString,"")
+        if(_messagingSystem != null){
+          flushBuffers()
+          _messagingSystem.info(obj.toString,"")
+        }
+        else{
+          System.out.println(obj.toString)
+        }
     }
 
     def read(prompt : String) : String = {
-        return _messagingSystem.readLine(prompt)
+        if(_messagingSystem != null){
+          return _messagingSystem.readLine(prompt)
+        }
+        else{
+          var ligne_lue:String =null;
+            var lecteur: java.io.InputStreamReader =new java.io.InputStreamReader(System.in);
+            var entree:java.io.BufferedReader =new java.io.BufferedReader(lecteur);
+            ligne_lue=entree.readLine();
+            return ligne_lue;
+        }
     }
 
 }
