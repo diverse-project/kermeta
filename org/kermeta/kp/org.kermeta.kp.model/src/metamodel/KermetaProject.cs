3d8a3fe379016dd7d73e34bc8adcf1accd51e54f
Didier Vojtisek <didier.vojtisek@inria.fr>
2011-10-26 07:12:41 +0000

    first simplification draft
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@16493 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: c842a12325ccefb546302cb4c910ad8942eb2f8e

--- org/kermeta/kp/org.kermeta.kp.model/src/metamodel/KermetaProject.cs
+++ org/kermeta/kp/org.kermeta.kp.model/src/metamodel/KermetaProject.cs
@@ -1,95 +1,51 @@
-
-
-SYNTAXDEF kp
+SYNTAXDEF org.kermeta.kp
 FOR <http://www.kermeta.org/kp/1.0.0>
 START KermetaProject
-OPTIONS { 
-    basePackage="org.kermeta.kp.editor"; 
-    uiBasePackage = "org.kermeta.kp.editor.ui";
-    srcFolder = "src/main/java";
-    srcGenFolder = "src/main/java-gen";
-    generateCodeFromGeneratorModel = "true";
-    tokenspace="0";
-    
+
+
+TOKENS {
+	DEFINE COMMENT $'//'(~('\n'|'\r'|'\uffff'))*$;
+	DEFINE INTEGER $('-')?('1'..'9')('0'..'9')*|'0'$;
+	DEFINE FLOAT $('-')?(('1'..'9') ('0'..'9')* | '0') '.' ('0'..'9')+ $;
 }
 
-TOKENS{
-    DEFINE SL_COMMENT$'//'(~('\n'|'\r'))*$;
-    DEFINE ML_COMMENT $'/*'.*'*/'$ ;
-    DEFINE INTEGER$('-')?('1'..'9')('0'..'9')*|'0'$;
-    DEFINE FLOAT$('-')?(('1'..'9') ('0'..'9')* | '0') '.' ('0'..'9')+ $;  
+
+TOKENSTYLES {
+	"KermetaProject" COLOR #7F0055, BOLD;
+	"name" COLOR #7F0055, BOLD;
+	"dependencies" COLOR #7F0055, BOLD;
+	"sources" COLOR #7F0055, BOLD;
+	"weaveDirectives" COLOR #7F0055, BOLD;
+	"options" COLOR #7F0055, BOLD;
+	"version" COLOR #7F0055, BOLD;
+	"group" COLOR #7F0055, BOLD;
+	"defaultMainClass" COLOR #7F0055, BOLD;
+	"defaultMainOperation" COLOR #7F0055, BOLD;
+	"Dependency" COLOR #7F0055, BOLD;
+	"url" COLOR #7F0055, BOLD;
+	"Source" COLOR #7F0055, BOLD;
+	"from" COLOR #7F0055, BOLD;
+	"SourceQuery" COLOR #7F0055, BOLD;
+	"query" COLOR #7F0055, BOLD;
+	"WeaveDirective" COLOR #7F0055, BOLD;
+	"mix" COLOR #7F0055, BOLD;
+	"target" COLOR #7F0055, BOLD;
+	"Option" COLOR #7F0055, BOLD;
+	"value" COLOR #7F0055, BOLD;
+	"StringExpression" COLOR #7F0055, BOLD;
+	"MixExpression" COLOR #7F0055, BOLD;
+	"left" COLOR #7F0055, BOLD;
+	"right" COLOR #7F0055, BOLD;
 }
 
-TOKENSTYLES{
-    "dependency" COLOR #A22000, BOLD;
-    "weaver-directive" COLOR #007F55, BOLD;
-    "merger-option" COLOR #CC8000, BOLD;
-    "SL_COMMENT" COLOR #348017;
-    "ML_COMMENT" COLOR #348017;
-}
 
-RULES{
-    
-    KermetaProject::= 
-   // "KermetaProject" _[WHITESPACE]* ":" _[WHITESPACE]* name['"','"'] _[WHITESPACE]* !0
-    "KermetaProject" ":"  name['"','"']  !0
-    (
-    	(
-	    	("version"  ":"  version['"','"']  ) |
-	    	("group"  ":"   group['"','"']  ) |
-	    	("defaultMainClass"  ":"   defaultMainClass['"','"']  ) |
-	    	("defaultMainOperation"  ":"  defaultMainOperation['"','"']  )
-	    ) !0
-    )*
-    "{"
-        (!1sources!0 | !1dependencies!0 |  !1options!0 | !1weaveDirectives )*!0
-    "}"!0
-    ("ref" "{" (!1ref)*!0 "}")? 
-    ;
-    
-    Source::=
-    "source"  "=" url['"','"']
-    ;
-    
-    SourceFolder::=  
-    "srcDir" "=" (folderName['"','"'] | url['"','"'] )  
-    ;
-    
-    SourceFile::= 
-    "srcFile" "=" (fileName['"','"'] | url['"','"'] ) 
-    ;
-    
-    SourceNSURI::=  
-    "srcNSURI" "=" url['"','"']  ("from" from['"','"'])?  
-    ;
-    
-    SourceQuery::=  
-    "srcQuery" "=" query['"','"'] ("from" from['"','"'] ("URL" "=" url['"','"'])?)?  
-    ;
-    
-    Dependency::=  
-    "dependency" name['"','"'] "URL" "=" url['"','"'] |
-    "dependency" name['"','"'] "=" depRef['"','"'] group['"','"']? version['"','"']?
-    ;
-    
-    WeaveDirective::= 
-    "weaver-directive" name['"','"']? "="  mix target   
-    ;
-    
-    Option::= 
-    "merger-option" name['"','"']? "=" value['"','"'] 
-    ;
-    
-    StringExpression::= 
-    value['"','"']
-    ;
-     
-    MixExpression::= 
-    "(" left  right ")" 
-    ;
-    
-    KermetaProjectRef ::= 
-    group['"','"']  ":" name['"','"'] ("URL" "=" url['"','"'])? ("[" version['"','"'] "]")?
-    ;
-    
+RULES {
+	KermetaProject ::= "KermetaProject" "{" ("name" ":" name['"','"'] | "dependencies" ":" dependencies | "sources" ":" sources | "weaveDirectives" ":" weaveDirectives | "options" ":" options | "version" ":" version['"','"'] | "group" ":" group['"','"'] | "defaultMainClass" ":" defaultMainClass['"','"'] | "defaultMainOperation" ":" defaultMainOperation['"','"'])* "}";
+	Dependency ::= "Dependency" "{" ("name" ":" name['"','"'] | "url" ":" url[])* "}";
+	Source ::= "Source" "{" ("url" ":" url['"','"'] | "from" ":" from[])* "}";
+	SourceQuery ::= "SourceQuery" "{" ("url" ":" url['"','"'] | "from" ":" from[] | "query" ":" query['"','"'])* "}";
+	WeaveDirective ::= "WeaveDirective" "{" ("name" ":" name['"','"'] | "mix" ":" mix | "target" ":" target)* "}";
+	Option ::= "Option" "{" ("name" ":" name['"','"'] | "value" ":" value['"','"'])* "}";
+	StringExpression ::= "StringExpression" "{" ("value" ":" value['"','"'])* "}";
+	MixExpression ::= "MixExpression" "{" ("left" ":" left | "right" ":" right)* "}";
 }
