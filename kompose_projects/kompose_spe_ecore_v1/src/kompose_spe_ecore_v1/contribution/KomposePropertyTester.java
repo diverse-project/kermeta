67c370d6e8174b04bc9c15b6d781c150186ecd49
mclavreu <mclavreu@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2011-11-16 09:39:21 +0000

    Last version of Kompose v1 for ECore
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@16630 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: f133b7a75219f9392e3888e9f00085a1c634ceef

---
+++ kompose_projects/kompose_spe_ecore_v1/src/kompose_spe_ecore_v1/contribution/KomposePropertyTester.java
@@ -0,0 +1,48 @@
+package kompose_spe_ecore_v1.contribution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+
+public class KomposePropertyTester extends PropertyTester {
+
+	private static final String METAMODEL = "http://www.eclipse.org/emf/2002/Ecore";
+	
+	public boolean test(Object receiver, String property, Object[] args,
+			Object expectedValue) {
+		IFile komposeModel = (IFile) receiver;
+		BufferedReader br = null;
+		String composerDefinition = "";
+		String composerMetamodel = "";
+		try {
+			br = new BufferedReader(new InputStreamReader(komposeModel.getContents()));
+			br.readLine();
+			composerDefinition = br.readLine();
+			String[] chunks = composerDefinition.split(" ");
+			for (int i = 0; i < chunks.length; i++) {
+				if (chunks[i].startsWith("metamodel")){
+					composerMetamodel = chunks[i].substring(chunks[i].indexOf("=")+2, chunks[i].lastIndexOf("\""));
+				}
+			}
+		} catch (CoreException e) {
+			System.out.println("KOMPOSE-ERROR: Impossible to open selected file");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("KOMPOSE-ERROR: Impossible to read selected file");
+			e.printStackTrace();
+		}
+		if (composerMetamodel.equals(METAMODEL)){
+			System.out.println("Property Tester returning true");
+			return true;
+		}
+		else{
+			System.out.println("Property Tester returning false");
+			return false;
+		}
+	}
+}
+
