b27082e2509896d753b6257ee76411ba94c6617c
Didier Vojtisek <didier.vojtisek@inria.fr>
2011-11-21 12:32:49 +0000

    kp mm and syntax changes to support new require/source modifiers
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@16700 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 9bed96b008c66f890ef8b7266e7d4adf1f6c16eb

--- org/kermeta/kp/org.kermeta.kp.editor.ui/src-gen/org/kermeta/kp/editor/ui/KpPropertyTester.java
+++ org/kermeta/kp/org.kermeta.kp.editor.ui/src-gen/org/kermeta/kp/editor/ui/KpPropertyTester.java
@@ -6,23 +6,7 @@
  */
 package org.kermeta.kp.editor.ui;
 
-public class KpPropertyTester extends org.eclipse.core.expressions.PropertyTester {
+public class KpPropertyTester {
 	
-	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
-		if (receiver instanceof org.eclipse.core.resources.IResource) {
-			org.eclipse.core.resources.IResource resource = (org.eclipse.core.resources.IResource) receiver;
-			return hasMatchingURI(resource);
-		} else if (receiver instanceof org.eclipse.ui.part.FileEditorInput) {
-			org.eclipse.ui.part.FileEditorInput editorInput = (org.eclipse.ui.part.FileEditorInput) receiver;
-			org.eclipse.core.resources.IFile file = editorInput.getFile();
-			return hasMatchingURI(file);
-		}
-		return false;
-	}
-	
-	private boolean hasMatchingURI(org.eclipse.core.resources.IResource resource) {
-		String path = resource.getLocationURI().getPath();
-		return path.endsWith("." + new org.kermeta.kp.editor.mopp.KpMetaInformation().getSyntaxName());
-	}
-	
+	// This class is currently empty, because launch support was disabled for this DSL.
 }
--- org/kermeta/kp/org.kermeta.kp.editor.ui/src-gen/org/kermeta/kp/editor/ui/launch/KpLaunchConfigurationTabGroup.java
+++ org/kermeta/kp/org.kermeta.kp.editor.ui/src-gen/org/kermeta/kp/editor/ui/launch/KpLaunchConfigurationTabGroup.java
@@ -7,14 +7,9 @@
 package org.kermeta.kp.editor.ui.launch;
 
 /**
- * A class that provides the tabs for the launch configuration.
+ * A class that provides the tabs for the launch configuration (currently
+ * disabled).
  */
-public class KpLaunchConfigurationTabGroup extends org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup {
-	
-	public void createTabs(org.eclipse.debug.ui.ILaunchConfigurationDialog dialog, String mode) {
-		// Set the overrideLaunchConfigurationTabGroup option to false to implement this
-		// method.
-		setTabs(new org.eclipse.debug.ui.ILaunchConfigurationTab[] {new org.kermeta.kp.editor.ui.launch.KpLaunchConfigurationMainTab()});
-	}
+public class KpLaunchConfigurationTabGroup {
 	
 }
