bab471a92c81141c5d3926125a09bbd4b5d444f4
Didier Vojtisek <didier.vojtisek@inria.fr>
2012-01-02 16:33:21 +0000

    added a normailized method in order to transform platform:/resource into file:/
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@16914 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: cfc4e071b819cb58df4c6a120e87582858d5e666

--- org/kermeta/utils/org.kermeta.utils.helpers/src/main/java/org/kermeta/utils/helpers/emf/EMFUriHelper.java
+++ org/kermeta/utils/org.kermeta.utils.helpers/src/main/java/org/kermeta/utils/helpers/emf/EMFUriHelper.java
@@ -8,6 +8,9 @@
 */
 package org.kermeta.utils.helpers.emf;
 
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+import org.eclipse.emf.ecore.resource.impl.ExtensibleURIConverterImpl;
+
 public class EMFUriHelper {
 
 		public static org.eclipse.emf.common.util.URI convertToEMFUri(java.net.URI javaUri){
@@ -17,5 +20,18 @@ public class EMFUriHelper {
 		public static java.net.URI convertToJavaUri(org.eclipse.emf.common.util.URI emfUri){
 			// current implementation is trivial, but may be enhanced if necessary
 			return java.net.URI.create(emfUri.toString());
+		}
+		
+		
+		public static org.eclipse.emf.common.util.URI normalize(org.eclipse.emf.common.util.URI emfUri){
+			
+			org.eclipse.emf.common.util.URI normalizedUri = new ExtensibleURIConverterImpl().normalize(emfUri);
+			if(emfUri.isPlatformResource() && normalizedUri.isPlatformResource()){
+				// still a platform resource, try with EcorePlugin
+				//System.out.println("[EMFUriHelper] platform:/resource not normalized, trying to resolve");
+				normalizedUri = EcorePlugin.resolvePlatformResourcePath(emfUri.toString().replaceFirst("platform:/resource", "")); 
+				//System.out.println("[EMFUriHelper] resolve result ="+normalizedUri);
+			}
+			return normalizedUri;
 		}
 }
