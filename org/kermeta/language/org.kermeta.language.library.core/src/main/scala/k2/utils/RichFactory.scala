67d4169de7fdd0df41ae6c36da4cff9482f9e253
Didier Vojtisek <didier.vojtisek@inria.fr>
2012-01-06 12:50:15 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@16950 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: d35c54700e9092b53a534e9cef18a0fc2a868999

--- org/kermeta/language/org.kermeta.language.library.core/src/main/scala/k2/utils/RichFactory.scala
+++ org/kermeta/language/org.kermeta.language.library.core/src/main/scala/k2/utils/RichFactory.scala
@@ -3,6 +3,6 @@ package k2.utils
 object KerRichFactory {
 	
 	def createStringBuffer = new StringBuilder
-	def createHashtable[K,T]() = new java.util.HashMap[K,T]()
-                  def createStack[K] = new java.util.Stack[K]
+	def createHashtable[K,T]() = new _root_.java.util.HashMap[K,T]()
+    def createStack[K] = new _root_.java.util.Stack[K]
 }
