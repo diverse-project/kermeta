c83b5c40b956f77f31d0bc1216b038d0dc081b0d
Arnaud Blouin <arnaud.blouin@irisa.fr>
2012-03-05 14:35:20 +0000

    code clean-up
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@17215 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 1163f527dba827aae1a5283553e0d16ef57d930c

--- org/kermeta/language/org.kermeta.language.texteditor.eclipse/src/main/java/org/kermeta/language/texteditor/eclipse/internal/KermetaTokenScanner.java
+++ org/kermeta/language/org.kermeta.language.texteditor.eclipse/src/main/java/org/kermeta/language/texteditor/eclipse/internal/KermetaTokenScanner.java
@@ -20,27 +20,27 @@ public interface KermetaTokenScanner extends org.eclipse.jface.text.rules.IToken
 	/**
 	 * Notify other components (e.g. marker, logger, ...) that there's no error after lexing the file
 	 */
-	public void notifyFileClear();
+	void notifyFileClear();
 	
 	/**
 	 * Notify other components (e.g. marker, logger, ...) whether there're some errors detected in the file after lexing
 	 */
-	public void notifyFileHasError();
+	void notifyFileHasError();
 	
 	/**
 	 * Return the status of the file
 	 * @return true if the file has errors, false otherwise
 	 */
-	public boolean isFileOnError();
+	boolean isFileOnError();
 	
 	/**
 	 * Set the status of the file to a new value
 	 * @param fileHasError the new boolean value
 	 */
-	public void setFileHasError(boolean fileHasError);
+	void setFileHasError(final boolean fileHasError);
 	
 	/**
 	 * Return the current IFile
 	 */
-	public IFile getCurrentFile();
+	IFile getCurrentFile();
 }
