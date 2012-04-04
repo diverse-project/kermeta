22ad2537cc17e42c9b6a7efa91dc043ce4ac6c08
Arnaud Blouin <arnaud.blouin@irisa.fr>
2012-04-04 11:54:24 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@17459 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: f38370d679430dba24829d0bc68868c41e45e73f

--- kompren_projects/org.kermeta.kompren.slicer.model/src/org2/kermeta/kompren/slicer/impl/SlicedClassImpl.java
+++ kompren_projects/org.kermeta.kompren.slicer.model/src/org2/kermeta/kompren/slicer/impl/SlicedClassImpl.java
@@ -9,8 +9,6 @@ import org.eclipse.emf.common.notify.NotificationChain;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
-import org.eclipse.emf.ecore.util.EcoreUtil;
-
 import org2.kermeta.kompren.slicer.SlicedClass;
 import org2.kermeta.kompren.slicer.SlicerPackage;
 import org2.kermeta.kompren.slicer.VarDecl;
