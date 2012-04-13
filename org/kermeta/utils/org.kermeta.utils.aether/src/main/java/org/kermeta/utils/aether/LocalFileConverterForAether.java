090efdef290f100404c60ef6f4e75f079b6bb3c5
Didier Vojtisek <didier.vojtisek@inria.fr>
2012-04-13 15:16:12 +0000

    added URI resolver that knows about maven
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@17578 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 1a8ddbbf6c5ae6153707b774e0b4a838b3399632

---
+++ org/kermeta/utils/org.kermeta.utils.aether/src/main/java/org/kermeta/utils/aether/LocalFileConverterForAether.java
@@ -0,0 +1,54 @@
+/*$Id:  $
+* License : EPL
+* Copyright : IRISA / INRIA 
+* ----------------------------------------------------------------------------
+* Creation date : 26 sept. 2011
+* Authors : 
+*      Didier Vojtisek <didier.vojtisek@inria.fr>
+*/
+package org.kermeta.utils.aether;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.kermeta.utils.aether.AetherUtil;
+import org.kermeta.utils.helpers.LocalFileConverter;
+import org.kermeta.utils.systemservices.api.messaging.MessagingSystem;
+
+public class LocalFileConverterForAether extends LocalFileConverter {
+
+	protected MessagingSystem logger;
+	protected String baseMsgGroup;
+	protected java.util.List<String> repositoriesUrl;
+	
+	public LocalFileConverterForAether(MessagingSystem messagingSystem , String baseMsgGroup, java.util.List<String> repositoriesUrl){
+		this.baseMsgGroup = baseMsgGroup;
+		this.logger = messagingSystem;
+		this.repositoriesUrl = repositoriesUrl;
+	}
+	
+	@Override
+	public java.net.URI convertSpecialURItoFileURI(java.net.URI javaUri) {
+		try {
+			URL javaUrl = javaUri.toURL();
+			if(javaUrl.getProtocol().equals("file")){
+				// already a file, nothing to do
+				return javaUri;
+			}
+			if(javaUrl.getProtocol().equals("mvn")){
+				AetherUtil aetherUtil = new AetherUtil(logger,baseMsgGroup);
+				File theFile = aetherUtil.resolveMavenArtifact4J(javaUrl.toString(), repositoriesUrl);
+				if(theFile.exists())
+					return java.net.URI.create("file://"+theFile.getAbsolutePath());
+			}
+			
+			
+		} catch (MalformedURLException e) {
+			return null;
+		}
+		
+		return null;
+	}
+
+}
