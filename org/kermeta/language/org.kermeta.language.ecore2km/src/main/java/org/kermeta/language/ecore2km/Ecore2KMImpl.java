8eced86f56ca24f12aa09e39dfc409b0d4e5c097
Didier Vojtisek <didier.vojtisek@inria.fr>
2012-04-24 15:14:21 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@17683 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 4b42bb702e6327ead1322740dc5e17e07b0a0f72

--- org/kermeta/language/org.kermeta.language.ecore2km/src/main/java/org/kermeta/language/ecore2km/Ecore2KMImpl.java
+++ org/kermeta/language/org.kermeta.language.ecore2km/src/main/java/org/kermeta/language/ecore2km/Ecore2KMImpl.java
@@ -20,7 +20,7 @@ public class Ecore2KMImpl  implements org.kermeta.language.ecore2km.api.Ecore2KM
   
     
 	public Ecore2KMImpl(){
-    	org.kermeta.language.language.ecore2kmrunner.MainRunner.init4eclipse();   	
+    	org.kermeta.language.language.ecore2kmrunner.MainRunner.init();   	
     }
     
 
