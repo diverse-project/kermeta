8a00da26f8e13b9fe805b8f6104f645a33eb6d8e
spillay <spillay@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2012-05-02 09:43:49 +0000

    Fixed added full names for exceptions in Base and Statements
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@17748 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 0004e5c150072634ce1b32b8c034eba148f61d9e

--- org/kermeta/alf/org.kermeta.alf.alf2km/src/main/java/org/kermeta/alf/alf2km/api/Alf2KMImpl4Eclipse.java
+++ org/kermeta/alf/org.kermeta.alf.alf2km/src/main/java/org/kermeta/alf/alf2km/api/Alf2KMImpl4Eclipse.java
@@ -3,17 +3,14 @@ package org.kermeta.alf.alf2km.api;
 import org.kermeta.language.structure.ModelingUnit;
 import org3.kermeta.alf.CLASSSTRUCT;
 
-public class Alf2KMImpl4Eclipse {
+public class Alf2KMImpl4Eclipse implements Alf2KM {
 	 public Alf2KMImpl4Eclipse(){
 		 my.group.org.kermeta.alf.alf2kmrunner.MainRunner.init4eclipse();    	
 	 }
-	    
-
-	   public ModelingUnit convert(final CLASSSTRUCT rootPackage) {
-	        
-		   my.group.org.kermeta.alf.alf2kmrunner.MainRunner.init4eclipse();
-	    
-	    	org3.kermeta.alf.alf2km.ALF2KM converter = org3.kermeta.alf.alf2km.KerRichFactory.createALF2KM();
-	        return converter.convert(rootPackage);
-	    }
+	@Override
+	public ModelingUnit convertPackage(CLASSSTRUCT rootPackage) {
+		my.group.org.kermeta.alf.alf2kmrunner.MainRunner.init4eclipse();    
+	    org3.kermeta.alf.alf2km.ALF2KM converter = org3.kermeta.alf.alf2km.KerRichFactory.createALF2KM();
+	    return converter.convert(rootPackage);
+	}
 }
