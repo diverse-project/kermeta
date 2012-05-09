d89913eea9f7c86c11aa129d688f668f26627091
spillay <spillay@3b3ea266-f0f1-470e-95cb-5b2c9abc2e95>
2012-05-09 14:37:39 +0000

    Updated PretyPrinter.scala wirh printOutline
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@17785 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 7f818c42a7c497cb26c7f695be16fedd6dd2626b

--- org/kermeta/language/org.kermeta.language.loader.kmt.scala/src/main/scala/org/kermeta/language/loader/kmt/scala/internal/printer/PrettyPrinter.scala
+++ org/kermeta/language/org.kermeta.language.loader.kmt.scala/src/main/scala/org/kermeta/language/loader/kmt/scala/internal/printer/PrettyPrinter.scala
@@ -15,12 +15,12 @@ import java.util.regex.Pattern
 
 object PrettyPrinter {
 
-
+	
   def main(args: Array[String]): Unit = {
 
     var res = new StringBuffer
     print(loadKM("/home/barais/workspaces/compiloV2/org.kermeta.language.loader.kmt.scala/parsed.km"),res)
-    println(res.toString)
+    println(res.toString)    
   }
 
   def loadKM(fileName: String): ModelingUnit = {
@@ -37,7 +37,513 @@ object PrettyPrinter {
     }
     return mu;
   }
+  
+  def printOutline(o: EObject, res: java.lang.StringBuffer): Unit = {
+    o match {
+      case m : ModelingUnit =>{
+        m.getPackages.foreach(p=>printOutline(p,res))
+        m.getOwnedTypeDefinition.foreach(p=>printOutline(p,res))
+      }
+      case p: Package => {
+        p.getKOwnedTags.foreach{tag =>
+          printOutline(tag, res)
+        }
+        res.append("package " + p.getName() + " {\n")
+        p.getNestedPackage.foreach(e => printOutline(e, res))
+        p.getOwnedTypeDefinition.foreach(e => printOutline(e, res))
+        res.append("\n}\n")
+      }
+      case pt: PrimitiveType =>{
+        pt.getKOwnedTags.foreach{tag =>
+          res.append("\t")
+          printOutline(tag, res)
+        }
+        res.append("\t")
+        res.append("alias " + pt.getName() + " : ")
+        printOutline(pt.getInstanceType, res)
+        res.append("\n")
+      }
+      case c: ClassDefinition => {
+        c.getKOwnedTags.foreach{tag =>
+          res.append("\t")
+          printOutline(tag, res)
+        }
+        res.append("\t")
+        if (c.getIsAbstract)
+          res.append("abstract ")
+        if (c.getIsAspect)
+          res.append("aspect ")
+        if (c.getIsSingleton)
+          res.append("singleton ")
+        res.append("class " + c.getName())
+        //Generic parameters
+        if (c.getTypeParameter.size()>0) {
+          res.append("<")
+          var i : Int = 0;
+          c.getTypeParameter.foreach{ tp =>
+            if (i==0) {
+              res.append(tp.getName)
+            } else {
+              res.append(", " + tp.getName)
+            }
+            tp.getSupertype match {
+              case urt : UnresolvedType => res.append(" : " + urt.getTypeIdentifier)
+              case (_) =>
+            }
+            i=i+1
+          }
+          res.append(">")
+        }
 
+        res.append(" {\n")
+        //c.getTypeParameter.foreach{ tp => res.append("," + tp.getName)}
+        c.getOwnedAttribute.foreach { att => printOutline(att, res) }
+        c.getOwnedOperation.foreach { op => printOutline(op, res) }
+        //TODO Generic
+        //TODO Invariant
+        res.append("\n\t}\n")
+      }
+      case p: Property => {
+        p.getKOwnedTags.foreach{tag =>
+          res.append("\t\t")
+          printOutline(tag, res)
+        }
+        if (p.getIsComposite)
+          res.append("\t\tattribute ")
+        else {
+          if (p.getIsDerived) {
+            res.append("\t\tproperty ")
+          }
+          else
+            res.append("\t\treference ")
+        }
+        if (p.getIsReadOnly)
+          res.append("readonly ")
+
+        res.append(p.getName + " : ")
+        printMultiplicityElementTypeType(p,res)
+          
+        //TODO opposite
+        //TODO getter setter for derived
+        res.append("\n")
+
+        if (p.getIsDerived) {
+
+          if (p.getGetterBody != null) {
+            res.append("\t\t\tgetter is ")
+            res.append(printOutline(p.getGetterBody, res))
+          }
+          if (p.getSetterBody != null) {
+            res.append("\t\t\tsetter is ")
+            res.append(printOutline(p.getSetterBody, res))
+          }
+        }
+
+      }
+      case op: Operation => {
+        /*
+        op.getKOwnedTags.foreach{tag =>
+          res.append("\t\t")
+          printOutline(tag, res)
+        }
+        res.append("\t\toperation " + op.getName)
+		*/
+        res.append(op.getName)
+        if (op.getTypeParameter.size()>0) {
+          res.append("<")
+          var i=0
+          op.getTypeParameter.foreach( tp => {
+            if (i!=0)
+              res.append(", ")
+            res.append(tp.getName)
+            tp.getSupertype match {
+              case urt : UnresolvedType => res.append(" : " + urt.getTypeIdentifier)
+              case (_) =>
+            }
+            i=i+1
+          })
+          res.append(">")
+        }
+
+        res.append("(")
+        var i = 0
+        op.getOwnedParameter.foreach(p => {
+          if (i != 0)
+            res.append(", ")
+          printOutline(p, res)
+          i = i + 1
+        })
+        res.append(") : ")
+        printMultiplicityElementTypeType(op,res)
+        //TODO lower upper superoperation
+        op.getPre.foreach(p=>{
+          res.append("\n\t\t\tpre " + p.getName + "is")
+          printOutline(p.getBody,res)
+
+        })
+        op.getPost.foreach(p=>{
+          res.append("\n\t\t\tpost " + p.getName + "is")
+          printOutline(p.getBody,res)
+
+        })
+        /*
+        res.append("\n\t\tis ")
+        if (op.getIsAbstract!= null && op.getIsAbstract)
+          res.append(" abstract ")
+        else {
+          printOutline(op.getBody, res)
+        //TODO pre post
+        }
+        //RaiseException
+        res.append("\n")
+        */
+      }
+      
+      case p : Parameter =>{
+        res.append(p.getName + " : " )
+        printMultiplicityElementTypeType(p,res)
+      }
+      case p :UnresolvedType=>{
+        res.append(p.getTypeIdentifier)
+        if (p.getGenerics.size>0)
+          res.append("<")
+        var i = 0
+        p.getGenerics.foreach(g => {
+          if (i != 0)
+            res.append(", ")
+          printOutline(g, res)
+          i = i + 1
+        })
+        if (p.getGenerics.size>0)
+          res.append(">")
+      }
+
+      
+      
+      case b: Block => {
+        res.append("\n\t\t\tdo\n")
+        b.getStatement.foreach(s => {
+          res.append("\t\t\t")
+          printOutline(s, res)
+          res.append("\n")
+        })
+
+        if (b.getRescueBlock.size != 0) {
+          b.getRescueBlock.foreach(s => {
+            res.append("rescue ")
+            printOutline(s, res)
+            res.append("\n")
+          })
+        }
+        res.append("\t\t\tend\n")
+        
+      }
+      case v: VariableDecl => {
+        res.append("var " + v.getIdentifier + " : ")
+        printMultiplicityElementTypeType(v.getType,res)
+        if (v.getInitialization != null) {
+          res.append(" init ")
+          printOutline(v.getInitialization, res)
+        }
+      }
+
+      case c: Conditional => {
+        res.append("if (")
+        printOutline(c.getCondition, res)
+        res.append(") then \n")
+        printOutline(c.getThenBody, res)
+        if (c.getElseBody != null) {
+          res.append("\n\t\t\t else\n")
+          printOutline(c.getElseBody, res)
+        }
+        res.append("\n\t\t\t end \n")
+
+      }
+
+      case l: Loop => {
+        if (l.getInitialization != null) {
+          res.append("\t\t\t from ")
+          printOutline(l.getInitialization, res)
+        }
+        res.append(" \n\t\t\tuntil ")
+        printOutline(l.getStopCondition, res)
+        res.append(" \n\t\t\tloop ")
+        printOutline(l.getBody, res)
+        res.append(" \n\t\t\tend\n ")
+
+      }
+
+      case l: LambdaExpression => {
+        res.append("{")
+        var i = 0
+        l.getParameters.foreach(p => {
+          if (i != 0)
+            res.append(", ")
+          printOutline(p, res)
+          i = i + 1
+        })
+        res.append("|")
+        printOutline(l.getBody, res)
+        res.append("\t\t\t}")
+      }
+      case p : LambdaParameter =>{
+        res.append(p.getName)
+        if (p.getType!= null){
+	        res.append(" : ")
+	        printOutline(p.getType,res)
+	       }
+	    }
+      case p : TypeReference =>{
+        printOutline(p.getType,res)
+      }
+
+      case i: IntegerLiteral => {
+        res.append(i.getValue)
+      }
+      case i: StringLiteral => {
+        var lit = i.getValue.replaceAll("\\\\","\\\\\\\\")        
+        lit = lit.replaceAll("\n","\\\\n")
+        lit = lit.replaceAll("\t","\\\\t")
+        lit = lit.replaceAll("\r","\\\\r")
+        lit = lit.replaceAll("\f","\\\\f")
+        lit = lit.replaceAll("\"","\\\\\"")
+        lit = lit.replaceAll("\'","\\\\\'")
+        res.append("\"" + lit + "\"")
+      }
+      case i: BooleanLiteral => {
+        res.append(i.getValue)
+      }
+
+
+      case i: VoidLiteral => {
+        res.append("void")
+      
+      }
+      case f : FunctionType =>{
+          res.append("<")
+        
+          printOutline(f.getLeft,res)
+          res.append("->")
+          printOutline(f.getRight,res)
+          
+        
+        
+        res.append(">")
+      }
+     
+      case u: UnresolvedCall => {
+        if (u.getTarget != null) {
+          printOutline(u.getTarget, res)
+        res.append("." )}
+        res.append(         u.getName)
+
+        var j = 0
+        if (u.getGenerics.size>0)
+          res.append("[")
+        u.getGenerics.foreach(p => {
+          if (j!=0) res.append(", ")
+          printOutline(p, res)
+          j = j+1
+        })
+        if (u.getGenerics.size>0)
+          res.append("]")
+
+        var i = 0
+        if (u.getParameters.size > 0)
+          res.append("(")
+        u.getParameters.foreach(p => {
+          if (i != 0)
+            res.append(", ")
+          printOutline(p, res)
+          i = i + 1
+        })
+        if (u.getParameters.size > 0) {
+          res.append(")")
+        }
+      }
+      case c:CallSuperOperation =>{
+        res.append("super." + c.getName)
+        var i = 0
+        if (c.getParameters.size > 0)
+          res.append("(")
+        c.getParameters.foreach(p => {
+          if (i != 0)
+            res.append(", ")
+          printOutline(p, res)
+          i = i + 1
+        })
+        if (c.getParameters.size > 0) {
+          res.append(")")
+        }
+      }
+     case c: CallOperation =>{
+        printOutline(c.getTarget,res)
+        res.append("." + c.getName)
+        var i = 0
+        if (c.getParameters.size > 0)
+          res.append("(")
+        c.getParameters.foreach(p => {
+          if (i != 0)
+            res.append(", ")
+          printOutline(p, res)
+          i = i + 1
+        })
+        if (c.getParameters.size > 0) {
+          res.append(")")
+        }
+        
+      }
+     case c: SelfExpression =>{
+    	 res.append("self")
+    	
+      }
+     
+      case c: ProductType =>{
+        res.append("[")
+        var i = 0
+        c.getType.foreach(p => {
+          if (i != 0)
+            res.append(",")
+          printOutline(p, res)
+          i = i + 1
+        })
+        res.append("]")
+      }
+
+     case c: Assignment =>{
+    	 printOutline(c.getTarget,res)
+    	 if (c.getIsCast != null && c.getIsCast)
+    		 res.append("?=")
+    	else
+    	 res.append(":=")
+    	 printOutline(c.getValue,res)
+    	 
+    	  
+      }
+
+      case c:CallProperty =>{
+        printOutline(c.getTarget,res)
+        res.append("." + c.getName)
+        var i = 0
+        if (c.getParameters.size > 0)
+          res.append("(")
+        c.getParameters.foreach(p => {
+          if (i != 0)
+            res.append(", ")
+          printOutline(p, res)
+          i = i + 1
+        })
+        if (c.getParameters.size > 0) {
+          res.append(")")
+        }
+      }
+      
+      case c:CallResult =>{
+        res.append("result" )
+      }
+      case c:CallVariable =>{
+        printOutline(c.getStaticType,res)
+        res.append("." + c.getName)
+        var i = 0
+        if (c.getParameters.size > 0)
+          res.append("(")
+        c.getParameters.foreach(p => {
+          if (i != 0)
+            res.append(", ")
+          printOutline(p, res)
+          i = i + 1
+        })
+        if (c.getParameters.size > 0) {
+          res.append(")")
+        }
+      }
+
+      case c: EnumerationLiteral => {
+        res.append(c.getName + "; ")
+      }
+
+      case c: Enumeration => {
+        res.append("enum " + c.getName + " {")
+        c.getOwnedLiteral.foreach(p=> printOutline(p, res))
+        res.append("}\n")
+      }
+
+      case c: Rescue => {
+        res.append("(" + c.getExceptionName + " : ")
+        printOutline(c.getExceptionType, res)
+        res.append(")\n\tdo\n")
+        c.getBody.foreach{exp => {
+          printOutline(exp, res)
+          res.append("\n")
+        }}
+        res.append("\tend\n")
+      }
+      
+      case c: CallEnumLiteral =>{
+        printOutline(c.getStaticEnumLiteral,res)
+        res.append("." + c.getName)
+        var i = 0
+        if (c.getParameters.size > 0)
+          res.append("(")
+        c.getParameters.foreach(p => {
+          if (i != 0)
+            res.append(", ")
+          printOutline(p, res)
+          i = i + 1
+        })
+        if (c.getParameters.size > 0) {
+          res.append(")")
+        }
+        
+      }
+      case c: CallValue =>{
+        res.append(c.getName)
+        var i = 0
+        
+        if (c.getParameters.size > 0)
+          res.append("(")
+        c.getParameters.foreach(p => {
+          if (i != 0)
+            res.append(", ")
+          printOutline(p, res)
+          i = i + 1
+        })
+        if (c.getParameters.size > 0) {
+          res.append(")")
+        }
+        
+      }
+
+      case t: Tag => {
+         res.append("@")
+        res.append(t.getName)
+        res.append(" \"")
+        res.append(t.getValue)
+        res.append("\"\n")
+      }
+
+      case j : JavaStaticCall => {
+        res.append("extern ")
+        res.append(j.getJclass)
+        res.append(".")
+        res.append(j.getJmethod)
+        res.append("(")
+        var i=0
+        j.getParameters.foreach( p => {
+          if (i != 0)
+            res.append(", ")
+          printOutline(p, res)
+          i=i+1
+        })
+        res.append(")")
+      }
+      case o:EObject => res.append("todo " + o.getClass )
+    }
+    return ;
+  }
+
+  
+  
   def print(o: EObject, res: java.lang.StringBuffer): Unit = {
     o match {
       case m : ModelingUnit =>{
