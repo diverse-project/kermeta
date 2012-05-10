478a45c6b98a4d1677d5725c3658ff9df1ce1562
Arnaud Blouin <arnaud.blouin@irisa.fr>
2012-05-10 16:36:10 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@17801 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 477cd350cbca8b09080e8b61ef255dc490130442

--- kompren_projects/org.kermeta.kompren.slicer.edit/src/kompren/slicer/edit/popup/actions/CreateMSFAction.java
+++ kompren_projects/org.kermeta.kompren.slicer.edit/src/kompren/slicer/edit/popup/actions/CreateMSFAction.java
@@ -1,6 +1,13 @@
 package kompren.slicer.edit.popup.actions;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.action.IAction;
 
 public class CreateMSFAction extends KomprenAction {
@@ -17,14 +24,35 @@ public class CreateMSFAction extends KomprenAction {
 	}
 	
 	
-	protected void compile(final IFile file) {
+	protected void compile(final IFile fileCompil) {
+		IFile file = fileCompil;
 		if(file.getFileExtension().equals("komprent")) {
 			CreateKomprenModelsAction action = new CreateKomprenModelsAction();
 			action.convertKomprentFile(file);
+			file = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(file.getFullPath().removeFileExtension().addFileExtension("kompren"));
 		}
-				
-		// org2.kermeta.kompren.slicer.KomprenCompiler compiler = new org2.kermeta.kompren.slicer.KomprenCompiler();
-		// compiler.compile(uriSlicer, "MSF"+file.getName());
+
+		String projectName = "MSF"+file.getName();
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IProject project = root.getProject(projectName);
+		
+		try {
+			if(!project.exists()) {
+				project.create(null);
+				project.open(null);
+				IProjectDescription description = project.getDescription();
+				description.setNatureIds(new String[] { "org.kermeta.language.texteditor.eclipse.nature.KermetaNatureID"});
+				project.setDescription(description, null);
+			}
+			else project.open(null);
+			
+			org.kermeta.kompren.org.kermeta.kompren.slicer.compilerrunner.MainRunner.init();
+			org.kermeta.kompren.org.kermeta.kompren.slicer.compiler.org2.kermeta.kompren.slicer.KomprenCompiler compiler =
+				org.kermeta.kompren.org.kermeta.kompren.slicer.compiler.org2.kermeta.kompren.slicer.KerRichFactory.createKomprenCompiler();
+			
+			compiler.compile("platform:/resource"+file.getFullPath(), "/"+project.getName()+"/kermeta/");
+			project.refreshLocal(IResource.DEPTH_INFINITE, null);
+		}catch(CoreException ex) { ex.printStackTrace(); }
 	}
 
 
