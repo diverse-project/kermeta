2af30720e83c900d7d76464c80890d4d28fceb3f
Arnaud Blouin <arnaud.blouin@irisa.fr>
2012-05-16 13:30:42 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@17901 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: aa536c142328340a11b22228754d362265066a09

---
+++ org/kermeta/kompren/org.kermeta.kompren.gwelet/src/org/kermeta/kompren/gwelet/Gwelet.java
@@ -0,0 +1,10 @@
+package org.kermeta.kompren.gwelet;
+
+import org.kermeta.kompren.gwelet.ui.GweletFrame;
+
+public class Gwelet {
+	public static void main(String[] args) {
+		GweletFrame frame = new GweletFrame();
+		frame.setVisible(true);
+	}
+}
---
+++ org/kermeta/kompren/org.kermeta.kompren.gwelet/src/org/kermeta/kompren/gwelet/ui/GweletFrame.java
@@ -0,0 +1,16 @@
+package org.kermeta.kompren.gwelet.ui;
+
+import javax.swing.JFrame;
+
+import fr.inria.zvtm.engine.VirtualSpaceManager;
+
+public class GweletFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	protected VirtualSpaceManager vsm = VirtualSpaceManager.INSTANCE;
+	
+	
+	public GweletFrame() {
+		super();
+	}
+}
