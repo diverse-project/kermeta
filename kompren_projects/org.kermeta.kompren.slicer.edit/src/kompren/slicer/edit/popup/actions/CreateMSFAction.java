09a19e6550905729d2a9371bfe0fc5f0d2cfba80
Arnaud Blouin <arnaud.blouin@irisa.fr>
2012-06-17 14:27:53 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@18149 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: ea9a16ff291b9faebbf1920706672facaece0984

--- kompren_projects/org.kermeta.kompren.slicer.edit/src/kompren/slicer/edit/popup/actions/CreateMSFAction.java
+++ kompren_projects/org.kermeta.kompren.slicer.edit/src/kompren/slicer/edit/popup/actions/CreateMSFAction.java
@@ -4,10 +4,8 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -26,21 +24,24 @@ public class CreateMSFAction extends KomprenAction {
 		for(final IFile file : paths)
 			compile(file);
 	}
-	
-	
+
+
 	protected void compile(final IFile fileCompil) {
 		IFile file = fileCompil;
 
 		if(file.getFileExtension().equals("komprent")) {
 			CreateKomprenModelsAction action = new CreateKomprenModelsAction();
 			action.convertKomprentFile(file);
+			System.out.println(file.getFullPath() + " " + file.getFullPath().removeFileExtension().addFileExtension("kompren"));
 			file = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(file.getFullPath().removeFileExtension().addFileExtension("kompren"));
+			System.out.println(file);
+			System.out.println(ResourcesPlugin.getWorkspace().getRoot().getFile(file.getFullPath().removeFileExtension().addFileExtension("kompren")));
 		}
 
 		final Resource resource = new ResourceSetImpl().getResource(URI.createFileURI(file.getLocation().toString()), true);
 		final Slicer slicer = (Slicer)resource.getContents().get(0);
 		final IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(slicer.getName());
-		
+
 		try {
 			if(!project.exists()) {
 				project.create(null);
@@ -50,11 +51,11 @@ public class CreateMSFAction extends KomprenAction {
 				project.setDescription(description, null);
 			}
 			else project.open(null);
-			
+
 			org.kermeta.kompren.org.kermeta.kompren.slicer.compilerrunner.MainRunner.init();
 			org.kermeta.kompren.org.kermeta.kompren.slicer.compiler.org2.kermeta.kompren.slicer.KomprenCompiler compiler =
 				org.kermeta.kompren.org.kermeta.kompren.slicer.compiler.org2.kermeta.kompren.slicer.KerRichFactory.createKomprenCompiler();
-			
+
 			compiler.compile("platform:/resource"+file.getFullPath());
 			project.refreshLocal(IResource.DEPTH_INFINITE, null);
 		}catch(CoreException ex) { ex.printStackTrace(); }
