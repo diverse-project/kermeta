e420a207b9fdeda6a8952f89f55bf034aae967d9
Arnaud Blouin <arnaud.blouin@irisa.fr>
2012-06-18 09:45:05 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@18157 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 397a3d70ce71a93a703afec53daa392164d74f09

--- kompren_projects/org.kermeta.kompren.slicer.texteditor/src/org/kermeta/kompren/slicer/texteditor/wizards/SampleNewWizard.java
+++ kompren_projects/org.kermeta.kompren.slicer.texteditor/src/org/kermeta/kompren/slicer/texteditor/wizards/SampleNewWizard.java
@@ -1,24 +1,36 @@
 package org.kermeta.kompren.slicer.texteditor.wizards;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.core.runtime.*;
-import org.eclipse.jface.operation.*;
-import java.lang.reflect.InvocationTargetException;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.core.resources.*;
-import org.eclipse.core.runtime.CoreException;
-import java.io.*;
-import org.eclipse.ui.*;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
 
 /**
- * This is a sample new wizard. Its role is to create a new file 
+ * This is a sample new wizard. Its role is to create a new file
  * resource in the provided container. If the container resource
- * (a folder or a project) is selected in the workspace 
+ * (a folder or a project) is selected in the workspace
  * when the wizard is opened, it will accept it as the target
  * container. The wizard creates one file with the extension
  * "komprent". If a sample multi-page editor (also available
@@ -29,8 +41,8 @@ public class SampleNewWizard extends Wizard implements INewWizard {
 	private SampleNewWizardPage page;
 	private ISelection selection;
 
-	
-	
+
+
 	/**
 	 * Constructor for SampleNewWizard.
 	 */
@@ -38,24 +50,24 @@ public class SampleNewWizard extends Wizard implements INewWizard {
 		super();
 		setNeedsProgressMonitor(true);
 	}
-	
-	
-	
+
+
+
 	@Override
 	public void addPages() {
 		page = new SampleNewWizardPage(selection);
 		addPage(page);
 	}
 
-	
-	
+
+
 	@Override
 	public boolean performFinish() {
 		final String containerName = page.getContainerName();
 		final String fileName = page.getFileName();
 		IRunnableWithProgress op = new IRunnableWithProgress() {
 			@Override
-			public void run(IProgressMonitor monitor) throws InvocationTargetException {
+			public void run(final IProgressMonitor monitor) throws InvocationTargetException {
 				try {
 					doFinish(containerName, fileName, monitor);
 				} catch (CoreException e) {
@@ -76,7 +88,7 @@ public class SampleNewWizard extends Wizard implements INewWizard {
 		}
 		return true;
 	}
-	
+
 	/**
 	 * The worker method. It will find the container, create the
 	 * file if missing or just replace its contents, and open
@@ -117,27 +129,27 @@ public class SampleNewWizard extends Wizard implements INewWizard {
 		});
 		monitor.worked(1);
 	}
-	
-	
-	
+
+
+
 	/**
 	 * We will initialize file contents with a sample text.
 	 */
 	private InputStream openContentStream() {
-		String contents = "slicer strict FooSlicer {\n\tdomain:\"\"\n\tinput:\n\tslicedClass:\n\tslicedRelation:\n}\n";
+		String contents = "slicer strict FooSlicer {\n\tdomain:\"\"\n\tinput:\n\tslicedClass:\n\tslicedProperty:\n}\n";
 		return new ByteArrayInputStream(contents.getBytes());
 	}
 
-	
-	
+
+
 	private void throwCoreException(final String message) throws CoreException {
 		IStatus status =
 			new Status(IStatus.ERROR, "org.kermeta.kompren.slicer.texteditor", IStatus.OK, message, null);
 		throw new CoreException(status);
 	}
 
-	
-	
+
+
 	@Override
 	public void init(final IWorkbench workbench, final IStructuredSelection select) {
 		this.selection = select;
