2c54bc8230ac8adc8d85761d704f04734951fa57
Didier Vojtisek <didier.vojtisek@inria.fr>
2012-08-30 13:38:45 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@18651 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 6054e5eff2145e97f1a0e8bf8046d4ad9421621f

--- org/kermeta/kp/org.kermeta.kp.wizard.eclipse/src/org/kermeta/kp/wizard/eclipse/preferences/PreferenceInitializer.java
+++ org/kermeta/kp/org.kermeta.kp.wizard.eclipse/src/org/kermeta/kp/wizard/eclipse/preferences/PreferenceInitializer.java
@@ -40,7 +40,7 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 				"\t}\n"+
 				"\tdependencies = {\n"+
 				"\t\t//default dependency to kermeta framework (try first in eclipse plugin, then look into maven repository)\n"+
-				"\t\tdependency \"library.core\" =\"platform:/plugin/org.kermeta.language.library.core\",\"mvn:org.kermeta.language/language.library.core/2.0.5-SNAPSHOT\"\n"+
+				"\t\tdependency \"library.core\" =\"platform:/plugin/org.kermeta.language.library.core\",\"mvn:org.kermeta.language/language.library.core/2.0.6\"\n"+
 				"\t}"+
 				"\tpackageEquivalences={\n"+ 
 				"\t\t// indicates the real java name of a given package\n"+
