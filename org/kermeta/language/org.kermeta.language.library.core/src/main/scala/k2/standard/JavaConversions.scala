d3dc36707a7439f1e457b2873d8f781d11d6dd2b
Didier Vojtisek <didier.vojtisek@inria.fr>
2012-09-06 16:25:07 +0000

    use a java.util.Set 2 java.util.List  in order to avoid a stackoverflow while trying to convert the Set into a RichKermetaSet
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@18665 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 729f1a2a15f8034749cddcce7912e5f6b411700a

--- org/kermeta/language/org.kermeta.language.library.core/src/main/scala/k2/standard/JavaConversions.scala
+++ org/kermeta/language/org.kermeta.language.library.core/src/main/scala/k2/standard/JavaConversions.scala
@@ -263,14 +263,77 @@ object JavaConversions {
     else
       new RichKermetaSequence[G](value)
   }
-  implicit def juSet2Kermeta[A](l: ju.Set[A]):KermetaSet[A] = {new RichKermetaSet(l) }
+  implicit def juSet2Kermeta[A](l: ju.Set[A]):KermetaSet[A] = {
+    new RichKermetaSet(new juSet2juListWrapper(l)) 
+  }
   implicit def juCollection2Kermeta[A](l: ju.Collection[A]):KermetaCol[A] = {
     if(l.isInstanceOf[ju.Set[A]]) 
-      new RichKermetaSet[A](l)
+      new RichKermetaSet[A](new juSet2juListWrapper(l))
     else
-      new RichKermetaSequence[A](l)
+      new RichKermetaSequence[A](l) // todo should also write a wrapper for Sequence other wise we may fall again in a stackoverflow
   }
 }
+
+/**
+ * Wrapper class that allows to manipulate a java.util.Set as a java.util.List
+ * ie.it implements the missing operation by raising NotImplementedException
+ */
+class juSet2juListWrapper[A](underlying : ju.Set[A]) extends ju.List[A]{
+	def size = underlying.size
+			
+    def iterator = underlying.iterator
+  
+    
+    override def add(elem: A) = underlying.add(elem)
+    override def add(pos: Int, elem: A) = {
+	  throw new RuntimeException("add(pos: Int, elem: A) not implemented when wrapping a java.util.Set into a java.util.List ")
+	}
+	override def addAll(coll: java.util.Collection[_ <: A]) = underlying.addAll(coll)
+    override def addAll(pos: Int, coll: java.util.Collection[_ <: A]) = {
+	  throw new RuntimeException("addAll(pos: Int, coll: java.util.Collection[_]) not implemented when wrapping a java.util.Set into a java.util.List ")
+	}
+	override def containsAll(coll: java.util.Collection[_]) = underlying.containsAll(coll)
+	
+	override def get(x$1: Int) ={
+	  throw new RuntimeException("get(x$1: Any) not implemented when wrapping a java.util.Set into a java.util.List ")
+	}
+	
+	
+	override def listIterator(x$1: Int) ={
+	  throw new RuntimeException("istIterator(x$1: Int) not implemented when wrapping a java.util.Set into a java.util.List ")
+	}
+	override def listIterator() ={
+	  throw new RuntimeException("istIterator() not implemented when wrapping a java.util.Set into a java.util.List ")
+	}
+	override def lastIndexOf(x$1: Object) ={
+	  throw new RuntimeException("lastIndexOf(x$1: Any) not implemented when wrapping a java.util.Set into a java.util.List ")
+	}
+	override def indexOf(x$1: Object) ={
+	  throw new RuntimeException("indexOf(x$1: Any) not implemented when wrapping a java.util.Set into a java.util.List ")
+	}
+	override def set(pos: Int, elem: A) = {
+	  throw new RuntimeException("set(pos: Int, elem: A) not implemented when wrapping a java.util.Set into a java.util.List ")
+	}
+    override def remove(elem: Object) = underlying.remove(elem)
+    override def remove(pos: Int) = {
+	  throw new RuntimeException("remove(pos: Int) not implemented when wrapping a java.util.Set into a java.util.List ")
+	}
+    override def removeAll(x$1: java.util.Collection[_]) = underlying.removeAll(x$1)
+    
+    override def retainAll(x$1: java.util.Collection[_]) = underlying.retainAll(x$1)
+    override def subList(pos1: Int, pos2: Int) = {
+	  throw new RuntimeException("subList(pos1: Int, pos2: Int) not implemented when wrapping a java.util.Set into a java.util.List ")
+	}
+  
+    def clear = underlying.clear
+
+    def isEmpty =underlying.isEmpty()
+    def contains(o : Object) = underlying.contains(o)
+    override def toArray() = underlying.toArray()
+    override def toArray[T](x$1: Array[T with java.lang.Object]) = { //underlying.toArray(x$1) not sure if this work
+	  throw new RuntimeException("toArray[T](x$1: Array[T with java.lang.Object]) not implemented when wrapping a java.util.Set into a java.util.List ")
+	}
+} 
 
 class EachContext {
 	var index:Integer = -1
