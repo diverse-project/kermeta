d56bf32d73b98dc51b726cfa265d85b77b767022
Didier Vojtisek <didier.vojtisek@inria.fr>
2012-10-09 08:49:02 +0000

    adding helper methods to simplify model manipulation
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@18820 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 0695b7164860a02238180ac651147dc94d6a0734

--- org/kermeta/language/org.kermeta.language.model/src/main/java/org/kermeta/language/util/ModelingUnit.java
+++ org/kermeta/language/org.kermeta.language.model/src/main/java/org/kermeta/language/util/ModelingUnit.java
@@ -11,6 +11,10 @@ package org.kermeta.language.util;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+
 import org.kermeta.language.structure.Metamodel;
 
 /**
@@ -20,20 +24,56 @@ import org.kermeta.language.structure.Metamodel;
  */
 public class ModelingUnit {
 	
-	protected List<Metamodel> metamodels = null; 
+	protected List<Metamodel> metamodels = null;
 	
-	public ModelingUnit(){
+	protected String name = null;
+	
+
+	public ModelingUnit(String name){
+		this.name = name;
 		metamodels = new ArrayList<Metamodel>();
 	}
 
-	public ModelingUnit(List<Metamodel> metamodels){
+	public ModelingUnit(String name, List<Metamodel> metamodels){
+		this.name = name;
 		metamodels = new ArrayList<Metamodel>();
 		this.metamodels.addAll(metamodels);
+	}
+	
+	public ModelingUnit(String name, EList<EObject> eObjects){
+		this.name = name;
+		metamodels = new ArrayList<Metamodel>();
+		for(EObject o : eObjects){
+			if(o instanceof Metamodel)
+				metamodels.add((Metamodel) o);
+		}
 	}
 	
 	public List<Metamodel> getMetamodels() {
 		if(metamodels == null) metamodels = new ArrayList<Metamodel>();
 		return metamodels;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Resource getMainEResource(){
+		for(Metamodel mm : metamodels){
+			return mm.eResource();
+		}
+		return null;
+	}
+	
+	public void gatherInMainEResource(){
+		Resource mainRes = getMainEResource();
+		for(Metamodel mm : metamodels){
+			mainRes.getContents().add(mm);
+		}
 	}
 
 }
