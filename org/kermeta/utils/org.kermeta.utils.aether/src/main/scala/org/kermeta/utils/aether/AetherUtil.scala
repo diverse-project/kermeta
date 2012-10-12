c85bb7f3940c3b223544851849c4021ee5016388
Didier Vojtisek <didier.vojtisek@inria.fr>
2012-10-12 15:13:57 +0000

    retrieved code from Francois so it now works with maven >=3.0.3
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@18840 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    
    Former-commit-id: 030de02d47fe3ee5f2824292f1f9aac826cba5d5

--- org/kermeta/utils/org.kermeta.utils.aether/src/main/scala/org/kermeta/utils/aether/AetherUtil.scala
+++ org/kermeta/utils/org.kermeta.utils.aether/src/main/scala/org/kermeta/utils/aether/AetherUtil.scala
@@ -14,10 +14,11 @@ package org.kermeta.utils.aether
  * limitations under the License.
  */
 
-import org.sonatype.aether.{ConfigurationProperties, RepositorySystem}
+import org.sonatype.aether.{ ConfigurationProperties, RepositorySystem }
 import org.apache.maven.repository.internal.DefaultServiceLocator
 import org.sonatype.aether.connector.file.FileRepositoryConnectorFactory
 import org.sonatype.aether.connector.async.AsyncRepositoryConnectorFactory
+import org.sonatype.aether.impl.internal.SimpleLocalRepositoryManagerFactory
 import org.sonatype.aether.impl.internal.EnhancedLocalRepositoryManagerFactory
 import org.sonatype.aether.resolution.ArtifactRequest
 import org.sonatype.aether.resolution.ArtifactResult
@@ -26,14 +27,13 @@ import org.sonatype.aether.spi.localrepo.LocalRepositoryManagerFactory
 import org.sonatype.aether.util.artifact.DefaultArtifact
 import org.apache.maven.repository.internal.MavenRepositorySystemSession
 import java.io.File
-import org.sonatype.aether.repository.{RepositoryPolicy, RemoteRepository, LocalRepository}
+import org.sonatype.aether.repository.{ RepositoryPolicy, RemoteRepository, LocalRepository }
 import org.sonatype.aether.artifact.Artifact
 import scala.collection.JavaConversions._
-import org.sonatype.aether.transfer.{TransferEvent, TransferListener}
+import org.sonatype.aether.transfer.{ TransferEvent, TransferListener }
 import java.net.MalformedURLException
 import org.kermeta.utils.systemservices.api.messaging.MessagingSystem
 import org.kermeta.utils.systemservices.api.impl.StdioSimpleMessagingSystem
-
 
 /**
  * User: ffouquet
@@ -41,64 +41,48 @@ import org.kermeta.utils.systemservices.api.impl.StdioSimpleMessagingSystem
  * Time: 15:06
  */
 
-class AetherUtil(val messagingSystem : MessagingSystem, val baseMsgGroup : String ) {
+class AetherUtil(val messagingSystem: MessagingSystem, val baseMsgGroup: String) extends AetherRepositoryHandler {
 
-  def this() = this(new StdioSimpleMessagingSystem(),"")
+  def this() = this(new StdioSimpleMessagingSystem(), "")
   
-  
-  def setOffline(b : Boolean ) = {newRepositorySystemSession.setOffline(b)}
-  
-  val newRepositorySystem: RepositorySystem = {
-    val locator = new DefaultServiceLocator()    
-    locator.addService(classOf[LocalRepositoryManagerFactory], classOf[EnhancedLocalRepositoryManagerFactory])
-    locator.addService(classOf[RepositoryConnectorFactory], classOf[FileRepositoryConnectorFactory])
-    locator.addService(classOf[RepositoryConnectorFactory], classOf[AsyncRepositoryConnectorFactory])
-   // locator.addService(classOf[RepositoryConnectorFactory], classOf[WagonRepositoryConnectorFactory] )
 
-    locator.getService(classOf[RepositorySystem])
-  }
-  
-  
   def resolveMavenArtifact(mavenurl: String, repositoriesUrl: List[String]): File = {
-    
+
     var file: File = null
     if (mavenurl.startsWith("mvn:")) {
-    	val url = mavenurl.substring(4)
-    	if (url.contains("!")) {
-    		val repourl = url.substring(0, url.indexOf("!"))
-    		val urlids = url.substring(url.indexOf("!") + 1)
-    		val part = urlids.split("/")
-    		if (part.size == 3) {
-    			file = resolveMavenArtifact(part(0), part(1), part(2), List(repourl))
-    		} else if (part.size == 2) {
-    		   file = resolveMavenArtifact(part(0), part(1), null, List(repourl))
-    		}
-    		else {
-    			throw new MalformedURLException("Bad MVN URL <mvn:[repourl!]groupID/artefactID[/version]> " +mavenurl)
-    		}
-    	}
-    	if (file == null) {
-	        val part = url.split("/")
-	        if (part.size == 3) {
-	          file = resolveMavenArtifact(part(0), part(1), part(2), repositoriesUrl)
-	        } else if (part.size == 2) {
-    		   file = resolveMavenArtifact(part(0), part(1), null, repositoriesUrl)
-    		}
-    		else {
-	    		throw new MalformedURLException("Bad MVN URL <mvn:[repourl!]groupID/artefactID[/version]> " +mavenurl)
-	        }
-    	}
-    }
-    else {
-		throw new MalformedURLException("Bad MVN URL <mvn:[repourl!]groupID/artefactID[/version]> " +mavenurl)
+      val url = mavenurl.substring(4)
+      if (url.contains("!")) {
+        val repourl = url.substring(0, url.indexOf("!"))
+        val urlids = url.substring(url.indexOf("!") + 1)
+        val part = urlids.split("/")
+        if (part.size == 3) {
+          file = resolveMavenArtifact(part(0), part(1), part(2), List(repourl))
+        } else if (part.size == 2) {
+          file = resolveMavenArtifact(part(0), part(1), null, List(repourl))
+        } else {
+          throw new MalformedURLException("Bad MVN URL <mvn:[repourl!]groupID/artefactID[/version]> " + mavenurl)
+        }
+      }
+      if (file == null) {
+        val part = url.split("/")
+        if (part.size == 3) {
+          file = resolveMavenArtifact(part(0), part(1), part(2), repositoriesUrl)
+        } else if (part.size == 2) {
+          file = resolveMavenArtifact(part(0), part(1), null, repositoriesUrl)
+        } else {
+          throw new MalformedURLException("Bad MVN URL <mvn:[repourl!]groupID/artefactID[/version]> " + mavenurl)
+        }
+      }
+    } else {
+      throw new MalformedURLException("Bad MVN URL <mvn:[repourl!]groupID/artefactID[/version]> " + mavenurl)
     }
     file
   }
-  
+
   def resolveMavenArtifact(mavenurl: String, repoURL: String): File = {
-    val repositoriesUrl :List[String] = List(repoURL)
-   // try{
-    	resolveMavenArtifact(mavenurl, repositoriesUrl)
+    val repositoriesUrl: List[String] = List(repoURL)
+    // try{
+    resolveMavenArtifact(mavenurl, repositoriesUrl)
     /*} catch {
         case _@e => {
           e.printStackTrace()
@@ -107,36 +91,36 @@ class AetherUtil(val messagingSystem : MessagingSystem, val baseMsgGroup : Strin
       }*/
   }
   def resolveMavenArtifact4J(groupID: String, artifactID: String, version: String, repositoriesUrl: java.util.List[String]): File =
-    resolveMavenArtifact(groupID, artifactID,  version, repositoriesUrl.toList)
+    resolveMavenArtifact(groupID, artifactID, version, repositoriesUrl.toList)
   def resolveMavenArtifact4J(mavenurl: String, repositoriesUrl: java.util.List[String]): File =
     resolveMavenArtifact(mavenurl, repositoriesUrl.toList)
-  
-  def resolveMavenArtifact(groupID: String, artifactID: String, version: String, repoURL: String): File = {    
-    val repositoriesUrl :List[String] = List(repoURL)
-    resolveMavenArtifact( groupID, artifactID, version, repositoriesUrl)
+
+  def resolveMavenArtifact(groupID: String, artifactID: String, version: String, repoURL: String): File = {
+    val repositoriesUrl: List[String] = List(repoURL)
+    resolveMavenArtifact(groupID, artifactID, version, repositoriesUrl)
   }
-  
+
   def resolveMavenArtifact(groupID: String, artifactID: String, version: String, repositoriesUrl: List[String]): File = {
     val artifact: Artifact = new DefaultArtifact(List(groupID, artifactID, version).mkString(":"))
     val artifactRequest = new ArtifactRequest
     artifactRequest.setArtifact(artifact)
 
     val repositories: java.util.List[RemoteRepository] = new java.util.ArrayList();
-    
+
     repositoriesUrl.foreach {
       repoURL =>
-	      val repo = new RemoteRepository
-	      repo.setId(repoURL.trim.replace("http://","").replace(':', '_').replace('/', '_').replace('\\', '_'))
-	      //repo.setUrl(repoURL.trim.replace(':', '_').replace('/', '_').replace('\\', '_'))
-	      repo.setUrl(repoURL)
-	      repo.setContentType("default")
-	      val repositoryPolicy = new RepositoryPolicy()
-	      repositoryPolicy.setChecksumPolicy(RepositoryPolicy.CHECKSUM_POLICY_WARN)
-	      repositoryPolicy.setUpdatePolicy(RepositoryPolicy.UPDATE_POLICY_ALWAYS)
-	      repo.setPolicy(true, repositoryPolicy)
-	      repositories.add(repo)
+        val repo = new RemoteRepository
+        repo.setId(repoURL.trim.replace("http://", "").replace(':', '_').replace('/', '_').replace('\\', '_'))
+        //repo.setUrl(repoURL.trim.replace(':', '_').replace('/', '_').replace('\\', '_'))
+        repo.setUrl(repoURL)
+        repo.setContentType("default")
+        val repositoryPolicy = new RepositoryPolicy()
+        repositoryPolicy.setChecksumPolicy(RepositoryPolicy.CHECKSUM_POLICY_WARN)
+        repositoryPolicy.setUpdatePolicy(RepositoryPolicy.UPDATE_POLICY_ALWAYS)
+        repo.setPolicy(true, repositoryPolicy)
+        repositories.add(repo)
     }
-    
+
     // add default central repo
     val centralRepo = new RemoteRepository
     centralRepo.setId("central")
@@ -146,85 +130,20 @@ class AetherUtil(val messagingSystem : MessagingSystem, val baseMsgGroup : Strin
 
     artifactRequest.setRepositories(repositories)
     var artefactResult: ArtifactResult = null;
-    
-    artefactResult = newRepositorySystem.resolveArtifact(newRepositorySystemSession, artifactRequest)
+
+    artefactResult = getRepositorySystem.resolveArtifact(getRepositorySystemSession, artifactRequest)
     artefactResult.getArtifact.getFile
   }
 
-  val newRepositorySystemSession = {
-    val session = new MavenRepositorySystemSession()
-    session.setTransferListener(new TransferListener(){
-      def transferInitiated(p1: TransferEvent) {
-        messagingSystem.initProgress(msgGroup+"."+session.hashCode(),"Transfert init    : "+p1.getResource.getResourceName+" from "+p1.getResource().getRepositoryUrl(),msgGroup, 1 )
-        //messagingSystem.debug("Transfert init for Artifact "+p1.getResource.getResourceName+" from "+p1.getResource().getRepositoryUrl(), msgGroup)
-      }
-
-      def transferStarted(p1: TransferEvent) {
-        messagingSystem.log(MessagingSystem.Kind.DevDEBUG,           "Transfert begin    : "+p1.getResource.getResourceName+" from "+p1.getResource().getRepositoryUrl(), msgGroup)
-      }
-
-      def transferProgressed(p1: TransferEvent) {
-        messagingSystem.debug("Transfert in progress for Artifact "+p1.getResource.getResourceName +"("+p1.getTransferredBytes()+"/"+p1.getResource().getContentLength()+") from "+p1.getResource().getRepositoryUrl(), msgGroup)
-      }
-
-      def transferCorrupted(p1: TransferEvent) {
-        messagingSystem.doneProgress(msgGroup+session.hashCode(),    "Transfert Corrupted: "+p1.getResource.getResourceName+" from "+p1.getResource().getRepositoryUrl(), msgGroup)
-        messagingSystem.error("TransfertCorrupted : "+p1.getResource.getResourceName+" from "+p1.getResource().getRepositoryUrl(), msgGroup)
-      }
-
-      def transferSucceeded(p1: TransferEvent) {
-        messagingSystem.doneProgress(msgGroup+"."+session.hashCode(), "Transfert success : "+p1.getResource.getResourceName+" from "+p1.getResource().getRepositoryUrl(), msgGroup)
-        //messagingSystem.debug("Transfert succeeded for Artifact "+p1.getResource.getResourceName+" from "+p1.getResource().getRepositoryUrl(), msgGroup)
-      }
-
-      def transferFailed(p1: TransferEvent) {
-        messagingSystem.doneProgress(msgGroup+"."+session.hashCode(), "Transfer Failed   : "+p1.getResource.getResourceName+" from "+p1.getResource().getRepositoryUrl(), msgGroup)
-        //messagingSystem.debug("TransferFailed : "+p1.getResource.getResourceName+" from "+p1.getResource().getRepositoryUrl(), msgGroup)
-      }
-    })
-    session.setUpdatePolicy(RepositoryPolicy.UPDATE_POLICY_ALWAYS)
-    session.setConfigProperty("aether.connector.ahc.provider", "jdk")
-    session.setLocalRepositoryManager(newRepositorySystem.newLocalRepositoryManager(new LocalRepository(System.getProperty("user.home").toString + "/.m2/repository")))
-    //TRY TO FOUND MAVEN CONFIGURATION
-    val configFile = new File(System.getProperty("user.home").toString + File.separator + ".m2" + File.separator + "settings.xml")
-    var foundUserSpecificLocalRepositorySettings : Boolean = false
-    if (configFile.exists()) {
-      val configRoot = scala.xml.XML.loadFile(configFile)
-      
-      configRoot.child.find(c => c.label == "localRepository").map {
-        localRepo =>
-    		messagingSystem.debug("Found localRepository value from settings.xml in user path => \"" + localRepo.text+"\"", msgGroup)
-    		session.setLocalRepositoryManager(newRepositorySystem.newLocalRepositoryManager(new LocalRepository(localRepo.text)))
-    	
-      }
-      configRoot.child.find(c => c.label == "offline").map {
-        offline =>
-    		messagingSystem.debug("Found offline value from settings.xml in user path => \"" + offline.text+"\"", msgGroup)
-    		offline.text match {
-    		case "true" => session.setOffline(true)
-    		case "yes" => session.setOffline(true)
-    		case _ =>
-    		} 
-    	}
-    	
-    } 
-    if(! foundUserSpecificLocalRepositorySettings){
-      val localRepoString = System.getProperty("user.home").toString + File.separator + ".m2" + File.separator +"repository"
-      messagingSystem.debug("localRepository not found in settings.xml, using "+localRepoString , msgGroup)
-      session.setLocalRepositoryManager(newRepositorySystem.newLocalRepositoryManager(new LocalRepository(localRepoString)))
-    }
-    session.getConfigProperties.put(ConfigurationProperties.REQUEST_TIMEOUT, 2000.asInstanceOf[java.lang.Integer])
-    session.getConfigProperties.put(ConfigurationProperties.CONNECT_TIMEOUT, 1000.asInstanceOf[java.lang.Integer])
-    session
-  }
   
-  val msgGroup :String = {
-    if(baseMsgGroup.equals("")) 
-      this.getClass().getName()+hashCode()
-    else  
-      baseMsgGroup+"."+this.getClass().getName()+hashCode()
+
+  val msgGroup: String = {
+    if (baseMsgGroup.equals(""))
+      this.getClass().getName() + hashCode()
+    else
+      baseMsgGroup + "." + this.getClass().getName() + hashCode()
   }
 
 }
 
-object AetherUtil{}
+object AetherUtil {}
