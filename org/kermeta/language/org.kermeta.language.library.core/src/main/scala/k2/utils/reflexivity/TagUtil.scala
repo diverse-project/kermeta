d4e9c7cfebf37e0f2e3d3d062a942f74fe4f4e93
Didier Vojtisek <didier.vojtisek@inria.fr>
2012-11-28 10:09:08 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@19076 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 0b53e883a7ba5c95383436abd480cf23025edd35

--- org/kermeta/language/org.kermeta.language.library.core/src/main/scala/k2/utils/reflexivity/TagUtil.scala
+++ org/kermeta/language/org.kermeta.language.library.core/src/main/scala/k2/utils/reflexivity/TagUtil.scala
@@ -18,4 +18,21 @@ object TagUtil {
   def hasEcoreTag(obj: KermetaModelElement): Boolean = {
     obj.getKOwnedTags().exists(e => "ecore".equals(e.asInstanceOf[Tag].getName()))
   }
+  
+  /**
+   * Check if a model element has an ScalaCompilerName tag
+   * @param obj model element to test
+   * @return true if ScalaCompilerName tag is found
+   */
+  def hasScalaCompilerNameTag(obj: KermetaModelElement): Boolean = {
+    obj.getKOwnedTags.exists(e => "ScalaCompilerName".equals(e.asInstanceOf[Tag].getName()))
+  }
+  /**
+   * Check if a model element has an ScalaCompilerName Tag
+   * @param obj model element to test
+   * @return the value of the tag
+   */
+  def getScalaCompilerNameTag(obj: KermetaModelElement): String = {
+    obj.getKOwnedTags().find(e => "ScalaCompilerName".equals(e.asInstanceOf[Tag].getName())).get.getValue()
+  }
 }
