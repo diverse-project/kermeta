a1b1ca2fcd7f69d11e8bd346f75c966e25d796ca
Arnaud Blouin <arnaud.blouin@irisa.fr>
2012-12-15 10:48:47 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@19135 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 9ed366307658a3fbdae309d17ca67527b94c66a0

--- org/kermeta/language/org.kermeta.language.km2llvm/src/main/C/lib_std/lib_std.c
+++ org/kermeta/language/org.kermeta.language.km2llvm/src/main/C/lib_std/lib_std.c
@@ -7,7 +7,7 @@
 //#######################
 // Object
 
-struct kermeta___standard__Object {
+struct kermeta_standard__Object {
 };
 //TODO
 
@@ -15,52 +15,52 @@ struct kermeta___standard__Object {
 
 //#######################
 // String
-struct kermeta___standard__String {
+struct kermeta_standard__String {
 	char* _str;
 };
 
-struct kermeta___standard__String* create_kermeta___standard_String() {
-	struct kermeta___standard__String* res = malloc(sizeof(struct kermeta___standard__String*));
+struct kermeta_standard__String* create_kermeta_standard__String() {
+	struct kermeta_standard__String* res = malloc(sizeof(struct kermeta_standard__String*));
 	return res;
 }
 
-void delete_kermeta___standard__String(struct kermeta___standard__String* s) {
+void delete_kermeta_standard__String(struct kermeta_standard__String* s) {
 	free(s->_str);
 	free(s);
 }
 
 
-void kermeta___standard__String_plus(struct kermeta___standard__String* self, struct kermeta___standard__String* s) {
+void kermeta_standard__String___plus(struct kermeta_standard__String* self, struct kermeta_standard__String* s) {
 	//TODO
 }
 
 
-void kermeta___standard__String_equals(struct kermeta___standard__String* self, struct kermeta___standard__Object* o) {
+void kermeta_standard__String___equals(struct kermeta_standard__String* self, struct kermeta_standard__Object* o) {
 	//TODO
 }
 
 
-void kermeta___standard__String_toString(struct kermeta___standard__String* self) {
+void kermeta_standard__String___toString(struct kermeta_standard__String* self) {
 	//TODO
 }
 
 
-void kermeta___standard__String_hashcode(struct kermeta___standard__String* self) {
+void kermeta_standard__String___hashcode(struct kermeta_standard__String* self) {
 	//TODO
 }
 
 
-void kermeta___standard__String_compareTo(struct kermeta___standard__String* self, struct kermeta___standard__String* s) {
+void kermeta_standard__String___compareTo(struct kermeta_standard__String* self, struct kermeta_standard__String* s) {
 	//TODO
 }
 
 
-void kermeta___standard__String_indexOf(struct kermeta___standard__String* self, struct kermeta___standard__String* s) {
+void kermeta_standard__String___indexOf(struct kermeta_standard__String* self, struct kermeta_standard__String* s) {
 	//TODO
 }
 
 
-void kermeta___standard__String_charAt(struct kermeta___standard__String* self) {
+void kermeta_standard__String___charAt(struct kermeta_standard__String* self) {
 	//TODO
 }
 
@@ -74,23 +74,23 @@ void kermeta___standard__String_charAt(struct kermeta___standard__String* self) 
 
 //#######################
 // Stdio
-void kermeta___io__Stdio_writeln(struct kermeta___standard__String* o) {
+void kermeta___io__Stdio_writeln(struct kermeta_standard__String* o) {
 	puts(o->_str);
 }
 
-void kermeta___io__Stdio_write(struct kermeta___standard__String* o) {
+void kermeta___io__Stdio_write(struct kermeta_standard__String* o) {
 	printf("%s", o->_str);
 }
 
-void kermeta___io__Stdio_errorln(struct kermeta___standard__String* o) {
+void kermeta___io__Stdio_errorln(struct kermeta_standard__String* o) {
 	fprintf(stderr, "%s\n", o->_str);
 }
 
-void kermeta___io__Stdio_error(struct kermeta___standard__String* o) {
+void kermeta___io__Stdio_error(struct kermeta_standard__String* o) {
 	fprintf(stderr, "%s", o->_str);
 }
 
-struct kermeta___standard_String* kermeta___io__Stdio_read(struct kermeta___standard__String* o) {
+struct kermeta_standard_String* kermeta___io__Stdio_read(struct kermeta_standard__String* o) {
 	//TODO
 	return NULL;
 }
--- org/kermeta/language/org.kermeta.language.km2llvm/src/main/C/lib_std/lib_std.s
+++ org/kermeta/language/org.kermeta.language.km2llvm/src/main/C/lib_std/lib_std.s
@@ -4,95 +4,95 @@ target triple = "x86_64-pc-linux-gnu"
 
 %struct._IO_FILE = type { i32, i8*, i8*, i8*, i8*, i8*, i8*, i8*, i8*, i8*, i8*, i8*, %struct._IO_marker*, %struct._IO_FILE*, i32, i32, i64, i16, i8, [1 x i8], i8*, i64, i8*, i8*, i8*, i8*, i64, i32, [20 x i8] }
 %struct._IO_marker = type { %struct._IO_marker*, %struct._IO_FILE*, i32 }
-%struct.kermeta___standard__String = type { i8* }
-%struct.kermeta___standard__Object = type {}
-%struct.kermeta___standard_String = type opaque
+%struct.kermeta_standard__String = type { i8* }
+%struct.kermeta_standard__Object = type {}
+%struct.kermeta_standard_String = type opaque
 
 @.str = private unnamed_addr constant [3 x i8] c"%s\00", align 1
 @stderr = external global %struct._IO_FILE*
 @.str1 = private unnamed_addr constant [4 x i8] c"%s\0A\00", align 1
 
-define %struct.kermeta___standard__String* @create_kermeta___standard_String() nounwind uwtable {
-  %res = alloca %struct.kermeta___standard__String*, align 8
+define %struct.kermeta_standard__String* @create_kermeta_standard__String() nounwind uwtable {
+  %res = alloca %struct.kermeta_standard__String*, align 8
   %1 = call noalias i8* @malloc(i64 8) nounwind
-  %2 = bitcast i8* %1 to %struct.kermeta___standard__String*
-  store %struct.kermeta___standard__String* %2, %struct.kermeta___standard__String** %res, align 8
-  %3 = load %struct.kermeta___standard__String** %res, align 8
-  ret %struct.kermeta___standard__String* %3
+  %2 = bitcast i8* %1 to %struct.kermeta_standard__String*
+  store %struct.kermeta_standard__String* %2, %struct.kermeta_standard__String** %res, align 8
+  %3 = load %struct.kermeta_standard__String** %res, align 8
+  ret %struct.kermeta_standard__String* %3
 }
 
 declare noalias i8* @malloc(i64) nounwind
 
-define void @delete_kermeta___standard__String(%struct.kermeta___standard__String* %s) nounwind uwtable {
-  %1 = alloca %struct.kermeta___standard__String*, align 8
-  store %struct.kermeta___standard__String* %s, %struct.kermeta___standard__String** %1, align 8
-  %2 = load %struct.kermeta___standard__String** %1, align 8
-  %3 = getelementptr inbounds %struct.kermeta___standard__String* %2, i32 0, i32 0
+define void @delete_kermeta_standard__String(%struct.kermeta_standard__String* %s) nounwind uwtable {
+  %1 = alloca %struct.kermeta_standard__String*, align 8
+  store %struct.kermeta_standard__String* %s, %struct.kermeta_standard__String** %1, align 8
+  %2 = load %struct.kermeta_standard__String** %1, align 8
+  %3 = getelementptr inbounds %struct.kermeta_standard__String* %2, i32 0, i32 0
   %4 = load i8** %3, align 8
   call void @free(i8* %4) nounwind
-  %5 = load %struct.kermeta___standard__String** %1, align 8
-  %6 = bitcast %struct.kermeta___standard__String* %5 to i8*
+  %5 = load %struct.kermeta_standard__String** %1, align 8
+  %6 = bitcast %struct.kermeta_standard__String* %5 to i8*
   call void @free(i8* %6) nounwind
   ret void
 }
 
 declare void @free(i8*) nounwind
 
-define void @kermeta___standard__String_plus(%struct.kermeta___standard__String* %self, %struct.kermeta___standard__String* %s) nounwind uwtable {
-  %1 = alloca %struct.kermeta___standard__String*, align 8
-  %2 = alloca %struct.kermeta___standard__String*, align 8
-  store %struct.kermeta___standard__String* %self, %struct.kermeta___standard__String** %1, align 8
-  store %struct.kermeta___standard__String* %s, %struct.kermeta___standard__String** %2, align 8
+define void @kermeta_standard__String___plus(%struct.kermeta_standard__String* %self, %struct.kermeta_standard__String* %s) nounwind uwtable {
+  %1 = alloca %struct.kermeta_standard__String*, align 8
+  %2 = alloca %struct.kermeta_standard__String*, align 8
+  store %struct.kermeta_standard__String* %self, %struct.kermeta_standard__String** %1, align 8
+  store %struct.kermeta_standard__String* %s, %struct.kermeta_standard__String** %2, align 8
   ret void
 }
 
-define void @kermeta___standard__String_equals(%struct.kermeta___standard__String* %self, %struct.kermeta___standard__Object* %o) nounwind uwtable {
-  %1 = alloca %struct.kermeta___standard__String*, align 8
-  %2 = alloca %struct.kermeta___standard__Object*, align 8
-  store %struct.kermeta___standard__String* %self, %struct.kermeta___standard__String** %1, align 8
-  store %struct.kermeta___standard__Object* %o, %struct.kermeta___standard__Object** %2, align 8
+define void @kermeta_standard__String___equals(%struct.kermeta_standard__String* %self, %struct.kermeta_standard__Object* %o) nounwind uwtable {
+  %1 = alloca %struct.kermeta_standard__String*, align 8
+  %2 = alloca %struct.kermeta_standard__Object*, align 8
+  store %struct.kermeta_standard__String* %self, %struct.kermeta_standard__String** %1, align 8
+  store %struct.kermeta_standard__Object* %o, %struct.kermeta_standard__Object** %2, align 8
   ret void
 }
 
-define void @kermeta___standard__String_toString(%struct.kermeta___standard__String* %self) nounwind uwtable {
-  %1 = alloca %struct.kermeta___standard__String*, align 8
-  store %struct.kermeta___standard__String* %self, %struct.kermeta___standard__String** %1, align 8
+define void @kermeta_standard__String___toString(%struct.kermeta_standard__String* %self) nounwind uwtable {
+  %1 = alloca %struct.kermeta_standard__String*, align 8
+  store %struct.kermeta_standard__String* %self, %struct.kermeta_standard__String** %1, align 8
   ret void
 }
 
-define void @kermeta___standard__String_hashcode(%struct.kermeta___standard__String* %self) nounwind uwtable {
-  %1 = alloca %struct.kermeta___standard__String*, align 8
-  store %struct.kermeta___standard__String* %self, %struct.kermeta___standard__String** %1, align 8
+define void @kermeta_standard__String___hashcode(%struct.kermeta_standard__String* %self) nounwind uwtable {
+  %1 = alloca %struct.kermeta_standard__String*, align 8
+  store %struct.kermeta_standard__String* %self, %struct.kermeta_standard__String** %1, align 8
   ret void
 }
 
-define void @kermeta___standard__String_compareTo(%struct.kermeta___standard__String* %self, %struct.kermeta___standard__String* %s) nounwind uwtable {
-  %1 = alloca %struct.kermeta___standard__String*, align 8
-  %2 = alloca %struct.kermeta___standard__String*, align 8
-  store %struct.kermeta___standard__String* %self, %struct.kermeta___standard__String** %1, align 8
-  store %struct.kermeta___standard__String* %s, %struct.kermeta___standard__String** %2, align 8
+define void @kermeta_standard__String___compareTo(%struct.kermeta_standard__String* %self, %struct.kermeta_standard__String* %s) nounwind uwtable {
+  %1 = alloca %struct.kermeta_standard__String*, align 8
+  %2 = alloca %struct.kermeta_standard__String*, align 8
+  store %struct.kermeta_standard__String* %self, %struct.kermeta_standard__String** %1, align 8
+  store %struct.kermeta_standard__String* %s, %struct.kermeta_standard__String** %2, align 8
   ret void
 }
 
-define void @kermeta___standard__String_indexOf(%struct.kermeta___standard__String* %self, %struct.kermeta___standard__String* %s) nounwind uwtable {
-  %1 = alloca %struct.kermeta___standard__String*, align 8
-  %2 = alloca %struct.kermeta___standard__String*, align 8
-  store %struct.kermeta___standard__String* %self, %struct.kermeta___standard__String** %1, align 8
-  store %struct.kermeta___standard__String* %s, %struct.kermeta___standard__String** %2, align 8
+define void @kermeta_standard__String___indexOf(%struct.kermeta_standard__String* %self, %struct.kermeta_standard__String* %s) nounwind uwtable {
+  %1 = alloca %struct.kermeta_standard__String*, align 8
+  %2 = alloca %struct.kermeta_standard__String*, align 8
+  store %struct.kermeta_standard__String* %self, %struct.kermeta_standard__String** %1, align 8
+  store %struct.kermeta_standard__String* %s, %struct.kermeta_standard__String** %2, align 8
   ret void
 }
 
-define void @kermeta___standard__String_charAt(%struct.kermeta___standard__String* %self) nounwind uwtable {
-  %1 = alloca %struct.kermeta___standard__String*, align 8
-  store %struct.kermeta___standard__String* %self, %struct.kermeta___standard__String** %1, align 8
+define void @kermeta_standard__String___charAt(%struct.kermeta_standard__String* %self) nounwind uwtable {
+  %1 = alloca %struct.kermeta_standard__String*, align 8
+  store %struct.kermeta_standard__String* %self, %struct.kermeta_standard__String** %1, align 8
   ret void
 }
 
-define void @kermeta___io__Stdio_writeln(%struct.kermeta___standard__String* %o) nounwind uwtable {
-  %1 = alloca %struct.kermeta___standard__String*, align 8
-  store %struct.kermeta___standard__String* %o, %struct.kermeta___standard__String** %1, align 8
-  %2 = load %struct.kermeta___standard__String** %1, align 8
-  %3 = getelementptr inbounds %struct.kermeta___standard__String* %2, i32 0, i32 0
+define void @kermeta___io__Stdio_writeln(%struct.kermeta_standard__String* %o) nounwind uwtable {
+  %1 = alloca %struct.kermeta_standard__String*, align 8
+  store %struct.kermeta_standard__String* %o, %struct.kermeta_standard__String** %1, align 8
+  %2 = load %struct.kermeta_standard__String** %1, align 8
+  %3 = getelementptr inbounds %struct.kermeta_standard__String* %2, i32 0, i32 0
   %4 = load i8** %3, align 8
   %5 = call i32 @puts(i8* %4)
   ret void
@@ -100,11 +100,11 @@ define void @kermeta___io__Stdio_writeln(%struct.kermeta___standard__String* %o)
 
 declare i32 @puts(i8*)
 
-define void @kermeta___io__Stdio_write(%struct.kermeta___standard__String* %o) nounwind uwtable {
-  %1 = alloca %struct.kermeta___standard__String*, align 8
-  store %struct.kermeta___standard__String* %o, %struct.kermeta___standard__String** %1, align 8
-  %2 = load %struct.kermeta___standard__String** %1, align 8
-  %3 = getelementptr inbounds %struct.kermeta___standard__String* %2, i32 0, i32 0
+define void @kermeta___io__Stdio_write(%struct.kermeta_standard__String* %o) nounwind uwtable {
+  %1 = alloca %struct.kermeta_standard__String*, align 8
+  store %struct.kermeta_standard__String* %o, %struct.kermeta_standard__String** %1, align 8
+  %2 = load %struct.kermeta_standard__String** %1, align 8
+  %3 = getelementptr inbounds %struct.kermeta_standard__String* %2, i32 0, i32 0
   %4 = load i8** %3, align 8
   %5 = call i32 (i8*, ...)* @printf(i8* getelementptr inbounds ([3 x i8]* @.str, i32 0, i32 0), i8* %4)
   ret void
@@ -112,12 +112,12 @@ define void @kermeta___io__Stdio_write(%struct.kermeta___standard__String* %o) n
 
 declare i32 @printf(i8*, ...)
 
-define void @kermeta___io__Stdio_errorln(%struct.kermeta___standard__String* %o) nounwind uwtable {
-  %1 = alloca %struct.kermeta___standard__String*, align 8
-  store %struct.kermeta___standard__String* %o, %struct.kermeta___standard__String** %1, align 8
+define void @kermeta___io__Stdio_errorln(%struct.kermeta_standard__String* %o) nounwind uwtable {
+  %1 = alloca %struct.kermeta_standard__String*, align 8
+  store %struct.kermeta_standard__String* %o, %struct.kermeta_standard__String** %1, align 8
   %2 = load %struct._IO_FILE** @stderr, align 8
-  %3 = load %struct.kermeta___standard__String** %1, align 8
-  %4 = getelementptr inbounds %struct.kermeta___standard__String* %3, i32 0, i32 0
+  %3 = load %struct.kermeta_standard__String** %1, align 8
+  %4 = getelementptr inbounds %struct.kermeta_standard__String* %3, i32 0, i32 0
   %5 = load i8** %4, align 8
   %6 = call i32 (%struct._IO_FILE*, i8*, ...)* @fprintf(%struct._IO_FILE* %2, i8* getelementptr inbounds ([4 x i8]* @.str1, i32 0, i32 0), i8* %5)
   ret void
@@ -125,19 +125,19 @@ define void @kermeta___io__Stdio_errorln(%struct.kermeta___standard__String* %o)
 
 declare i32 @fprintf(%struct._IO_FILE*, i8*, ...)
 
-define void @kermeta___io__Stdio_error(%struct.kermeta___standard__String* %o) nounwind uwtable {
-  %1 = alloca %struct.kermeta___standard__String*, align 8
-  store %struct.kermeta___standard__String* %o, %struct.kermeta___standard__String** %1, align 8
+define void @kermeta___io__Stdio_error(%struct.kermeta_standard__String* %o) nounwind uwtable {
+  %1 = alloca %struct.kermeta_standard__String*, align 8
+  store %struct.kermeta_standard__String* %o, %struct.kermeta_standard__String** %1, align 8
   %2 = load %struct._IO_FILE** @stderr, align 8
-  %3 = load %struct.kermeta___standard__String** %1, align 8
-  %4 = getelementptr inbounds %struct.kermeta___standard__String* %3, i32 0, i32 0
+  %3 = load %struct.kermeta_standard__String** %1, align 8
+  %4 = getelementptr inbounds %struct.kermeta_standard__String* %3, i32 0, i32 0
   %5 = load i8** %4, align 8
   %6 = call i32 (%struct._IO_FILE*, i8*, ...)* @fprintf(%struct._IO_FILE* %2, i8* getelementptr inbounds ([3 x i8]* @.str, i32 0, i32 0), i8* %5)
   ret void
 }
 
-define %struct.kermeta___standard_String* @kermeta___io__Stdio_read(%struct.kermeta___standard__String* %o) nounwind uwtable {
-  %1 = alloca %struct.kermeta___standard__String*, align 8
-  store %struct.kermeta___standard__String* %o, %struct.kermeta___standard__String** %1, align 8
-  ret %struct.kermeta___standard_String* null
+define %struct.kermeta_standard_String* @kermeta___io__Stdio_read(%struct.kermeta_standard__String* %o) nounwind uwtable {
+  %1 = alloca %struct.kermeta_standard__String*, align 8
+  store %struct.kermeta_standard__String* %o, %struct.kermeta_standard__String** %1, align 8
+  ret %struct.kermeta_standard_String* null
 }
