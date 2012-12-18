e0524d43b80cbd1d791823944a7147c6bef73ceb
Arnaud Blouin <arnaud.blouin@irisa.fr>
2012-12-18 17:07:44 +0000

    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/kermeta/trunk@19163 3b3ea266-f0f1-470e-95cb-5b2c9abc2e95
    Former-commit-id: 737a5714b1dfcb08e1af87083cae6a6bce4a41e1

--- org/kermeta/language/org.kermeta.language.km2llvm/src/main/C/lib_std/String.c
+++ org/kermeta/language/org.kermeta.language.km2llvm/src/main/C/lib_std/String.c
@@ -12,18 +12,21 @@ inline struct kermeta_standard__Object* kermeta_standard__String___container(cha
 }
 
 
-void kermeta_standard__String___plus(char* self, char* s) {
+char* kermeta_standard__String___plus(char* s, char* self) {
+	char* res = calloc(strlen(s)+strlen(self)+1, sizeof(char*));
+	strcat(res, self);
+	strcat(res, s);
+	return res;
+}
+
+
+void kermeta_standard__String___equals(struct kermeta_standard__Object* o, char* self) {
 	//TODO
 }
 
 
-void kermeta_standard__String___equals(char* self, struct kermeta_standard__Object* o) {
-	//TODO
-}
-
-
-void kermeta_standard__String___toString(char* self) {
-	//TODO
+char* kermeta_standard__String___toString(char* self) {
+	return self;
 }
 
 
@@ -32,17 +35,21 @@ void kermeta_standard__String___hashcode(char* self) {
 }
 
 
-void kermeta_standard__String___compareTo(char* self, char* s) {
-	//TODO
+int kermeta_standard__String___compareTo(char* s, char* self) {
+	return strcmp(self, s);
 }
 
 
-void kermeta_standard__String___indexOf(char* self, char* s) {
-	//TODO
+int kermeta_standard__String___indexOf(char* s, char* self) {
+	char* found = strstr(self, s);
+	int res;
+	if(found==NULL) res = -1;
+	else res = found - self;
+	return res;
 }
 
 
-void kermeta_standard__String___charAt(char* self) {
-	//TODO
+char kermeta_standard__String___charAt(int index, char* self) {
+	return index<0||index>=strlen(self) ? 0 : self[index];
 }
 
